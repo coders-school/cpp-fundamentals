6077f9bc9cbff7a64109e2b186f4393539c70ea2
Łukasz Ziobroń <lukasz@coders.school>
2021-03-13 20:23:24 +0100

    Update fibonacci.hpp

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -7,7 +7,7 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    if (sequence <= 2) {
+    if (sequence < 2) {
         return 1;
     }
     return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
