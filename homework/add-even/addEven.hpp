c6d78c3d0ea3c741074875a1880d6104c92fbbdd
Łukasz Ziobroń <lukasz@coders.school>
2021-05-05 12:09:32 +0200

    nwd-nww homework and workflow left

--- homework/add-even/addEven.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
-}
--- homework/vector-of-shared-ptrs/solution/vectorFunctions.hpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <memory>
-#include <vector>
-
-std::vector<std::shared_ptr<int>> generate(int count);
-void add10(const std::vector<std::shared_ptr<int>>& vec);
-void sub10(int* const ptr);
-void print(const std::vector<std::shared_ptr<int>>& vec);
-void sub10(const std::vector<std::shared_ptr<int>>& vec);
