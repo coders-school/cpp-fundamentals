2cdf3ae03574d0c3316ca3bc0c80e540be4b749e
Jakub Stępień <macista.bukaj@gmail.com>
2021-05-07 12:50:41 +0200

    adding fibonacci_iterative and fibonacci_recursive

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,27 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(0==sequence){
+        return(0);
+    }
+    if(1==sequence){
+        return(1);
+    }
+    int fib=1, fibs1=1, fibs2=0;
+    for(int i=2; i<sequence; ++i){
+        fibs2=fibs1;
+        fibs1=fib;
+        fib=fibs1+fibs2;
+    }
+    return fib;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(0==sequence){
+        return(0);
+    }
+    if(1==sequence){
+        return(1);
+    }
+    return fibonacci_recursive(sequence-2)+fibonacci_recursive(sequence-1);
 }
