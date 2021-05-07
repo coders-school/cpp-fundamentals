89597aec0839de65f6052b21522ed3e26ad8f619
Gyrydymptum <gyrydymptum@gmail.com>
2021-05-07 15:41:14 +0200

    adding works fibonacci_iterative

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,9 +1,18 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int first = 0;
+    int second = 1;
+    int third = 1;
+        if (sequence < 2) 
+            return sequence;
+        else for (int i = 1; i < sequence; i++){
+        third = first + second;
+        first = second;
+        second = third;}
+            return third;
 }
+    
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
