9ad392af1805b883e7ab1599805ebd659707d6c4
Jakub Kaczmarski <kuba.kaczmarski312@gmail.com>
2021-05-07 20:57:51 +0200

    Added recursive and iterative implementation

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,34 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    }
+    if (sequence < 3) {
+        return 1;
+    }
+    if (sequence == 3) {
+        return 2;
+    }
+    int i = 3;
+    int num1 = 1;
+    int num2 = 2;
+    int temp = 0;
+    while (sequence > i) {
+
+        temp = num1 + num2;
+        num1 = num2;
+        num2 = temp;
+        i++;
+    }
+    return temp;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+    if (sequence <= 1) {
+        return sequence;
+    }
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+
     return 0;
 }
