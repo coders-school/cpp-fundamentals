3bde0ba9bbfe32550c888eda23a78004cd332d0c
Jakub Kaczmarski <kuba.kaczmarski312@gmail.com>
2021-05-07 19:55:54 +0200

    Added addition case

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,9 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+    if(command.compare("add") == 0)
+    {
+        return std::to_string((first + second));
+    };
     return "";
 }
