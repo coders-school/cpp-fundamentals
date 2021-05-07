757c6b27c24acd08cfa215240832a987e6498611
Warp-12 <pl.walery@gmail.com>
2021-05-07 14:21:35 +0200

    Homework nwd-nww done. all tests passed

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,10 +2,45 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+
+    if (rhs == 0) {
+        return lhs;
+    }
+    if (lhs == 0) {
+        return rhs;
+    }
+
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+
+    while (lhs != rhs) {
+        if (lhs < rhs) {
+            rhs -= lhs;
+        } else {
+            lhs -= rhs;
+        }
+    }
+
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 || rhs == 0) {
+        return 0;
+    }
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+
+    if (lhs < rhs) {
+        return NWW(rhs, lhs);
+    }
+
+    int nww = lhs;
+
+    while (nww % rhs != 0) {
+        nww += lhs;
+    }
+
+    return nww;
 }
