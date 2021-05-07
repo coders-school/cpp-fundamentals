555885cc14cc3bf341fee7593113b1778aa0d16d
Gyrydymptum <gyrydymptum@gmail.com>
2021-05-07 21:11:43 +0200

    adding works nwd

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+if (lhs<0) lhs=-lhs;
+    if (rhs<0) rhs=-rhs;
+      if (lhs==0) return rhs;
+      else if (rhs==0) return lhs;
+      else {
+        while( lhs != rhs )
+        if( lhs < rhs ) rhs -= lhs; 
+        else lhs -= rhs;
+        }return lhs;
 }
 
 int NWW(int lhs, int rhs) {
