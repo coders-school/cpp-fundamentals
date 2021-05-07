f699467610d7adc68ffbc06fdb594047c2dfb326
Ireneusz Bohatyrewicz <ibohatyrewicz@gmail.com>
2021-05-07 19:23:09 +0200

    adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,22 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (rhs == 0) return lhs;
+    if (lhs<0) lhs*=-1;
+    if (rhs<0) rhs*=-1;
+    while(lhs % rhs !=0) {
+
+        lhs %= rhs;
+        auto tmp = rhs;
+        rhs = lhs;
+        lhs = tmp;
+    }
+    return rhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 || rhs ==0) return 0;
+    if (lhs<0) lhs*=-1;
+    if (rhs<0) rhs*=-1;
+    return lhs*rhs/NWD(lhs, rhs);
 }
