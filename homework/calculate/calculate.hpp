26737c46c898b48ca52e939b7f1f4e420460ae05
Jakub Stępień <macista.bukaj@gmail.com>
2021-05-07 16:31:31 +0200

    adding Calculate implementation

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,10 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    if (!command.compare("add")){return std::to_string(first+second);}
+    if (!command.compare("subtract")){return std::to_string(first-second);}
+    if (!command.compare("multiply")){return std::to_string(first*second);}
+    if (!command.compare("divide")&&second){return std::to_string(first/second);}
+    if (!command.compare("divide")&&!second){return "Division by 0";}
+    return "Invalid data";
 }
