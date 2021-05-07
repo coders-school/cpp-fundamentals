2de01743706126ac9086beebb42b43146b8bf4d8
Warp-12 <pl.walery@gmail.com>
2021-05-07 14:08:23 +0200

    Homework calculate done, all tests passed

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,23 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    int result = 0;
+    if (command == "add") {
+        result = first + second;
+    } else if (command == "subtract") {
+        result = first - second;
+    } else if (command == "multiply") {
+        result = first * second;
+    } else if (command == "divide") {
+        if (second != 0) {
+            result = first / second;
+        } else {
+            return "Division by 0";
+        }
+
+    } else {
+        return "Invalid data";
+    }
+
+    return std::to_string(result);
 }
