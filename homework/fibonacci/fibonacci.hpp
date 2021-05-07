f6df46c881b4dff6a24c785de73171f72c194cce
Warp-12 <pl.walery@gmail.com>
2021-05-07 14:15:30 +0200

    Homework fibonacci done, all tests passed

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,32 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    unsigned int result;
+    int a = 1, b = 1;
+
+    if (sequence == 0) {
+        result = 0;
+    } else if (sequence == 1 || sequence == 2) {
+        result = 1;
+    } else {
+        for (size_t i = 3; i <= sequence; i++) {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+    }
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    } else if (sequence == 1) {
+        return 1;
+    } else if (sequence > 1) {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    } else {
+        return 0;
+    }
 }
