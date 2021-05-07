5b34f2bcaf5ccdda490c74253b56875e24094ae5
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2021-05-07 18:34:04 +0200

    Added iteratice function

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
-}
+    if (sequence == 0 ) {
+	    return 0;
+    }else if (sequence == 1) {
+	    return 1;
+    }else {    
+    	int x = 0, y = 1, z = 0;
+   	for(int i = 0 ; i < sequence-1 ; i++){
+	  	  z = x + y;
+	  	  x = y;
+	  	  y = z;
+    	}
+    	return z;
+    }
+ }
 
 int fibonacci_recursive(int sequence) {
     if(sequence == 0 ) {
--- homework/fibonacci/main.cpp
+++ homework/fibonacci/main.cpp
@@ -2,7 +2,8 @@
 #include "fibonacci.hpp"
 
 int main() {
-  //  std::cout << fibonacci_iterative(10) << "\n";
+
+    std::cout << fibonacci_iterative(10) << "\n";
     std::cout << fibonacci_recursive(10) << "\n";
 
     return 0;
