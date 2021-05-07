cdcb2c9e9dffd566f78a7029be40f86ce739fe89
Jakub Kaczmarski <kuba.kaczmarski312@gmail.com>
2021-05-07 19:56:53 +0200

    Added division case

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,6 +5,13 @@ std::string calculate(const std::string& command, int first, int second) {
     if(command.compare("add") == 0)
     {
         return std::to_string((first + second));
-    };
+    }else if(command.compare("divide") == 0)
+    {
+        if(second == 0)
+        {
+            return "Division by 0";
+        }
+        return std::to_string((first / second));
+    }
     return "";
 }
