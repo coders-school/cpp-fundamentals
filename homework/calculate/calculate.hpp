c92856ab58695656a9c8df3d288756d4ae97cd7c
rizzet97 <nit.pawel@gmail.com>
2021-05-07 17:17:43 +0200

    Implemented subtraction

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -7,6 +7,9 @@ std::string calculate(const std::string& command, int first, int second) {
     if (command == "add"){
         first += second;
         return (std::to_string(first));
+    } else if (command == "subtract"){
+        first -= second;
+        return (std::to_string(first));
     } else {
         return "";
     }
