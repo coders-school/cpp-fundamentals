81316723e655604ffd754f99a7073934748165b7
Jakub Kaczmarski <kuba.kaczmarski312@gmail.com>
2021-05-07 20:02:13 +0200

    Added multiplication, substraction, division and invalid data case

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,17 +1,19 @@
 #pragma once
+
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) {
-    if(command.compare("add") == 0)
-    {
+std::string calculate(const std::string &command, int first, int second) {
+    if (command.compare("add") == 0) {
         return std::to_string((first + second));
-    }else if(command.compare("divide") == 0)
-    {
-        if(second == 0)
-        {
+    } else if (command.compare("subtract") == 0) {
+        return std::to_string((first - second));
+    } else if (command.compare("divide") == 0) {
+        if (second == 0) {
             return "Division by 0";
         }
         return std::to_string((first / second));
+    } else if (command.compare("multiply") == 0) {
+        return std::to_string((first * second));
     }
-    return "";
+    return "Invalid data";
 }
