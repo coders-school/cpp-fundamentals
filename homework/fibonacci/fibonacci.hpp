909b174622703bd4f46eeece7afaeab9e5195eee
Gyrydymptum <gyrydymptum@gmail.com>
2021-05-07 15:43:59 +0200

    adding works fibonacci_recursive

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -15,6 +15,6 @@ int fibonacci_iterative(int sequence) {
     
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+if (sequence < 2) return sequence;
+else return fibonacci_recursive (sequence-1)+fibonacci_recursive (sequence-2);
 }
