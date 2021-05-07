3bcfea049b2cd745ded6fd102d03ce8004174db5
lukasz-kukulka <qqla83@gmail.com>
2021-05-07 12:11:44 +0100

    Add divide command

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -8,5 +8,13 @@ std::string calculate(const std::string& command, int first, int second) {
         return std::to_string(first - second);
     } else if (command == "multiply"){
         return std::to_string(first * second);
+    }else if (command == "divide"){
+        if (first == 0 || second == 0){
+            return "Division by 0";
+        } else{
+            return std::to_string(first / second);
+        }
+        
+    }
     return "";
 }
