a6c602e81400c27ecbb2dea6cd832004e0681142
BarTes8 <barbara.tessema@gmail.com>
2021-05-07 18:45:05 +0200

    Iteratively works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence == 0){
+        return 0;
+    }
+    if(sequence == 1){
+        return 1;
+    }
+    int total = 0, secondLast = 0, last = 1; 
+    for(int i = 2; i <= sequence; i++){
+        total = last + secondLast;
+        secondLast = last;
+        last = total;
+
+    }
+    return last;
 }
 
 int fibonacci_recursive(int sequence) {
