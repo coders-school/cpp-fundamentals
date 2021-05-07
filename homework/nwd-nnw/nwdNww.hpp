f700c33db27b08484f9b87c55da207ed1e5f711e
raxonpc <tomeczekorlowski1@gmail.com>
2021-05-07 12:58:43 +0200

    GCD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(rhs < 0)
+    {
+        rhs = -rhs;
+    }
+    if(lhs < 0)
+    {
+        lhs = -lhs;
+    }
+    while(rhs != 0)
+    {
+        int temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
