8b2271d754aa11041022cb5480b8a4f22df34103
BarTes8 <barbara.tessema@gmail.com>
2021-05-07 17:34:22 +0200

    dividing works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -11,5 +11,13 @@ std::string calculate(const std::string& command, int first, int second) {
     else if(command == "multiply"){
         return std::to_string(first * second);
     }
+    else if(command == "divide"){
+        if(second != 0){
+            return std::to_string(first / second);
+        }
+        else{
+            return "Division by 0";
+        }
+    }
     return "";
 }
