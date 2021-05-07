402b3c8be9c2bdc973bb0536d270a4f8821a3706
Piotr <piotrku91@github.com>
2021-05-07 17:26:49 +0200

    NWD function

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs)
+{
+    if (lhs < 0)
+        lhs = std::abs(lhs); // Absolute value of lhs
+    if (rhs < 0)
+        rhs = std::abs(rhs); // Absolute value of rhs
+    while (rhs) // while rhs is 1
+    {
+        int rkeeper = rhs; // copy value to temp variable
+        rhs = lhs % rhs; // get rhs by modulo
+        lhs = rkeeper; // if while is still active assign to lhs value from rkeeper
+    };
+    return lhs;
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
+int NWW(int lhs, int rhs)
+{
     return -1;
 }
