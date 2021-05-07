68a1e964f0c8a048a7db8e27e91e99f5ff6b7436
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-05-07 20:06:05 +0200

    calculate_add

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,11 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
+    if(command == "add"){
+        return std::to_string(first+second);
+    }
+   
+         
+
     return "";
 }
