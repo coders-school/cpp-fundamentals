42d1c5ede89170fa196632d59e3e6e13b1fea9c7
Jakub Kubalka <jk.kubalka@gmail.com>
2021-05-07 18:57:03 +0200

    Calculate_ch.1

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,44 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+    int r;
+    std::string result;
+    if (command == "add")
+    {
+        r = first + second;
+        result =std::to_string(r);
+        return result;
+    }
+    else if(command == "multiply")
+    {
+        r = first * second;
+        result =std::to_string(r);
+        return result;
+    }
+    else if(command == "divide")
+    {
+        if(second == 0)
+        {
+            result = "Division by 0";
+            return result;
+        }
+        else
+        {
+            r = first / second;
+            result =std::to_string(r);
+            return result;
+        }
+    }
+    else if(command == "subtract")
+    {
+        r = first - second;
+        result =std::to_string(r);
+        return result;
+    }
+    else
+    {
+        result = "Invalid data";
+        return result;
+    }
     return "";
 }
