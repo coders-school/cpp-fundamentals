ff6bf3f63e3d7c2331cbf89ebe003df23320681d
rizzet97 <nit.pawel@gmail.com>
2021-05-07 20:14:01 +0200

    NWD added

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,35 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int remainder = -1, greater = 0, smaller = 0;
+    
+    if (rhs == 0){
+        return lhs;
+    } else if (lhs == 0){
+        return rhs;
+    }
+    
+    if (rhs < 0){
+        rhs *= -1;
+    } else if (lhs < 0){
+        lhs *= -1;
+    }
+    
+    if (lhs >= rhs){
+        greater = lhs;
+        smaller = rhs;
+    } else {
+        greater = rhs;
+        smaller = lhs;
+    }
+
+    while (remainder != 0){
+        remainder = greater % smaller;
+        greater = smaller;
+        smaller = remainder;
+    } 
+     
+    return greater;
 }
 
 int NWW(int lhs, int rhs) {
