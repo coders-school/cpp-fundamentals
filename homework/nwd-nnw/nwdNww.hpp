1ef5c40780a99af233bbac17c9457217de1dd431
BarTes8 <barbara.tessema@gmail.com>
2021-05-07 22:27:15 +0200

    LCM works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs < 0){
+        lhs = -lhs;
+    }
+    if(rhs < 0){
+        rhs = -rhs;
+    }
+    while (rhs){
+        std::swap(lhs %= rhs, rhs);
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
