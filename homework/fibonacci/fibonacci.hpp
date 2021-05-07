d414265e6471db05eea7ad7da38b89522e6f5516
raxonpc <tomeczekorlowski1@gmail.com>
2021-05-07 12:41:58 +0200

    Recursive solution works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -6,6 +6,19 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+    if(sequence == 0 || sequence == 1)
+    {
+        //0 - 0; 1 - 1
+        return sequence;
+    }
+    else if(sequence < 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return fibonacci_recursive(sequence - 1)
+        + fibonacci_recursive(sequence - 2);
+    }
     return 0;
 }
