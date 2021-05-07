8601a8c562d3e7ac9fd10a986d2e968a9b5b356b
rizzet97 <nit.pawel@gmail.com>
2021-05-07 17:43:02 +0200

    Implemented protection against unwanted input commands

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -21,6 +21,6 @@ std::string calculate(const std::string& command, int first, int second) {
         return (std::to_string(first));
       }
     } else {
-        return "";
+        return "Invalid data";
     }
 }
