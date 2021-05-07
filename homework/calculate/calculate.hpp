3620510effa45735b42a3e9677570a127e4499ca
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-05-07 20:24:02 +0200

    calculate_multiply

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -9,6 +9,9 @@ std::string calculate(const std::string& command, int first, int second) {
     if(command == "subtract"){
         return std::to_string(first-second);
     }
+    if(command == "multiply"){
+        return std::to_string(first*second);
+    }
    
          
 
