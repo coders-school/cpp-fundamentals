ee449eb993c76e1f18ef58d8fde492d010e3a024
lukasz-kukulka <qqla83@gmail.com>
2021-05-07 12:05:39 +0100

    Add multiply command

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -4,8 +4,9 @@
 std::string calculate(const std::string& command, int first, int second) {
     if (command == "add"){
         return std::to_string(first + second);
-    } else if (command == "add"){
-        return std::to_string(first + second);
-    }
+    } else if (command == "substract"){
+        return std::to_string(first - second);
+    } else if (command == "multiply"){
+        return std::to_string(first * second);
     return "";
 }
