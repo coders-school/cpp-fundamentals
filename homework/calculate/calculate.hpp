793aeae0db0a67064288fc0a36e1f326da42a937
Kruza <palatonista@gmail.com>
2021-05-07 17:14:31 +0200

    Adding works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,18 @@
 #pragma once
 #include <string>
+#include <ctype.h>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+std::string calculate(const std::string& command, int first, int second) 
+{
+    // Convert string to lower case
+    std::string com_in = command;
+    for(int i = 0; i < com_in.length(); i++)
+        com_in[i] = tolower(com_in[i]);
+
+    // Adding "+"
+    if (com_in.compare("add") == 0)
+        return std::to_string(first + second);
+
+
     return "";
 }
