dc4c5179715d73a15350b7a61a3a70fcdd23d077
Piotr <piotrku91@github.com>
2021-05-07 17:50:36 +0200

    NWD,NWW / Comments correction

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -6,16 +6,16 @@ int NWD(int lhs, int rhs)
         lhs = std::abs(lhs); // Absolute value of lhs
     if (rhs < 0)
         rhs = std::abs(rhs); // Absolute value of rhs
-    while (rhs) // while rhs is 1
+    while (rhs)              // while rhs is 1
     {
         int rkeeper = rhs; // copy value to temp variable
-        rhs = lhs % rhs; // get rhs by modulo
-        lhs = rkeeper; // if while is still active assign to lhs value from rkeeper
+        rhs = lhs % rhs;   // get rhs by modulo
+        lhs = rkeeper;     // assign to lhs value from rkeeper
     };
     return lhs; // pass lhs as score
 }
 
 int NWW(int lhs, int rhs)
 {
-    return (!lhs || !rhs) ? 0 : std::abs(lhs*rhs)/NWD(lhs,rhs); // if lhs or rhs are 0 return 0 if not -> count correct value and return
+    return (!lhs || !rhs) ? 0 : std::abs(lhs * rhs) / NWD(lhs, rhs); // if lhs or rhs are 0 return 0 if not -> count correct value and return
 }
