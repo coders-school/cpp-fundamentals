c3b91c0d1126c88ad4e00fc7e52876f0acd7a21e
Jan Tobiczyk <j.tobiczyk43@gmail.com>
2021-05-08 12:11:13 +0200

    rm

--- homework/calculate/main.cpp
+++ homework/calculate/main.cpp
@@ -2,29 +2,20 @@
 #include <string>
 #include "calculate.hpp"
 
-using namespace std;
-string add (int, int);
-
 int main() {
     while (true) {
-        cout << "Provide command: \"add\", \"subtract\","
+        std::cout << "Provide command: \"add\", \"subtract\","
                   << " multiply\", divide\" or \"quit\" to exit\n";
-        string command{};
-        cin >> command;
+        std::string command{};
+        std::cin >> command;
         if (command == "quit")
             break;
         int first{};
         int second{};
-        cout << "Provide two numbers: ";
-        cin >> first >> second;
-        cout << "Result: " << calculate(command, first, second) << "\n";
+        std::cout << "Provide two numbers: ";
+        std::cin >> first >> second;
+        std::cout << "Result: " << calculate(command, first, second) << "\n";
     }
 
     return 0;
-}
-string add  (int x, int y)
-{
- int r = x+y;
- string result = to_string(r);
- return result; 
 }
