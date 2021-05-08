e30edafd37e317136b0c543a28b1bebb588c38cc
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-05-09 00:37:02 +0200

    recursive version works too

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -19,6 +19,13 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+
+    if (sequence >= 3) {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
+    else if (sequence > 0) {
+        return 1;
+    }
     return 0;
+
 }
