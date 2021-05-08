f2d2618d65addf73328a1250c1bce87fdf8385c2
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-05-09 00:25:20 +0200

    iterative version works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    int fibN = 0;
+    if(sequence > 1) {
+        int fibMinus2 = 0;
+        int fibMinus1 = 1;
+        for(int i = 2; i <= sequence; ++i) {
+            fibN = fibMinus1 + fibMinus2;
+            fibMinus2 = fibMinus1;
+            fibMinus1 = fibN;
+        }
+    }    
+    else if(sequence == 1) {
+        fibN = 1;
+    }
+    return fibN;
 }
 
 int fibonacci_recursive(int sequence) {
