5d8aa0e019d54552ed9671f1749d76ab8df31971
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-05-08 19:24:28 +0200

    NWD

--- homework/nwd-nnw/main.cpp
+++ homework/nwd-nnw/main.cpp
@@ -2,8 +2,8 @@
 #include "nwdNww.hpp"
 
 int main() {
-  std::cout << "NWW(255, 15) = " << NWW(255, 15) << "\n";
-  std::cout << "NWD(255, 15) = " << NWD(255, 15) << "\n";
+  std::cout << "NWW(255, 15) = " << NWW(0, 0) << "\n";
+  std::cout << "NWD(255, 15) = " << NWD(0, 0) << "\n";
 
   return 0;
 }
--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,19 @@
 #pragma once
+#include <cstdlib>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int temp;
+    while(rhs!=0){
+	    temp=rhs;
+	    rhs=lhs%rhs;
+	    lhs=temp;
+    }
+    return  abs(lhs);
+
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+        
+   
+  return 0;    
 }
