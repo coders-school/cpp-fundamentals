331a8e4cbd88025356e372729aaff0daf4b841e6
Michał Antoniuk <michantoniuk@gmail.com>
2021-05-08 20:17:33 +0200

    Done iterative function

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int fib_top = 0;
+    int num1 = 1;
+    int num2 = 0;
+    
+    for (int i = 0 ; i < sequence; i++) {
+        num2 = fib_top + num1;
+        fib_top = num1;
+        num1 = num2;
+    }
+
+    return fib_top;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+    // TODO: num1our implementation goes here
     return 0;
 }
