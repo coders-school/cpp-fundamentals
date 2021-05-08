2a7e05b5e2ed9d76f74c980ec60ca36875a43ed4
Krzysztof Kryczka <kw.kryczka@gmail.com>
2021-05-08 22:40:12 +0200

    divide works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -11,5 +11,11 @@ std::string calculate(const std::string& command, int first, int second) {
     else if (command == "multiply"){
 		return std::to_string(first * second);	
 	}
+    else if (command == "divide"){		
+        if (second == 0){
+			return "Division by 0";
+		}
+		return std::to_string(first / second);	
+	}
     return "Invalid data";
 }
