7bcb9278135e1d84e566c385d21f5a908ff467c7
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-05-08 22:51:41 +0200

    all commands work

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -12,7 +12,9 @@ std::string calculate(const std::string& command, int first, int second) {
     if (command == "multiply") {
         return std::to_string(first * second);
     }
-
+    if (command == "divide") {
+        return second != 0 ? std::to_string(first / second) : "Division by 0";
+    }
 
     return "Invalid data";
 }
