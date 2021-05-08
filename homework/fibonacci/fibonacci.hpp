f3efad6800bfaac541269c65e1655c8e48e8e8a5
kondorski <thegreatvaultofknowledge@gmail.com>
2021-05-09 01:23:08 +0200

    Iterative almost works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,24 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int t1 = 0;
+    int t2 = 1;
+    int next;
+    if(sequence == 0) { 
+        return 0;
+    }
+    else if (sequence == 1 || sequence == 2){
+        return 1;
+    }
+    else if(sequence >= 2){
+        for(int i = 1; i < sequence; i++){
+            next = t1 + t2;
+            t1 = t2;
+            t2 = next;
+        }
+        return next;
+    }
+    
 }
 
 int fibonacci_recursive(int sequence) {
