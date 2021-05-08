77beee87a268a0181a2086241c2c244be5cbd1ed
Jakub Kubalka <jk.kubalka@gmail.com>
2021-05-08 14:05:10 +0200

    Iterative_first_change

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,36 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
+    
+    
+    int tab[sequence+1];
+    tab[0]=0;
+    tab[1]=1;
+    for (int i = 2; i<=sequence; i++)
+    {
+        tab[i]=tab[i-2]+tab[i-1];
+    }
+
+    sequence = tab[sequence];
+
+    if (sequence == 0)
+    {
+        return tab[0];
+    }
+    else if (sequence == 1)
+    {
+        return tab[1];
+    }
+    else 
+    {
+        return sequence;
+    }
+    
     return 0;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+
+    //
     return 0;
 }
