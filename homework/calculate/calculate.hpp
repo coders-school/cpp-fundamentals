90144d75a6d2eedbc288dab3d8ae4f36c66db871
lukasz-kukulka <qqla83@gmail.com>
2021-05-08 14:52:40 +0100

    fixup! Complete homework calculate task

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,12 +5,7 @@ std::string calculate(const std::string& command, int first, int second) {
     if (command == "add"){
         return std::to_string(first + second);
     } else if (command == "subtract"){
-        // if(second < 0){
-        //     second = second * (-1);
-        //     return std::to_string(first + second);
-        // } else {
         return std::to_string(first - second);
-        }
     } else if (command == "multiply"){
         return std::to_string(first * second);
     }else if (command == "divide"){
