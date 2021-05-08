f1f80bed3ead1fe2b73d5808cf0305c55a045aa6
jantob6637 <83110306+jantob6637@users.noreply.github.com>
2021-05-08 13:27:42 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,46 +1,43 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) 
-{
-if (command == "add")
+std::string calculate(const std::string& command, int first, int second) {
+    if (command == "add")
     {
         int r = first + second;
-        std::string result = std::to_string(r);
-        return result; 
-    }       
-    else if (command == "multiply")
-    {
-        int r = first * second;
-        std::string result = std::to_string(r);
-        return result; 
+        std::string result =std::to_string(r);
+        return result;
     }
-    else if (command == "substract")
+    else if(command == "multiply")
     {
-        int r = first - second;
-        std::string result = std::to_string(r);
-        return result; 
+       int r = first * second;
+        std::string result =std::to_string(r);
+        return result;
     }
-    else if (command == "divide")
+    else if(command == "divide")
     {
         if(second == 0)
         {
-        std::string result = "Division by 0";
-        return result;
+           std::string  result = "Division by 0";
+            return result;
         }
-             else
-            {
-                int r = first / second;
-                std::string result = std::to_string(r);
-                return result;
-            } 
+        else
+        {
+          int  r = first / second;
+           std::string  result =std::to_string(r);
+            return result;
+        }
+    }
+    else if(command == "subtract")
+    {
+       int r = first - second;
+       std::string  result =std::to_string(r);
+        return result;
     }
     else
-        {
-        std::string result="Invalid data";
-        return result; 
-        }
-    
-
+    {
+        std::string result = "Invalid data";
+        return result;
+    }
     return "";
 }
