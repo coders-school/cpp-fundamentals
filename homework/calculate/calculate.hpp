208fab8e9745c807e9e4df81a7dc5391b75a5379
Marcin Mazurek <marcinmazurek06@gmail.com>
2021-05-08 15:21:52 +0200

    Cosmetic changes of calculate

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,9 +2,9 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-	std::string result;
+	std::string result{};
 	if (!command.compare("add"))
-			result = std::to_string(first + second);
+		result = std::to_string(first + second);
 	else if (!command.compare("subtract"))
 		result = std::to_string(first - second);
 	else if (!command.compare("multiply"))
