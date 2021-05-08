460eec3ee77534df840b4dde4e211fe477fe3761
Piotr Rogalski <progalski89@gmail.com>
2021-05-09 01:03:30 +0200

    nwdnww

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,40 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int higher {0};
+
+    if(lhs > rhs)
+    {
+        higher = lhs >= 0 ? lhs : -lhs;
+    }
+    else
+    {
+        higher = rhs >= 0 ? rhs : -rhs;
+    }
+
+    for(int i {higher}; i > 0; --i)
+    {
+        if(lhs%i == 0 && rhs%i == 0)
+        {
+            return i;
+        }
+    }
+    return 0;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 || rhs == 0)
+    {
+        return 0;
+    }
+
+    int result {lhs};
+    while(true)
+    {
+        if(result%rhs == 0)
+        {
+            return result >= 0 ? result : -result;
+        }
+        result += lhs;
+    }
 }
