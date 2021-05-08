0cee364c0c5b2163e6468107abf28ea1f36fcf86
Michał Antoniuk <michantoniuk@gmail.com>
2021-05-08 17:39:48 +0200

    Done NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+// Modified Euclidean algorithm:
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+    if (rhs == 0) {
+        return lhs;
+    }
+    return NWD(rhs, lhs % rhs);
 }
 
 int NWW(int lhs, int rhs) {
