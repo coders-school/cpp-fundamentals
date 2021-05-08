b53359e43f90c7c4aecda74e4656a6be61d0958e
Marcin Mazurek <marcinmazurek06@gmail.com>
2021-05-08 15:42:08 +0200

    Add fibonacci functions

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
+#include <vector>
+
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+	if (sequence < 0)
+		return -1; //error
+
+	std::vector<int> fib = { 0, 1, 1 };
+
+	int add{};
+	for (auto i{ 3 }; i <= sequence; ++i)
+	{
+		fib.push_back(fib[i - 1] + fib[i - 2]);
+	}
+
+	return fib[sequence];
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+	if (sequence < 0)
+		return -1; //error
+	if (sequence == 0 || sequence == 1)
+		return sequence;
+
+	return (fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1));
 }
+
