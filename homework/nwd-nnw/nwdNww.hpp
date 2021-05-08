959273c82294073d49975b753692ca6662ff9771
Jakub Kubalka <jk.kubalka@gmail.com>
2021-05-08 15:55:46 +0200

    NWD_working_1

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
+    if(lhs==0 && rhs==0){return 0;}
+    int divider;
+    if (lhs>rhs){divider = lhs;}
+    else {divider = rhs;}
+    while(lhs%divider!=0 || rhs%divider!=0)
+    {
+        --divider;
+    }
+
+    return divider;
+    
     return -1;
 }
 
