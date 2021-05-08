db124dc79f10bf5131a020959659d136962d6c61
Robert Gruberski <rgruberski@gmail.com>
2021-05-08 19:44:20 +0200

    Add Fibonacci iterative version

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+    int a = 0;
+    int b = 1;
+
+    for( int fibonacci_sequence_counter = 0; fibonacci_sequence_counter < sequence; fibonacci_sequence_counter++ )
+    {
+        b += a;
+        a = b - a;
+    }
+
+    return a;
 }
 
 int fibonacci_recursive(int sequence) {
