06ff981fac26ab68e5045b68eef53d25328bb92f
MichalZ23 <mich.zawalich@gmail.com>
2021-05-08 22:57:27 +0200

    adding calculate

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,33 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+	std::string result = "";
+	if(command == "add")
+	{
+		result = std::to_string(first + second);
+	}
+	else if(command == "subtract")
+        {
+                result = std::to_string(first - second);
+        }
+	else if(command == "multiply")
+        {
+                result = std::to_string(first * second);
+        }
+	else if(command == "divide")
+        {
+		if (second == 0)
+		{
+			result = "Division by 0";
+		}
+		else
+		{
+                	result = std::to_string(first / second);
+		}
+        }
+	else
+	{
+		result = "Invalid data";
+	}
+	return result;
 }
