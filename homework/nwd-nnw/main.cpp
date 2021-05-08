216017c4522ffd3c5fd5591412b65de18c317207
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2021-05-08 14:17:31 +0200

    Added NWD function

--- homework/nwd-nnw/main.cpp
+++ homework/nwd-nnw/main.cpp
@@ -2,8 +2,9 @@
 #include "nwdNww.hpp"
 
 int main() {
-  std::cout << "NWW(255, 15) = " << NWW(255, 15) << "\n";
+  //std::cout << "NWW(255, 15) = " << NWW(255, 15) << "\n";
   std::cout << "NWD(255, 15) = " << NWD(255, 15) << "\n";
-
+  std::cout << NWD(0,5) << "\n";
+  std::cout << NWD(-255,15) << "\n";
   return 0;
 }
--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int temp;
+    while(rhs!=0){
+	    temp=rhs;
+	    rhs=lhs%rhs;
+	    lhs=temp;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
