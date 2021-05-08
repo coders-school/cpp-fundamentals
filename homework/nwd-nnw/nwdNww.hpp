a5cfe16898db46d20c97e3aab0e03540992326df
Mateusz <gasiorowskii.mateusz@gmail.com>
2021-05-08 12:11:26 -0700

    everythink work

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,10 +2,31 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    int x;  
+    rhs = abs(rhs);
+    lhs = abs(lhs);
+
+    while (rhs){
+        x = rhs;
+        rhs = lhs % rhs;
+        lhs = x;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    // TODO: Implement me 
+    int result;
+    rhs = abs(rhs);
+    lhs = abs(lhs);
+
+    if ((rhs == 0) || (lhs == 0)){
+        result = 0;
+    }
+    else{
+        result = (rhs*lhs)/NWD(lhs,rhs);
+    }
+
+
+    return result;
 }
