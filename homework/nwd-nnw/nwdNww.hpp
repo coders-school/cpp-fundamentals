28d2f58ba0c4ebf307df689497936edb3c77d7df
Marcin Mazurek <marcinmazurek06@gmail.com>
2021-05-08 16:09:14 +0200

    Provide NWW and NWD functions

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,46 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+	if (!lhs)
+		return rhs;
+	if (!rhs)
+		return lhs;
+	
+	if (lhs < 0)
+		lhs = abs(lhs);
+	if (rhs < 0)
+		rhs = abs(rhs);
+
+	int i{ lhs < rhs ? lhs : rhs };
+	int nwd{};
+	while (i > 0) {
+		if (!(lhs % i) && !(rhs % i)) {
+			nwd = i;
+			break;
+		}
+		--i;
+	}
+	return nwd;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+	if (lhs == 0 || rhs == 0)
+		return 0;
+
+	if (lhs < 0)
+		lhs = abs(lhs);
+	if (rhs < 0)
+		rhs = abs(rhs);
+
+	int i{ lhs > rhs ? lhs : rhs };
+	int nww{ lhs * rhs };
+	while (i <= nww) {
+		if (!(i % lhs) && !(i % rhs)) {
+			nww = i;
+			break;
+		}
+		++i;
+	}
+	return nww;
 }
+
