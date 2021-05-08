e9278be3733757c77f1ac3688abf29fa1b371a4b
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-05-08 22:35:41 +0200

    subtracting and multiplying also works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -6,6 +6,12 @@ std::string calculate(const std::string& command, int first, int second) {
     if (command == "add") {
         return std::to_string(first + second);
     }
+    if (command == "subtract") {
+        return std::to_string(first - second);
+    }
+    if (command == "multiply") {
+        return std::to_string(first * second);
+    }
 
 
     return "Invalid data";
