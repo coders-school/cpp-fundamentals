ddef7dc026ca84a1aa87e40886d16e61b2f970c9
marcin-dolowy <marcindolowy00@gmail.com>
2021-05-08 23:03:21 +0200

    Added NWW

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -14,14 +14,14 @@ int NWD(int lhs, int rhs) {
 }
 
 int NWW(int lhs, int rhs) {
-    // int temp, result;
-    // result = lhs * rhs;
-    // while(rhs != 0)
-    // {
-    //     temp = rhs;
-    //     rhs = lhs % rhs;
-    //     lhs = temp;
-    // }
-    // result /= lhs;
-    // return result;
+    int temp, result;
+    result = lhs * rhs;
+    while(rhs != 0)
+    {
+        temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
+    }
+    result /= lhs;
+    return result;
 }
