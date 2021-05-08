f40dc58e848adb6aadd99ca1392f07ec7acc58f8
marcin-dolowy <marcindolowy00@gmail.com>
2021-05-08 22:13:42 +0200

    Added fibonacci_recursive

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -14,6 +14,11 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0){
+        return 0;
+    }
+    if (sequence < 3){
+        return 1;
+    }
+    return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
 }
