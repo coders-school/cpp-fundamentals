fdb897ae024b35c407ad3e7af2451ffb88affb59
MichalZ23 <mich.zawalich@gmail.com>
2021-05-09 18:11:15 +0200

    adding NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,7 +2,28 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    if(lhs *  rhs == 0)
+    {
+	return lhs == 0? rhs : lhs;
+    }
+    int remainder = -1;
+    int a = lhs;
+    int b = rhs;
+    while (remainder != 0)
+    {
+	remainder = a % b;
+	if(remainder != 0)
+	{
+		a = b;
+		b = remainder;
+	}
+    }
+    if(b<0)
+    {
+	b *= -1;
+    }
+
+    return b;
 }
 
 int NWW(int lhs, int rhs) {
