f52b2624a8f3e1834a5668744d7b601e0eb4de9b
lukasz-kukulka <qqla83@gmail.com>
2021-05-09 22:26:15 +0100

    Add NWD function

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -12,12 +12,10 @@ int NWD(int lhs, int rhs) {
         temp = left % right;
         left = right;
         right = temp;
-        //78:48=1, reszty 30 48:30=1, reszty 1830:18=1, reszty 1218:12=1, reszty 612:6=2, reszty 0
     }
     return abs( left );
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return 1;
+    return lhs/NWD(lhs, rhs)*rhs;
 }
