67796ca1bf92469bc33d5830d499e443019b9f4b
opiotrowsky <piotrowsky.o@gmail.com>
2021-05-09 22:47:40 +0200

    Added function subtract

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -6,4 +6,7 @@ std::string calculate(const std::string& command, int first, int second) {
     if(command == "add") {
         return std::to_string(first + second);
     }
+    if(command == "subtract") {
+        return std::to_string(first - second);
+    }
 }
