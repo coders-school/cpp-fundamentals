72bc621b31ecc98d68eb8131614dc2fe9be5fae3
mmsobotka <milenasobotka@gmail.com>
2021-05-09 16:18:53 +0200

    Revert "suport for add command"
    This reverts commit 6bdafb355ae81fb7810c2af3db2a2e586f85710c.

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,11 +2,6 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-	int result = 0;
-
-	if (command == "add")
-	{
-		result = first + second;
-	}
-    return  std::to_string(result);
+    // TODO: Implement your solution here and return proper value
+    return "";
 }
