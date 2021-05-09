dff769e53fc8c909a954a3388b55f978ee75bfe9
lukasz-kukulka <qqla83@gmail.com>
2021-05-09 16:50:39 +0100

    Add complete fibonacci iterative function

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,17 @@
 #pragma once
+#include <vector>
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int fibOne = 0, fibTwo = 1;
+    for (int i = 0; i < sequence; i++){
+        fibOne += fibTwo;
+        fibTwo = fibOne - fibTwo;
+    }
+    return fibOne;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    
+    return fibonacci_iterative(sequence);
 }
