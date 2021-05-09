5163545c64f8c297ddca9bc029651b7730f64c66
mmsobotka <milenasobotka@gmail.com>
2021-05-09 16:42:14 +0200

    support for multiply command

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -7,11 +7,14 @@ std::string calculate(const std::string& command, int first, int second) {
 	if (command == "add")
 	{
 		result = first + second;
-	}
-    
+	}    
     else if (command == "subtract")
 	{
 		result = first - second;
+	}
+    else if (command == "multiply")
+	{
+		result = first * second;
 	}
 	return  std::to_string(result);
 }
