e6ce67856b73ee2d089a6e62597f891ccfc21b4f
lukasz-kukulka <qqla83@gmail.com>
2021-05-09 20:32:58 +0100

    Add complete fibonacci recursive function

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -11,7 +11,10 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    
-    return fibonacci_iterative(sequence);
+    if (sequence < 1){
+        return 0;
+    } else if (sequence == 2 || sequence == 1) { 
+        return 1;
+    }
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
