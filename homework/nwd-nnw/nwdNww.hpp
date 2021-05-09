1af2bd6bc9a371f36d981e46b5d415e66dee0a9c
mmsobotka <milenasobotka@gmail.com>
2021-05-09 17:32:45 +0200

    NWD add

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,39 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+	lhs = abs(lhs);
+	rhs = abs(rhs);
+	if (lhs == 0)
+	{
+		return rhs;
+	}
+	else if (rhs == 0)
+	{
+		return lhs;
+	}
+
+	int result = lhs;
+
+	while (true)
+	{
+		if (lhs != rhs)
+		{
+			if (lhs > rhs)
+			{
+				result = lhs - rhs;
+				lhs = result;
+			}
+			else if (rhs > lhs)
+			{
+				result = rhs - lhs;
+				rhs = result;
+			}
+		}
+		else
+		{
+			return result;
+		}
+	}
 }
 
 int NWW(int lhs, int rhs) {
