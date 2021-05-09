5d7440d9c24e30dd289d9f1069b057571b80412e
Pawel Bieniasz <pawel.bieniasz@protonmail.com>
2021-05-09 12:15:53 +0200

    fibonacci_iterative

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {return 0;}
+    if (sequence <= 2) {return 1;}
+    int f1 = 1;
+    int f2 = 1;
+    int f3 = 0;
+    for (size_t i = 3; i <= sequence; i++)
+    {
+        f3 = f1 + f2;
+        f1 = f2;
+        f2 = f3;
+    }
+    return f3;
 }
 
 int fibonacci_recursive(int sequence) {
