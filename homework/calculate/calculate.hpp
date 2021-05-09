673274d1d914f5a4fcf1c554e0a8966ec26f8bbf
opiotrowsky <piotrowsky.o@gmail.com>
2021-05-09 22:50:18 +0200

    Added function multiply

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -9,4 +9,7 @@ std::string calculate(const std::string& command, int first, int second) {
     if(command == "subtract") {
         return std::to_string(first - second);
     }
+    if(command == "multiply") {
+        return std::to_string(first * second);
+    }
 }
