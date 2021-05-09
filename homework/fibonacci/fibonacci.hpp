3a10486e2986f404e53327c8458476512c326bda
pako2200 <pawelkowlw@gmail.com>
2021-05-09 22:32:46 +0200

    interative seq

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_iterative(int sequence)
+{
+    int top {0};
+    int previus {0};
+    int current {1};
+    
+
+    for(int i{0}; i < sequence; i++){
+        previus = current;
+        current = top;
+        top = previus + current;
+    }
+
+    return top;
 }
 
-int fibonacci_recursive(int sequence) {
+int fibonacci_recursive(int sequence)
+{
     // TODO: Your implementation goes here
     return 0;
 }
