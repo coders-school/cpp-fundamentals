12916127bb5dd27ee9282460fc30136548a25283
marcin-dolowy <marcindolowy00@gmail.com>
2021-05-09 11:57:52 +0200

    Tests passed

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,31 +1,25 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    if (rhs != 0)
-        return NWD(rhs, lhs % rhs);
-    if (lhs < 0) {
-        return lhs * -1;
+
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+
+    int temp;
+    while (rhs != 0)
+    {
+        temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
     }
     return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    if(lhs == rhs){
-        if (lhs < 0) {
-            return lhs * -1;
-        }
-        else return lhs;
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+    if(lhs == 0 || rhs == 0){
+            return 0;
     }
-    if (lhs < rhs) {
-        return NWW(rhs, lhs);
-    }
-    int k = lhs;
-    while (k % rhs != 0) {
-        k += lhs;
-    }
-    if(k < 0)
-    {
-    return k * -1;
-    }
-        return k;
+    return (lhs * rhs) / NWD(lhs, rhs);
 }
