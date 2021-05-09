c79ef38eeda4f2ecbc2c753feb46b2fe25ee1da4
lukasz-kukulka <qqla83@gmail.com>
2021-05-09 22:23:22 +0100

    Add NWD function

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,23 @@
 #pragma once
+#include <cstdlib>
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int temp, left = lhs, right = rhs;
+    if (left < right){
+        right = lhs;
+        left = rhs;
+    }
+    while (right != 0){
+        temp = left % right;
+        left = right;
+        right = temp;
+        //78:48=1, reszty 30 48:30=1, reszty 1830:18=1, reszty 1218:12=1, reszty 612:6=2, reszty 0
+    }
+    return abs( left );
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    return 1;
 }
