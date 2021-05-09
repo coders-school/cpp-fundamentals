a8c7e88642ec5aae9af62d9e10fd40665fac2086
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-05-09 12:48:05 +0200

    NWD works... at least passes the test

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,34 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if (lhs == 0 && rhs == 0) {
+        return 0;
+    }
+    else if (lhs == 0 && rhs != 0) {
+        return rhs;
+    }
+    else if (rhs == 0 && lhs != 0) {
+        return lhs;
+    }
+    
+    int lowerNumber = std::min(std::abs(lhs), std::abs(rhs));
+    int higherNumber = std::max(std::abs(lhs), std::abs(rhs));
+    // Alternative way to set the higherNumber - could be a bit more efficient?
+    // int higherNumber = (lhs == lowerNumber ? rhs : lhs);
+    int divisor = lowerNumber;
+    int i = 2;
+    while(divisor > 1) {
+        if (higherNumber % divisor == 0) {
+            return divisor;
+        }
+        if (lowerNumber % i == 0) {
+            divisor = lowerNumber / i;
+        }
+        ++i;
+    }
+    return 1;
+
 }
 
 int NWW(int lhs, int rhs) {
