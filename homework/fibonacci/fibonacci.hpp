a0616dd47c9855b5ce08bf501e0409586562d34d
MorawskiR <r.morawski92@gmail.com>
2021-05-10 12:58:29 +0200

    implemmentation works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,31 @@
 #pragma once
-
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
-}
-
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
-}
+ 
+ int fibonacci_iterative(int sequence) {
+ if(sequence == 0)
+ {
+     return 0;
+ }
+ if(sequence == 1 || sequence == 2 )
+ {
+     return 1;
+ }
+ int i = 2, num1 = 0, num2 = 1;
+ int sumPrev;
+ int suma = 2;
+     while (i <= sequence )
+     {
+       sumPrev = num1 + num2;
+       num1 = num2;
+       num2 = sumPrev;
+       i++;
+     }
+     return sumPrev;
+ }
+ 
+ int fibonacci_recursive(int sequence) {
+             if(sequence <=1)
+         {
+             return sequence;
+         }
+          return fibonacci_recursive(sequence - 1 ) +  fibonacci_recursive(sequence - 2 );
+ }
