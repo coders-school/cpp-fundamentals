00f5d726a7a343095945bd10466c687381cdf75e
karo <crazyayane07@gmail.com>
2021-05-10 19:23:12 +0200

    adding works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_iterative(int sequence)
+ {
+	if (sequence < 2)
+	{
+		return sequence;
+	}
+
+	int result = 1;
+	int prevResult = 0;
+
+	for (int i = 1; i < sequence; i++) 
+	{
+		int tempResult = result;
+		result += prevResult;
+		prevResult = tempResult;
+	}
+
+	return result;
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_recursive(int sequence) 
+{
+	if (sequence < 2)
+	{
+		return sequence;
+	}
+
+	return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);	
 }
