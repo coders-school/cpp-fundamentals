ea3471f3fa9f0da05d39f5aaea7340322b97d5d2
kondorski <thegreatvaultofknowledge@gmail.com>
2021-05-10 14:19:49 +0200

    Alterntive version of NWD and NWW functions

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,10 +1,43 @@
 #pragma once
-#include <numeric>
+//#include <numeric>
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    return std::gcd(lhs, rhs);
+    int abslhs = abs(lhs);
+    int absrhs = abs(rhs);
+    int temp;
+
+    if(abslhs == 0 && absrhs == 0){
+        return 0;
+    }
+    if(absrhs == 0){
+        return lhs;
+    }
+    if(abslhs == 0){
+        return rhs;
+    }
+    else {
+        do {
+            if(abslhs > absrhs){
+                temp = abslhs;
+                abslhs = absrhs;
+                absrhs = temp;    
+            }
+            absrhs -= abslhs;  
+        } while(absrhs != 0);
+        return abslhs;
+    }    
 }
 
 int NWW(int lhs, int rhs) {
-    return std::lcm(lhs, rhs);
+    int abslhs = abs(lhs);
+    int absrhs = abs(rhs);
+    
+    if(abslhs == 0 || absrhs == 0){
+        return 0;
+    }
+
+    else {
+        return (abslhs / NWD(abslhs, absrhs)) * absrhs;
+    }
 }
