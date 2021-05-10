cc991d43a766e13dd34a6f39ad1715d2bcdc73fb
Łukasz Ziobroń <lukasz@coders.school>
2021-05-10 09:40:45 +0200

    Only add-even homework left

--- homework/calculate/calculate.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
-}
