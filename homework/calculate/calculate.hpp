00c07073fb09cdd98d545052a16553f033340962
Kacu <k.ptychu@gmail.com>
2021-05-10 19:38:24 +0200

    implemented calculate

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,21 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    if (command == "add") {
+        return std::to_string(first + second);
+    }
+    if (command == "subtract") {
+        return std::to_string(first - second);
+    }
+    if (command == "multiply") {
+        return std::to_string(first * second);
+    }
+    if (command == "divide") {
+        if (second == 0) {
+        return "Division by 0";
+            return "Invalid data";
+        }
+        return std::to_string(first / second);
+    }    
+    return "Invalid data";
 }
