0a8b2b571d5527486fb9ad67d43ae83b89c4243c
MorawskiR <r.morawski92@gmail.com>
2021-05-10 13:12:28 +0200

    NWW works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,21 @@
 #pragma once
-
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
-}
-
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
-}
+  
+ int NWD(int lhs, int rhs) {
+ 
+     while(lhs!=rhs)
+        if(lhs>rhs)
+            lhs-=rhs; //lub a = a - b;
+        else
+            rhs-=lhs; //lub b = b-a
+     return lhs; // lub b - obie zmienne przechowują wynik NWD(a,b)
+     return rhs;
+ }
+ 
+ int NWW(int lhs, int rhs) {
+  if (lhs < rhs)
+     return NWW(rhs, lhs);
+   int k = lhs;
+   while (k % rhs != 0)
+     k += lhs;
+   return k;
+ }
