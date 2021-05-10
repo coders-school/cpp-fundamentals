47622d89fb6b4d95f453b8415a0fa251b8cc4a9b
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-05-10 11:43:40 +0200

    Add recursive and iterative fibonacci

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int last{ 0 };
+	int beforeLast{ 1 };
+
+	int seekedValue{ 0 };
+	for (int i{ 0 }; i < sequence; ++i)
+	{
+		last = beforeLast;
+		beforeLast = seekedValue;
+		seekedValue = last + beforeLast;
+	}
+	return seekedValue;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0)
+		return 0;
+	if (sequence == 1)
+		return 1;
+	return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
 }
