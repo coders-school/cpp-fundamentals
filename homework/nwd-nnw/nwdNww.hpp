fbcda5cdc1fb40e9c671df48b6ee0f57dd513f9b
Kacu <k.ptychu@gmail.com>
2021-05-10 21:55:03 +0200

    implemented NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,14 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    while (rhs != 0) {
+        const int tmp = lhs % rhs;
+        lhs = rhs;
+        rhs = tmp;
+    }
+    return std::abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
 }
