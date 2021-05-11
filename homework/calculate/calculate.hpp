740a6d3e0e7cdb291218b5ced8f6387ed7984e91
Runer <runer53248@gmail.com>
2021-05-12 00:37:03 +0200

    remove duplicated converting line

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,25 +2,28 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    std::string result = "Invalid data";
+    std::string result{};
     int calculation;
+
     if(command.compare("add") == 0){
         calculation = first + second;
-        result = std::to_string(calculation);
     }else if(command.compare("subtract") == 0){
         calculation = first - second;
-        result = std::to_string(calculation);
     }else if(command.compare("multiply") == 0){
         calculation = first * second;
-        result = std::to_string(calculation);
     }else if(command.compare("divide") == 0){
         if(second != 0){
             calculation = first / second;
-            result = std::to_string(calculation);
         }else{
             result = "Division by 0";
         }
+    }else{
+        result = "Invalid data";
     }
+
+    if(result.empty()){
+        result = std::to_string(calculation);
+    }
+    
     return result;
 }
