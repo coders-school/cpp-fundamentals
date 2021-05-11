fb763ae11c0388f7e547e7775744360b2fb5e019
pwiatr3 <pwiatr3@yahoo.pl>
2021-05-11 15:10:59 +0200

    NWD working

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,7 +2,20 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    int pom;
+    
+    
+lhs=abs(lhs);
+rhs=abs(rhs);
+    
+	while(rhs!=0)
+    {
+		pom = rhs;
+		rhs = lhs%rhs;
+		lhs = pom;	
+	}
+	
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
