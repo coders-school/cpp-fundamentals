bc0f63aebe6dd188e9fd457a42f54621fdc2dc72
opiotrowsky <piotrowsky.o@gmail.com>
2021-05-11 17:50:25 +0200

    Working iterative Fibonacci

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int first = 1, second = 1, third;
+    
+    if(sequence == 0) {
+        return 0;
+    }
+    else if(sequence == 2 or sequence == 1) {
+        return first;
+    }
+
+    for(int i = 2; i < sequence; i++) {
+        third = first + second;
+        first = second;
+        second = third;
+    }
+    return third;
 }
 
 int fibonacci_recursive(int sequence) {
