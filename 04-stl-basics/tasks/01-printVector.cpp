0387c7616d8e589a74bb16655267f8558d44ca06
macmar1906 <71896373+macmar1906@users.noreply.github.com>
2021-05-11 07:41:48 +0200

    Update 01-printVector.cpp

--- 04-stl-basics/tasks/01-printVector.cpp
+++ 04-stl-basics/tasks/01-printVector.cpp
@@ -4,6 +4,11 @@
 
 // Implement printVector to print the content of a given vector on screen.
 // Each string in a new line.
+void printVector(std::vector<std::string> input_vec){
+	std::cout << input_vec[0] << '\n';
+	std::cout << input_vec[1] << '\n';
+	std::cout << input_vec[2] << '\n';
+}
 
 int main() {
     std::vector<std::string> vec {
