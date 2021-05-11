d755a772dd02a1d1ec8d5f44a33530c255423855
pwiatr3 <pwiatr3@yahoo.pl>
2021-05-11 12:35:56 +0200

    fibonacci iterative works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,6 +2,24 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
+        int an1 = 1; // a(n-1) - n-1 element of fibonacci series
+    int an2 = 0; // a(n-2) - n-2 element of fibonacci series
+    int an = 0; // a(n) - n element of fibonacci series
+
+    if(sequence == 0){
+        return 0;
+    }
+    if(sequence == 1){
+        return 1;
+    }
+    if (sequence >= 2){
+        for(int n = 2; n <= sequence; n++){
+            an = an2 + an1;
+            an2 = an1;
+            an1 = an;
+        }
+        return an;
+    }
     return 0;
 }
 
