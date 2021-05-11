94132e4247623aed7980c6b37b0592fc1fde7ae3
pwiatr3 <pwiatr3@yahoo.pl>
2021-05-11 11:56:00 +0200

    adding works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,10 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    if(command == "add"){
+        return(std::to_string(first + second));
+    }
+    else{
+        return "Invalid data";
+    }
 }
