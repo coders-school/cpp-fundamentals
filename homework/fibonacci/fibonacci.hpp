770162ace1d380caba6134191602f51de254f1eb
pwiatr3 <pwiatr3@yahoo.pl>
2021-05-11 12:36:58 +0200

    fibonacci recursive works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -25,5 +25,14 @@ int fibonacci_iterative(int sequence) {
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
+        if(sequence == 0){
+        return 0;
+    }
+    if(sequence == 1){
+        return 1;
+    }
+    if(sequence >= 2) {
+        return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
+    }
     return 0;
 }
