1681683e9c36008c455e42e209d3455134352ed3
macmar1906 <71896373+macmar1906@users.noreply.github.com>
2021-05-11 08:22:15 +0200

    Update 02-concatenateVector.cpp

--- 04-stl-basics/tasks/02-concatenateVector.cpp
+++ 04-stl-basics/tasks/02-concatenateVector.cpp
@@ -3,6 +3,22 @@
 
 // Implement concatenateVector.
 // It should take 2 vectors and return one which has alternately elements from the first and the other one.
+auto concatenateVector(std::vector<int> in1, std::vector<int> in2){
+	std::vector<int> out;
+	int i_max;
+
+	if (in1.size() > in2.size()) {
+		i_max = in1.size();
+	}else{
+		i_max = in2.size();
+	}
+
+	for(auto i=0;i<i_max;++i){
+		out.push_back(in1[i]);
+		out.push_back(in2[i]);
+	}
+	return out;
+}
 
 int main() {
     std::vector<int> vec1 {1, 2, 3, 4, 5};
