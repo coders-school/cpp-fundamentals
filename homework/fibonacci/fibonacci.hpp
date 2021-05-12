1ad81208506b66571da3a99172bc119438ddbe17
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-05-12 10:58:42 +0200

    Implement fibonacci iterative

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,19 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 1) {
+        return sequence;
+    }
+    int last{ 1 };
+    int nextToLast{ 1 };
+    int result{ 1 };
+    for (int i = 2; i < sequence; ++i) {
+        result = last + nextToLast;
+        nextToLast = last;
+        last = result;
+    }
+
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
