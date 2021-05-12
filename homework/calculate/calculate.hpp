a5b99be3f076314729c06843360d0ed5c7444ae8
Marek Burzacki <lawier@wp.pl>
2021-05-12 22:01:31 +0200

    additing add and subtract

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,13 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    std::string result;
+    if (command == "add"){
+       return std::to_string(first + second);
+    }else{
+    if (command == "subtract"){
+	return std::to_string(first - second);    
+    }
+    }
+    return ""; 
 }
