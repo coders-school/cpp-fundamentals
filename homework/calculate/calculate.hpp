e92c2fe384ea84fe2d79d2ed071ccde4d8af49a5
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-05-12 21:19:03 +0200

    division works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -10,6 +10,14 @@ std::string calculate(const std::string& command, int first, int second) {
     }
     else if(command == "multiply"){
         return std::to_string(first * second);
+    }
+    else if(command == "divide"){
+        if(second != 0){
+            return std::to_string(first / second);
+        }
+        else{
+            return "Division by 0";
+        }   
     }  
 
     return "";
