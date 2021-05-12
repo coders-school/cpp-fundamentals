40fb795ab02ad02d46af360f7a458815223bde0e
Marek Burzacki <lawier@wp.pl>
2021-05-12 22:42:29 +0200

    additing works divide

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -7,11 +7,15 @@ std::string calculate(const std::string& command, int first, int second) {
     if (command == "add"){
        return std::to_string(first + second);
     }else{
-    if (command == "subtract"){
-	return std::to_string(first - second);    
+        if (command == "subtract"){
+	    return std::to_string(first - second);    
     }else{
-    if (command == "multiply"){
-        return std::to_string(first * second);
+        if (command == "multiply"){
+            return std::to_string(first * second);
+    }else{
+	if(command == "divide"){
+	    return std::to_string(first / second);
+	}
     }
     }
     }
