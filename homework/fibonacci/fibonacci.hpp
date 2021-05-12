ff8ae0ebf93121811db396f98ab2dda2da384447
SirArt34 <sslon34@poczta.onet.pl>
2021-05-12 20:39:56 +0200

    iterat

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
-}
+    if (sequence == 0 || sequence == 1){
+        return sequence;
+        }
 
+        auto fst = 1u;
+        auto sec = 1u;
+
+        for (size_t i = 0; i < sequence-2; i++){
+            auto temp = sec;
+            sec += fst;
+            fst = temp;
+        }
+        return sec;
+    }
+    
+    
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+ //   
+   return 0;
 }
