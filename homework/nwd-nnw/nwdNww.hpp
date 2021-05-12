a77dd8346bd408fc55648d4fd7b9cf144bab5998
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-05-12 23:05:28 +0200

    NWD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,7 +1,30 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
+    int rest;
+    if (lhs < 0){
+        lhs *= -1;
+    }
+    if (rhs < 0){
+        rhs *= -1;
+    }
+    if(lhs != 0 and rhs != 0){        
+        while(rhs != 0 ){
+            rest = lhs%rhs;
+            lhs = rhs;
+            rhs = rest;
+        }
+        return lhs;
+    }
+    else if(lhs != 0 and rhs == 0){
+        return lhs;
+    } 
+    else if(lhs == 0 and rhs != 0){
+        return rhs;
+    } 
+    else{
+        return 0;
+    }
     return -1;
 }
 
