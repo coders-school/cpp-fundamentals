400acd66f05626fd7196095d81b1f046f2199976
Mikhael <michal.grzelczak@gmail.com>
2021-05-12 15:36:24 +0200

    Adding works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,45 @@
 #pragma once
 #include <string>
+#include <map>
+
+typedef const enum
+{
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE
+} menuItem_t;
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+
+    static const std::map<const std::string, menuItem_t> menuMap {
+        {"add", ADD}, 
+        {"subtract", SUBTRACT}, 
+        {"multiply", MULTIPLY}, 
+        {"divide", DIVIDE} };
+
+    try {
+        switch(menuMap.at(command))
+        {
+            case ADD:
+                return std::to_string(first+second);
+            case SUBTRACT:
+                return std::to_string(first-second);
+            case MULTIPLY:
+                return std::to_string(first*second);
+            case DIVIDE:
+                if(0 != second) {
+                    return std::to_string(first/second);
+                }
+                else {
+                    return "Division by 0";
+                }
+            default:
+                return "Invalid data";
+        }
+    } 
+    catch (const std::out_of_range& e) {
+        return "Invalid data";
+    }
+
 }
