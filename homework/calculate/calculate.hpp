a5df0a8fd4b7e3939d78b456ef7aceae74b4f96e
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-05-12 21:21:09 +0200

    should return invalid data works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -18,6 +18,9 @@ std::string calculate(const std::string& command, int first, int second) {
         else{
             return "Division by 0";
         }   
+    }
+    else{
+        return "Invalid data";
     }  
 
     return "";
