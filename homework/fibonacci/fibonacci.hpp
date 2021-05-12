a7c8042cb63e68d654fe414dea63e88dad27ba00
Runer <runer53248@gmail.com>
2021-05-13 00:21:45 +0200

    fibonacci_iterative works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
     if(sequence < 2){
         return sequence;
     }
-
-    return 0;
+    int result = 0;
+    int prePreFibonaci = 0;
+    int preFibonaci = 1;
+    for(int i = 2; i <= sequence; ++i){
+        result = prePreFibonaci + preFibonaci;
+        prePreFibonaci = preFibonaci;
+        preFibonaci = result;
+    }
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
