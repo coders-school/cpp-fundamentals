c144552b68796bdb4a3c87cf3f1cb70462d02366
Piotr Jaworski <piotr.jaworski9@o2.pl>
2021-05-13 11:51:11 +0200

    NWD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,14 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+    
+    if(rhs != 0){
+        return NWD(rhs, lhs%rhs);
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
