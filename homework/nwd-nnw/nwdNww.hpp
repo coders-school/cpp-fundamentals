40a1b6298794ea0b3a2f785a256f61432a8cd542
Krzysztof Kryczka <kw.kryczka@gmail.com>
2021-05-13 21:41:27 +0200

    NWD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,26 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if(rhs == 0){
+        return lhs;
+    }
+
+    if(lhs < 0){
+        lhs = std::abs(lhs);
+    }
+
+    if(rhs < 0){
+        rhs = std::abs(rhs);
+    }
+
+    while(rhs != 0){
+        int temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
+    }
+
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
