87601e9cc85354a9c0241aa3f565fd8ef5273e08
Runer <runer53248@gmail.com>
2021-05-13 21:48:20 +0200

    NWW tests passed

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -10,12 +10,12 @@ int NWD(int lhs, int rhs) {
     if(rhs < 0){
         rhs = -rhs;
     }
+    if(lhs == 0 or rhs == 0){
+        return lhs + rhs;
+    }
     int max = rhs;
     if(lhs > rhs){
         max = lhs;
-    }
-    if(lhs == 0 or rhs == 0){
-        return lhs + rhs;
     }
     int result = 1;
     for(int i = max/2; i>1; --i){
@@ -28,6 +28,6 @@ int NWD(int lhs, int rhs) {
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int result = (lhs*rhs)/NWD(lhs, rhs);
+    return result;
 }
