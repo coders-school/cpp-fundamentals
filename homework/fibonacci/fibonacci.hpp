8c3bce62b5e5ce20878e391f5f2fac7f0ab4c78e
aniolm <maciej.aniol.programista@gmail.comgit config --global user.name Your>
2021-05-13 08:41:36 +0300

    Iterative & recursive function working

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,32 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int Fn_2{0};
+    int Fn_1{1};
+
+    if(sequence==0){
+        return 0;
+    }else if(sequence==1){
+        return 1;
+    }else{
+        
+        for(int i=0;i<sequence;i++)
+        {
+            Fn_1+=Fn_2;
+            Fn_2=Fn_1-Fn_2;           
+        }
+        return Fn_2;
+    }
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence==0){
+        return 0;
+    }else if(sequence==1){
+        return 1;
+    }else{
+        return fibonacci_recursive(sequence-2)+fibonacci_recursive(sequence-1);
+    }
+	
+   
 }
--- homework/fibonacci/main.cpp
+++ homework/fibonacci/main.cpp
@@ -2,8 +2,8 @@
 #include "fibonacci.hpp"
 
 int main() {
-    std::cout << fibonacci_iterative(10) << "\n";
-    std::cout << fibonacci_recursive(10) << "\n";
+    std::cout << fibonacci_iterative(5) << "\n";
+    std::cout << fibonacci_recursive(5) << "\n";
 
     return 0;
 }
