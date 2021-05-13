30e545596bcb606d9db34bca83d2ede6b52eda29
Kruza <palatonista@gmail.com>
2021-05-13 19:18:56 +0200

    NWD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs) 
+{
+    int tmp = 0;
+    
+	while(rhs != 0)
+    {
+		tmp = rhs;
+		rhs = lhs % rhs;
+		lhs = tmp;	
+	}
+
+    return abs(lhs);
 }
 
-int NWW(int lhs, int rhs) {
+int NWW(int lhs, int rhs) 
+{
     // TODO: Implement me :)
     return -1;
 }
