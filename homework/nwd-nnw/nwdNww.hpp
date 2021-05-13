3205d77674633800ea8b9a9c175718a5aa995391
Runer <runer53248@gmail.com>
2021-05-13 21:41:47 +0200

    NWD non zero value tests passed

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -4,7 +4,24 @@ int NWD(int lhs, int rhs) {
     if(lhs == rhs){
         return lhs;
     }
-    return -1;
+    if(lhs < 0){
+        lhs = -lhs;
+    }
+    if(rhs < 0){
+        rhs = -rhs;
+    }
+    int max = rhs;
+    if(lhs > rhs){
+        max = lhs;
+    }
+    int result = 1;
+    for(int i = max/2; i>1; --i){
+        if(lhs % i == 0 and rhs % i == 0){
+            result = i;
+            break;
+        }
+    }
+    return result;
 }
 
 int NWW(int lhs, int rhs) {
