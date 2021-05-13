b37e9fdd8bdb53a1cd16e7ff09b9b42848a5ae1f
MateuszToJa <nauka.programowania.ma@gmail.com>
2021-05-13 15:39:35 +0200

    Add Calcualte

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,27 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+std::string calculate(const std::string &command, int first, int second)
+{
+    if (command == "add")
+    {
+        return std::to_string(first + second);
+    }
+    if (command == "subtract")
+    {
+        return std::to_string(first - second);
+    }
+    if (command == "multiply")
+    {
+        return std::to_string(first * second);
+    }
+    if (command == "divide")
+    {
+        if (second == 0) {
+            return "Division by 0";
+        }
+        return std::to_string(first / second);
+    }
+
+    return "Invalid data";
 }
