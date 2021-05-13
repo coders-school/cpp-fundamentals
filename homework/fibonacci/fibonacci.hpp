93d21e8acfea5f5e21bdae959a3d9e3ff1bcecf8
Mikhael <michal.grzelczak@gmail.com>
2021-05-13 10:54:02 +0200

    Adding works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,38 @@
 #pragma once
+#include <iostream>
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    const int first_terms[2] = {0, 1};
+    
+    if(sequence <= 1) {
+        return first_terms[sequence];
+    }
+
+    int * fibonacci;
+    fibonacci = new int [sequence+1];
+
+    if (fibonacci == nullptr)
+        std::cout << "Error: memory could not be allocated";
+
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+
+    for(size_t i = 2; i <= sequence; i++) {
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
+
+    int ith_fibbonaci = fibonacci[sequence];
+    delete[] fibonacci;
+
+    return ith_fibbonaci;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    const int first_terms[2] = {0, 1};
+    
+    if(sequence <= 1) {
+        return first_terms[sequence];
+    }
+
+    return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
 }
