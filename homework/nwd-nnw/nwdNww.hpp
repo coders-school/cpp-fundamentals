addac7c790890187466f0afefcbef11db484ae84
SirArt34 <sslon34@poczta.onet.pl>
2021-05-14 17:55:56 +0200

    adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,44 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+if (rhs == 0) {
+        return lhs;
+    }
+    if (lhs == 0) {
+        return rhs;
+    }
+
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+
+    while (lhs != rhs) {
+        if (lhs < rhs) {
+            rhs -= lhs;
+        } else {
+            lhs -= rhs;
+        }
+    }
+
+    return lhs;
 }
 
+
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    
+    if (lhs == 0 || rhs == 0){
+        return 0;
+    }
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+
+    if (lhs < rhs){
+        return NWW(rhs, lhs);
+    }
+    int nww_1 = lhs;
+
+    while (nww_1 % rhs !=0){
+        nww_1 += lhs;
+    }    
+    return nww_1;
+
 }
