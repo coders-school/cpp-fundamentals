0505278a2c6a3f766868e92448add151464e0d39
Pawel Bieniasz <pawel.bieniasz@protonmail.com>
2021-05-14 21:12:53 +0200

    generatesequence

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,12 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> vec;
+    if (count <= 0) { return vec; }
+    for (size_t i = 1; i <= count; ++i) {
+        vec.emplace_back(i * step);
+    }
+    return vec;
 }
+
+
