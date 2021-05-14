f8072c4b0686d6952c54bc0414c69aec5f0e45df
Warp-12 <pl.walery@gmail.com>
2021-05-14 10:46:51 +0200

    Homework add-even done, all tests passed

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,15 @@
 #pragma once
+#include <iostream>
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
-    return -1;
+    int sumEven = 0;
+    for (const auto& el : numbers) {
+        if (el % 2 == 0) {
+            sumEven += el;
+        }
+    }
+    return sumEven;
 }
