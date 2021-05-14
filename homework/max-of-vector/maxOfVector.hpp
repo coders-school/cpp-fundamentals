2e2269012a2748f73f810e9351063b27faff124f
miczel <michal.soloducha@gmail.com>
2021-05-14 14:16:17 +0200

    Max of vector works

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,10 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    // auto result = std::max_element(vec.begin(), vec.end());
+    int result = vec.front();
+    for (auto element : vec) {
+        if (element > result) result = element;
+    }
+    return result;
 }
