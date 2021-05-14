a4154b4f750d2b8d35db910003c46684705849a2
miczel <michal.soloducha@gmail.com>
2021-05-14 11:08:45 +0200

    Iterative approach works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,7 +1,19 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
+    if (sequence == 0) return 0;
+    if (sequence == 1) return 1;
+    int antepenulminate = 0;
+    int penultimate = 1;
+    int result = 0;
+    if (sequence > 2) {
+        for (int i = 3; i <= sequence; i++) {
+            result = antepenulminate + penultimate;
+            antepenulminate = penultimate;
+            penultimate = result;
+        }
+        return result;
+    }
     return 0;
 }
 
