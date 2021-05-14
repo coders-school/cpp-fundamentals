2162481b88f0f68e546cdaa423a306c1bae33598
Mikhael <michal.grzelczak@gmail.com>
2021-05-14 11:53:29 +0200

    Adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,26 @@
 #pragma once
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    std::vector<int> numbers = {lhs, rhs};
+    std::sort(numbers.begin(), numbers.end());
+
+    if( 0 == numbers[1] ) return numbers[0];
+
+    while (numbers[0] % numbers[1]) {
+        int temp = numbers[0] % numbers[1];
+        numbers[0] = numbers[1];
+        numbers[1] = temp;
+    }
+
+    return std::abs(numbers[1]);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if( 0 != NWD(lhs, rhs)) return std::abs( (lhs * rhs) / NWD(lhs, rhs) );
+    else return 0;
 }
