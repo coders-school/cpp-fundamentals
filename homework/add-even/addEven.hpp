69c98cd1fd75d8d8e7cae136d5d2a65fdccd9a29
miczel <michal.soloducha@gmail.com>
2021-05-14 13:21:20 +0200

    addEven working

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,12 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int result = 0;
+    // for (auto i = vector.begin(); i = vector.end(); i++) // alternatywna petla for
+    for(int element : numbers) {
+        if (element%2 == 0) {
+            result += element;
+        }
+    }
+    return result;
 }
