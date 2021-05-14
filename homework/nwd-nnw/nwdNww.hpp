8ca984c03fcd24f0141d51c3dddcc498a7438f8e
dadus15 <dadus15@hotmail.com>
2021-05-14 19:16:11 +0100

    adding nww-nwd

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,25 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+  int temp;
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+
+    while( rhs != 0 )
+    {
+        temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
+    }
+
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if ( lhs == 0 or rhs == 0 ) return 0;
+    
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+
+    return lhs / NWD(lhs, rhs) * rhs;
 }
