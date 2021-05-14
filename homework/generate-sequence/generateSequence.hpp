e0ea14800af9cc99d717497d9d5b64472a55c207
Jakub Kubalka <jk.kubalka@gmail.com>
2021-05-14 12:56:04 +0200

    Vector generated

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,19 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    
+    if (count <= 0)
+    {
+        return {};
+    }
+    std::vector<int> generatedvector;
+    generatedvector.reserve(count);
+    int firstStep = step;
+    while (count > 0)
+    {
+        generatedvector.push_back(step);
+        step += firstStep;
+        --count;
+    }
+    return generatedvector;
 }
