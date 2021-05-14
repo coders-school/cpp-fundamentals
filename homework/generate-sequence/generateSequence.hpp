115f4df9618d247854229b45d82fa9bcf590c7cf
Warp-12 <pl.walery@gmail.com>
2021-05-14 11:00:15 +0200

    Homework generate-sequence done, all tests passed

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,7 +1,28 @@
 #pragma once
+#include <cmath>
+#include <iostream>
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
-    return {};
+    std::vector<int> vec;
+
+    if (count <= 0) {
+        return vec;
+    }
+    if (step == 0) {
+        for (size_t i = 0; i < count; i++) {
+            vec.push_back(0);
+        }
+
+        return vec;
+    }
+    int element = step;
+    for (size_t i = 1; i <= count; i++) {
+        //   std::cout << element << "\n";
+        vec.push_back(element);
+        element += step;
+    }
+
+    return vec;
 }
