a957f650e51aaea7ef8356fc5397d7f00b481c71
Marcin$ <marcin.s10335@gmail.com>
2021-05-14 22:39:53 +0200

    Add Rest Function and 'if' statemnt when command not compared to map

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,6 +5,12 @@ std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
     std::string retunValue="";
     std::map<std::string, int> mapValues= {{"add",0},{"subtract",1},{"multiply",2},{"divide",3}};
+
+    if(mapValues.find(command) == mapValues.end())
+    {
+        return retunValue="Invalid data";
+    }
+    
     switch(mapValues[command])
     {
         case 0:
@@ -13,7 +19,17 @@ std::string calculate(const std::string& command, int first, int second) {
         case 1:
             retunValue = std::to_string(first -second);
             break;
-    
+        case 2:
+            retunValue = std::to_string(first * second);
+            break;
+        case 3:
+            if(second == 0)
+            {
+                retunValue = "Division by 0";
+            }
+            else
+                retunValue = std::to_string( first / second);
+            break;
         default:
             retunValue = "Invalid data";
     }
