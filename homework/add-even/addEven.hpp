fbfcf26e76ff45e78fa345bf078f90a799e3cc9f
Mikhael <michal.grzelczak@gmail.com>
2021-05-14 15:09:40 +0200

    Adding works

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,10 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int evenSum = 0;
+
+    for (auto elem : numbers) {
+        if(not (elem % 2)) evenSum += elem;
+    }
+    return evenSum;
 }
