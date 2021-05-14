f13788d7278b94885a18fcf6d444690d0a1222de
miczel <michal.soloducha@gmail.com>
2021-05-14 07:32:06 +0200

    FIX - delete unnecessary comments

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,13 +2,9 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // std::cout << command << first << second << "\n";
-    // std::string result("");
-    // std::cout << "test 1 " << result << "\n";
-
     // if (command.compare("add") == 1) {
     if (command == "add") {
-        std::cout << "test 1 " << "\n";
+        // std::cout << "test 1 " << "\n";
         return std::to_string(first + second);
     }
     if (command == "subtract") {
