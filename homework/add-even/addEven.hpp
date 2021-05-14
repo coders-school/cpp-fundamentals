ed4ce05123e2f19e6e6da79dc665e686a7a36887
Jakub Kubalka <jk.kubalka@gmail.com>
2021-05-14 12:11:14 +0200

    Every even added

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,15 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    
+    int evenAdded = 0;
+    for(auto value : numbers)
+    {
+        if(value%2 == 0)
+        {
+            evenAdded +=value;
+        }
+        else{}
+    }
+    return evenAdded;
 }
