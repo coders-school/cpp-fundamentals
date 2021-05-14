651ab3f65044728e1737d5397b789a46af172662
dadus15 <dadus15@hotmail.com>
2021-05-14 18:45:23 +0100

    adding fibonacci

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,16 @@
-#pragma once
-
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int a = 0;
+    int b = 1;
+     for (int i = 0; i < sequence; ++i) {
+        b += a;
+        a = b - a;
+    }
+    return a;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence < 2) {
+        return sequence;
+    }
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
