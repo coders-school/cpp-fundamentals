e42ec560576f747c48fe5222395d7a0da82455f1
LechWilk <lech.wilk@gmail.com>
2021-05-14 22:28:07 +0200

    printV builded

--- 04-stl-basics/tasks/01-printVector.cpp
+++ 04-stl-basics/tasks/01-printVector.cpp
@@ -5,6 +5,8 @@
 // Implement printVector to print the content of a given vector on screen.
 // Each string in a new line.
 
+void printVector(std::vector<std::string>);
+
 int main() {
     std::vector<std::string> vec {
         "Hello Coders School!",
@@ -13,4 +15,10 @@ int main() {
     };
     printVector(vec);
     return 0;
+}
+
+void printVector(std::vector<std::string> V)  {
+    for (auto element:V)    {
+        std::cout<<element<<'\n';
+    }
 }
