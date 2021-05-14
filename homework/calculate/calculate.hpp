382f40330ab9c1eca2ee983980620e1d82e01ce3
miczel <michal.soloducha@gmail.com>
2021-05-14 07:18:10 +0200

    Add calculate

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,7 +5,29 @@ std::string calculate(const std::string& command, int first, int second) {
     // std::cout << command << first << second << "\n";
     // std::string result("");
     // std::cout << "test 1 " << result << "\n";
-    if (command.compare("add") == 1)
-        return std::to_string(first + second);
 
+    // if (command.compare("add") == 1) {
+    if (command == "add") {
+        std::cout << "test 1 " << "\n";
+        return std::to_string(first + second);
+    }
+    if (command == "subtract") {
+        return std::to_string(first - second);
+    }
+    if (command == "multiply") {
+        return std::to_string(first * second);
+    }
+    if (command == "divide") {
+        if (second == 0) {
+            return "Division by 0";
+        }
+        return std::to_string(first / second);
+    }
+    return "Invalid data";
+
+
+    // wersja ze switch:
+    // switch (command) {
+    //     case std::string("add"): return std::to_string(first + second);
+    // }
 }
