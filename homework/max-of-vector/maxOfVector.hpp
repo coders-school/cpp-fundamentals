92d3cac850a1da399df03dc357144dea58ba9e2c
Pawel Bieniasz <pawel.bieniasz@protonmail.com>
2021-05-14 20:33:50 +0200

    maxofvector

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,11 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max = std::numeric_limits<int>::min();
+    for (const auto& i : vec) {
+        if (i > max) {
+            max = i;
+        }
+    }
+    return max;
 }
