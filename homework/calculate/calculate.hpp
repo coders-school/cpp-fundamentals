240963f9e47e919f50b966135f03f91fdcbf1ad2
Marcin$ <marcin.s10335@gmail.com>
2021-05-14 21:29:01 +0200

    Add map to map string into swith int values

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,16 @@
 #pragma once
 #include <string>
-
+#include <map>
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    std::string retunValue="";
+    std::map<std::string, int> mapValues= {{"add",0},{"subtract",1},{"multiply",2},{"divide",3}};
+    switch(mapValues[command])
+    {
+    
+        default:
+            retunValue = "Invalid data";
+    }
+
+    return retunValue;
 }
