5f9ebe047c252d6f4c86d05606d06c5e395c6c3d
Paweł Głowacki <glowacki.pawel89@gmail.com>
2021-05-14 15:36:57 +0200

    adding works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,22 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int myArr[sequence + 2];
+    myArr[0] = 0;
+    myArr[1] = 1;
+    
+    for(int i = 2; i <= sequence; i++) {
+        myArr[i] = myArr[i - 1] + myArr[i - 2];
+    }
+
+    return myArr[sequence];
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    if(sequence <= 1) {\
+        return sequence;
+    } else {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
 }
