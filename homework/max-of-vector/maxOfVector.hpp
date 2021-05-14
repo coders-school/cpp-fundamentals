8e1d796a7350e97b389da7b7a7a6feb1209cfef3
lukasz-kukulka <qqla83@gmail.com>
2021-05-14 12:08:50 +0100

    fixup! Homework max of vector

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -5,9 +5,5 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     auto result = std::max_element(vec.begin(), vec.end());
-    // std::sort(vec.begin(), vec.end(), [](int a, int b) {
-    //     return a > b;
-    // });
-    // return vec.begin();
     return *result;
 }
