5f7e563914c5b27c2ee1a904ff6d173a70342633
miczel <michal.soloducha@gmail.com>
2021-05-14 12:05:50 +0200

    NWD and NWW works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(rhs != 0) return NWD(rhs, lhs%rhs);
+    if (lhs < 0) lhs = -lhs;
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 && rhs ==0) return 0;
+    int result = lhs/NWD(lhs, rhs)*rhs;
+    if (result < 0) result = -result;
+    return result;
 }
