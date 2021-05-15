e59df928f7a97c785be4ff0ec4f7685bd29716b7
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-05-15 12:29:18 +0200

    maxOfVector

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,16 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <algorithm>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    auto max = vec[0];
+    for(const auto& el:vec)
+    {
+        if(max<el)
+        {
+            max = el;
+        }
+    }
+    return max;
 }
