c8564be7991899ab91cff37c8bb81c6461e1e22b
Olaf Kucharski <olaf.kucharski@op.pl>
2021-05-15 06:13:56 -0400

    adding works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,28 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int first = 0;
+    int second = 1;
+    if(!sequence){
+        return 0;
+    }
+    if(sequence == 1){
+        return 1;
+    }
+    for(int i=1;i<sequence;i++){
+        int result = first + second;
+        first = second;
+        second = result;
+    }
+    return second;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(!sequence){
+        return 0;
+    }
+    if(sequence == 1){
+        return 1;
+    }
+    return fibonacci_recursive(sequence-2) + fibonacci_recursive(sequence-1);
 }
