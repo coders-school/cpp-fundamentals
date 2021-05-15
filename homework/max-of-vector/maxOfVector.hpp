d78e8ba75404082027fadff26d582a68377c2249
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-05-15 12:23:57 +0200

    Implement classic iterative solution

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,18 @@
 #pragma once
-#include <vector>
+
 #include <limits>
+#include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    if (vec.empty()) {
+        return 0;
+    }
+    auto max_of_vec{ std::numeric_limits<int>::min() };
+    for (auto el : vec) {
+        if (el > max_of_vec) {
+            max_of_vec = el;
+        }
+    }
+
+    return max_of_vec;
 }
