1b93e51617055682b9d7a60ee64381775907f248
Paweł Głowacki <glowacki.pawel89@gmail.com>
2021-05-15 11:49:28 +0200

    adding works

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -4,5 +4,11 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+  int maxNum{std::numeric_limits<int>::min()};
+  for(int i = 0; i < vec.size(); i++) {
+    if(vec[i] > maxNum) {
+      maxNum = vec[i];
+    }
+  }
+    return maxNum;
 }
