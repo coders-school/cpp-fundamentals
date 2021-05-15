71a40f52b316a71845e68d207ba33c60d33ac207
Sebastian302653 <yoda21@op.pl>
2021-05-15 20:40:04 +0200

    nwd-nww

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,24 +1,27 @@
 #pragma once
-
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
 int NWD(int lhs, int rhs) {
-    while(lhs!= rhs)
-    {
-    	if(lhs>rhs)
-    		lhs=lhs-rhs;
-    	else
-    		rhs=rhs-lhs;
-    }
-    return lhs;
+  lhs = abs(lhs);
+  rhs = abs(rhs);
+ 
+  auto i{lhs};
+  while (lhs != 0) {
+    i = lhs;
+    lhs = rhs % lhs;
+    rhs = i;
+  }
+  return rhs;
 }
 
 int NWW(int lhs, int rhs) {
-    int tmp;
-    while(rhs!= 0)
-    {
-    	tmp = rhs;
-    	rhs = lhs % rhs;
-    	lhs = tmp;
 
-    }
-    return lhs;
+  lhs = abs(lhs);
+  rhs = abs(rhs);
+
+  if (lhs == 0 || rhs == 0) {
+    return 0;
+  }
+  return lhs / NWD(lhs, rhs) * rhs;
 }
