33fe1d876291b97b5e804649a554ef20a7edef5b
Artur Michałowski <artur.michalowski93@gmail.com>
2021-05-15 12:19:16 +0200

    reformatting

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -4,10 +4,9 @@
 
 int addEven(const std::vector<int>& numbers) {
     int sum {0};
-    std::for_each(numbers.begin(), numbers.end(), 
-            [&sum](int element){
+    std::for_each(numbers.begin(), numbers.end(), [&sum](int element) {
                  if(element % 2 == 0) {
-                     sum += element;
+                    sum += element;
                  }
             });
     return sum;
