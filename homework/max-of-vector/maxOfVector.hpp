6f0302d194557bbf787c9e586e28700f8bac49a5
raxonpc <tomeczekorlowski1@gmail.com>
2021-05-15 18:08:38 +0200

    Implemented function

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,9 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <algorithm>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(const std::vector<int>& vec) 
+{
+    return *std::max_element(vec.cbegin(), vec.cend());
 }
