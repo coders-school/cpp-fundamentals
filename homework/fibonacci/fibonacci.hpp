9247fe3686c6b52749658ce7aa914d496861a956
Sebastian302653 <yoda21@op.pl>
2021-05-15 17:26:53 +0200

    fibonacci

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,23 @@
 #pragma once
-
+#include<iostream>
+#include<vector>
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    std::vector<int> numbers;
+    numbers.push_back(0);
+    numbers.push_back(1);
+
+
+    for(int i=2; i<=sequence; i++)
+    {
+    	numbers.push_back(numbers[i - 2] + numbers[i - 1]);
+
+    }
+
+    return numbers[sequence];
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+	if(sequence<2)
+    	return sequence;
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
