42c9d365675f3b87392ea5ef810af24a0fa69d1b
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-05-15 15:33:18 +0200

    Addition

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,12 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+std::string calculate(const std::string& command, int first, int second) 
+{
+ // TODO: Implement your solution here and return proper value
+    if(command == "add")
+    {
+        return std::to_string(first + second);
+    }
     return "";
 }
