3d91edc24a77c23bd933e319bf96f6ba89715ba1
PawelYak <pawelstojak@gmail.com>
2021-05-15 16:10:03 +0200

    addEven impl

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,16 @@
 #pragma once
+#include <iostream>
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+int addEven(const std::vector<int> &numbers) {
+  auto r{0};
+  for (auto i : numbers) {
+    std::cout << i;
+    if (i % 2 == 0) {
+      r += i;
+    }
+  }
+  std::cout << '\n';
+  std::cout << "r= " << r << '\n';
+  return r;
 }
