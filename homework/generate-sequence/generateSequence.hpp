d424d2bb06480e9dcb20b5326688695c3ad29b15
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-05-15 15:10:41 +0200

    generateSequance

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,18 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+   
+    std::vector<int> vec{step};
+
+    if(count<=0)
+    {
+        return {};
+    }
+    //vec.reserve(count);
+   for(size_t i{1}; i<count; i++)
+   {
+       vec.emplace(vec.end(),vec[i-1]+step);
+   }
+    
+    return vec;
 }
