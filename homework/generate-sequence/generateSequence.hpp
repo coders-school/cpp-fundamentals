97deb8d236b839bce2f9ce39cd25138de34f6050
pwiatr3 <pwiatr3@yahoo.pl>
2021-05-15 11:15:01 +0200

    generateSequence works

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -3,5 +3,13 @@
 
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
-    return {};
+    std::vector<int> vec {step};
+    if(count <= 0){
+        return {};
+    }
+    for(size_t i = 1; i< count; i++){
+        vec.push_back(vec[i - 1] +  step);
+    }
+
+    return vec;
 }
