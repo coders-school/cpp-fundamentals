7157d374181c38b4a298735cf375a7579ace2989
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-05-15 11:45:14 +0200

    Implement classic iterative solution

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,14 @@
 #pragma once
+
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int sum_of_even{};
+    for (auto el : numbers) {
+        if (el % 2 == 0) {
+            sum_of_even += el;
+        }
+    }
+
+    return sum_of_even;
 }
