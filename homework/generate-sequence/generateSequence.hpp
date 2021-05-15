7b9dcdcf772fce58afe19135a658e8c60e8086cc
raxonpc <tomeczekorlowski1@gmail.com>
2021-05-15 18:05:59 +0200

    Implemented function

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,7 +1,23 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 
-std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+std::vector<int> generateSequence(int count, int step) 
+{
+    if(count <= 0)
+    {
+        return std::vector<int>{};
+    }
+
+    std::vector<int> vec(count);
+    
+    int temp{step};
+
+    for(int& num : vec)
+    {
+        num = step;
+        step += temp;
+    }
+    
+    return vec;
 }
