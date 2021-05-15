01feec8ee0b4ae3a242b43ad997903e1201492a2
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-05-15 17:17:35 +0200

    /Adding Fibonacci itterative

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,16 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence < 2){
+    	return sequence;
+    }
+    int a = 1;
+    int b = 1;
+    while (sequence--> 2){
+    	b += a;
+    	a = b - a;
+    }
+    return b;
 }
 
 int fibonacci_recursive(int sequence) {
