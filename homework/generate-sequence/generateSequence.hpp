e82c9a6201ec83467337a75c7d6c153f0f4c1cb2
PawelYak <pawelstojak@gmail.com>
2021-05-15 21:54:31 +0200

    return of empty vector while step is zero

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,9 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
+  if (step == 0) {
+  return std::vector<int>{};
+  }
   std::vector<int> vec{step};
   for (auto i = 1; i < count; i++) {
     vec.push_back(step + vec[i - 1]);
