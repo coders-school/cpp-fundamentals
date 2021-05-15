bb7556dc2cb415252f3a0a8d4da571f3e3a01a7d
Marcin$ <marcin.s10335@gmail.com>
2021-05-15 14:00:42 +0200

    Add Fibonacci Iterative

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,21 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+
+    if(sequence < 2)
+    {
+        return sequence;
+    }
+    // 0 1 1 2 3 5 8 13 21 34 
+    int first =1;
+    int second =1;
+    while(sequence-- > 2)
+    {
+        second = second + first;
+        first = second - first;
+    }
+
+    return second;
 }
 
 int fibonacci_recursive(int sequence) {
