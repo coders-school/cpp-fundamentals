33adab9d19b0d7fbe7fcd5e29a9226eb5aabc736
PawelYak <pawelstojak@gmail.com>
2021-05-15 16:13:19 +0200

    ready

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -5,12 +5,9 @@
 int addEven(const std::vector<int> &numbers) {
   auto r{0};
   for (auto i : numbers) {
-    std::cout << i;
     if (i % 2 == 0) {
       r += i;
     }
   }
-  std::cout << '\n';
-  std::cout << "r= " << r << '\n';
   return r;
 }
