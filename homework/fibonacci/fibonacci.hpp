cfbe84d7a396c0d19dba34e701f798c9a71bd88b
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-05-15 17:38:40 +0200

    Fibonacci

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
+int fibonacci_iterative(int sequence) 
+{
     // TODO: Your implementation goes here
-    return 0;
+    if(sequence < 2)
+    {
+        return sequence;
+    }
+    int first = 1;
+    int secound = 1;
+
+    while(sequence-- > 2)
+    {
+        secound += first;
+        first = secound - first;
+
+    }
+    return secound;
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_recursive(int sequence) 
+{
+   if (sequence <= 1)
+   {
+       return sequence;
+   }
+   return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
