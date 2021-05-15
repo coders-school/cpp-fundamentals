a45ab6ebe62562681255c84c082369cd76e03712
Artur Michałowski <artur.michalowski93@gmail.com>
2021-05-15 11:54:02 +0200

    All tests passed

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,14 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int sum {0};
+    std::for_each(numbers.begin(), numbers.end(), 
+            [&sum](int element){
+                 if(element % 2 == 0) {
+                     sum += element;
+                 }
+            });
+    return sum;
 }
