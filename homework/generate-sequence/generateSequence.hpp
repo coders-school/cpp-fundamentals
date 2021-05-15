162786b39b1a94a255259f18f749be5a86c0b739
Paweł Głowacki <glowacki.pawel89@gmail.com>
2021-05-15 12:09:18 +0200

    adding works

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -3,5 +3,13 @@
 
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
-    return {};
+    if(count <= 0) {
+        return {};
+    }
+    std::vector<int> vec{};
+    vec.push_back(step);
+    for(int i = 1; i < count; i++) {
+        vec.push_back(vec[i - 1] + step);
+    }
+    return vec;
 }
