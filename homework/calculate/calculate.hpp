5e0dd11c9725b6aeff9f4cce8f751dd2477bd438
Sebastian302653 <yoda21@op.pl>
2021-05-15 16:57:04 +0200

    last chance

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,33 +1,23 @@
 #pragma once
 #include <string>
-#include <map>
+
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    std::string returnValue="";
-    std::map<std::string, int> mapValues= {{"add",0},{"subtract",1},{"multiply",2},{"divide",3}};
-    switch(mapValues[command])
-    {
-        case 0:
-            returnValue = std::to_string(first + second);
-            break;
-
-        case 1:
-            returnValue = std::to_string(first - second);
-            break;
-
-        case 2:
-            returnValue = std::to_string(first * second);
-            break;
-
-        case 3:
-
-            if(second == 0)
-                returnValue = "Invalid data";
-            returnValue = std::to_string(first / second);
-            break;
-
-        default:
-            returnValue = "Invalid data";
+    if (command == "add"){
+        return std::to_string(first + second);
+    }else if (command == "subtract"){
+        return std::to_string(first - second);
+        } else if (command == "multiply"){
+            return std::to_string(first * second);
+            }else if (command == "divide"){
+                if (second == 0){
+                return "Division by 0";
+            }else{
+                return std::to_string(first / second);
+                }
+            }   
+    else{   
+        return "Invalid data";
     }
-    return returnValue;
+    return "";
 }
