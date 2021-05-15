bed68fa0d5cfdc18294ba3d075035ed43feb236d
Artur Michałowski <artur.michalowski93@gmail.com>
2021-05-15 12:49:33 +0200

    Adding a solution for max-of-vector task

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,25 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <algorithm>
+
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max = vec.at(0);
+    for_each(vec.begin(), vec.end(), [&max](int element){
+        if (element > max) {
+            max = element;
+        }
+    });
+    return max;
 }
+/*
+Please review this code. Which solution is better?
+
+int maxOfVector(const std::vector<int>& vec) {
+    std::vector<int> newVector = vec;
+    std::sort(newVector.begin(), newVector.end());
+    return newVector.at(newVector.size()-1);
+}
+
+*/
