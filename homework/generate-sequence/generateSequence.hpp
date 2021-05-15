acd7b556e547188d9b1bb815cc5a011f3a32f366
Marcin Dołowy <81704885+marcin-dolowy@users.noreply.github.com>
2021-05-15 11:24:18 +0200

    Works

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,25 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> result;
+    if(step == 0)
+    {
+        for(int i = 0; i < count; ++i)
+        {
+            result.emplace_back(0);
+        }
+    }
+    else if(step < 0)
+    {
+        for(int i = step*-1; i<= count * step * -1; i+=(step*-1))
+        {
+            result.emplace_back(-i);
+        }
+    }
+    else {
+        for (int i = step; i <= count * step; i += step) {
+            result.emplace_back(i);
+        }
+    }
+    return result;
 }
