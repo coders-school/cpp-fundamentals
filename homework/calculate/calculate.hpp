cf992cfc9bfd7502658f739d6b2ec834b7ed5c08
adrianz261 <adrian.zuchowski@outlook.com>
2021-05-16 00:30:17 +0100

    Added commands.

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,31 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
+    if (command == "")
+    {
+        return "";
+    }
+
+    if (command == "add")
+    {
+        return std::to_string(first + second);
+    }
+    else if (command == "subtract")
+    {
+        return std::to_string(first - second);
+    }
+    else if (command == "multiply")
+    {
+        return std::to_string(first * second);
+    }
+    else if (command == "divide")
+    {
+        return (second != 0) ? std::to_string(first / second) : 0;
+    }
+    else if (command == "quit")
+    {
+        return "";
+    }
+
     return "";
 }
