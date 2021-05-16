8de149d85216bc2c614a61f2e6c96f5bf936db80
Arek Ogiewa <arkadiusz.ogiewa@gmail.com>
2021-05-16 12:40:08 +0200

    addition added

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,24 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    
+    std::string res {};
+    
+    if (command == "add"){
+	    res = std::to_string(first + second);
+    }
+    else if (command == "subtract"){
+    }
+    else if (command == "multiply"){
+    }
+    else if (command == "divide"){
+	    if (second = 0)
+		    res == "Division by 0";
+	    else
+    }
+    else
+	    res = "Invalid data";
+    
+    return res;
+
 }
