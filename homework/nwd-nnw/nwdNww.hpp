4600f1ded4d80d8c03e24346c216d195003afef8
opiotrowsky <piotrowsky.o@gmail.com>
2021-05-16 20:41:06 +0200

    Working NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,33 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    int num0, num1;
+    if(lhs < 0){
+        lhs *= (-1);
+    }
+    if(rhs < 0) {
+        rhs *= (-1);
+    }
+    if(lhs < rhs) {
+        num0 = lhs;
+        lhs = rhs;
+        rhs = num0;
+    }
+    if(rhs == 0) {
+        return lhs;
+    }
+    if(lhs % rhs == 0) {
+        return rhs;
+    }
+    else{
+        while(lhs % rhs != 0) {
+            num1 = lhs;
+            lhs = rhs;
+            rhs = num1 % rhs;
+        }
+        return rhs;
+    }
 }
 
 int NWW(int lhs, int rhs) {
