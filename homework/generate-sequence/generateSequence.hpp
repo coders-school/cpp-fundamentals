c88a61b1ac88a8128ab04de9e4eeafd3391ba38a
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-05-16 23:52:39 +0200

    Second version. I'm not really sure it's better than first.

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -5,10 +5,8 @@ std::vector<int> generateSequence(int count, int step) {
 
     std::vector<int> sequence;
     if (count > 0) {
-        int value = step;
-        for(std::size_t i = 0; i < count; i++) {
-            sequence.push_back(value);
-            value += step;
+        for(std::size_t i = 1; i <= count; ++i) {
+            sequence.push_back(step * i);
         }
     }
     return sequence;
