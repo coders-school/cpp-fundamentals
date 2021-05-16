685cfb3c3effe379e9b933dac1e888e8b0d95490
Mateusz <gasiorowskii.mateusz@gmail.com>
2021-05-16 07:12:45 -0700

    program works

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -4,5 +4,13 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    int result = vec[0];
+
+    for(const auto& el : vec ){
+        if (el > result) {
+            result = el;
+        }
+    }
+
+    return result;
 }
