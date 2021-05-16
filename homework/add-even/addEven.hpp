7e9c098e91ea6fa6e32bbaf18b4a6cb434a6fb4d
MorawskiR <r.morawski92@gmail.com>
2021-05-16 23:06:44 +0200

    homework2

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,11 +2,15 @@
 #include <vector>
 int addEven(const std::vector<int>& numbers)
 {
-    int sum = 0;
+     int sum = 0;
+    if(numbers.capacity() ==0)
+        return sum;
+
     for(auto const& el: numbers)
         {
             if(el % 2 == 0)
                 sum+=el;
         }
         return sum;
+
 }
