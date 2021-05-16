de2c71f9f1ef6b18e7dd797611d34e0ad78646ad
Arek Ogiewa <arkadiusz.ogiewa@gmail.com>
2021-05-16 23:14:19 +0200

    NWD implemented

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,9 +2,21 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
-}
 
+	if(lhs<0)
+		lhs = -lhs;
+	if(rhs<0)
+		rhs = -rhs;
+
+	int tmp {};
+	while(rhs){
+		tmp = lhs % rhs;
+		lhs = rhs;
+		rhs = tmp;
+	}
+	return lhs;
+}
+    
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
     return -1;
