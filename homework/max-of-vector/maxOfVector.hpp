7aede8eb8bf4676dfb8085d000e408a95649dd29
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-05-16 23:14:24 +0200

    shorter/easier solution works

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,12 +1,18 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 
 int maxOfVector(const std::vector<int>& vec) {
-    int max = vec.front();
-    for (int element : vec) {
-        if (element > max) {
-            max = element;
-        }
-    }
-    return max;
+    return *std::max_element(vec.begin(), vec.end());
 }
+
+// first version below - typical solution
+// int maxOfVector(const std::vector<int>& vec) {
+//     int max = vec.front();
+//     for (int element : vec) {
+//         if (element > max) {
+//             max = element;
+//         }
+//     }
+//     return max;
+// }
