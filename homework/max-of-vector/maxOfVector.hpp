5c5acb6d6b56a3078851ad3c762e51c12e6f38a9
Maciej M <macmar1906@gmail.com>
2021-05-16 17:33:29 +0200

    maxofvector ready

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -4,5 +4,10 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+	int max = std::numeric_limits<int>::min();
+	
+	if(vec.size() == 1) return vec[0];
+	else
+	for(auto el : vec) if(el > max) max = el;
+    return max;
 }
