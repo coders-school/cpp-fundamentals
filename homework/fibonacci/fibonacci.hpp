7ec7fa3d2654528f2c7887eacf22977117972385
adrianz261 <adrian.zuchowski@outlook.com>
2021-05-16 18:38:54 +0100

    100% tests passed.

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    if (sequence < 1)
+    {
+        return 0;
+    }
+
+    int i2 = 0;
+    int i1 = 1;
+    int result = 1;
+    
+    for (;sequence > 1; --sequence)
+    {
+        result = i2 + i1;
+        i2 = i1;
+        i1 = result;
+    }
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (!sequence)
+    {
+        return 0;
+    }
+    return (sequence > 2) ? fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1) : 1;
 }
