7fa2ee92b3d8708df1ce493cc3aaf44b91a07587
Jan Tobiczyk <j.tobiczyk43@gmail.com>
2021-05-16 14:32:28 +0200

    First of all

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,24 @@
 #pragma once
 #include <vector>
+#include <numeric>
+int sum;
+std::vector<int> vector;
+int addEven(const std::vector<int>& numbers)
+ {
+     if(!numbers.empty())
+     {
+            for(int i =0; i<numbers.size(); i++)
+             {
+            if(numbers[i] % 2 == 0)
+            {
+            vector.push_back(numbers[i]);
+            }
+    }
+    sum = std::accumulate(vector.begin(), vector.end(), 0);
+    return sum;
+     }
+     else
+     sum = 0;
 
-int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+     return sum;
 }
