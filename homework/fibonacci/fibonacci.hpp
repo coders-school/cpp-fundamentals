85dc2935b65f730140d733f915443077f54b9aea
Arek Ogiewa <arkadiusz.ogiewa@gmail.com>
2021-05-16 16:21:28 +0200

    Fibonacci iteratively added

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,22 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+
+    	int first {};
+	int second  {1};
+	int sum {};
+
+	for (int i = 0; i<=sequence; i++){
+		if (i<=1){
+			sum = i;
+		}
+		else{
+			sum = first + second;
+			first = second;
+			second = sum;
+		}
+	}
+	return sum;
 }
 
 int fibonacci_recursive(int sequence) {
