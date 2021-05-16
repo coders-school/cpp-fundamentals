40ec8fc88798b84823b6993ad0c1800f23075d8d
adrianz261 <adrian.zuchowski@outlook.com>
2021-05-16 11:35:45 +0100

    100% tests passed.

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,12 +2,7 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    if (command == "")
-    {
-        return "";
-    }
-
+    
     if (command == "add")
     {
         return std::to_string(first + second);
@@ -22,12 +17,8 @@ std::string calculate(const std::string& command, int first, int second) {
     }
     else if (command == "divide")
     {
-        return (second != 0) ? std::to_string(first / second) : 0;
-    }
-    else if (command == "quit")
-    {
-        return "";
+        return (second != 0) ? std::to_string(first / second) : "Division by 0";
     }
 
-    return "";
+    return "Invalid data";
 }
