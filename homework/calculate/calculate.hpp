d41e8f77840afedb21780cc874aa30c35c7437c5
Łukasz Nitka <lukasnit@wp.pl>
2021-05-17 00:18:57 +0200

    end works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,23 +2,31 @@
 #include <string>
 
 
-std::string calculate(const std::string& command, int first, int second) {
+std::string calculate(const std::string& command, int first, int second) 
+{
    
-   if(command == "add ")
+   if(command == "add")
    {
-   return std::to_string(first+second);
+   return std::to_string(first + second);
    }
    else if(command == "subtract") 
    {
-   return std::to_string(first-second);
+   return std::to_string(first - second);
    }
    else if(command == "multiply") 
    {
-   return std::to_string(first*second);
+   return std::to_string(first * second);
    }
    else if(command == "divide") 
    {
-   return std::to_string(first/second);
+   if(second==0)
+   {
+   return "Division by 0";
+   }
+   else
+   {
+   return std::to_string(first / second);
+   }
    }
    else return "Invalid data";
    
