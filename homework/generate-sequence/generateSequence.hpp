fbb521c87d8ed6cbeaabc3754425737dc9130e26
Marcin$ <marcin.s10335@gmail.com>
2021-05-16 13:00:49 +0200

    Add GenereateSeq function

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -3,5 +3,18 @@
 
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
-    return {};
+    if(count <= 0)
+    {
+        return {};
+    }
+    std::vector<int> returnedVec{};
+    returnedVec.reserve(count);
+    returnedVec.emplace_back(step);
+    
+    for(int i=1; i< count; ++i)
+    {
+        returnedVec.emplace_back(returnedVec[i-1]+step);
+    }
+
+    return returnedVec;
 }
