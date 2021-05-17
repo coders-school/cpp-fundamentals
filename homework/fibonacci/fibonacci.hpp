5867fb7fd8a8c8b1cf8427850c1fc8f55e10a629
Łukasz Nitka <lukasnit@wp.pl>
2021-05-17 10:15:39 +0200

    fib iter

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,28 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+int fibonacci_iterative(int sequence) 
+{
+   
+   int x=1;
+   int y=1;
+   int z=0;
+   if(sequence ==0)return 0;
+   else if(sequence == 1)return x;
+   else if(sequence == 2) return x;
+   
+   else
+   {
+   
+   	for (int i=3; i<=sequence ; i++)
+   	{
+   	 z=y;
+         y=y+x;
+         x=z;
+   	}
+       return y;
+   }
+   
 }
 
 int fibonacci_recursive(int sequence) {
