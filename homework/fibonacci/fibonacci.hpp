2f3cbd71985746f264f16d7240a402adb398b82e
Marek Burzacki <lawier@wp.pl>
2021-05-17 19:10:51 +0200

    addet fibonacci iterative

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,13 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int a=0, b=1, pom=1, i;
+    for (i=0; i<sequence; i++){
+        pom = a;
+        a= a + b;
+        b= pom;
+    }
+    return a;
 }
 
 int fibonacci_recursive(int sequence) {
