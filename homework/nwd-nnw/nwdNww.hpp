162830fa066f1ffca91ba6a4f8277495a69a6f3a
Łukasz Nitka <lukasnit@wp.pl>
2021-05-17 14:55:10 +0200

    test NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,19 @@
 #pragma once
+#include<cmath>
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs) 
+{
+    int r=0;
+    lhs=abs(lhs);
+    rhs=abs(rhs);
+    
+    while(rhs>0)
+    {
+    	r=lhs%rhs;
+    	lhs=rhs;
+    	rhs=r;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
