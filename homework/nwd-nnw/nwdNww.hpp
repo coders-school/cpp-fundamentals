0c5beedc1b30eb345514e59357d1dfab907f7de9
Bartosz Korczu <bartosz.korczynski@gmail.com>
2021-05-17 16:04:01 +0200

    all works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,33 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int pom;
+    
+	while(rhs!=0)
+    {
+		pom = rhs;
+		rhs = lhs%rhs;
+		lhs = pom;	
+	} if (lhs<0){
+            return lhs/-1;	
+	} else
+     return lhs;   
 }
 
+
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+     int nww;
+    int pom;
+    int c = lhs*rhs;
+    if(lhs==0||rhs==0){
+	return 0;
+	} while(rhs!=0){
+		pom = rhs;
+		rhs = lhs%rhs;
+		lhs = pom;
+        	} nww = c/lhs;
+        	if(nww<0) {
+        	return nww/-1;
+        	} else
+        	return nww;
 }
