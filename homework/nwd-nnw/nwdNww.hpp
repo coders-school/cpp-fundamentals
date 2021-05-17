b32ce9ba4efa5a8b93c82e29cb55d4865a2acf21
adrianz261 <adrian.zuchowski@outlook.com>
2021-05-17 18:30:01 +0100

    Fix.

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,15 +1,37 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    
+    if (!lhs || !rhs) {
+        return 0;
+    }
+
+    lhs = (lhs > 0) ? lhs : (-1) * lhs;
+    rhs = (rhs > 0) ? rhs : (-1) * rhs;
+
+    int a = (lhs > rhs) ? lhs : rhs;
+    int b = (lhs < rhs) ? lhs : rhs;
+
+    while (a != b)
+    {
+        a = a - b;
+        if ( a < b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+    return a;
 }
 
 int NWW(int lhs, int rhs) {
     
     if (!lhs || !rhs) {
-        return -1;
+        return 0;
     }
+
+    lhs = (lhs > 0) ? lhs : (-1) * lhs;
+    rhs = (rhs > 0) ? rhs : (-1) * rhs;
 
     int nww = (lhs > rhs) ? lhs : rhs;
     const int less = (lhs < rhs) ? lhs : rhs;
