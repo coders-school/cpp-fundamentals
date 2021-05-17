069d9744022e7bce441036ec70999374655d9ba0
Marek Burzacki <lawier@wp.pl>
2021-05-17 23:17:18 +0200

    addet works nwdNww

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,10 +2,24 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    int pom;
+    while (rhs !=0){
+        pom = lhs % rhs;
+        lhs = rhs;
+        rhs = pom;
+    }
+    if(lhs<0){
+        return lhs*(-1);
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    int pom, k;
+    k= lhs * rhs;
+    if (lhs == 0 or rhs == 0) {
+        return 0;
+    }
+    return std::abs((lhs * rhs) / NWD(lhs, rhs));
 }
