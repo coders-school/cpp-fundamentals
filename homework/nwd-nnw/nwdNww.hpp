4dd4894f83c8d328905c9b61fc19380cd868d322
Mateusz Serafin <mateusz97.serafin@gmail.com>
2021-05-18 20:10:14 +0200

    NWD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,7 +2,19 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+
+    if(lhs < 0) lhs *= -1;
+    if(rhs < 0) rhs *= -1;
+
+    int temp = {0};
+    while (rhs != 0)
+    {
+        temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
+    }
+    
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
