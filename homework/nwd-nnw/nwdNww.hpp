1f9d580d98dc72801675d1f7aec5b4b62ccef3fa
jakjan95 <jakubjanusz95@gmail.com>
2021-05-18 18:27:01 +0200

    GCD added

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,10 @@
 #pragma once
-
+#include <cmath>
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+  if (rhs == 0) {
+    return std::abs(lhs);
+  }
+  return NWD(rhs, lhs % rhs);
 }
 
 int NWW(int lhs, int rhs) {
