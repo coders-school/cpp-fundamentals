7ffa976836362a41f36d42af7442946e11774cf2
Arek Ogiewa <arkadiusz.ogiewa@gmail.com>
2021-05-18 12:22:40 +0200

    Max number of vector implemented

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -4,5 +4,13 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+
+    	int max {std::numeric_limits<int>::min()};
+	int num {};
+
+	for(const auto& num : vec)
+		if(max < num)
+			max = num;
+	
+	return max; 
 }
