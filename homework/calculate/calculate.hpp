d3b86f380595630c827390ee03818fa8e3ac3092
jakjan95 <jakubjanusz95@gmail.com>
2021-05-18 17:43:37 +0200

    Divide works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,6 +5,8 @@ namespace {
 const std::string addCommand = "add";
 const std::string subtractCommand = "subtract";
 const std::string multiplyCommand = "multiply";
+const std::string divideCommand = "divide";
+
 } // namespace
 
 std::string calculate(const std::string &command, int first, int second) {
@@ -14,6 +16,8 @@ std::string calculate(const std::string &command, int first, int second) {
     return std::to_string(first - second);
   } else if (command == multiplyCommand) {
     return std::to_string(first * second);
+  } else if (command == divideCommand) {
+    return second == 0 ? "Division by 0" : std::to_string(first / second);
   }
   return "Invalid data";
 }
