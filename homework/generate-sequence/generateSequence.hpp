5e04a891607344a945feedc8010bebf0ebefd06d
aniolm <maciej.aniol.programista@gmail.comgit config --global user.name Your>
2021-05-18 20:42:27 +0300

    Function to generate vector with seqence of ints complete

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,12 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> sequence;
+    int number = 0; 
+     
+    for(int i = 0; i < count; i++){
+        number += step;
+        sequence.push_back(number);
+    }
+    return sequence;
 }
