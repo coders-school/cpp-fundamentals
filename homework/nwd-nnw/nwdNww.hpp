7100d8bf48adb022a84815ad9f8858d259b5219f
rajder007 <krzysztof.zaczyk7@gmail.com>
2021-05-18 22:52:13 +0200

    nwd worked!

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,10 +2,30 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 || rhs == 0) {
+        return std::max(lhs,rhs);
+    } else if(lhs < 0 || rhs < 0) {
+        return std::min(std::abs(lhs),std::abs(rhs));
+    } else {
+        while(lhs!=rhs)
+        if(lhs>rhs)
+           lhs-=rhs; 
+        else
+           rhs-=lhs; 
+        return lhs; 
+    }
+
+
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    int pom;
+  	while(rhs!=0)
+	{
+    	pom = rhs;
+    	rhs = lhs%rhs;
+    	lhs = pom;  
+  	}
+    return lhs;
 }
