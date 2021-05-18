81c55b58c246ac931ba71cb3bf5f15ce701c3b39
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-05-18 19:07:00 +0200

    generateSequence function added - D4rth

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,11 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    int x = step;
+    std::vector<int> vec;
+    for (int i = 0; i < count; ++i) {
+        vec.push_back(step);
+        step += x;
+    }   
+    return vec;
 }
