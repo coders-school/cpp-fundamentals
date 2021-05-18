134f403cf4515f9a827ff9fbb7bdd336c347a3c0
jakjan95 <jakubjanusz95@gmail.com>
2021-05-18 17:39:34 +0200

    Substract works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,11 +3,15 @@
 
 namespace {
     const std::string addCommand = "add";
+    const std::string subtractCommand = "subtract";
+
 }
 
 std::string calculate(const std::string &command, int first, int second) {
   if (command == addCommand) {
     return std::to_string(first + second);
+  } else if (command == subtractCommand) {
+    return std::to_string(first - second);
   }
   return "Invalid data";
 }
