9784971f1208184003db3741fc7ceb8ba449c995
jakjan95 <jakubjanusz95@gmail.com>
2021-05-18 17:41:41 +0200

    Multiply works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,16 +2,18 @@
 #include <string>
 
 namespace {
-    const std::string addCommand = "add";
-    const std::string subtractCommand = "subtract";
-
-}
+const std::string addCommand = "add";
+const std::string subtractCommand = "subtract";
+const std::string multiplyCommand = "multiply";
+} // namespace
 
 std::string calculate(const std::string &command, int first, int second) {
   if (command == addCommand) {
     return std::to_string(first + second);
   } else if (command == subtractCommand) {
     return std::to_string(first - second);
+  } else if (command == multiplyCommand) {
+    return std::to_string(first * second);
   }
   return "Invalid data";
 }
