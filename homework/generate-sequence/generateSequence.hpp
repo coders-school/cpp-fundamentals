5214f6f344bd465e7dcc9d571b8ce2dcb643233f
Kacu <k.ptychu@gmail.com>
2021-05-18 23:37:28 +0200

    completed generateSequence function

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,7 +1,12 @@
 #pragma once
+#include <algorithm>
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    if (count < 1) {
+        return std::vector<int>();
+    }
+    std::vector<int> result(count, step);
+    std::generate(result.begin(), result.end(), [i = 0, step]() mutable { return step * (++i); } );
+    return result;   
 }
