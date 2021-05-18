f82bf23e8ab6588f920264bdcff13afa44ef8cac
pako2200 <pawelkowlw@gmail.com>
2021-05-18 19:33:11 +0200

    nwd&nww

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,32 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs)
+{
+    int rest;
+    if (rhs == 0)
+        return lhs;
+    else if (lhs == 0)
+        return rhs;
+    else
+    {
+        do
+        {
+            rest = lhs % rhs;
+            lhs = rhs;
+            rhs = rest;
+        } while (rest != 0);
+        {
+            return abs(lhs);
+        }
+    }
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int lhs, int rhs)
+{
+    if (rhs == 0)
+        return rhs;
+    else if (lhs == 0)
+        return lhs;
+    else
+        return abs((lhs * rhs) / NWD(lhs, rhs));
 }
