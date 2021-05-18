7330d976c4fa220a13158f8fd0354b18ab92e1e8
Paweł Żydek <pzp1995@gmail.com>
2021-05-18 20:33:07 +0200

    subtract added

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,5 +5,7 @@ std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
     if (command == "add") {
         return std::to_string(first + second); 
-    } 
+    } else if (command == "subtract") {
+        return std::to_string(first - second);
+    }
 }
