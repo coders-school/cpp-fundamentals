9fcbbb3321fb03e4de651d064dd361d518c3878f
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-05-18 17:46:34 +0200

    maxOfVector function added - D4rth

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,13 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    
+    int new_max = vec[0];
+
+    for (int i = 0; i < vec.size(); ++i) {
+        if(new_max < vec[i]) {
+            new_max = vec[i];                     
+        }
+    }  
+    return new_max;
 }
