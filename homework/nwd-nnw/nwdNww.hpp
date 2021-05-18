0912d4fefbab7a233f0663b9d66fead97ad01167
rajder007 <krzysztof.zaczyk7@gmail.com>
2021-05-18 23:08:01 +0200

    adding nww

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -20,12 +20,14 @@ int NWD(int lhs, int rhs) {
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    int pom;
-  	while(rhs!=0)
-	{
-    	pom = rhs;
-    	rhs = lhs%rhs;
-    	lhs = pom;  
-  	}
-    return lhs;
+    if(lhs == 0 || rhs == 0) {
+        return 0;
+    } else if(lhs < 0 || rhs < 0) {
+        return std::abs(lhs);
+    } else if (lhs < rhs)
+            return NWW(rhs, lhs);
+        int k = lhs;
+        while (k % rhs != 0)
+            k += lhs;
+        return k;
 }
