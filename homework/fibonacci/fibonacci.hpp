f49667296b8447d58e9ee621facce2571ddfa2cb
Mateusz Serafin <mateusz97.serafin@gmail.com>
2021-05-18 19:55:14 +0200

    fibonacci_iterative works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,22 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+
+    if(sequence == 0)
+        return 0;
+
+    int x = {1};
+    int y = {1};
+    for(int i = 2; i < sequence; ++i){
+        if(y > x){
+            x += y;
+        }
+        else{
+            y += x;
+        }
+    }
+
+    return x > y ? x : y;
 }
 
 int fibonacci_recursive(int sequence) {
