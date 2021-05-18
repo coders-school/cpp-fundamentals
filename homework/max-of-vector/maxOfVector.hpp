03eb457c7fdc1e346e6ce57ef56f9c0626c4ce62
Łukasz Nitka <lukasnit@wp.pl>
2021-05-18 23:14:07 +0200

    max vec v.1

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,15 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <algorithm>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(const std::vector<int>& vec) 
+{
+  std::vector<int>copy;
+  copy = vec; 
+    
+  sort(copy.begin(), copy.end(), std::greater<int>());
+  
+  return copy.front();  
+    
 }
