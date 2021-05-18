df7b1050d622fc23cee3b274276cc369c2df1b5c
Zyragon <Zyragon@gmail.com>
2021-05-18 12:38:56 +0200

    adding worksmake

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int prevprev, prev = 0, curr = 1;
+    for (int i = 0; i < sequence - 1; i++) {
+        prevprev = prev;
+        prev = curr;
+        curr = prevprev + prev;
+    }
+    return curr;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0 || sequence == 1) {
+        return sequence;
+    }
+    else
+    //fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence + 2)
+    return (fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2));
 }
