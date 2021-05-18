f97183d77a41f76ab50e1e396c29e078216e0b7b
Arek Ogiewa <arkadiusz.ogiewa@gmail.com>
2021-05-18 13:12:04 +0200

    vectorSequence added

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -3,5 +3,14 @@
 
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
-    return {};
+
+    	std::vector<int> vect {};
+	int sum {};
+	if(count >= 0)
+		for(size_t i=0; i<count; i++){
+			sum += step;
+			vect.push_back(sum);
+		}
+
+	return vect;
 }
