c6c2f9c28b6478a499bfe1a31b77541a81f4198f
Łukasz Nitka <lukasnit@wp.pl>
2021-05-19 10:47:09 +0200

    gen-seq v.1

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,7 +1,24 @@
 #pragma once
-#include <vector>
+#include<vector>
+#include<algorithm>
+#include<iostream>
 
-std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+std::vector<int> generateSequence(int count, int step) 
+{
+   if(count<0)
+   {
+   count = 0;
+   }
+   
+   std::vector<int>v2(count);
+   int s=step;
+   int n=0;
+   
+   std::generate(v2.begin(),v2.end(),[&n,s]()
+   {
+   return n+= s;
+   }
+   );
+   
+   return v2;   
 }
