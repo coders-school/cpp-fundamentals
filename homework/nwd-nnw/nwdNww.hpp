e55cc276b3a382085aca595910f72c2aef3ac0a8
Paweł Żydek <pzp1995@gmail.com>
2021-05-19 17:27:40 +0200

    NWD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,7 +2,19 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    if (lhs <= 0 || rhs <= 0) {
+        std::cout << "the numbers must be positive";
+        return 1;
+    } else {
+        int second;
+
+        while (rhs != 0) {
+            second = rhs;
+            rhs = lhs % rhs;
+            lhs = second;
+        }
+        return lhs;
+    }
 }
 
 int NWW(int lhs, int rhs) {
