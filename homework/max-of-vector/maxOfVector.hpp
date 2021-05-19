86dd2a9fe36751ee7c04ac0ab30574ceea98606a
kondorski <thegreatvaultofknowledge@gmail.com>
2021-05-19 17:18:20 +0200

    maxOfVector all cases

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -4,18 +4,11 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    if(vec.size() ==  1){
-        return vec[0];
-    }
-    else{
-        int maxValue = 0;
+        int maxValue = std::numeric_limits<int>::min();
         for(auto element : vec){
-            if(element) > maxValue){
+            if(element > maxValue){
                 maxValue = element;
             }
         }
         return maxValue;
-    }
-
-    return {};
 }
