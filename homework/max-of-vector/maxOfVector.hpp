43682e8a35a60140668b6a33aa495b0156066dbc
kondorski <thegreatvaultofknowledge@gmail.com>
2021-05-19 17:08:52 +0200

    MaxOfVector first implementation

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,21 @@
 #pragma once
 #include <vector>
-#include <limits>
+#include <limits> 
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
+    if(vec.size() ==  1){
+        return vec[0];
+    }
+    else{
+        int maxValue = 0;
+        for(auto element : vec){
+            if(element) > maxValue){
+                maxValue = element;
+            }
+        }
+        return maxValue;
+    }
+
     return {};
 }
