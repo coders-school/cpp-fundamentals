7e138e477579223269a33a537323aa30c1eca01c
Krzysztof C <krzysztof.ciecko.kontakt@gmail.com>
2021-05-19 23:02:40 +0200

    all good

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,32 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+
+    if(sequence == 0)
+        return 0;
+    else if(sequence == 1)
+        return 1;
+    
+    int previous = 0;
+    int next = 1;
+    for(int i = 0; i<sequence-1; i++)
+    {
+        int temp = next;
+        next+=previous;
+        previous=temp;
+    }
+    return next;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    
+    if(sequence == 0)
+        return 0;
+    else if(sequence == 1)
+        return 1;
+    else 
+        return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
+    
 }
+
