51245829cd3f2764ef1bacb72409a6928776998e
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-05-19 21:51:14 +0200

    Adding function with max value

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -4,5 +4,12 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    int temp {std::numeric_limits<int> :: min()};
+    int elem {};
+
+    for (const auto& elem : vec) {
+        if (elem > temp) {
+            temp = elem;
+        }
+    } return temp;
 }
