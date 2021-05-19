90e8c9459fd0dda553fc7af272c2ad1ca5944791
DawidSwistak <dawidswistak98@interia.pl>
2021-05-19 19:27:57 +0200

    fibonacci itterative - works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,21 @@
 #pragma once
-
+#include <iostream>
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int fib1 = 0;
+    int fib2 = 1;
+
+    if(!sequence)
+    {
+        return 0;
+    }
+
+    for(int i = 2; i <= sequence; i++)
+    {
+        auto temp = fib2;
+        fib2 = fib1 + fib2;
+        fib1 = temp;               
+    }
+    return fib2;
 }
 
 int fibonacci_recursive(int sequence) {
