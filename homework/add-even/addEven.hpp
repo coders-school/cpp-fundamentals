3483146b6adab1b6945fb644d6de7b6f2ea7f887
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-05-19 18:05:42 +0200

    Formatted Document

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,25 +2,23 @@
 #include <vector>
 #include <numeric>
 
-
-int addEven(const std::vector<int>& numbers) 
+int addEven(const std::vector<int> &numbers)
 {
     std::vector<int> even_numbers{};
 
     int sum;
     bool even;
 
-    for(auto& el : numbers) 
+    for (auto &el : numbers)
     {
         even = (el % 2); //Checking if number is even or not.
 
-        if(even == 0)
-        {  
+        if (even == false)
+        {
             even_numbers.push_back(el);
         }
     }
 
     sum = std::accumulate(even_numbers.begin(), even_numbers.end(), 0);
     return sum;
-
 }
