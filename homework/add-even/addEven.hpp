8aa51ee3e7875e0c7befbbafb21c582cbf32c028
opiotrowsky <piotrowsky.o@gmail.com>
2021-05-19 22:09:47 +0200

    Working function of adding even numbers

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,15 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    std::vector<int> evens;
+    int suma = 0;
+    for(const size_t& el : numbers) {
+        if(el % 2 == 0) {
+            evens.push_back(el);
+        }
+    }
+    for(const size_t& el : evens) {
+        suma += el;
+    }
+    return suma;
 }
