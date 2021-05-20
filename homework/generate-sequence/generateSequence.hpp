afca5cd3b8a50b4678ff93bcc47da19cee612468
kondorski <thegreatvaultofknowledge@gmail.com>
2021-05-20 15:43:35 +0200

    generateSequence using do-while

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,12 +2,17 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    std::vector<int> numbers;
+    std::vector<int> numbers{};
     int temp {0};
-
-    for(int i = 0; i < count; i++){
-        temp += step;
-        numbers.push_back(temp);
+    if(count <= 0){
+        return numbers;
     }
-    return numbers;
+    else{
+        do{
+            temp += step;
+            numbers.push_back(temp);
+            count--;
+        }while(count > 0);
+        return numbers;
+    }
 }
