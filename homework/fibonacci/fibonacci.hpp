5a0a3e7e946a87859107a6c01e791f8d0072552d
LechWilk <lech.wilk@gmail.com>
2021-05-20 22:44:09 +0200

    recurcive added with no more cin

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,26 +1,28 @@
 #pragma once
-#include <iostream>
 
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-std::cin>>sequence;
-int n=1;
-int n1=0;
-int temp=0;
 
-if (sequence<=1) return sequence;
-else{
+    int n=1;
+    int n1=0;
+    int temp=0;
+
+    if (sequence<=1) return sequence;
+
     for (int i=2;i<=sequence;i++)    {
         temp=n+n1;
         n1=n;
         n=temp;
     }
-}
-    return n;
+
+        return n;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+
+if (sequence<=1) return sequence;
+return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
+
 }
