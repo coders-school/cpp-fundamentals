83bc08e22994ebd6a405ce82972c03856e434840
LechWilk <lech.wilk@gmail.com>
2021-05-20 22:24:32 +0200

    iterative done with cin

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,23 @@
 #pragma once
+#include <iostream>
+
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+std::cin>>sequence;
+int n=1;
+int n1=0;
+int temp=0;
+
+if (sequence<=1) return sequence;
+else{
+    for (int i=2;i<=sequence;i++)    {
+        temp=n+n1;
+        n1=n;
+        n=temp;
+    }
+}
+    return n;
 }
 
 int fibonacci_recursive(int sequence) {
