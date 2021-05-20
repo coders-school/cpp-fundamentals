4e0fbae05b854e8d88cfabc470ac9f15e891939e
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-05-20 17:29:57 +0200

    Max of vector test passed

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -2,7 +2,16 @@
 #include <vector>
 #include <limits>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(const std::vector<int> &vec)
+{
+    int max_element = vec[0];
+    std::vector<int> vec_max;
+    for (auto &el : vec)
+    {
+        if (el > max_element)
+        {
+            max_element = el;
+        }
+    }
+    return max_element;
 }
