36de85f55bb9c1a5f6c320e66295ffb1e166a649
Michał Zdziech <m.zdziech82@gmail.com>
2021-05-20 21:38:10 +0200

    adding an exception for division by 0

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,20 +3,30 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    double result = {};
-    
-    if(command.compare("add")) {
+    int result = {};
+   // "Invalid data"
+    if(command == "add") {
         result = first + second;
     }
-    if(command.compare("subtract")) {
+    else if(command == "subtract") {
         result = first - second;
     }
-    if(command.compare("multiply")) {
+    else if(command == "multiply") {
         result = first * second;
     }
-    if(command.compare("divide")) {
-        result = first / second;
+    else if(command == "divide") {
+        
+        if(second != 0) {
+            result = first / second;
+        }
+        else {
+            return "Division by 0";
+        }
+        
+    }
+    else {
+        return "Invalid data";
     }
 
-    return std::to_string(result) ;
+    return std::to_string( result );
 }
