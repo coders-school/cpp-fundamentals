103de980fd8e99e5f791bf3d05b6df0bb1370bb2
Łukasz Sieradzki <kirsten@poczta.fm>
2021-05-20 21:28:00 +0200

    NWD & NWW are ok

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,24 @@
+#include <cstdlib>
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    int tempVal;
+        
+    while(rhs!=0)
+    {
+        tempVal = rhs;
+        rhs = lhs%rhs;
+        lhs = tempVal;    
+    }
+    if(lhs<0)
+        return lhs*(-1);
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 or rhs == 0) {
+        return 0;
+    }
+    return abs(lhs*rhs/NWD(lhs, rhs));
 }
