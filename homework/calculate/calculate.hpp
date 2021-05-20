c3f745d6f67207b7f3a5df801a5673bf021bad7f
Michał Zdziech <m.zdziech82@gmail.com>
2021-05-20 21:05:02 +0200

    additional functionality add

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,11 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    double result = {};
+    
+    if(command.compare("add")) {
+        result = first + second;
+    }
+
+    return std::to_string(result) ;
 }
