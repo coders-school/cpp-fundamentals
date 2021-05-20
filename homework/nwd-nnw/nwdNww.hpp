bb2b0ebe4c3cdce73679f10ac4eb30f969f5885b
Olaf Kucharski <olaf.kucharski@op.pl>
2021-05-20 12:58:41 -0400

    adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 && rhs == 0) return 0;
+    if(lhs < 0) lhs *= -1;
+    if(rhs < 0) rhs *= -1;
+    int divider = (lhs > rhs) ? lhs : rhs;
+    while(lhs % divider || rhs % divider){
+        divider--;
+    }
+    return divider;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(!lhs || !rhs) return 0;
+    int x = (lhs < rhs) ? lhs : rhs;
+    int y = (lhs < rhs) ? rhs : lhs;
+    int multiple = x;
+    while(multiple % y){
+        multiple += x;
+    }
+    if(multiple < 0){
+        multiple *= -1;
+    }
+    return multiple;
 }
