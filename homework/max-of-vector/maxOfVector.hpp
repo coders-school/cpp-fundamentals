ef9c8b3b9dd09b30e5ba696d8eb97dfa75dbc780
opiotrowsky <piotrowsky.o@gmail.com>
2021-05-22 22:13:41 +0200

    Working pointing to the largest element

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,14 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <algorithm>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int number = std::numeric_limits<int>::min();
+    for(auto el : vec) {
+        if(number < el) {
+            number = el;
+        }
+    }
+    return number;
 }
