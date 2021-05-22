ad9e20ec7858237c1e443a4040723cd292571267
Michał Zdziech <m.zdziech82@gmail.com>
2021-05-22 14:36:41 +0200

    creating an array for fibonacci numbers

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_iterative(int sequence ) {
+    
+    if(sequence == 0) return -1;
+
+    // n = n-1 + n -2;
+    int tab[sequence]={};
+
+
+    return sum_fib;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
+
+
+
+
     return 0;
 }
