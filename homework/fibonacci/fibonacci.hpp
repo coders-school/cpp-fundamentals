80bb98c9d64f843abfee2231499aa50097164b84
Michał Zdziech <m.zdziech82@gmail.com>
2021-05-22 14:42:35 +0200

    return last fib number

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,18 +2,29 @@
 
 int fibonacci_iterative(int sequence ) {
     
-    if(sequence == 0) return -1;
+    if(sequence == 0) return -1; 
 
     // n = n-1 + n -2;
     int tab[sequence]={};
 
+    tab[0] = 1;
+    tab[1] = 1;
+    int sum_fib = {};
+    size_t i = 2;
+
+    for(; i < sequence; ++i)
+    {
+        tab[i] = tab[i-1] + tab[i-2];
+    }
+
+    sum_fib = tab[i-1];
 
     return sum_fib;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-
+    
 
 
 
