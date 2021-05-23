64853abeeaf44cc231dbc06f1f8133e059b12947
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-05-23 18:13:07 +0200

    Homework

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,7 +1,24 @@
 #pragma once
 #include <vector>
 
-std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+std::vector<int> generateSequence(int count, int step)
+{
+    std::vector<int> afterGeneratedSequence;
+
+    int temp = step;
+
+    for (int i = 1; i <= count; ++i)
+    {
+        afterGeneratedSequence.push_back(step);
+        step += temp;
+        //std::cout << countedElements.size() << " | ";
+        //std::cout << countedElements.capacity() << "\n";
+    }
+    afterGeneratedSequence.shrink_to_fit();
+
+    //std::cout << countedElements.size() << " | ";
+    //std::cout << countedElements.capacity() << "\n";
+
+    return afterGeneratedSequence;
 }
+
