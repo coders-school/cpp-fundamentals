85f75cfc633f191547ad10027ce7cba5991c84c2
Michał Zdziech <m.zdziech82@gmail.com>
2021-05-23 14:00:09 +0200

    code review and repair fibonacci iterative function.

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,24 +2,27 @@
 
 int fibonacci_iterative(int sequence ) {
     
-    if(sequence == 0) return 0; 
-
-    // n = n-1 + n -2;
-    int tab[sequence]={};
-
-    tab[0] = 1;
-    tab[1] = 1;
-    int sum_fib = {};
-    size_t i = 2;
-
-    for(; i < sequence; ++i)
-    {
-        tab[i] = tab[i-1] + tab[i-2];
+    int last_fib = {};
+    int tab[sequence] = {}; 
+    int i = 2;
+    
+    if(sequence == 0) {
+        return 0;
     }
-
-    sum_fib = tab[i];
-    delete [] tab;
-    return sum_fib;
+    else if( sequence == 1 || sequence == 2 ) {
+        return 1;
+    }
+    else
+    {
+        tab[0] = 1;
+        tab[1] = 1;
+        for (; i < sequence; i++)
+        {
+            tab[i] = tab[i-1] + tab[i-2];
+        }
+        last_fib = tab[i-1];
+        return last_fib;
+    }
 }
 
 int fibonacci_recursive(int sequence) {
@@ -27,8 +30,6 @@ int fibonacci_recursive(int sequence) {
     if(sequence == 0) return 0;
     if( sequence == 1 || sequence == 2 ) return 1;
     else {
-        return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
+        return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2) ;
     }
-
-    return 1;
 }
