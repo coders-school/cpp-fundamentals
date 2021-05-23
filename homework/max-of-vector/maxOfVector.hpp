22ea1145a72a0402cca1b0cbb7868af1940f46bb
Marcin Mazurek <marcinmazurek06@gmail.com>
2021-05-23 14:09:26 +0200

    maxOfVector() function added

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -2,7 +2,11 @@
 #include <vector>
 #include <limits>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(const std::vector<int>& vec)
+{
+    auto max{std::numeric_limits<int>::lowest()};
+    for(const auto& var : vec)
+        if(var > max)
+            max = var;
+    return max;
 }
