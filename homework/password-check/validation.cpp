2821247760f6d008199b0c700013d188c5f181e2
BarTes8 <barbara.tessema@gmail.com>
2021-05-24 19:11:51 +0200

    changed last case to default -> warnings gone

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -2,30 +2,23 @@
 
 std::string getErrorMessage(ErrorCode error) {
     switch(error) {
-    case ErrorCode::Ok : {
+    case ErrorCode::Ok : 
         return "Ok";
         break;
-    }
-    case ErrorCode::PasswordNeedsAtLeastNineCharacters : {
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters : 
         return "Password needs to have at least nine characters";
         break;
-    }
-    case ErrorCode::PasswordNeedsAtLeastOneNumber : {
+    case ErrorCode::PasswordNeedsAtLeastOneNumber : 
         return "Password needs to have at least one number";
         break;
-    }
-    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter : {
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter : 
         return "Password needs to have at least one special character";
         break;
-    }
-    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter : {
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter : 
         return "Password needs to have at least one uppercase letter";
         break;
-    }
-    case ErrorCode::PasswordsDoNotMatch : {
+    default:
         return "Passwords do not match";
-        break;
-    }
     }
 }
 
