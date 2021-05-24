e9f59384f352ad33ba1021fdde0555a8f356f24d
Adam Wroblewski <a.wroblewski77@gmail.com>
2021-05-24 23:31:09 +0200

    implemented fibonacci recursive

---
+++ homework/fibonacci/build/CMakeFiles/fibonacci-ut.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fibonacci-ut.dir/test.cpp.o"
+  "fibonacci-ut"
+  "fibonacci-ut.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fibonacci-ut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
