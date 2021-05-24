50c7dda8f5042854f6fd02aa085d0b091a96ddb2
jakjan95 <jakubjanusz95@gmail.com>
2021-05-24 17:01:00 +0200

    maxOfVector works

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,19 @@
 #pragma once
-#include <vector>
 #include <limits>
+#include <vector>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(const std::vector<int>& vec)
+{
+    if (vec.empty()) {
+        return {};
+    }
+
+    int max_element = std::numeric_limits<int>::min();
+    for (const auto& el : vec) {
+        if (el > max_element) {
+            max_element = el;
+        }
+    }
+
+    return max_element;
 }
