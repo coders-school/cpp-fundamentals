88a1f247eb6a2b85f08cd09eb17f4165b098370e
Adam Wroblewski <a.wroblewski77@gmail.com>
2021-05-24 22:53:59 +0200

    Function calculate implemented

---
+++ 04-stl-basics/tasks/01-printVector.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+// Implement printVector to print the content of a given vector on screen.
+// Each string in a new line.
+
+void printVector(std::vector<std::string> vec)
+{
+    for (const auto &s : vec)
+    {
+        std::cout << s;
+    }
+}
+
+int main()
+{
+    std::vector<std::string> vec{
+        "Hello Coders School!",
+        "Welcome to the best C++ course ever",
+        "Man, this is crazy :)"};
+    printVector(vec);
+    return 0;
+}
