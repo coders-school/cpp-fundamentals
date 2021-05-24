25f3017b7887a628dda6f81c639ecf27ebd52e97
Adam Wroblewski <a.wroblewski77@gmail.com>
2021-05-25 00:08:43 +0200

    implemented NWW and NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,40 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs)
+{
+    if (lhs < 0)
+        lhs *= -1;
+    else if (lhs == 0)
+        return rhs;
+    if (rhs < 0)
+        rhs *= -1;
+    else if (rhs == 0)
+        return lhs;
+
+    while (rhs != lhs)
+    {
+        if (rhs > lhs)
+        {
+            rhs = rhs - lhs;
+        }
+        else
+        {
+            lhs = lhs - rhs;
+        }
+    }
+
+    return lhs;
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int lhs, int rhs)
+{
+    if (lhs < 0)
+        lhs *= -1;
+    else if (lhs == 0)
+        return 0;
+    if (rhs < 0)
+        rhs *= -1;
+    else if (rhs == 0)
+        return 0;
+    return (lhs * rhs / (NWD(lhs, rhs)));
 }
