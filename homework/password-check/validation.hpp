9e27acdc0e1d30c4526d0c7a197daa10437f69f5
Artur Michałowski <artur.michalowski93@gmail.com>
2021-05-24 18:23:43 +0200

    add declaration for checkPassword

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -16,3 +16,4 @@ enum class ErrorCode {
 std::string getErrorMessage(const ErrorCode&);
 bool doPasswordsMatch(const std::string&, const std::string&);
 ErrorCode checkPasswordRules(const std::string&);
+ErrorCode checkPassword(const std::string&, const std::string&);
