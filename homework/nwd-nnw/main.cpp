6b3907dad5d01dcd9916f5a32085017e0b2d552f
LechWilk <lech.wilk@gmail.com>
2021-05-24 09:00:00 +0200

    NWD failed; problem with removing elements from list.

--- homework/nwd-nnw/main.cpp
+++ homework/nwd-nnw/main.cpp
@@ -2,8 +2,14 @@
 #include "nwdNww.hpp"
 
 int main() {
-  std::cout << "NWW(255, 15) = " << NWW(255, 15) << "\n";
-  std::cout << "NWD(255, 15) = " << NWD(255, 15) << "\n";
+  int l,r;
+  std::cin>>l;
+  std::cin>>r;
+  
+
+
+  std::cout << "NWW("<<l<<","<<r<<") = " << NWW(l, r) << "\n";
+  std::cout << "NWD("<<l<<","<<r<<") = " << NWD(l, r) << "\n";
 
   return 0;
 }
--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -7,23 +7,36 @@ void print(int,int);
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
+    print(lhs,rhs);
+    int CommonDiv =1;
+    for(auto el_lhs:intoPrimes(lhs)) {
+        for(auto el_rhs:intoPrimes(rhs)) {
+            if (el_lhs<el_rhs) break;
+            if (el_lhs==el_rhs!=0) {
+                CommonDiv*=el_lhs;
+                el_lhs=0;
+                el_rhs=0;
+            }
+        }
+    }
  
-    return -1;
+    return CommonDiv;
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    
+    return lhs*rhs/NWD(lhs,rhs);
 }
 
 
 
 
 //*********************************************************
-void print(int lhs,int rhs) {
-    for (auto el:intoPrimes(lhs)) std::cout<<el<<" ";
+void print(int l,int r) {
+    for (auto el:intoPrimes(l)) std::cout<<el<<" ";
     std::cout<<"\n";
-    for (auto el:intoPrimes(rhs)) std::cout<<el<<" ";
+    for (auto el:intoPrimes(r)) std::cout<<el<<" ";
     std::cout<<"\n";   
 }
 
