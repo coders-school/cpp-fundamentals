1861a83c2f101535e7df81ca4388485042458bbe
Runer <runer53248@gmail.com>
2021-05-25 01:27:42 +0200

    Implemented validation basic functionality

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,33 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::map<ErrorCode, std::string> errorsNames = {
+    {ErrorCode::Ok, "Ok"},
+    {ErrorCode::PasswordNeedsAtLeastNineCharacters, "Password needs to have at least nine characters"},
+    {ErrorCode::PasswordNeedsAtLeastOneNumber, "Password needs to have at least one number"},
+    {ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter, "Password needs to have at least one special character"},
+    {ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter, "Password needs to have at least one uppercase letter"},
+    {ErrorCode::PasswordsDoNotMatch, "Passwords do not match"}
+};
+
+std::string getErrorMessage(ErrorCode error){
+    return errorsNames[error];
+}
+
+bool doPasswordsMatch(std::string first, std::string second){
+    if(first.compare(second)){
+        return false;
+    }
+    return true;
+}
+
+ErrorCode checkPasswordRules(std::string password){
+    unsigned char lottery = std::rand() % 5;
+    return static_cast<ErrorCode>(lottery);
+}
+
+ErrorCode checkPassword(std::string first, std::string second){
+    if(!doPasswordsMatch(first, second)){
+        return ErrorCode::PasswordsDoNotMatch;
+    }
+    return checkPasswordRules(first);
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,21 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#pragma once
+
+#include <map>
+#include <string>
+
+enum class ErrorCode : unsigned char {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+
+std::string getErrorMessage(ErrorCode error);
+
+bool doPasswordsMatch(std::string first, std::string second);
+
+ErrorCode checkPasswordRules(std::string password);
+
+ErrorCode checkPassword(std::string first, std::string second);
