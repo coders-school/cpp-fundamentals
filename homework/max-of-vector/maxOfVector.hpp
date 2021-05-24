2bcf02b707584d277fcf02d9e02d43986f8ed354
Marek Burzacki <lawier@wp.pl>
2021-05-24 22:09:29 +0200

    Added Works maxOfVector

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <bits/stdc++.h>
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    int result = 0;
+    result = *max_element(vec.begin(), vec.end());
+    return result;
 }
