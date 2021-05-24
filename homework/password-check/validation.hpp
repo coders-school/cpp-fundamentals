3cb178d14633485699bebaa9576b92636efa55fd
BarTes8 <barbara.tessema@gmail.com>
2021-05-24 16:17:17 +0200

    added getErrorMessage declaration

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 
 enum class ErrorCode {
     Ok,
@@ -8,3 +9,5 @@ enum class ErrorCode {
     PasswordNeedsAtLeastOneUppercaseLetter,
     PasswordsDoNotMatch
 }
+
+std::string getErrorMessage(ErrorCode);
