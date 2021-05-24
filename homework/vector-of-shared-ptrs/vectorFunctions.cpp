0d41e8f90e086c2b3b3689cd3e0876d074d300ec
Piotr <piotrku91@github.com>
2021-05-24 18:00:31 +0200

    created new files

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,10 @@
+#include "vectorFunctions.hpp"
+#include <vector>
+#include <memory>
+
+std::vector<std::shared_ptr<int>> generate(int count)
+{
+
+return 
+
+};
---
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+std::vector<std::shared_ptr<int>> generate(int count);
