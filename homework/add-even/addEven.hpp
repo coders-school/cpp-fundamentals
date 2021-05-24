6fd3d32682584f4f81e13b3dbc8c7a8357f42cf4
Marek Burzacki <lawier@wp.pl>
2021-05-24 21:25:18 +0200

    Added works addEven

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -4,5 +4,11 @@
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
-    return -1;
+    int result=0;
+    for (auto elm : numbers){
+        if (elm % 2 == 0){
+            result = result + elm;
+        }
+    }
+    return result;
 }
