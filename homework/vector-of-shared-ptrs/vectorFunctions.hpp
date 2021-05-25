7b931a4138327de89f8af159d00ae53dd8b376c8
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-05-25 21:23:52 +0200

    Add 'using' directive and use it within a header

--- homework/vector-of-shared-ptrs/vectorFunctions.hpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -3,8 +3,10 @@
 #include <memory>
 #include <vector>
 
+using shared_pointers = std::vector<std::shared_ptr<int>>;
+
 std::vector<std::shared_ptr<int>> generate(int count);
-void print(const std::vector<std::shared_ptr<int>>& pointers);
-void add10(const std::vector<std::shared_ptr<int>>& pointers);
+void print(const shared_pointers& pointers);
+void add10(const shared_pointers& pointers);
 void sub10(int* const pointer);
-void sub10(const std::vector<std::shared_ptr<int>>& pointers);
+void sub10(const shared_pointers& pointers);
