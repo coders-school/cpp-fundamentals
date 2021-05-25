64e8e05e263bbe33c20b9554a43d9640bd062291
PawelZydek <83175937+PawelZydek@users.noreply.github.com>
2021-05-25 23:44:51 +0200

    Update maxOfVector.hpp
    added max of vector

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -2,7 +2,15 @@
 #include <vector>
 #include <limits>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(std::vector<int> vec) {
+    
+    int size = vec.size(), first = 0;
+    for (int i = 0; i < size; i++) {
+        if (i == 0) {
+            first = vec.at( 0 );
+        } else if (first < vec.at( i )) {
+            first = vec.at( i );
+        }
+    }
+    return first;
 }
