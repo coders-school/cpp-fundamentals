3aafaed421891ddba1bbcb13563c2b9d8a841429
Marek Burzacki <lawier@wp.pl>
2021-05-25 22:47:26 +0200

    Added works generateSequence

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -3,5 +3,11 @@
 
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
-    return {};
+    std::vector<int>vec_seqe;
+    int pom=step;
+    for (int i = 1; i <= count; i++){
+    vec_seqe.push_back(pom);
+    pom += step;
+    }
+    return vec_seqe;
 }
