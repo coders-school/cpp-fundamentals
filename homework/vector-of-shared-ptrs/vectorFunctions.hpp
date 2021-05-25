b64587a6ad0be47a4a401fb374a7372b96d44660
BarTes8 <barbara.tessema@gmail.com>
2021-05-25 17:54:39 +0200

    added sub10 function overload declaration

--- homework/vector-of-shared-ptrs/vectorFunctions.hpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -8,3 +8,4 @@ std::vector<std::shared_ptr<int>> generate(int);
 void print(const std::vector<std::shared_ptr<int>>&);
 void add10(const std::vector<std::shared_ptr<int>>&);
 void sub10(int* const);
+void sub10(const std::vector<std::shared_ptr<int>>&);
