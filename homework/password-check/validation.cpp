bfd8f487b00c962d0a922d28b275098ab31fcbb5
Runer <runer53248@gmail.com>
2021-05-25 02:36:47 +0200

    Add rules in checkPasswordRules

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -6,27 +6,44 @@ std::map<ErrorCode, std::string> errorsNames = {
     {ErrorCode::PasswordNeedsAtLeastOneNumber, "Password needs to have at least one number"},
     {ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter, "Password needs to have at least one special character"},
     {ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter, "Password needs to have at least one uppercase letter"},
-    {ErrorCode::PasswordsDoNotMatch, "Passwords do not match"}
-};
+    {ErrorCode::PasswordsDoNotMatch, "Passwords do not match"}};
 
-std::string getErrorMessage(ErrorCode error){
+std::string getErrorMessage(ErrorCode error) {
     return errorsNames[error];
 }
 
-bool doPasswordsMatch(const std::string& first, const std::string& second){
-    if(first.compare(second)){
+bool doPasswordsMatch(const std::string& first, const std::string& second) {
+    if (first.compare(second)) {
         return false;
     }
     return true;
 }
 
-ErrorCode checkPasswordRules(const std::string& password){
-    unsigned char lottery = std::rand() % 5;
-    return static_cast<ErrorCode>(lottery);
+bool isAnyLetter(const std::string& password, int (&func)(int)) {
+    bool result = std::any_of(begin(password), end(password), [&func](int letter) {
+        return func(letter);
+    });
+    return result;
 }
 
-ErrorCode checkPassword(const std::string& first, const std::string& second){
-    if(!doPasswordsMatch(first, second)){
+ErrorCode checkPasswordRules(const std::string& password) {
+    if (password.length() < 9) {
+        return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+    }
+    if (!isAnyLetter(password, isupper)) {
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    }
+    if (!isAnyLetter(password, isdigit)) {
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    }
+    if (!isAnyLetter(password, ispunct)) {
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    }
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(const std::string& first, const std::string& second) {
+    if (!doPasswordsMatch(first, second)) {
         return ErrorCode::PasswordsDoNotMatch;
     }
     return checkPasswordRules(first);
