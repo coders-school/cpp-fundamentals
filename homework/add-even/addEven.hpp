79d3253855ec4a22170edb7cc81450537b7021cc
Michał Zdziech <m.zdziech82@gmail.com>
2021-05-25 20:39:56 +0200

    implemented summing even number function

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,13 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int sum_vec = {};
+    for(const auto& number: numbers)
+    {
+        if(number % 2 == 0) {
+            sum_vec += number;
+        }
+    }
+
+   return sum_vec;
 }
