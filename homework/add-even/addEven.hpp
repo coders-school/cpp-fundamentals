22820b3cbe7cff8ea76fa903808bfa38cf7fcbd5
PawelZydek <83175937+PawelZydek@users.noreply.github.com>
2021-05-25 21:58:23 +0200

    Update addEven.hpp
    adding sum of even numbers

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <iostream>
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
@@ -8,20 +7,12 @@ int addEven(const std::vector<int>& numbers) {
     for (const auto& el : numbers) {
         if (el == 0) {
             sum += el;
-        }
-        else if (el % 2 != 0) {
+        } else if (el % 2 != 0) {
             
-        }
-        else {
+        } else {
             sum += el;
         }
     }
     std::cout << "result = " << sum;
-    return 0;  
-}
-
-int main()
-{
-    std::vector<int> vec{ 21, 20, 18, 16, 19 };
-    auto result = addEven(vec);  // result = 6;
+    return sum;  
 }
