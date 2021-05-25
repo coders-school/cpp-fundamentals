2d68bbba560c773db94abbe5fc66f62160bf11bf
Barbara Tessema <74462021+BarTes8@users.noreply.github.com>
2021-05-25 18:17:43 +0200

    Update vectorFunctions.hpp

--- homework/vector-of-shared-ptrs/vectorFunctions.hpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include <algorithm>
+#include <iostream>
 #include <memory>
 #include <vector>
-#include <iostream>
 
 std::vector<std::shared_ptr<int>> generate(int);
 void print(const std::vector<std::shared_ptr<int>>&);
