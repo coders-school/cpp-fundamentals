33f42a75f1bfbd217f678da17f27ffcebdbd7f9a
Jakub Stępień <macista.bukaj@gmail.com>
2021-05-26 18:05:23 +0200

    create validation.hpp, create getErrorMessage and doPasswordsMatch

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,43 @@
+#include <cctype>
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode code)
+{
+    switch(code)
+    {
+    case ErrorCode::Ok:
+        return "Ok";
+        
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        return "PasswordNeedsAtLeastNineCharacters";
+        
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        return "PasswordNeedsAtLeastOneNumber";
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+        return "PasswordNeedsAtLeastOneSpecialCharacter";
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        return "PasswordNeedsAtLeastOneUppercaseLetter";
+    case ErrorCode::PasswordsDoNotMatch:
+        return "PasswordsDoNotMatch";
+    }
+}
+
+bool doPasswordsMatch(std::string Passwords1, std::string Passwords2)
+{
+    return Passwords1==Passwords2;
+}
+
+ErrorCode doPasswordsMatch(std::string Passwords)
+{
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPasswordRules(std::string Passwords)
+{
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(std::string Passwords1, std::string Passwords2)
+{
+    return ErrorCode::Ok;
+}
