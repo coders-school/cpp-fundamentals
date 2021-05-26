0ebc29b6a428f3fa7b52f17a4840d10ad90e707d
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-05-26 14:32:10 +0200

    initial function nwd

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,24 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    
+    auto result = 1;
+    auto range = 0;
+
+    if (lhs > rhs){
+        range = rhs;
+    }else{
+        range = lhs;
+    }
+
+    for (auto i=1; i <= range; ++i){
+        if(lhs%i == 0 and rhs%i == 0){
+            result = i;
+        }
+    }
+
+
+    return result;
 }
 
 int NWW(int lhs, int rhs) {
