1aa99365e96e8ab728e1d7756acc3bdb9f2156dc
kondorski <thegreatvaultofknowledge@gmail.com>
2021-05-26 12:50:53 +0200

    getErrorMessage switch, enum and function definitions

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,45 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+
+int checkPassword(const std::string &password, const std::string &)
+{
+    
+    return 1;
+}
+
+bool doPasswordsMatch(const std::string &, const std::string &)
+{
+
+    return true;
+}
+
+int checkPasswordRules(const std::string &)
+{
+    return 1;
+}
+
+std::string getErrorMessage(int errorCode)
+{
+    switch (errorCode)
+    {
+    case 0:
+        return "Ok";
+        break;
+    case 1:
+        return "Password needs to have at least nine characters";
+        break;
+    case 2:
+        return "Password needs to have at least one number";
+        break;
+    case 3:
+        return "Password needs to have at least one special character";
+        break;
+    case 4:
+        return "Password needs to have at least one uppercase letter";
+        break;
+    case 5:
+        return "Passwords do not match";
+    default:
+        break;
+    }
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,19 @@
+#pragma once
+#include <string>
+
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+
+enum ErrorCode {
+   Ok,
+   PasswordNeedsAtLeastNineCharacters,
+   PasswordNeedsAtLeastOneNumber,
+   PasswordNeedsAtLeastOneSpecialCharacter,
+   PasswordNeedsAtLeastOneUppercaseLetter,
+   PasswordsDoNotMatch,
+};
+
+bool doPasswordsMatch(const std::string&, const std::string&);
+int checkPasswordRules(const std::string&);
+int checkPassword(const std::string&, const std::string&);
+std::string getErrorMessage(int);
