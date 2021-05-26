bbb2e92f85830a496a09cf2ffe1afb5e9350cdcb
PawelYak <pawelstojak@gmail.com>
2021-05-26 22:14:31 +0200

    checkPassword and partially PasswordRules added

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,18 +1,32 @@
 #include "validation.hpp"
+#include <algorithm>
+#include <cctype>
 #include <string>
 
-std::string getErrorMessage(ErrorCode errorCode) {
-    return "";
-}
+std::string getErrorMessage(ErrorCode errorCode) { return ""; }
 
 bool doPasswordsMatch(std::string password, std::string passwordConfirm) {
-    return false;
+  return false;
 }
 
 ErrorCode checkPasswordRules(std::string password) {
-    return ErrorCode::Ok;
+  if (password.length() < 9) {
+    return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+  } else if (!(std::any_of(password.begin(), password.end(), isdigit))) {
+    return ErrorCode::PasswordNeedsAtLeastOneNumber;
+  }
+   else if (!(std::any_of(password.begin(), password.end(), ???))) {
+    return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+   }
+   else if (!(std::any_of(password.begin(), password.end(), isupper))) {
+    return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
 }
+return ErrorCode::Ok;
 
-ErrorCode checkPassword(std::string password, std::string passwordConfirm) {
-    return ErrorCode::Ok;
+ErrorCode checkPassword(const std::string &password,
+                        const std::string &passwordConfirm) {
+  if (doPasswordsMatch(std::string password, std::string passwordConfirm){
+    checkPasswordRules(std::string password);
+  }
+    return ErrorCode::PasswordsDoNotMatch;
 }
