651fbcc696cbb14507dafea6c31eb5a90623df24
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-05-26 14:15:07 +0200

    first change in validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,12 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#include <iostream>
+#include <string>
+
+enum class ErrorCode {
+
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
