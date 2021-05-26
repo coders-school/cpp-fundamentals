6d3ee7506ffabc68bf4aa6d015c6bcd448569a1a
MalarzDawid <github@dawidmalarz.pl>
2021-05-26 18:01:51 +0200

    Fibonacci iterative done

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int number = 1;
+    int number_prev = 0;
+    int temp = 0;
+    
+    if (sequence < 2) {
+        return sequence;
+    } 
+    else {
+        for (int i=2; i<=sequence; i++) {
+            temp = number + number_prev;
+            number_prev = number;
+            number = temp;
+        }
+    }
+    return number;
 }
 
 int fibonacci_recursive(int sequence) {
