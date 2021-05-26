d6f7c00abbb3040617d26904b8f73bf1abebf793
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-05-26 21:49:50 +0200

    getErrorMessage function seems to work correctly.

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,7 +1,23 @@
 #include "validation.hpp"
 
 std::string getErrorMessage(ErrorCode error) {
-    return "";
+
+    if (error == ErrorCode::PasswordNeedsAtLeastNineCharacters) {
+        return "Password needs to have at least nine characters";
+    }
+    if (error == ErrorCode::PasswordNeedsAtLeastOneNumber) {
+        return "Password needs to have at least one number";
+    }
+    if (error == ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter) {
+        return "Password needs to have at least one special character";
+    }
+    if (error == ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter) {
+        return "Password needs to have at least one uppercase letter";
+    }
+    if (error == ErrorCode::PasswordsDoNotMatch) {
+        return "Passwords do not match";
+    }
+    return "Ok";
 }
 
 bool doPasswordsMatch(std::string firstPassword, std::string secondPassword) {
