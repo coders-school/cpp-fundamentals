b24c3247f5390aad52672fae0c39a2e21a8b4c92
PawelZydek <83175937+PawelZydek@users.noreply.github.com>
2021-05-26 20:46:31 +0200

    Update generateSequence.hpp
    Added vector of sum

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -3,5 +3,16 @@
 
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
-    return {};
+    int sum = step;
+    std::vector<int> sum_vector{};
+    for (int i = 0; i < count; i++) {
+        if (i == 0) {
+            sum_vector.push_back(sum);
+        }
+        else {
+            sum += step;
+            sum_vector.push_back(sum);
+        }   
+    }
+    return sum_vector;
 }
