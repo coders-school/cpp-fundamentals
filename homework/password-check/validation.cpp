ff14cebcb6be5ee0f98b43e99042551bf83a0016
Mikhael <michal.grzelczak@gmail.com>
2021-05-26 22:40:11 +0200

    Adding work

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,66 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+#define MIN_PASS_LEN (int)9
+
+std::string getErrorMessage(ErrorCode errorCode) {
+    std::string returnString;
+
+    switch (errorCode) {
+    case ErrorCode::Ok:
+        returnString = "Ok";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        returnString = "Password needs to have at least nine characters";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        returnString = "Password needs to have at least one number";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+        returnString = "Password needs to have at least one special character";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        returnString = "Password needs to have at least one uppercase letter";
+        break;
+    case ErrorCode::PasswordsDoNotMatch:
+        returnString = "Passwords do not match";
+        break;
+    }
+
+    return returnString;
+}
+
+bool doPasswordsMatch(std::string firstPassword, std::string secondPassword) {
+    if (firstPassword == secondPassword) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+ErrorCode checkPasswordRules(std::string passwordToCheck) {
+    if (MIN_PASS_LEN > std::distance(passwordToCheck.cbegin(), passwordToCheck.cend())) {
+        return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+    }
+
+    if (std::none_of(passwordToCheck.cbegin(), passwordToCheck.cend(), [](unsigned char c) { return std::isdigit(c); })) {
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    }
+
+    if (std::none_of(passwordToCheck.cbegin(), passwordToCheck.cend(), [](unsigned char c) { return std::ispunct(c); })) {
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    }
+
+    if (std::none_of(passwordToCheck.cbegin(), passwordToCheck.cend(), [](unsigned char c) { return std::isupper(c); })) {
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    }
+
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(std::string firstPassword, std::string secondPassword) {
+    if (not doPasswordsMatch(firstPassword, secondPassword)) {
+        return ErrorCode::PasswordsDoNotMatch;
+    } else {
+        return checkPasswordRules(firstPassword);
+    }
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,19 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#pragma once
+
+#include <algorithm>
+#include <cctype>
+#include <string>
+
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch,
+};
+
+std::string getErrorMessage(ErrorCode errorCode);
+bool doPasswordsMatch(std::string firstPassword, std::string secondPassword);
+ErrorCode checkPasswordRules(std::string passwordToCheck);
+ErrorCode checkPassword(std::string firstPassword, std::string secondPassword);
