f2c95875bcff5959e2b87b5e182d3837f9c93096
Kruza <palatonista@gmail.com>
2021-05-26 15:50:19 +0200

    Add Even works

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,22 @@
 #pragma once
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+int addEven(const std::vector<int>& numbers) 
+{
+    if (numbers.size() == 0)
+    {
+        std::cout << "Provided vector is empty!\n";
+        return 0;
+    }
+
+    auto sum = 0;
+    for (auto element : numbers)
+    {
+        if (element % 2 == 0)
+        {
+            sum += element;
+        }
+    }
+
+    return sum;
 }
