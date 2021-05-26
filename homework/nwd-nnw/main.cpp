b53a256705025450159270d040eeadd0983b1689
Krzysztof C <krzysztof.ciecko.kontakt@gmail.com>
2021-05-26 13:04:58 +0200

    nwd i nww ->  good

--- homework/nwd-nnw/main.cpp
+++ homework/nwd-nnw/main.cpp
@@ -3,7 +3,15 @@
 
 int main() {
   std::cout << "NWW(255, 15) = " << NWW(255, 15) << "\n";
+  std::cout << "NWW(-10, -2) = " << NWW(-10, -2) << "\n";
   std::cout << "NWD(255, 15) = " << NWD(255, 15) << "\n";
+  std::cout << "NWD(-10, -2) = " << NWD(-10, -2) << "\n";
+  std::cout<<std::endl;
+  std::cout << "NWW(255, 15) = " << NWW(255, 15) << "\n";
+  std::cout << "NWW(0, 0) = " << NWW(0, 0) << "\n";
+  std::cout << "NWD(0, 0) = " << NWD(0, 0) << "\n";
+  std::cout << "NWD(-10, -2) = " << NWD(-10, -2) << "\n";
+
 
   return 0;
 }
--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
+// void swap(int& a, int& b)
+// {
+//     int temp = a;
+//     a = b;
+//     b = temp;
+// }
+
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    int tmp;
+    while(rhs != 0){
+        tmp = lhs % rhs;
+        lhs = rhs;
+        rhs = tmp;
+    }
+    if(lhs < 0)
+        lhs*=-1;
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 && rhs ==0)
+        return 0;
+    int temp = lhs*rhs/NWD(lhs,rhs);
+    if(temp < 0)
+        temp*=-1;
+    return temp;
 }
