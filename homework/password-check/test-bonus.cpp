49430e74f06110b98135f23cef87ac9210ec09b5
Łukasz Nitka <lukasnit@wp.pl>
2021-05-26 12:39:12 +0200

    test_change

--- homework/password-check/test-bonus.cpp
+++ homework/password-check/test-bonus.cpp
@@ -6,10 +6,7 @@ const std::string TOO_FEW_CHARS = "aA1!";
 const std::string MISSING_NUMBERS = "abcABC!@#";
 const std::string MISSING_SPECIAL_CHARS = "abcABC123";
 const std::string MISSING_UPPERCASE = "abc123!@#";
-const std::string MIS_U = "***q111000l";
-const std::string PROPER_PASSWORD_b = "Rt$44pAm*23";
-const std::string TOO_FEW_CHARS_b = "";
-const std::string MISSING_NUMBERS_b = "aaaaaaaB!!!!!!";
+
 
 
 
@@ -19,8 +16,5 @@ TEST(checkPasswordRulesBonusTests, returnProperErrorCodes) {
     EXPECT_EQ(checkPasswordRules(MISSING_NUMBERS), ErrorCode::PasswordNeedsAtLeastOneNumber);
     EXPECT_EQ(checkPasswordRules(MISSING_SPECIAL_CHARS), ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter);
     EXPECT_EQ(checkPasswordRules(MISSING_UPPERCASE), ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter);
-    EXPECT_EQ(checkPasswordRules(MIS_U), ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter);
-    EXPECT_EQ(checkPasswordRules(PROPER_PASSWORD_b), ErrorCode::Ok);
-    EXPECT_EQ(checkPasswordRules(TOO_FEW_CHARS_b), ErrorCode::PasswordNeedsAtLeastNineCharacters);
-    EXPECT_EQ(checkPasswordRules(MISSING_NUMBERS_b), ErrorCode::PasswordNeedsAtLeastOneNumber);
+    
 }
