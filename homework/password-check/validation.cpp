d2e40b1bdfdebd9e58a97b3e93cdfa54716a9dbc
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2021-05-27 13:15:08 +0200

    Added 2 and 3 function

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,23 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+#include <stdlib.h>
+#include <time.h>
+
+ std::string getErrorMessage(ErrorCode error)
+ 
+ bool checkPasswordRules(std::string str1, std::string str2){
+
+        if(str1!=str2){
+           return false;
+        }
+
+      return true;
+ }
+ 
+ErrorCode checkPasswordRules(std::string){
+    
+     srand(time(NULL));
+     int rand_number = rand()%5;
+    
+     return static_cast<ErrorCode>(rand_number);
+} 
+
