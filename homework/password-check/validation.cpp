76f8ce6fc93ad5c56ad449b35dfd90cf13fe1eef
Piotr Rogalski <progalski89@gmail.com>
2021-05-27 15:23:47 +0200

    one command one line rule applied

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -7,13 +7,34 @@ std::string getErrorMessage(const ErrorCode t_ErrorCode)
     // No break instructions due to presence of returns
     switch (t_ErrorCode)
     {
-    case ErrorCode::Ok: return "Ok";
-    case ErrorCode::PasswordNeedsAtLeastNineCharacters: return "Password needs to have at least nine characters";
-    case ErrorCode::PasswordNeedsAtLeastOneNumber: return "Password needs to have at least one number";
-    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter: return "Password needs to have at least one special character";
-    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter: return "Password needs to have at least one uppercase letter";
-    case ErrorCode::PasswordsDoNotMatch: return "Passwords do not match";
-    default: return "Invalid error code";
+        case ErrorCode::Ok:
+        {
+            return "Ok";
+        }
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        {
+            return "Password needs to have at least nine characters";
+        }
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        {
+            return "Password needs to have at least one number";
+        }
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+        {
+            return "Password needs to have at least one special character";
+        }
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        {
+            return "Password needs to have at least one uppercase letter";
+        }
+        case ErrorCode::PasswordsDoNotMatch:
+        {
+            return "Passwords do not match";
+        }
+        default:
+        {
+            return "Invalid error code";
+        }
     }
 }
 
