e0bf51e12ef362c76dbdea8eb529bdf9454dfcf9
Łukasz Nitka <lukasnit@wp.pl>
2021-05-27 11:49:39 +0200

    v1

--- homework/vector-of-shared-ptrs/main.cpp
+++ homework/vector-of-shared-ptrs/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <string>
+#include <memory>
+#include <vector>
 #include "vectorFunctions.hpp"
 
 int main() {
     auto vec = generate(10);
-    print(vec);
-    add10(vec);
-    print(vec);
-    sub10(vec);
-    print(vec);
+      print(vec);
+  //  add10(vec);
+  //  print(vec);
+  //  sub10(vec);
+  //  print(vec);
 
     return 0;
 }
---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,26 @@
+# include "vectorFunctions.hpp"
+
+
+std::vector<std::shared_ptr<int>> generate(int count)
+{
+
+std::vector<std::shared_ptr<int>>vec;
+
+    for(int i=0; i<=count; i++)
+    {
+     vec.push_back(std::make_shared<int>(i));
+    }
+    return vec;
+
+}
+
+void print(std::vector<std::shared_ptr<int>>vec2)
+{
+
+   
+   
+    for(const auto& el:vec2)
+    {
+    std::cout<<*el<<'\n';
+    }
+}
