ec7f2ecf470f60118a4b86691b69f10019da45ac
Jan Tobiczyk <j.tobiczyk43@gmail.com>
2021-05-27 13:59:30 +0200

    repeat

---
+++ homework/password-check/validation.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <cctype>
+#include <algorithm>
+
+enum class ErrorCode
+{
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+std::string getErrorMessage(ErrorCode);
+bool doPasswordsMatch(const std::string&, const std::string&);
+ErrorCode checkPasswordRules(const std::string&);
+ErrorCode checkPassword(const std::string&,const std::string&);
+
+
+
+
+
+
