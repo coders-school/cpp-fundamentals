de45ef1366a110a4a45ad53d5d54658c70cd9d5d
miczel <michal.soloducha@gmail.com>
2021-05-27 13:23:24 +0200

    Correct strings

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -4,11 +4,11 @@
 std::string getErrorMessage(ErrorCode error) {
     switch (error) {
     case ErrorCode::Ok : return "Ok";
-    case ErrorCode::PasswordNeedsAtLeastNineCharacters : return "PasswordNeedsAtLeastNineCharacters";
-    case ErrorCode::PasswordNeedsAtLeastOneNumber : return "PasswordNeedsAtLeastOneNumber";
-    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter : return "PasswordNeedsAtLeastOneSpecialCharacter";
-    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter : return "PasswordNeedsAtLeastOneUppercaseLetter";
-    case ErrorCode::PasswordsDoNotMatch : return "PasswordsDoNotMatch";
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters : return "Password needs to have at least nine characters";
+    case ErrorCode::PasswordNeedsAtLeastOneNumber : return "Password needs to have at least one number";
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter : return "Password needs to have at least one special character";
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter : return "Password needs to have at least one uppercase letter";
+    case ErrorCode::PasswordsDoNotMatch : return "Passwords do not match";
     default : return "Invalid error code";
     }
 }
