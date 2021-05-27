c39ecaf346535336f1d970ee94980d3daa122180
Jan Tobiczyk <j.tobiczyk43@gmail.com>
2021-05-27 17:06:32 +0200

    fix

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+#include <memory>
+
+std::vector<std::shared_ptr<int>> generate(int);
+void print(std::vector<std::shared_ptr<int>>);
+void add10(std::vector<std::shared_ptr<int>>);
+void sub10(int * const);
+void sub10(std::vector<std::shared_ptr<int>>);
