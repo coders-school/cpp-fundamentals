0b0a18c5920ebcef9ddfef9f7eb4c25db82b3dc2
Sebastian302653 <yoda21@op.pl>
2021-05-29 20:14:01 +0200

    vector

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,49 @@
+#include "vectorFunctions.hpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+std::vector<std::shared_ptr<int>> generate(int count)
+{
+	std::vector<std::shared_ptr<int>> vec;
+	for(int i = 0; i < count; i++)
+	{
+		vec.push_back(std::make_shared<int>(i));
+
+	}
+	return vec;
+}
+
+void print(std::vector<std::shared_ptr<int>> vec)
+{
+	for(auto i: vec)
+	{
+		std::cout << *i; 
+	}
+}
+
+void add10(std::vector<std::shared_ptr<int>> vec)
+{
+	for(auto i: vec)
+	{
+		if(i)
+			*i += 10; 
+	}
+}
+
+void sub10(int* const ptr)
+{
+	if(ptr == nullptr)
+		*ptr -= 10;
+}
+
+void sub10(std::vector<std::shared_ptr<int>> vec)
+{
+	for(auto i: vec)
+	{
+		if(i)
+			sub10(i.get());
+	}
+}
---
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <memory>
+
+
+std::vector<std::shared_ptr<int>> generate(int count);
+void print(std::vector<std::shared_ptr<int>>);
+void add10(std::vector<std::shared_ptr<int>>);
+void sub10(int* const);
+void sub10(std::vector<std::shared_ptr<int>>);
