16c7b558a42aff2c5541bb5e9fbf473a6b3b5c54
Krzysztof Kryczka <kw.kryczka@gmail.com>
2021-05-29 15:03:12 +0200

    validation.cpp empty functions

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,19 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode error){
+    
+    return "";
+}
+
+bool doPasswordsMatch(const std::string& password, const std::string& repeatPassword){
+    return false;
+}
+
+ErrorCode checkPasswordRules(const std::string& password){
+    return ErrorCode::PasswordsDoNotMatch;
+}
+
+ErrorCode checkPassword(const std::string& password, const std::string& repeatPassword){
+    return ErrorCode::PasswordsDoNotMatch;
+}
