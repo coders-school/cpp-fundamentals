5ff9dd698a315e40286e6128edf50e0ea2e3af94
Arek Ogiewa <arkadiusz.ogiewa@gmail.com>
2021-05-30 20:46:21 +0200

    ErrorCode type and function declarations added in .hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,22 @@
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+
+#pragma once
+
+
+enum class ErrorCode {
+    Ok, // '0'
+    PasswordNeedsAtLeastNineCharacters, // '1'
+    PasswordNeedsAtLeastOneNumber,  // '2'
+    PasswordNeedsAtLeastOneSpecialCharacter,    // '3'
+    PasswordNeedsAtLeastOneUppercaseLetter, // '4'
+    PasswordsDoNotMatch,    // '5'
+};
+
+getErrorMessage(int error);
+
+bool doPasswordsMatch(std::string pass1, std::string pass2);
+
+int checkPasswordRules(std::string pass);
+
+int checkPassword(std::string pass1, std::string pass2);
