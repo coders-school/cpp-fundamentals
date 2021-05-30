0c8b1a00d2ffdc690d9686b91ba6d4cf33bd9cd2
Piotr Kosc <piotr.kosc@op.pl>
2021-05-30 22:36:43 +0200

    Made iterative method

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,15 +1,20 @@
 #pragma once
 
-/*int fibonacci_iterative(int sequence) {
-    for (int i = 0; i < sequence; ++i) {
-        if (i == 0) return 0;
-        if (i == 1) return 1;
-        else {
-            return (i-2) + (i-1);
+int fibonacci_iterative(int sequence) {
+    int a = 0;      //wyraz n-2
+    int b = 1;      //wyraz n-1
+    int sum = 0;
+    if (sequence == 1 || sequence == 2) return 1;
+    else {
+        for (int i = 0; i < sequence - 1; ++i) {
+        
+            sum = a + b;
+            b += a;
+            a = b - a;
         }
+        return sum;
     }
-    return 0;
-}*/
+}
 
 int fibonacci_recursive(int sequence) {
     if (sequence == 0) return 0;
