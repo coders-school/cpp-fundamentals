31d16b18b805a6cd15d68336cb12c9baef483a5a
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-05-30 19:24:59 +0200

    fixes

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -9,14 +9,14 @@ std::vector<std::shared_ptr<int>> generate(int count)
         vec.push_back(std::make_shared<int>(i));
     }
     return vec;
-}
+};
 void print(std::vector<std::shared_ptr<int>> vec)
 {
     for (auto element : vec)
     {
         std::cout << *element << "\n";
     }
-}
+};
 void add10(std::vector<std::shared_ptr<int>> vecAdd)
 {
     for (auto element : vecAdd)
@@ -26,16 +26,19 @@ void add10(std::vector<std::shared_ptr<int>> vecAdd)
             *element = *element + 10;
         }
     }
-}
+};
 void sub10(int *const ptr)
 {
-    *ptr = *ptr - 10;
-}
+    if (ptr)
+    {
+        *ptr = *ptr - 10;
+    }
+};
 void sub10(std::vector<std::shared_ptr<int>> vecSub)
 {
     for (auto element : vecSub)
     {
-       int *const ptr = element.get();
-       sub10(ptr);
+        int *const ptr = element.get();
+        sub10(ptr);
     }
-}
+};
