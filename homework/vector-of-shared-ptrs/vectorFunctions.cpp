b4ae5205ade9dc6ec16b01b9b60c04e0a5271432
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-05-30 18:20:06 +0200

    Adding header file and vector

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,12 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count)
+{
+    std::vector < std::shared_ptr<int> vec;
+    vec.reserve(count);
+    for (int i = 0; i < count; i++)
+    {
+        vec.push_back(std::make_shared<int>(i));
+    }
+    return vec;
+};
