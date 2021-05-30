a9e1c6f4e08080125d85118a303db0069abb6bc0
Piotr Kosc <piotr.kosc@op.pl>
2021-05-30 21:54:51 +0200

    Made recursive method

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
+/*int fibonacci_iterative(int sequence) {
+    for (int i = 0; i < sequence; ++i) {
+        if (i == 0) return 0;
+        if (i == 1) return 1;
+        else {
+            return (i-2) + (i-1);
+        }
+    }
     return 0;
-}
+}*/
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) return 0;
+    if (sequence == 1) return 1;
+    return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
 }
+
