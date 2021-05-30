b38fae8c3121892c907d7a45814baf6bd1ad30e8
pwiatr3 <pwiatr3@yahoo.pl>
2021-05-30 20:30:26 +0200

    adding validation functions

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,20 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode errCode){
+    switch(errCode){
+        case Ok: return "Ok";
+
+    }
+    return "Ok";
+}
+
+bool doPasswordsMatch(std::string password1, std::string password2){
+    return true;
+}
+ErrorCode checkPasswordRules(std::string password){
+    return Ok;
+}
+ErrorCode checkPassword(std::string password1, std::string password2){
+    return Ok;
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,16 @@
+#include <string>
+
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+enum ErrorCode {Ok,
+                PasswordNeedsAtLeastNineCharacters,
+                PasswordNeedsAtLeastOneNumber,
+                PasswordNeedsAtLeastOneSpecialCharacter,
+                PasswordNeedsAtLeastOneUppercaseLetter,
+                PasswordsDoNotMatch
+                };
+
+std::string getErrorMessage(ErrorCode errCode);
+bool doPasswordsMatch(std::string password1, std::string password2);
+ErrorCode checkPasswordRules(std::string password);
+ErrorCode checkPassword(std::string password1, std::string password2);
