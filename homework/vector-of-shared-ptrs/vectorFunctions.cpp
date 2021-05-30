ae384a378b9d6b88392e616d46550b8082dc5d47
miczel <michal.soloducha@gmail.com>
2021-05-30 11:40:22 +0200

    formatting improved

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,6 +1,7 @@
 #include "vectorFunctions.hpp"
 
-std::vector<std::shared_ptr<int>> generate(int count){
+std::vector<std::shared_ptr<int>> generate(int count) {
+
     std::vector<std::shared_ptr<int>> vec;
     vec.reserve(count + 1);
     for (int i = 0; i < count; i++) {
@@ -9,24 +10,28 @@ std::vector<std::shared_ptr<int>> generate(int count){
     return vec;
 };
 
-void print(std::vector<std::shared_ptr<int>> vec){
+void print(std::vector<std::shared_ptr<int>> vec) {
+
     for (auto el : vec) {
         std::cout << *el << " ";
     }
     std::cout << "\n";
 };
 
-void add10(std::vector<std::shared_ptr<int>> vec){
+void add10(std::vector<std::shared_ptr<int>> vec) {
+
     for (auto el : vec) {
         if (el) *el += 10;
     }
 };
 
-void sub10(int* const ptr){
+void sub10(int* const ptr) {
+
     if (ptr) *ptr -= 10;
 };
 
-void sub10(std::vector<std::shared_ptr<int>> vec){
+void sub10(std::vector<std::shared_ptr<int>> vec) {
+
     for (auto el : vec) {
         int* const ptr = el.get();
         sub10(ptr);
