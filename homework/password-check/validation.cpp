47472f6dea5818246c109f7866e74fbe52b2f49a
Arek Ogiewa <arkadiusz.ogiewa@gmail.com>
2021-05-30 20:50:24 +0200

    function headers added to validation.cpp

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,16 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+
+
+getErrorMessage(int error){
+}
+
+bool doPasswordsMatch(std::string pass1, std::string pass2){
+}
+
+int checkPasswordRules(std::string pass){
+}
+
+int checkPassword(std::string pass1, std::string pass2){
+}
+
