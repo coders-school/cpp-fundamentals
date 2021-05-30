592aa26b21ee7c7af794d8a9b823d5170aa063b9
miczel <michal.soloducha@gmail.com>
2021-05-30 11:41:15 +0200

    reserve range fixed

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -3,7 +3,7 @@
 std::vector<std::shared_ptr<int>> generate(int count) {
 
     std::vector<std::shared_ptr<int>> vec;
-    vec.reserve(count + 1);
+    vec.reserve(count);
     for (int i = 0; i < count; i++) {
         vec.push_back(std::make_shared<int>(i));
     }
