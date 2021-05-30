77613c62a2d9e1ce6fed845c542d7d1051c23ccc
Marcin$ <marcin.s10335@gmail.com>
2021-05-30 18:40:44 +0200

    Add function to header file and complete two functinos

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,35 @@
+#include <map>
+#include <string>
+#include <algorithm>
 #include "validation.hpp"
 // TODO: Put implementations here
+
+static const std::map<ErrorCode, std::string>& convertEnumsToString() {
+    std::map<ErrorCode, std::string> result{
+        {ErrorCode::Ok, "Ok"},
+        {ErrorCode::PasswordNeedsAtLeastNineCharacters, "PasswordNeedsAtLeastNineCharacters"},
+        {ErrorCode::PasswordNeedsAtLeastOneNumber, "PasswordNeedsAtLeastOneNumber"},
+        {ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter, "PasswordNeedsAtLeastOneSpecialCharacter"},
+        {ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter, "PasswordNeedsAtLeastOneUppercaseLetter"},
+        {ErrorCode::PasswordsDoNotMatch, "PasswordsDoNotMatch"}};
+    return result;
+}
+std::string getErrorMessage(ErrorCode er) {
+    auto res = convertEnumsToString();
+    return res.at(er);
+}
+bool doPasswordsMatch(std::string password1, std::string password2) {
+    if(password1.size() != password2.size()){
+        return false;
+    }
+    auto pair = std::mismatch(password1.begin(),password1.end(),password2.begin());
+    if(pair.first == password1.end()){
+        return false;
+    }
+    return true;
+
+}
+ErrorCode checkPasswordRules(std::string password) {
+}
+ErrorCode checkPassword(std::string password1, std::string password2) {
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,19 @@
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+#pragma once
+#include <string>
+#include <map>
+
+enum class ErrorCode {
+    Ok = 0,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch,
+};
+static const std::map<ErrorCode, std::string>& convertEnumsToString(); 
+std::string getErrorMessage(ErrorCode);
+bool doPasswordsMatch(std::string, std::string);
+ErrorCode checkPasswordRules(std::string);
+ErrorCode checkPassword(std::string, std::string);
