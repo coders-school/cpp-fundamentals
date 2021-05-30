c52d358ac40ee7d759d3bd8136270c8cb2b9d845
Pawel Bieniasz <pawel.bieniasz@protonmail.com>
2021-05-30 20:55:52 +0200

    password-check base

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,40 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode error) {
+	switch (error)
+	{
+	case ErrorCode::Ok:
+		return "Ok";
+	case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+		return "Password needs to have at least nine characters";
+	case ErrorCode::PasswordNeedsAtLeastOneNumber:
+		return "Password needs to have at least one number";
+	case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+		return "Password needs to have at least one special character";
+	case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+		return "Password needs to have at least one uppercase letter";
+	case ErrorCode::PasswordsDoNotMatch:
+		return "Passwords do not match";
+	default:
+		return "Error unknown";
+	}
+}
+bool doPasswordsMatch(std::string password, std::string reprepeatedPassword) {
+	if (password == reprepeatedPassword) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+ErrorCode checkPasswordRules(std::string password) {
+	if (password.size() < 9) { return ErrorCode::PasswordNeedsAtLeastNineCharacters; }
+
+	return ErrorCode::Ok;
+}
+ErrorCode checkPassword(std::string password, std::string repeatedPassword) {
+	if (doPasswordsMatch(password, repeatedPassword)) {
+		return checkPasswordRules(password);
+	}
+	return  ErrorCode::PasswordsDoNotMatch;
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,18 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#pragma once
+#include <string>
+#include <cctype>
+
+enum class ErrorCode
+{
+	Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+
+std::string getErrorMessage(ErrorCode);
+bool doPasswordsMatch(std::string password, std::string repeatedPassword);
+ErrorCode checkPasswordRules(std::string password);
+ErrorCode checkPassword(std::string password, std::string repeatedPassword);
