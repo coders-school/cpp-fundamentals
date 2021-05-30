f487b50a1863e290576e53789d1a96c64e01d494
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-05-30 19:08:28 +0200

    sub 10 functions

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,9 +1,8 @@
 #include "vectorFunctions.hpp"
-#include <iostream>
 
 std::vector<std::shared_ptr<int>> generate(int count)
 {
-    std::vector < std::shared_ptr<int> vec;
+    std::vector<std::shared_ptr<int>> vec;
     vec.reserve(count);
     for (int i = 0; i < count; i++)
     {
@@ -18,13 +17,25 @@ void print(std::vector<std::shared_ptr<int>> vec)
         std::cout << *element << "\n";
     }
 }
-void add10(std::vector<std::shared_ptr<int>> vecSum)
+void add10(std::vector<std::shared_ptr<int>> vecAdd)
 {
-    for (auto element : vecSum)
+    for (auto element : vecAdd)
     {
         if (element)
         {
             *element = *element + 10;
         }
+    }
+}
+void sub10(int *const ptr)
+{
+    *ptr = *ptr - 10;
+}
+void sub10(std::vector<std::shared_ptr<int>> vecSub)
+{
+    for (auto element : vecSub)
+    {
+       int *const ptr = element.get();
+       sub10(ptr);
     }
 }
