da5024ba4d8a87a287b24456e92a3681b26a68b0
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-05-30 18:51:10 +0200

    Adding function add10

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,8 +1,9 @@
 #include "vectorFunctions.hpp"
+#include <iostream>
 
 std::vector<std::shared_ptr<int>> generate(int count)
 {
-    std::vector<std::shared_ptr<int>> vec;
+    std::vector < std::shared_ptr<int> vec;
     vec.reserve(count);
     for (int i = 0; i < count; i++)
     {
@@ -15,5 +16,15 @@ void print(std::vector<std::shared_ptr<int>> vec)
     for (auto element : vec)
     {
         std::cout << *element << "\n";
+    }
+}
+void add10(std::vector<std::shared_ptr<int>> vecSum)
+{
+    for (auto element : vecSum)
+    {
+        if (element)
+        {
+            *element = *element + 10;
+        }
     }
 }
