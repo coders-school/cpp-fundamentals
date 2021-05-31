9864ba9371462d4c9c398bdeb540b0c0e0b1a061
Michał Przymusiński <wrzaskun000@gmail.com>
2021-06-01 00:10:24 +0200

    add fibon functions

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,32 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
+int fibonacci_iterative(int sequence)
+{
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 1)
+    {
+        return sequence;
+    }
+
+    int out = 0;
+    int a = 0;
+    int b = 1;
+    while (sequence-- > 1)
+    {
+        out = a + b;
+        a = b;
+        b = out;
+    }
+
+    return out;
 }
 
-int fibonacci_recursive(int sequence) {
+int fibonacci_recursive(int sequence)
+{
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 1)
+    {
+        return sequence;
+    }
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
