811c38bda427685f53dde9df9fc4c91a020b5f7d
Arek Ogiewa <arkadiusz.ogiewa@gmail.com>
2021-05-31 16:31:43 +0200

    vectorFunctions.hpp/cpp created

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,19 @@
+#include "vectorFunctions.hpp"
+
+
+std::vector<std::shared_ptr<int>> generate(int count){
+    std::vector<std::shared_ptr<int>> result;
+    return result;
+}
+
+void print(std::vector<std::shared_ptr<int>> vec){
+}
+
+void add10(std::vector<std::shared_ptr<int>> vec){
+}
+
+void sub10(int* const pointer){
+}
+
+void sub10(std::vector<std::shared_ptr<int>> vec){
+}
---
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include<string>
+#include<vector>
+#include<memory>
+
+
+std::vector<std::shared_ptr<int>> generate(int count);
+
+void print(std::vector<std::shared_ptr<int>> vec);
+
+void add10(std::vector<std::shared_ptr<int>> vec);
+
+void sub10(int* const pointer);
+
+void sub10(std::vector<std::shared_ptr<int>> vec);
