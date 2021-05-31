eda4d70deb72220d8fe9146d9e57cd1c0b1a82aa
LechWilk <lech.wilk@gmail.com>
2021-05-31 08:32:58 +0200

    rev0:maxOfVector compiled

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -4,5 +4,8 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    int el_max=0;
+    for(auto el:vec)
+        if (el>el_max) el_max=el;
+    return el_max;
 }
