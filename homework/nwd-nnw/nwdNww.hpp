43076420f231aa9b08a574e656bd7cc2921ca525
Michał Przymusiński <wrzaskun000@gmail.com>
2021-06-01 01:54:12 +0200

    add nww nwd

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,36 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs)
+{
+	// TODO: Implement me :)
+	while (lhs != rhs)
+	{
+		if (lhs > rhs)
+		{
+			lhs -= rhs;
+		}
+		else
+		{
+			rhs -= lhs;
+		}
+	}
+	return rhs;
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int lhs, int rhs)
+{
+	// TODO: Implement me :)
+	if (lhs < rhs)
+	{
+		return NWW(rhs, lhs);
+	}
+
+	int k = lhs;
+
+	while (k % rhs != 0)
+	{
+		k += lhs;
+	}
+
+	return k;
 }
