69c7d48b1a694ad8ae69c0cefbb4c3bdb6a125ea
pwiatr3 <pwiatr3@yahoo.pl>
2021-05-31 10:46:02 +0200

    password-check works

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -4,17 +4,33 @@
 std::string getErrorMessage(ErrorCode errCode){
     switch(errCode){
         case Ok: return "Ok";
-
+        case PasswordNeedsAtLeastNineCharacters: return "Password needs to have at least nine characters";
+        case PasswordNeedsAtLeastOneNumber: return "Password needs to have at least one number";
+        case PasswordNeedsAtLeastOneSpecialCharacter: return "Password needs to have at least one special character";
+        case PasswordNeedsAtLeastOneUppercaseLetter: return "Password needs to have at least one uppercase letter";
+        case PasswordsDoNotMatch: return "Passwords do not match";
+        default: return "Wrong code";
     }
-    return "Ok";
 }
 
 bool doPasswordsMatch(std::string password1, std::string password2){
-    return true;
+    if(password1 == password2){
+        return true;
+    }
+    else{
+        return false;
+    }  
 }
 ErrorCode checkPasswordRules(std::string password){
-    return Ok;
+    return static_cast<ErrorCode>(rand() % 5);
 }
+
 ErrorCode checkPassword(std::string password1, std::string password2){
+    if(doPasswordsMatch(password1, password2)){
+        return checkPasswordRules(password1);
+    }
+    else{
+        return PasswordsDoNotMatch;
+    }
     return Ok;
 }
