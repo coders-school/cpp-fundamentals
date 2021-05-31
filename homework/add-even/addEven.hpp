b1b83b578504c35735be5b4590fb3f32b81d8f2e
LechWilk <lech.wilk@gmail.com>
2021-05-31 08:22:31 +0200

    rev1 compiled and tested OK

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -4,7 +4,8 @@
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
+    int sum = 0;
     for(auto el:numbers)    
-        if(el%2==0) sum+=*el;
+        if(el%2==0) sum+=el;
     return sum;
 }
