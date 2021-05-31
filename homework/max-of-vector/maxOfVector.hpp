4c38e25e5281e35b061ca061b38f8ab1c75f95ad
LechWilk <lech.wilk@gmail.com>
2021-05-31 08:41:32 +0200

    rev1:maxOfVector tested OK

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #include <vector>
-#include <limits>
+#include <limits.h>
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    int el_max=0;
+    int el_max=INT_MIN;
     for(auto el:vec)
         if (el>el_max) el_max=el;
     return el_max;
