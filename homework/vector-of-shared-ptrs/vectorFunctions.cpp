31ae6545d247760c949321de367c38fa4af2e48d
Arek Ogiewa <arkadiusz.ogiewa@gmail.com>
2021-05-31 16:57:20 +0200

    vector generated

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -2,8 +2,10 @@
 
 
 std::vector<std::shared_ptr<int>> generate(int count){
-    std::vector<std::shared_ptr<int>> result;
-    return result;
+    std::vector<std::shared_ptr<int>> vec;
+    for (int i=0; i<=count; i++)
+        vec.push_back(std::make_shared<int>(i));
+    return vec;
 }
 
 void print(std::vector<std::shared_ptr<int>> vec){
