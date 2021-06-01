a5ba8ce1e76401059e75488bae4091988b6d0e1e
Michał Przymusiński <wrzaskun000@gmail.com>
2021-06-02 00:41:00 +0200

    add part of function structures.

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,28 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+
+std::string getErrorMessage(const ErrorCode code)
+{
+	for (auto& el : errorCodes)
+	{
+		if (code == el.code) {
+			return el.message;
+		}
+	}
+	return {};
+}
+
+bool doPasswordsMatch(const std::string_view lhs, const std::string_view rhs)
+{
+	return {};
+}
+
+ErrorCode checkPasswordRules(const std::string_view password)
+{
+	return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(const std::string& password, const std::string& repeatedPassword)
+{
+	return ErrorCode::Ok;
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,44 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+﻿// TODO: I'm empty :) Put enum and function headers here.
+#pragma once
+#include <string>
+#include <vector>
+
+enum class ErrorCode : unsigned {
+	Ok = 0,
+	PasswordNeedsAtLeastNineCharacters,
+	PasswordNeedsAtLeastOneNumber,
+	PasswordNeedsAtLeastOneSpecialCharacter,
+	PasswordNeedsAtLeastOneUppercaseLetter,
+	PasswordsDoNotMatch
+};
+
+struct ErrorCodePair {
+	ErrorCode code;
+	std::string message;
+};
+
+static const std::vector<ErrorCodePair> errorCodes{
+	{ErrorCode::Ok, "Ok"},
+	{ErrorCode::PasswordNeedsAtLeastNineCharacters, "PasswordNeedsAtLeastNineCharacters"},
+	{ErrorCode::PasswordNeedsAtLeastOneNumber, "PasswordNeedsAtLeastOneNumber"},
+	{ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter, "PasswordNeedsAtLeastOneSpecialCharacter"},
+	{ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter, "PasswordNeedsAtLeastOneUppercaseLetter"},
+	{ErrorCode::PasswordsDoNotMatch, "PasswordsDoNotMatch"}
+};
+
+std::string getErrorMessage(const ErrorCode code);
+
+//która otrzyma dwa hasła i sprawdzi czy są identyczne. Powinna zwrócić odpowiednią wartość typu bool.
+bool doPasswordsMatch(const std::string_view lhs, const std::string_view rhs);
+
+
+//która przyjmie jedno hasło i losowo zwróci jeden z kodów błędu PasswordNeedsAtLeast* lub Ok.
+ErrorCode checkPasswordRules(const std::string_view password);
+
+/*
+ Powinna ona przyjąć dwa hasła i używać funkcji doPasswordsMatch() w celu określenia czy hasła
+ się zgadzają. W przypadku gdy się nie zgadzają ma ona zwrócić kod błędu PasswordsDoNotMatch,
+ a w przeciwnym przypadku powinna wywołać funkcję checkPasswordRules() i
+ zwrócić kod błędu zwrócony przez nią.
+*/
+ErrorCode checkPassword(const std::string& password, const std::string& repeatedPassword);
