4e07d885d2752fd1fd30ab8537807e6b9ff1714c
Michał Przymusiński <wrzaskun000@gmail.com>
2021-06-01 22:04:44 +0200

    add nwdnww

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -3,23 +3,23 @@
 int NWD(int lhs, int rhs)
 {
 	// TODO: Implement me :)
-	while (lhs != rhs)
-	{
-		if (lhs > rhs)
-		{
-			lhs -= rhs;
-		}
-		else
-		{
-			rhs -= lhs;
-		}
-	}
-	return rhs;
+	lhs = std::abs(lhs);
+	rhs = std::abs(rhs);
+
+	return rhs == 0 ? lhs : NWD(rhs, lhs % rhs);
 }
 
 int NWW(int lhs, int rhs)
 {
 	// TODO: Implement me :)
+
+	lhs = std::abs(lhs);
+	rhs = std::abs(rhs);
+
+	if (lhs < 1 || rhs < 1) {
+		return 0;
+	}
+
 	if (lhs < rhs)
 	{
 		return NWW(rhs, lhs);
