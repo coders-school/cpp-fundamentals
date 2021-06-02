08ebd70997ae0d661e645708ef0ef4474c2c1ec8
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-06-02 14:53:56 +0200

    initial functions, enum and map

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,28 @@
 #include "validation.hpp"
+#include <string>
+#include <iostream>
+#include <vector>
 // TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode error_code){
+
+
+}
+
+ErrorCode checkPassword(std::string password,std::string repeatedPass){
+
+}
+
+
+int main(){
+    std::string password;
+    std::string repeatedPassword;
+    std::cout << "Set new password: ";
+    std::cin >> password;
+    std::cout << "Repeat password: ";
+    std::cin >> repeatedPassword;
+    auto result = checkPassword(password, repeatedPassword);
+    std::cout << getErrorMessage(result) << '\n';
+
+    return 0;
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,24 @@
+#pragma once
 // TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#include <map>
+
+enum ErrorCode {
+Ok,
+PasswordNeedsAtLeastNineCharacters,
+PasswordNeedsAtLeastOneNumber,
+PasswordNeedsAtLeastOneSpecialCharacter,
+PasswordNeedsAtLeastOneUppercaseLetter,
+PasswordDoNotMatch
+};
+
+std::map <ErrorCode,std::string> Map_Error_String{
+{Ok, "Ok"},
+{PasswordNeedsAtLeastNineCharacters, "Password needs to have at least nine characters"},
+{PasswordNeedsAtLeastOneNumber, "Password needs to have at least one number"},
+{PasswordNeedsAtLeastOneSpecialCharacter, "Password needs to have at least one special character"},
+{PasswordNeedsAtLeastOneUppercaseLetter, "Password needs to have at least one uppercase letter"},
+{PasswordDoNotMatch,"Passwords do not match"}
+};
+
+std::string getErrorMessage(ErrorCode);
+ErrorCode checkPassword(std::string, std::string );
