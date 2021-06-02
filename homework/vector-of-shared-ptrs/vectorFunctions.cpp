f54103260a8c74fad25bb64d1da3bf350f004632
pwiatr3 <pwiatr3@yahoo.pl>
2021-06-02 09:14:21 +0200

    vectorFunctions work

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -2,7 +2,7 @@
 
 std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> tempVector(count);
-    for (int i = 0; i < count; i++) {
+    for (size_t i = 0; i < count; i++) {
         tempVector[i] =  std::make_shared<int> (i);
     }
     return tempVector;
@@ -15,15 +15,23 @@ void print(const std::vector<std::shared_ptr<int>>& vec) {
 }
 
 void add10(std::vector<std::shared_ptr<int>>& vec) {
-    for (const auto& item : vec) {
-        *item += 10;
-    }
+   for (const auto& item : vec) {
+       if (item != nullptr) {
+            *item += 10;
+       }
+    }  
 }
 
 void sub10(int* vec) {
-
+    if (vec != nullptr) {
+        *vec -= 10;
+    }
 }
 
 void sub10(std::vector<std::shared_ptr<int>> vec) {
-
+       for (const auto& item : vec) {
+       if (item != nullptr) {
+            *item -= 10;
+       }
+    } 
 }
