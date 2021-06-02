9c29c6a0912dd65769ef4f688a24cc224373789b
opiotrowsky <piotrowsky.o@gmail.com>
2021-06-02 17:36:55 +0200

    Working getErrorMessage and doPasswordsMatch functions.

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,46 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cctype>
+#include <algorithm>
+
+std::string getErrorMessage(ErrorCode ErrorCode) {
+    int Ecode = static_cast<int>(ErrorCode);
+
+    switch (Ecode) {
+    case 0: {
+        return "Ok";
+        break;
+    }
+    case 1: {
+        return "Password needs to have at least nine characters";
+        break;
+    }
+    case 2: {
+        return "Password needs to have at least one number";
+        break;
+    }
+    case 3: {
+        return "Password needs to have at least one special character";
+        break;
+    }
+    case 4: {
+        return "Password needs to have at least one uppercase letter";
+        break;
+    }
+    case 5: {
+        return "Passwords do not match";
+        break;
+    }
+    }
+}
+
+bool doPasswordsMatch(std::string password, std::string repeatedPassword) {
+    if (password == repeatedPassword) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+
+std::string getErrorMessage(ErrorCode ErrorCode);
+bool doPasswordsMatch(std::string password, std::string repeatedPassword);
+ErrorCode checkPasswordRules(std::string password);
+ErrorCode checkPassword(std::string password, std::string repeatedPassword);
