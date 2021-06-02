e786127cb4db1818a03497f7e1fc45756d2740c3
rizzet97 <nit.pawel@gmail.com>
2021-06-02 18:05:08 +0200

    Initial commit

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,78 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(enum ErrorCode codeToPrint){
+  switch(codeToPrint){
+    case Ok:
+      return "Ok";
+    case PasswordNeedsAtLeastNineCharacters:
+      return "Password needs to have at least nine characters";
+    case PasswordNeedsAtLeastOneNumber:
+      return "Password needs to have at least one number";
+    case PasswordNeedsAtLeastOneSpecialCharacter:
+      return "Password needs to have at least one special character";
+    case PasswordNeedsAtLeastOneUppercaseLetter:
+      return "Password needs to have at least one uppercase letter";
+    case PasswordsDoNotMatch:
+      return "Passwords do not match";
+    default:
+      return "Undefined error";
+  }
+}
+
+bool doPasswordsMatch(std::string originalPassword, std::string repeatedPassword){
+  int compare_value{
+            std::string{originalPassword}.compare(std::string{repeatedPassword})
+  };
+  if (compare_value == 0)
+    return true;
+  else
+    return false;
+}
+
+enum ErrorCode checkPasswordRules(std::string userPassword){
+  if(!checkCharacterCount(userPassword)){
+    return PasswordNeedsAtLeastNineCharacters;
+  }
+  if(!checkForNumbers(userPassword)){
+    return PasswordNeedsAtLeastOneNumber;
+  }
+  if(!checkForSpecialCharacters(userPassword)){
+    return PasswordNeedsAtLeastOneSpecialCharacter;
+  }
+  if(!checkForUppercase(userPassword)){
+    return PasswordNeedsAtLeastOneUppercaseLetter;
+  }
+  return Ok;
+}
+
+enum ErrorCode checkPassword(std::string originalPassword, std::string repeatedPassword){
+  if(doPasswordsMatch(originalPassword, repeatedPassword) == false)
+    return PasswordsDoNotMatch;
+  else
+    return checkPasswordRules(originalPassword);
+}
+
+//poprzedni wygląd checkPassword()
+/*
+int diceRollValue = rand() % 5;
+enum ErrorCode diceRollResult = static_cast<ErrorCode>(diceRollValue);
+return diceRollResult;
+*/
+
+//nowe, dla ambitnych - wrzucić w checkPassword
+//po co wywołuję 1-linijkową func której potrzebuję tylko w innej funkcji
+bool checkCharacterCount(std::string userPassword){
+  return (userPassword.size() >= 9);
+}
+
+bool checkForNumbers(std::string userPassword){
+    return (std::any_of(userPassword.begin(), userPassword.end(), ::isdigit));
+}
+
+bool checkForSpecialCharacters(std::string userPassword){
+    return (std::any_of(userPassword.begin(), userPassword.end(), ::ispunct));
+}
+
+bool checkForUppercase(std::string userPassword){
+    return (std::any_of(userPassword.begin(), userPassword.end(), ::isupper));
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,30 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#pragma once
+#include <string>
+#include <algorithm>
+
+enum ErrorCode {
+  Ok = 0,
+  PasswordNeedsAtLeastNineCharacters = 1,
+  PasswordNeedsAtLeastOneNumber = 2,
+  PasswordNeedsAtLeastOneSpecialCharacter = 3,
+  PasswordNeedsAtLeastOneUppercaseLetter = 4,
+  PasswordsDoNotMatch = 5
+};
+
+//basic
+std::string getErrorMessage(enum ErrorCode);
+
+bool doPasswordsMatch(std::string, std::string);
+
+enum ErrorCode checkPasswordRules(std::string);
+
+enum ErrorCode checkPassword(std::string, std::string);
+
+//new, additional task
+bool checkCharacterCount(std::string);
+
+bool checkForNumbers(std::string);
+
+bool checkForSpecialCharacters(std::string);
+
+bool checkForUppercase(std::string);
