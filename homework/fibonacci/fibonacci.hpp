5e91a9bb77246a7ad6baa6b8596b32844541525a
Ivan Solianyk <solivan.707@gmail.com>
2021-06-04 00:55:16 +0200

    grammar fix

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -4,7 +4,7 @@ int fibonacci_iterative(int sequence) {
     if(sequence <= 1)
         return sequence;
     int n2 = 0, n1 = 1, n;
-    for (int i = 2, i <= sequence, i++){
+    for (int i = 2; i <= sequence; i++){
             n = n2 + n1;
             n2 = n1;
             n1 = n;
