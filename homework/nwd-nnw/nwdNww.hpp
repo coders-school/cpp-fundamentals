c53e5250323b08c1b2cd31a8b0776a8fa61dd652
Ivan Solianyk <solivan.707@gmail.com>
2021-06-04 12:11:24 +0200

    NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,24 +1,19 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    int nwd = 1; 
-    for(int i=2; i<=lhs && i <=rhs; i++){
-        while(!(lhs%i) && !(rhs%i)){
-            nwd*=i;
-            lhs/=i;
-            rhs/=i;
-        }
-        while(lhs/i == lhs*i){
-            lhs/=i;
-        }
-        while(rhs/i == rhs*i){
-            rhs/=i;
+    if (lhs < 0) lhs*=-1;
+    if (rhs < 0) rhs*=-1;
+    while(lhs != 0 && rhs != 0){
+        if(lhs > rhs){
+            lhs%=rhs;
+        }else{
+            rhs%=lhs;
         }
     }
-    return nwd;        
+    return lhs + rhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    return -1;        
+         
 }
