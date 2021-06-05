9757a1cdf49ac1e85fad5f5dbb2e81de39c7c0de
Ivan Solianyk <solivan.707@gmail.com>
2021-06-05 18:37:29 +0200

    doPasswordsMatch

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,12 +1,50 @@
 #include "validation.hpp"
 #include <string>
 
+bool doPasswordsMatch(std::string password, std::string repeatedPassword){
+    if (password.compare(repeatedPassword)){
+        return false;
+    }else{
+        return true;
+    }
+}
+
 ErrorCode checkPassword(std::string password, std::string repeatedPassword){
+    if (!(doPasswordsMatch(password, repeatedPassword))){
+        return ErrorCode::PasswordsDoNotMatch;
+    }
     if (password.size() < 9){
         return ErrorCode::PasswordNeedsAtLeastNineCharacters;
     }
+    bool number = false;
+    bool specialCh = false;
+    bool upperCaseCh = false;
+    for(int i = 0; i <= 9 ; i++){
+        for (int ch : password){
+            if (ch == i){
+                number = true;
+            }
+        }
+    }
+    for(int i = 0; i <= 9 ; i++){
+        for (int ch : password){
+            if (ch == i){
+                number = true;
+            }
+        }
+    }
+    for(int i = 65; i <= 90 ; i++){
+        for (int ch : password){
+            if (ch == char(i)){
+                upperCaseCh = true;
+            }
+        }
+    }
+
     return ErrorCode::Ok;
 }
+
+
 
 std::string getErrorMessage(ErrorCode code){
     switch (code){
