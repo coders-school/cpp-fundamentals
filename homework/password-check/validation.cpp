816073db36a472ea43253cf61c0e883a774fc9e6
Adam Wroblewski <a.wroblewski77@gmail.com>
2021-06-06 00:04:07 +0200

    Zadania podstawowe zaimplementowane

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,69 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+
+std::string getErrorMessage(const ErrorCode &e)
+{
+    std::string answer;
+
+    switch (e)
+    {
+    case ErrorCode::Ok:
+        answer = "Ok";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        answer = "Password needs to have at least nine characters";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        answer = "Password needs to have at least one number";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+        answer = "Password needs to have at least one special character";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        answer = "Password needs to have at least one uppercase letter";
+        break;
+    case ErrorCode::PasswordsDoNotMatch:
+        answer = "Passwords do not match";
+        break;
+    default:
+        answer = "Ok";
+        break;
+    }
+    return answer;
+}
+
+bool doPasswordsMatch(const std::string &p1, const std::string &p2)
+{
+    if (p1 == p2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+ErrorCode checkPasswordRules(const std::string p1)
+{
+    int randomNumber = rand() & 4; //generate random number from 0 to 4
+    ErrorCode er = static_cast<ErrorCode>(randomNumber);
+
+    return er;
+}
+
+ErrorCode checkPassword(const std::string &p1, const std::string &p2)
+{
+    ErrorCode answer;
+
+    if (doPasswordsMatch(p1, p2))
+    {
+        answer = checkPasswordRules(p1);
+    }
+    else
+    {
+        answer = ErrorCode::PasswordsDoNotMatch;
+    }
+
+    return answer;
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,24 @@
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+#pragma once
+
+#include <string>
+#include <stdlib.h>
+
+enum class ErrorCode
+{
+    Ok = 0,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+
+std::string getErrorMessage(const ErrorCode &e);
+
+bool doPasswordsMatch(const std::string &p1, const std::string &p2);
+
+ErrorCode checkPasswordRules(const std::string p1);
+
+ErrorCode checkPassword(const std::string &p1, const std::string &p2);
