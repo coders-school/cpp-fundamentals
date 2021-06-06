aa909bfd103831b658be3700eebfe585614b5a75
Bartlomiej P <gitswist@gmail.com>
2021-06-06 18:01:39 +0200

    Fibonaci solved by using vectors

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,23 @@
 #pragma once
+#include <vector>
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_iterative(int sequence) 
+{
+    std::vector<int>fibonacciVec {0 , 1};
+    fibonacciVec.reserve(sequence);
+
+    for(int i = 2; i <= sequence; i++)
+    {
+       //Add the previous 2 numbers
+       fibonacciVec.push_back(fibonacciVec[i - 1] + fibonacciVec[i - 2]);
+    }
+
+    return fibonacciVec[sequence];
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_recursive(int n) 
+{
+    if(n <= 1)
+        return n;
+    return fibonacci_iterative(n-1) + fibonacci_iterative(n-2);
 }
