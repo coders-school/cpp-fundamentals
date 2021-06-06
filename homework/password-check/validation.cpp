9a8f141a070a05d2feb5d4fa4140a470a56960cf
Bartlomiej P <gitswist@gmail.com>
2021-06-06 19:20:14 +0200

    Switch added

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,7 +1,40 @@
+#include <iostream>
+
 #include "validation.hpp"
 
 
 void getErrorMessage(ErrorCode errorType)
 {
-    
+    switch(errorType)
+    {
+        case ErrorCode::Ok:
+        {
+            std::cout<<"The password is OK"<<"\n";
+        }
+        break;
+        
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        {
+            std::cerr<<"Password needs at least nine characters"<<"\n";
+        }
+        break;
+        
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        {
+            std::cerr<<"Password needs at least one number"<<"\n";
+        }
+        break;
+
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        {
+            std::cerr<<"Password needs at least one upper case letter"<<"\n";
+        }       
+        break; 
+
+        case ErrorCode::PasswordsDoNotMatch:
+        {
+            std::cerr<<"Passwords do not match"<<"\n";
+        }       
+        break;
+    }
 }
