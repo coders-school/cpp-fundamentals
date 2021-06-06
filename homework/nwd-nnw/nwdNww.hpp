aa35a76b32ffb04b606947c655d667ca9eb325cc
Bartlomiej P <gitswist@gmail.com>
2021-06-06 12:16:04 +0200

    Solved with Euclidian recursion method

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+int NWD(int a, int b)
+{
+    if(b!=0)
+    	return NWD(b,a%b);
+    else 
+        return std::abs(a);
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int a, int b) 
+{   
+    int var = NWD(a,b);
+    if(var == 0)
+        return 0;
+    else
+        return std::abs((a * b) / NWD(a, b));
 }
