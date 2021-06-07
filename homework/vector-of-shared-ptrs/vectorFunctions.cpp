acb3ba9282144f83b04dac95b681748e5b31bd84
Kruza <palatonista@gmail.com>
2021-06-08 00:06:37 +0200

    Add10 v1 works

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -22,7 +22,10 @@ void print(std::vector<std::shared_ptr<int>> vecIn)
 
 void add10(std::vector<std::shared_ptr<int>> vecIn)
 {
-
+    for(auto &element : vecIn)
+    {
+        *element += 10;
+    }
 }
 
 void sub10(const int *a)
