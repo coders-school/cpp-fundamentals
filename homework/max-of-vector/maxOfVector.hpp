c672ffa017199d04d0fae720c0f4415cac2f9e4b
MalarzDawid <github@dawidmalarz.pl>
2021-06-08 19:39:28 +0200

    maxOfVector done

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,15 @@
 #pragma once
 #include <vector>
+#include <list>
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max_value = vec[0];
+
+    for (const auto& element: vec) {
+        if (element > max_value) {
+            max_value = element;
+        }
+    }
+    return max_value;
 }
