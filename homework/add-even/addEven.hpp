b4560014636e3927dcd639abaa687a0ce46990c9
Piotr Kosc <piotr.kosc@op.pl>
2021-06-08 22:12:00 +0200

    Made addEven function

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,15 @@
 #pragma once
 #include <vector>
+#include <iostream>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int suma = 0;
+    for (auto element: numbers)
+    {
+        if ((element & 1) == 0)   
+        {
+            suma += element;
+        }
+    }
+    return suma;
 }
