0827b6ae277cd67861605a9594a62512133872c8
Marcin Dołowy <81704885+marcin-dolowy@users.noreply.github.com>
2021-06-09 12:08:21 +0200

    Test passed

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+std::vector<std::shared_ptr<int>> generate(int count);
+void print(const std::vector<std::shared_ptr<int>>& vec);
+void add10(const std::vector<std::shared_ptr<int>>& vec);
+void sub10(int* const ptr);
+void sub10(const std::vector<std::shared_ptr<int>>& vec);
