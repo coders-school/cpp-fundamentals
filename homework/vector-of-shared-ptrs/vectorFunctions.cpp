4595db5d593bd3736b3d4a5248c7cdbf3529932b
rajder007 <krzysztof.zaczyk7@gmail.com>
2021-06-09 11:13:40 +0200

    fix

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -21,7 +21,9 @@ void print(const std::vector<std::shared_ptr<int>> vec) {
 
 void add10(std::vector<std::shared_ptr<int>> vec) {
     for(auto& value: vec) {
-        *value += 10;
+        if(value) {
+            *value += 10;
+        }
     }
 }
 
