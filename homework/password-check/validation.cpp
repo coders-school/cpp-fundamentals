2bc86df9af17f5e1677b8f027eb811f7d80880a5
MalarzDawid <github@dawidmalarz.pl>
2021-06-10 06:38:15 +0200

    Implement getErrorMessage

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,20 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+#include "string"
+
+std::string getErrorMessage(ErrorCode errorType) {
+    switch (errorType) {
+        case ErrorCode::Ok:
+            return "Ok";
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+            return "Password needs to have at least nine characters";
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+            return "Password needs to have at least one number";
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+            return "Password needs to have at least one special character";
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+            return "Password needs to have at least one uppercase letter";
+        case ErrorCode::PasswordsDoNotMatch:
+            return "Passwords do not match";
+    };
+    return "None";
+};
