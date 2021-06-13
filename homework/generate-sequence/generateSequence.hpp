8fb00cde9098278589774f4a4860cdb314c22048
Michał Przymusiński <wrzaskun000@gmail.com>
2021-06-13 23:51:25 +0200

    add generate sequence

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,7 +1,17 @@
 #pragma once
 #include <vector>
 
-std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+std::vector<int> generateSequence(int count, int step)
+{
+	// TODO: Implement me :)
+	if (count < 1) {
+		return {};
+	}
+	std::vector<int> result;
+	result.reserve(count);
+	for (int i = 0, value = step; i < count; i++, value += step)
+	{
+		result.push_back(value);
+	}
+	return result;
 }
