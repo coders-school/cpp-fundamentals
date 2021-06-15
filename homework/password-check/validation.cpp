faffacce889a56d5f6edd0341414f59e9d4f87cd
Olaf Kucharski <olaf.kucharski@op.pl>
2021-06-15 12:34:46 -0400

    kolejne

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,62 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode er){
+    switch(er){
+        case Ok: return "Ok"; break;
+        case PasswordNeedsAtLeastNineCharacters: return "Password needs to have at least nine characters"; break;
+        case PasswordNeedsAtLeastOneNumber: return "Password needs to have at least one number"; break;
+        case PasswordNeedsAtLeastOneSpecialCharacter: return "Password needs to have at least one special character"; break;
+        case PasswordNeedsAtLeastOneUppercaseLetter: return "Password needs to have at least one uppercase letter"; break;
+        case PasswordsDoNotMatch: return "Passwords do not match"; break;
+    }
+    return "";
+}
+
+bool doPasswordsMatch(std::string psw1, std::string psw2){
+    if(psw1 == psw2){
+        return true;
+    }
+    return false;
+}
+
+ErrorCode checkPasswordRules(std::string password){
+    srand(time(NULL));
+    if(password.length() < 9){
+        return PasswordNeedsAtLeastNineCharacters;
+    }
+    bool check = false;
+    for(int i=0;i<password.length();i++){
+        if(isdigit(password[i])){
+            check = true;
+        }
+    }
+    if(!check){
+        return PasswordNeedsAtLeastOneNumber;
+    }
+    check = false;
+    for(int i=0;i<password.length();i++){
+        if(password[i] == '!' || password[i] == '@' || password[i] == '#' || password[i] == '$' || password[i] == '%' || password[i] == '^' || password[i] == '&' || password[i] == '*'){
+            check = true;
+        }
+    }
+    if(!check){
+        return PasswordNeedsAtLeastOneSpecialCharacter;
+    }
+    check = false;
+    for(int i=0;i<password.length();i++){
+        if(isupper(password[i])){
+            check = true;
+        }
+    }
+    if(!check){
+        return PasswordNeedsAtLeastOneUppercaseLetter;
+    }
+    return Ok;
+}
+
+ErrorCode checkPassword(std::string psw1, std::string psw2){
+    if(doPasswordsMatch(psw1, psw2)){
+        return checkPasswordRules(psw2);
+    }
+    return PasswordsDoNotMatch;
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,15 @@
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+
+#pragma once
+#include <string>
+
+enum ErrorCode {Ok, PasswordNeedsAtLeastNineCharacters, PasswordNeedsAtLeastOneNumber, PasswordNeedsAtLeastOneSpecialCharacter, PasswordNeedsAtLeastOneUppercaseLetter, PasswordsDoNotMatch};
+
+std::string getErrorMessage(ErrorCode er);
+
+bool doPasswordsMatch(std::string psw1, std::string psw2);
+
+ErrorCode checkPasswordRules(std::string password);
+
+ErrorCode checkPassword(std::string psw1, std::string psw2);
