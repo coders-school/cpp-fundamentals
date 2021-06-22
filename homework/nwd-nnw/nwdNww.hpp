d805aad0abceedcaef55b34e3e5bef767b38836b
krzychtof <kkowalczyk.it@gmail.com>
2021-06-22 22:02:28 +0200

    adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if(rhs != 0){
+        return NWD(rhs, lhs % rhs);
+    }
+    return std::abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if(rhs == 0)
+         return rhs;
+
+    return std::abs(lhs * rhs / NWD(lhs, rhs));
 }
