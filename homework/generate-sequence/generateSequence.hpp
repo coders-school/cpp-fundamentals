407a27d355a234fd1affd1f9af56f29efb3c4bd8
Krzysztof Kowalczyk <57789687+krzychtof@users.noreply.github.com>
2021-06-23 12:48:13 +0200

    adding works

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,7 +1,17 @@
-#pragma once
-#include <vector>
-
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    
+    std::vector<int> vec;
+    if (count > 0) {
+        vec.reserve(count);
+
+        int element = step;
+        vec.push_back(element);
+
+        for (size_t idx = 1; idx < count; ++idx) {
+            element += step;
+            vec.push_back(element);
+        }
+        return vec;
+    }
+    return vec;
 }
