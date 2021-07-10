8f969f2587dc1e93e6d3ea901e3b90753ffb63d8
Oskar Woloch <wymiataka213@gmail.com>
2021-07-10 20:04:50 +0200

    adding works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    if (sequence < 2) {
+        return sequence;
+    }
+
+    int first = 1;
+    int second = 1;
+
+    while (sequence-- > 2) {
+        second += first;
+        first = second - first;
+    }
+
+    return second;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    if (sequence < 2) {
+        return sequence;
+    }
+
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+
+
 }
--- homework/fibonacci/main.cpp
+++ homework/fibonacci/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "fibonacci.hpp"
 
-int main() {
-    std::cout << fibonacci_iterative(10) << "\n";
-    std::cout << fibonacci_recursive(10) << "\n";
+int main()
+{
+     std::cout << fibonacci_iterative(10) << "\n";
+   std::cout << fibonacci_recursive(10) << "\n";
 
     return 0;
 }
