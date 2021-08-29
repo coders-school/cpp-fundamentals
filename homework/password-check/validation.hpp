ace1689d8286ed36135bb5760a4c7d2ba3a26199
Michał Zdziech <m.zdziech82@gmail.com>
2021-08-29 20:30:03 +0200

    Add definition of error codes

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,10 @@
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+enum class ErrorCode {
+  Ok,
+  PasswordNeedsAtLeastNineCharacters,
+  PasswordNeedsAtLeastOneNumber,
+  PasswordNeedsAtLeastOneSpecialCharacter,
+  PasswordNeedsAtLeastOneUppercaseLetter,
+  PasswordsDoNotMatch
+};
