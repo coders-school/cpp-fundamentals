3921fdf1f2639c97ff631e34abb2de59ad70f483
Michał Zdziech <m.zdziech82@gmail.com>
2021-08-29 21:09:02 +0200

    added functionality, did password match

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,20 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+std::string getErrorMessage(ErrorCode err) {
+
+}
+bool doPasswordsMatch(const std::string& pwd1,const std::string& pwd2 ) {
+
+	if(pwd1.compare(pwd2) == 0) {
+		return true;
+	}
+	return false;
+}
+ErrorCode checkPasswordRules(const std::string& pwd) {
+	srand(time(nullptr));
+	auto resultRules = static_cast<ErrorCode>(rand() % 4);
+	return resultRules;
+}
+ErrorCode checkPassword(const std::string& pwd1, const std::string& pwd2 ) {
+
+}
