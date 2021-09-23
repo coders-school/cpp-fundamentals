a5d0cfad21ab272032b679d85571a9c3bd964de7
Daniel <daniel.holowieniec@gmail.com>
2021-09-23 20:38:47 +0200

    finished

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,30 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs==0)
+        return rhs;
+    if(rhs==0)
+        return lhs;
+    if(lhs<0)
+        lhs=-lhs;
+    if(rhs<0)
+        rhs=-rhs;
+    while(lhs!=rhs)
+    {
+        if(lhs>rhs)
+            lhs=lhs-rhs;
+        else
+            rhs=rhs-lhs;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs==0 || rhs==0)
+        return 0;  
+    if(lhs<0)
+        lhs=-lhs;
+    if(rhs<0)
+        rhs=-rhs; 
+    return lhs*rhs/NWD(lhs,rhs);
 }
