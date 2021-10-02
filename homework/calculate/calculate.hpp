a9f949f12b528cc710bb8461d695e5d4f14515b0
Rajzell <janicki.rm@gmail.com>
2021-10-02 11:07:28 +0200

    Calculate finished with a switch command

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,46 @@
 #pragma once
 #include <string>
 
+    enum string_code{
+    	eAdd,
+	eSubstract,
+	eDivide,
+	eMultiply,
+	eOther
+    };
+
+    string_code command_code (const std::string& command){
+    	if(command == "add") return eAdd;
+    	if(command == "subtract") return eSubstract;
+	if(command == "divide") return eDivide;
+	if(command == "multiply") return eMultiply;
+	return eOther;
+    }
+
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    	std::string result{};
+	switch(command_code(command)){
+		case eAdd:
+			result = std::to_string(first + second);
+			break;
+		case eSubstract:
+			result = std::to_string(first - second);
+			break;
+		case eDivide:
+			if (second == 0){
+				result = "Division by 0";
+			}
+			else {
+				result = std::to_string(first/second);
+			}
+			break;
+		case eMultiply:
+			result = std::to_string(first*second);
+			break;
+		default:
+			result = "Invalid data";
+			break;
+	}
+	return result;
 }
