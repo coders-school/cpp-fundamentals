0f4a9c279c66e8c681554dd8d62ebdc618e0be14
Krzysztof Kucharski <krzysztof85@gmail.com>
2021-10-07 12:41:26 +0200

    Code formatting has been improved.

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,33 +1,23 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string &command, int first, int second)
-{
+std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
 
     std::string result;
 
-    if (command == "add")
-    {
+    if (command == "add") {
         result = std::to_string(first + second);
-    }
-    else if (command == "subtract")
-    {
+    } else if (command == "subtract") {
         result = std::to_string(first - second);
-    }
-    else if (command == "multiply")
-    {
+    } else if (command == "multiply") {
         result = std::to_string(first * second);
-    }
-    else if (command == "divide")
-    {
+    } else if (command == "divide") {
         if (second)
             result = std::to_string(first / second);
         else
             result = "Division by 0";
-    }
-    else
-    {
+    } else {
         result = "Invalid data";
     }
 
