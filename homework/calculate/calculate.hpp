e04407c53fc765f6e3394e352108f1ee1eaab3d7
Krzysztof Kucharski <krzysztof85@gmail.com>
2021-10-07 12:54:56 +0200

    Code optimization - no helper variable resoult.

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -4,22 +4,17 @@
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
 
-    std::string result;
-
     if (command == "add") {
-        result = std::to_string(first + second);
+        return std::to_string(first + second);
     } else if (command == "subtract") {
-        result = std::to_string(first - second);
+        return std::to_string(first - second);
     } else if (command == "multiply") {
-        result = std::to_string(first * second);
+        return std::to_string(first * second);
     } else if (command == "divide") {
         if (second)
-            result = std::to_string(first / second);
+            return std::to_string(first / second);
         else
-            result = "Division by 0";
-    } else {
-        result = "Invalid data";
+            return "Division by 0";
     }
-
-    return result;
+    return "Invalid data";
 }
