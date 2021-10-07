0b0ffc02601f7aa941ac2336a36e8254b7a08c43
Krzysztof Kucharski <krzysztof85@gmail.com>
2021-10-07 15:36:58 +0200

    fibonacci_iterative works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,14 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+
+    int fibFirst = 0;
+    int fibSecond = 1;
+    for (int i = 0; i < sequence; i++) {
+        fibSecond += fibFirst;
+        fibFirst = fibSecond - fibFirst;
+    }
+    return fibFirst;
 }
 
 int fibonacci_recursive(int sequence) {
