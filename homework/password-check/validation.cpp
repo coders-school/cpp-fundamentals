9ea9d5ee11a2513617cef32ac532d3410c6b9340
Daniel <daniel.holowieniec@gmail.com>
2021-10-27 13:21:44 +0200

    poczatek

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,64 @@
 #include "validation.hpp"
+#include <ctime> 
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+int checkPassword(std::string first, std::string second)
+{
+    if(doPasswordMatch)
+        return checkPasswordRules(std::string pass);
+    else
+        ErrorCode(5);
+}
+
+std::string getErrorMessage(int code)
+{
+    switch (code)
+    {
+    case 0:
+        std::cout<<"Ok";
+        break;
+    case 1:
+        std::cout<<"Password needs at least nine characters";
+        break;
+    case 2:
+        std::cout<<"Password needs at least one number";
+        break;
+    case 3:
+        std::cout<<"Password needs at least one special character";
+        break;
+    case 4:
+        std::cout<<"Password needs at least one uppercase letter";
+        break;
+    case 5:
+        std::cout<<"Passwords do not match";
+        break;
+    default:
+
+        break;
+    }
+}
+
+int checkPasswordRules(std::string pass)
+{
+    srand(time(NULL));
+    return rand()%5;
+}
+
+int checkPassword(std::string first, std::string second)
+{
+    if(doPasswordMatch)
+        return checkPasswordRules(first);
+    else 
+        return 5;
+}
+
+bool doPasswordMatch(std::string first, std::string second)
+{
+    if(first==second)
+        return true;
+    return false;
+}
+
 // TODO: Put implementations here
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,24 @@
+#pragma once
+#include <string>
+
+enum ErrorCode{
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLEastOneUppercaseLetter,
+    PasswordDoNotMatch
+};
+
+int checkPassword(std::string, std::string);
+
+std::string getErrorMessage(int code);
+
+int checkPasswordRules(std::string pass);
+
+int checkPassword(std::string first, std::string second);
+
+bool doPasswordMatch(std::string, std::string);
+
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
