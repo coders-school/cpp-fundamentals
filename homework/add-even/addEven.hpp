03c1c4971c6d6c7e48c8bd45ef5a2d13fcd55c8e
Rajzell <janicki.rm@gmail.com>
2021-11-06 15:25:10 +0100

    Add Even working

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,19 @@
 #pragma once
 #include <vector>
 
+    bool checkIfEven(int number){
+        return number%2==0;
+    }
+
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
-    return -1;
+
+    int sumOfEven {};
+    for(const int number : numbers){
+        if (checkIfEven(number))
+            sumOfEven += number;
+    }
+
+    return sumOfEven;
 }
