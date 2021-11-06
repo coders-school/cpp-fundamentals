e312e58fd1e2a025773937382de1084da659639b
Rajzell <janicki.rm@gmail.com>
2021-11-06 18:57:43 +0100

    Header file ready, getErrorMessage ready

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,20 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(enum errorCode)    // returns a communicate based on the given error code
+{
+
+}
+
+bool doPasswordsMatch(std::string password_first, std::string password_second) //returns boolean whether two provided password were identical
+{
+
+}
+
+ErrorCode checkPasswordRules(std::string password) //returns a random error code. Takes one password (string)
+{
+
+}
+ErrorCode checkPassword(std::string password_first, std::string password_second) //returns an error code. Takes two passwords.
+{
+
+}
