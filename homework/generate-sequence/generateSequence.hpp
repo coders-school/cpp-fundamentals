60917c46a674b0a2982be20962fecf59bfe694d5
Rajzell <janicki.rm@gmail.com>
2021-11-06 16:00:21 +0100

    generateSequence completed

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -3,5 +3,13 @@
 
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
-    return {};
+
+    if (count > 0){
+        std::vector<int> intVector(count,step);
+        for (int i=0; i<intVector.size(); ++i)
+            intVector[i] = (i+1)*step;
+        return intVector;
+    }
+    else
+        return std::vector<int> {};
 }
