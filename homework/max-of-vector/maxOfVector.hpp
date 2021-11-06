da395a37b05664581db9a5fd0e212f8063513043
Rajzell <janicki.rm@gmail.com>
2021-11-06 15:40:27 +0100

    maxOfVector completed

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <climits>
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    int max = INT_MIN;
+    for(const int number:vec){
+        max = std::max(max, number);
+    }
+    return max;
 }
