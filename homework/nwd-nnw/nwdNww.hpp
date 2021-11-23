cc70dcee3903097530279091312e275b097b0361
AnnGgaa1 <94783777+AnnGgaa1@users.noreply.github.com>
2021-11-23 13:28:42 +0100

    the first attemt in nwwNwd

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,10 +2,17 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
+    while (lhs!=rhs){
+        if(lhs<rhs){
+            rhs=rhs-lhs;
+        }else lhs=lhs-rhs;
+    }
     return -1;
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    int x = lhs*rhs/NWD(lhs,rhs);
+    return x;
+    
 }
