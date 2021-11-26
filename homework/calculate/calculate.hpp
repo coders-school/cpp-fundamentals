66cc81d95693daba2863c9b83484b6ca9daef8fb
Aleksander <aleksander.ramut@wp.pl>
2021-11-26 23:41:01 +0100

    adding works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,6 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     if (command=="add") {return std::to_string(first+second);}
+    else if (command=="subtract") {return std::to_string(first-second);}
     //return "";
 }
