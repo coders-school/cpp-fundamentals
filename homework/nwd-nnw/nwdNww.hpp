d9543a0c26a2a69d40b6ddd0fb2b2fd18da9e9c8
Aleksander <aleksander.ramut@wp.pl>
2021-11-27 19:32:14 +0100

    adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int number;
+    if (lhs>rhs) number=rhs;
+    else number=rhs;
+    while ((lhs%number!=0)||(rhs%number!=0))
+    {
+       number--;
+    }
+    return number;
 }
 
 int NWW(int lhs, int rhs) {
