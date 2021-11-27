456c3ec8e11723207375b7f65db350bc3cd91471
AnnGgaa1 <94783777+AnnGgaa1@users.noreply.github.com>
2021-11-27 19:39:47 +0100

    new implementation

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+   int NWD(int lhs, int rhs) {
+
+    while (lhs!=rhs){
+            if(lhs<rhs){
+                rhs=rhs-lhs;}
+                else {lhs=lhs-rhs;}
+    return lhs;
+        }
+        
+    return 1;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
+   
+    int num = lhs*rhs/NWD(lhs,rhs);
+        return num;
     return -1;
 }
