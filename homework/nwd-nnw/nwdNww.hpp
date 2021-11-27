87759599755770c06010b32a5ce5c22f4ec4dac5
Aleksander <aleksander.ramut@wp.pl>
2021-11-27 19:54:26 +0100

    adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,17 +1,22 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    int number;
-    if (lhs>rhs) number=rhs;
-    else number=rhs;
-    while ((lhs%number!=0)||(rhs%number!=0))
-    {
-       number--;
-    }
-    return number;
+	int rest;
+	if (rhs == 0) return lhs;
+	else if (lhs == 0) return rhs;
+	else {
+		do {
+		rest = lhs % rhs;
+		lhs = rhs;
+		rhs = rest;
+		} while (rest != 0); 
+		return abs(lhs);	
+	}
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+	if (rhs == 0) return rhs;
+	else if (lhs == 0) return lhs;
+	else return abs((lhs * rhs) / NWD(lhs,rhs));
 }
+
