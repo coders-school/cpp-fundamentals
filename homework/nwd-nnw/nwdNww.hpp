de85dbad9427e888148486438a85eed6bebca2bc
AnnGgaa1 <94783777+AnnGgaa1@users.noreply.github.com>
2021-11-28 15:23:34 +0100

    specific conditions
    equal to 0, equal to each other , added myNWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,39 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int abs(int a){
+    if (a >= 0) return a;
+     else return a*(-1);
 }
 
+
+    int NWD(int lhs, int rhs) {
+        int myNWD;
+        lhs = abs(lhs);
+        rhs=abs(rhs);
+
+        if (lhs == 0)
+            myNWD= rhs;
+        if (rhs == 0)
+            myNWD= lhs;
+
+        if (lhs == rhs)
+            myNWD= lhs;
+
+        while( lhs != rhs )
+            if( lhs < rhs ) 
+                rhs = rhs -lhs; 
+            else 
+            lhs = lhs -rhs;
+        myNWD= lhs;
+    
+    return myNWD;
+        }
+
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
+   
+lhs = abs(lhs);
+        rhs=abs(rhs);
+    int num = lhs*rhs/NWD(lhs,rhs);
+        return num;
     return -1;
 }
