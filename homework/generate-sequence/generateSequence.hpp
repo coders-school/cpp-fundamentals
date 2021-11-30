13864a847c6994e7958593cfbd912c009444df97
Aleksander <aleksander.ramut@wp.pl>
2021-11-30 02:57:39 +0100

    adding works

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,12 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    
+    std::vector<int> numbers{};
+    if (count<=0) return numbers;
+    for (int i=0;i<count;++i)
+    {
+        numbers.push_back(step*(i+1));
+    }
+    return numbers;
 }
