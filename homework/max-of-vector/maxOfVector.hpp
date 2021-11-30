6312efcadeee0512000558d7fced6a1f22e9088f
Aleksander <aleksander.ramut@wp.pl>
2021-11-30 02:37:48 +0100

    adding works

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,12 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    
+    int number=vec[0];
+    for (auto element : vec)
+    {
+        if(element>=number) number=element;
+    }
+    
+    return number;
 }
