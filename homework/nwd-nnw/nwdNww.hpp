5d891e4552222297dc8c48a4d29a9950c48a5b24
MarcinH <marcin@LAPTOP-87T8I5GG.localdomain>
2021-12-03 12:04:42 +0100

    ev works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0) return rhs;
+    if (rhs == 0) return lhs;
+    
+    lhs = lhs < 0 ? -1 * lhs : lhs;
+    rhs = rhs < 0 ? -1 * rhs : rhs;
+    
+    while (lhs != rhs){
+        lhs > rhs ? lhs -= rhs : rhs -= lhs;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (!lhs || !rhs) return 0;
+    int numerator = lhs * rhs < 0 ? -1 * lhs * rhs : lhs * rhs;
+    return numerator / NWD(lhs,rhs);
 }
