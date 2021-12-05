1f7543a756705917423f90d5b126475936267d3f
Thomas <93840059+tomyk729@users.noreply.github.com>
2021-12-05 18:16:02 +0100

    fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+     int* a;
+    a = new int[sequence];
+    a[0]=1;
+    a[1]=1;
+    for(size_t i=2;i<sequence ; i++){
+        a[i]=a[i-1]+a[i-2];
+    }
+     return a[sequence-1];
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence==1 || sequence ==2){
+        return 1;
+    } else { 
+        return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
+    }
 }
