a6affd8a857380517b1a2e79a44094091eafdd6c
AnnGgaa1 <94783777+AnnGgaa1@users.noreply.github.com>
2021-12-06 17:31:50 +0100

    changed condition for lhs != rhs

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,20 +1,49 @@
 #pragma once
 
-   int NWD(int lhs, int rhs) {
+ using namespace std;
 
-    while (lhs!=rhs){
-            if(lhs<rhs){
-                rhs=rhs-lhs;}
-                else {lhs=lhs-rhs;}
-    return lhs;
-        }
-        
-    return 1;
+
+int my_abs(int a){
+    if (a >= 0) return a;
+     else return a*(-1);
 }
 
+
+
+int NWD(int lhs, int rhs) {
+        int myNWD;
+        lhs = my_abs(lhs);
+        rhs = my_abs(rhs);
+
+
+if (lhs == 0 && rhs == 0) {myNWD = lhs; };
+
+ if (lhs == 0){myNWD= rhs;}
+    else if (rhs == 0) {myNWD= lhs;};
+
+ if (lhs != 0 && rhs != 0)
+    {
+        while (lhs != rhs)
+        {
+            if (rhs > lhs){rhs -= lhs;}
+            else
+            {lhs -= rhs;}
+        }
+        myNWD=lhs;
+    };
+    return myNWD;
+}
+
+
 int NWW(int lhs, int rhs) {
-   
-    int num = lhs*rhs/NWD(lhs,rhs);
-        return num;
-    return -1;
+int num;
+lhs = my_abs(lhs);
+rhs = my_abs(rhs);
+   if (NWD(lhs,rhs)!=0){
+
+    num = (lhs*rhs)/NWD(lhs,rhs);
+       } else 
+       num=0;
+       
+       return num; 
 }
