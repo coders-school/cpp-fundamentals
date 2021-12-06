539d988e04361d62cb7d41bc3f71dc2b869c55fb
Thomas <93840059+tomyk729@users.noreply.github.com>
2021-12-06 18:43:53 +0100

    NWD NWW

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,41 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+   {
+	int pom;
+
+  	while(b!=0)
+	{
+    	pom = b;
+    	b = a%b;
+    	a = pom;  
+  	}
+	if (a<0){
+		a*=-1;
+	}
+    return a;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int pom;
+	int c=a, d=b;
+	int e;
+  	while(b!=0)
+	{
+    	pom = b;
+    	b = a%b;
+    	a = pom;  
+  	}
+	if (a<0){
+		a*=-1;
+	}
+	if(a==0){
+		return 0;
+	}else {
+	e=(c/a) * d;
+	if (e<0){
+		e*=-1;
+	}
+	return e;	
+	}
 }
