415bfc2ce9725550107b31eb345b4a9fd0b43464
Thomas <93840059+tomyk729@users.noreply.github.com>
2021-12-08 23:40:41 +0100

    Update maxOfVector.hpp

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,11 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+  int hold = 0;
+    for (size_t i=0; i<vec.size();i++){
+            if(hold<vec[i]){
+                hold=vec[i];
+            }
+        }
+    return hold;
 }
