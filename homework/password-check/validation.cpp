f283220ab86680db3ab7947f8f3192bc81c5385e
Aleksander <aleksander.ramut@wp.pl>
2021-12-08 23:22:08 +0100

    adding works

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,40 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+ErrorCode checkPassword(std::string password, std::string repeatedPassword)
+{
+    if (doPasswordsMatch(password, repeatedPassword))
+    {
+        return checkPasswordRules(password);
+    }
+    else return ErrorCode::PasswordsDoNotMatch;
+}
+
+bool doPasswordsMatch(std::string password, std::string repeatedPassword)
+{
+    if(password==repeatedPassword) return true;
+    else return false;
+}
+
+ErrorCode checkPasswordRules(std::string password)
+{
+    srand(time(NULL));
+    int number=rand()%5;
+    switch(number)
+    {
+        case 0: return ErrorCode::Ok; break;
+        case 1: return ErrorCode::PasswordNeedsAtLeastNineCharacters; break;
+        case 2: return ErrorCode::PasswordNeedsAtLeastOneNumber; break;
+        case 3: return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter; break;
+        case 4: return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter; break;
+    }
+}
+
+std::string getErrorMessage(ErrorCode result)
+{
+    if(result==ErrorCode::Ok) return "Ok";
+    if(result==ErrorCode::PasswordNeedsAtLeastNineCharacters) return "PasswordNeedsAtLeastNineCharacters";
+    if(result==ErrorCode::PasswordNeedsAtLeastOneNumber) return "PasswordNeedAtLeastOneNumber";
+    if(result==ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter) return "PasswordNeedsAtLeastOneSpecialCharacter";
+    if(result==ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter) return "PasswordNeedsAtLeastOneUppercaseLetter";
+    if(result==ErrorCode::PasswordsDoNotMatch) return "PasswordDoNotMatch";
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,17 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#include <iostream>
+#include <ctime>
+
+enum class ErrorCode
+{
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+
+ErrorCode checkPassword(std::string password, std::string repeatedPassword);
+bool doPasswordsMatch(std::string password, std::string repeatedPassword);
+ErrorCode checkPasswordRules(std::string password);
+std::string getErrorMessage(ErrorCode result);
