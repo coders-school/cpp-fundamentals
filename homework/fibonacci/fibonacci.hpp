b58678edb7aa30180e5c1cb21b46dffc73f7c16f
wybitny-szkodnik <95853360+wybitny-szkodnik@users.noreply.github.com>
2021-12-09 11:02:24 +0100

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,10 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    return 1;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    return 2;
 }
