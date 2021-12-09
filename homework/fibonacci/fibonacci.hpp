a9a9890bdd907ac2d19f1cc24e9d4cccfdd264b0
Daniel-Set <94791227+Daniel-Set@users.noreply.github.com>
2021-12-09 16:02:08 +0100

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int x {0}, y {1}, z{0};
+    for(int i=0; i<sequence; ++i){
+            z = x + y;
+            x = y;
+            y = z;
+        };
+    return x;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence == 0)
+     {return 0;}
+     else if(sequence == 1 || sequence == 2)
+     {return 1;}
+     return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
 }
