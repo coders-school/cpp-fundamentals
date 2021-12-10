cb53613b00c917c42309327ceded9bcb17c3401b
Jakub Dworzański <dworzanski.jakub@gmail.com>
2021-12-11 00:27:11 +0100

    Add recursive fibonacci

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -6,6 +6,10 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 1) {
+        return sequence;
+    }
+    else {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
 }
