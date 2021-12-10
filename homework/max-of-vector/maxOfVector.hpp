3a82f4c9ae51fd5501038e85f9d494f1c6654dcb
Daniel-Set <94791227+Daniel-Set@users.noreply.github.com>
2021-12-10 19:53:35 +0100

    Update maxOfVector.hpp

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <limits>
 
-int maxOfVector( std::vector<int>& vec) {
+int maxOfVector(std::vector<int>& vec) {
     std::sort(v.begin(), v.end());
     return v.back();
 }
