a6ec2b631d493a74f746fc292c157e702cdf4064
Jakub Dworzański <dworzanski.jakub@gmail.com>
2021-12-11 00:08:00 +0100

    Add subtraction

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,6 +5,8 @@
 std::string calculate(const std::string& command, int first, int second) {
     if (command == "add") {
         return std::to_string(first + second);
+    } else if (command == "subtract") {
+        return std::to_string(first - second);
     }
     return "";
 }
