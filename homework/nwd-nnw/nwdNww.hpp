90beb93a1f5c6732a3b873bea529d3b686d47a81
Piotrek <barabasz.piotrek@gmail.com>
2021-12-11 02:40:15 +0100

    adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,10 +2,19 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+	while (lhs != rhs) {
+		if (lhs < rhs) {
+			rhs = rhs - lhs;
+		}
+		else { 
+			lhs = lhs - rhs;
+		}
+	}
+	return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+	// TODO: Implement me :)
+	int result = (lhs * rhs) / NWD(lhs, rhs);
+	return result;
 }
