9b23858187019d0931ebb7d55605327bc0107c1e
Daniel-Set <94791227+Daniel-Set@users.noreply.github.com>
2021-12-11 11:43:53 +0100

    Update generateSequence.hpp

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,10 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> vec;
+    for(int i = 0; i < count; ++i){
+        int el = i * step + step;
+        vec.emplace_back(el);
+    }
+    return vec;
 }
