c096a262c5204c2d938b10469ce44c7aa451afb1
Bartiszny <29127947+Bartiszny@users.noreply.github.com>
2021-12-12 18:36:18 +0100

    My implementation

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,27 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+   if (sequence == 0)
+		return 0;
+	else if (sequence == 1)
+		return 1;
+	else {
+		int temp[sequence];
+		temp[0] = 0;
+		temp[1] = 1;
+		for (short i = 2; i <= sequence ; i++){
+			temp[i] = temp[i-1] + temp[i-2];
+		}
+		return temp[sequence];
+	}
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0)
+		return 0;
+	else if (sequence == 1)
+		return 1;
+	else {
+		return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+	}
 }
