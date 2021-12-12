3e5ce10db88013841e8fa20974ef9b0c25a9a267
Bartiszny <29127947+Bartiszny@users.noreply.github.com>
2021-12-12 18:32:29 +0100

    Fix tests

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,7 +2,7 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-	std::string result{"Division by 0"};
+	std::string result{"Invalid Data"};
 	if(command == "add"){
 		result = std::to_string(first + second);
 	} else if(command == "subtract") {
@@ -12,7 +12,7 @@ std::string calculate(const std::string& command, int first, int second) {
 			} else if(command == "divide") {
 					if (second == 0){
 						//std::cout<<"No division by 0!";
-						return result;
+						return "Division by 0";
 					} else {
 						result = std::to_string(first / second);
 					} 
