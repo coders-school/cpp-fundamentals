c54ebbf79ed48656c239d50e7794c41b3af366e9
MarcinH <marcin@DESKTOP-SVODHPV.localdomain>
2021-12-12 18:50:03 +0100

    getErrorMessage() -> if into switch

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -2,22 +2,18 @@
 // TODO: Put implementations here
 
 std::string getErrorMessage(ErrorCode error){
-    if (error == ErrorCode::Ok){
-        return "OK";
-    }
-    if (error == ErrorCode::PasswordNeedsAtLeastNineCharacters){
-        return "Password needs to have at least nine characters";
-    }
-    if (error == ErrorCode::PasswordNeedsAtLeastOneNumber){
-        return "Password needs to have at least one number";
-    }
-    if (error == ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter){
-        return "Password needs to have at least one special character";
-    }
-    if (error == ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter){
-        return "Password needs to have at least one uppercase letter";
-    }
-    if (error == ErrorCode::PasswordsDoNotMatch){
-        return "Passwords do not match";
+    switch(error){
+        case ErrorCode::Ok:
+            return "Ok";
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+            return "Password needs to have at least nine characters";
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+            return "Password needs to have at least one number";
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+            return "Password needs to have at least one special character";
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+            return "Password needs to have at least one uppercase letter";
+        case ErrorCode::PasswordsDoNotMatch:
+            return "Passwords do not match";
     }
 }
