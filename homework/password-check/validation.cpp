9977d0cd80973a152e3ae458dbd7c343b176160c
MarcinH <marcin@DESKTOP-SVODHPV.localdomain>
2021-12-12 18:42:37 +0100

    enum class, getErrorMessage() added

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,23 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode error){
+    if (error == ErrorCode::Ok){
+        return "OK";
+    }
+    if (error == ErrorCode::PasswordNeedsAtLeastNineCharacters){
+        return "Password needs to have at least nine characters";
+    }
+    if (error == ErrorCode::PasswordNeedsAtLeastOneNumber){
+        return "Password needs to have at least one number";
+    }
+    if (error == ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter){
+        return "Password needs to have at least one special character";
+    }
+    if (error == ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter){
+        return "Password needs to have at least one uppercase letter";
+    }
+    if (error == ErrorCode::PasswordsDoNotMatch){
+        return "Passwords do not match";
+    }
+}
