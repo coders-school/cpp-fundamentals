683106e20f6a5213ff677b2a93c594d2ba9ad2fb
Sss49 <84204929+Sss49@users.noreply.github.com>
2021-12-15 20:51:17 +0100

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -4,10 +4,10 @@
 std::string calculate(const std::string& command, int first, int second) {
   
 
-	if (command == "add") {std::cout<<"result: "<< first + second; }
-	else if (command == "subtract") { std::cout<<"\nresult: "<< first - second; }
-	else if (command == "multiply") { std::cout<<"\nresult: "<<first * second; }
-	else if (command == "divide" && first != 0 && second != 0  ) { std::cout<<"\nresult: "<<first / second; }
+	if (command == "add") { first + second; }
+	else if (command == "subtract") {  first - second; }
+	else if (command == "multiply") { first * second; }
+	else if (command == "divide" && first != 0 && second != 0  ) { first / second; }
 	else if (command == "divide") {std::cout<<"Division by 0"; }
 	else { std::cout << "Invalid data"; }
 	
