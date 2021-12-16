353b9cfd166e8b1f15bbe4170e06f95853ad62bf
YoSiem <95193135+YoSiem@users.noreply.github.com>
2021-12-16 01:09:03 +0100

    Update 03-modifyPointers.cpp
    Im not sure if its correct, plz tell me if no

--- 02-types/tasks/03-modifyPointers.cpp
+++ 02-types/tasks/03-modifyPointers.cpp
@@ -4,6 +4,16 @@
 // foo() should modify value under passed pointer to 10
 // bar() should modify value under passed pointer to 20
 // Can we have a pointer to const or a const pointer?
+        // I think so... 
+
+void foo(int * const number){
+    *number = 10;
+}
+
+void bar(int *number){
+    *number = 20;
+}
+
 
 int main() {
     int number = 5;
