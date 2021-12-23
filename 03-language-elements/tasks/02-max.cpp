6f549ebcb527adb8a249b7639db2a09982db656b
Maciej Białczak <m.bialczak@protonmail.com>
2021-12-23 10:58:07 +0100

    02-max.cpp patch suggestion (#767)
    Added additional checks into main function to cover all the possibilities of 3 numbers sent to a function.

--- 03-language-elements/tasks/02-max.cpp
+++ 03-language-elements/tasks/02-max.cpp
@@ -3,9 +3,12 @@
 // Write your function here
 
 int main() {
-    std::cout << "max (1, 2, 3): " << max(1, 2, 3) << "\n";
-    std::cout << "max (2, 3, 1): " << max(2, 3, 1) << "\n";
-    std::cout << "max (3, 2, 1): " << max(3, 2, 1) << "\n";
+    std::cout << "max (1, 2, 3): " << max (1, 2, 3) << "\n";
+    std::cout << "max (1, 3, 2): " << max (1, 3, 2) << "\n";
+    std::cout << "max (2, 3, 1): " << max (2, 3, 1) << "\n";
+    std::cout << "max (2, 1, 3): " << max (2, 1, 3) << "\n";
+    std::cout << "max (3, 2, 1): " << max (3, 2, 1) << "\n";
+    std::cout << "max (3, 1, 2): " << max (3, 1, 2) << "\n";
 
     return 0;
 }
