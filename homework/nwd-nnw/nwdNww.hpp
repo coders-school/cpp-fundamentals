c801dc69b43e34ec835d25c6e5c7c36f43311094
mBialczak <82645568+mBialczak@users.noreply.github.com>
2021-12-24 17:44:26 +0100

    NWD working

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+#include <cmath>
+
+int NWD (int lhs, int rhs)
+{
+    // resolve problem of negative arguments
+    lhs = abs (lhs);
+    rhs = abs (rhs);
+
+    while (rhs != 0) {
+        int rest = lhs % rhs;
+        lhs = rhs;
+        rhs = rest;
+    }
+
+    return lhs;
 }
 
-int NWW(int lhs, int rhs) {
+int NWW (int lhs, int rhs)
+{
     // TODO: Implement me :)
     return -1;
 }
