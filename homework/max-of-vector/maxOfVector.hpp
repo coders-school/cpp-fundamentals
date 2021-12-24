809e28d26f5ec054316f781bd440ae237b8832d0
Jakub Dworzański <dworzanski.jakub@gmail.com>
2021-12-24 16:55:34 +0100

    Solve maxOfVector task

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,12 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    auto i = vec.begin();
+    int max_el = *i;
+    while (++i != vec.end()) {
+        if (*i > max_el) {
+            max_el = *i;
+        }
+    }
+    return max_el;
 }
