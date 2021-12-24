f98ce9889d3d7df4b386557aac718c5038014b50
mBialczak <82645568+mBialczak@users.noreply.github.com>
2021-12-24 11:20:52 +0100

    All calculate tests passed

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -12,6 +12,12 @@ std::string calculate (const std::string& command, int first, int second)
     else if (command == "multiply") {
         return std::to_string (first * second);
     }
+    else if (command == "divide") {
+        if (second == 0) {
+            return "Division by 0";
+        }
+        return std::to_string (first / second);
+    }
 
-    return "Invalid value";
+    return "Invalid data";
 }
