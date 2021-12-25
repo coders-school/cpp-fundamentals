2acca3a5868e0350ed8f6a66d1735c6d228cfcf3
mBialczak <82645568+mBialczak@users.noreply.github.com>
2021-12-25 14:02:20 +0100

    AddEven implemented and tested OK

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,15 @@
 #pragma once
+#include <algorithm>
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+int addEven (const std::vector<int>& numbers)
+{
+    int sum { 0 };
+    for (const auto& el : numbers) {
+        if (el % 2 == 0) {
+            sum += el;
+        }
+    }
+
+    return sum;
 }
