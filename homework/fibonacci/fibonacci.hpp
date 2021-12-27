1e1aa0eb502114d20f49cf3297151565a23a6ddc
MPelikanow <michal.p574@gmail.com>
2021-12-27 16:46:04 +0100

    added iterative and recursive ways of calculating the Fibonacci sequence

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,38 @@
 #pragma once
 
+
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int i = 0;
+    int result = 0;
+    int new_result = 0;
+    int old_result = 0;
+    while (i <= sequence) {
+        if (i == 0) {
+            result = 0;
+            i++;
+        } else if (i == 1) {
+            old_result = result;
+            result = 1;
+            i++;
+        } else {
+            new_result = result + old_result;
+            old_result = result;
+            result = new_result;
+            i++;
+        }
+    }
+    return result;
 }
 
+
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int result = 0;
+    if (sequence == 0) {
+        result = 0;
+    } else if (sequence == 1) {
+        result = 1;
+    } else {
+        result = fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
+    }
+    return result;
 }
