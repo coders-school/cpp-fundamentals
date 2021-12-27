fa6b3804ad919a330b8e7f172d3028d6535662e2
MPelikanow <michal.p574@gmail.com>
2021-12-27 14:34:05 +0100

    added remaining functions

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,11 +1,43 @@
 #pragma once
+#include <iostream>
 #include <string>
+#include <map>
+#include <iomanip>
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
     std::string result;
-    if (command == "add") {
-        result = std::to_string(first + second);
+    std::map<std::string, int> math_operations {
+        {"add", 1},
+        {"subtract", 2},
+        {"multiply", 3},
+        {"divide", 4},
+    };
+    switch (math_operations[command]) {
+        case (1):
+            result = std::to_string(first + second);
+            break;
+        case (2): {
+            result = std::to_string(first - second);
+            break;
+        }
+        case (3): {
+            result = std::to_string(first * second);
+            break;
+        }
+        case (4): {
+            if (second == 0) {
+                result = "Division by 0";
+            } else {
+                result = std::to_string(first / second);
+            }
+            break;
+        }
+        default: {
+            result = "Invalid data";
+            break;
+        }
+
     }
     return result;
 }
