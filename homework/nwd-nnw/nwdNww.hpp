5a2ecc6bf3682fa7acfc8b71c25e426c79a13730
MPelikanow <michal.p574@gmail.com>
2021-12-28 12:08:16 +0100

    lcmGcd

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,33 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int r_lhs, int r_rhs) {
+    int aux;
+    int lhs = abs(r_lhs);
+	int rhs = abs(r_rhs);
+    
+    if (lhs==0 or rhs==0) {
+        return lhs + rhs;
+    }
+
+	while(rhs!=0)
+    {
+		aux = rhs;
+		rhs = lhs%rhs;
+		lhs = aux;	
+	}
+    return lhs;
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int r_lhs, int r_rhs) {
+	int result;
+	int lhs = abs(r_lhs);
+	int rhs = abs(r_rhs);
+
+	if (lhs == 0 or rhs == 0) {
+		result = 0;
+	}
+	else {
+		result = (lhs * rhs) / NWD(lhs, rhs);
+	}
+	return result;
 }
