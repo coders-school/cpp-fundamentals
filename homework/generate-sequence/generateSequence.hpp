598e8b03185ff7e03c9c6ca302ca5c6d4f5584eb
MPelikanow <michal.p574@gmail.com>
2021-12-28 13:35:00 +0100

    genSeq

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,7 +1,14 @@
 #pragma once
 #include <vector>
+#include <iostream>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> seq_vec {};
+    if (count > 0) {
+        for (int i=0; i<count; i++) {
+            int el = step * (i+1);
+            seq_vec.push_back(el);
+        }
+    }
+    return seq_vec;
 }
