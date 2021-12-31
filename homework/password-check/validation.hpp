8ac40be1b4549866bbeb6bde8f01ccf0655fac06
MPelikanow <michal.p574@gmail.com>
2021-12-31 01:01:09 +0100

    added validation

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,96 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#pragma once
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>   
+#include <algorithm>
+#include <cctype>
+
+enum class ErrorCode
+{
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch,
+};
+
+
+const bool doPasswordsMatch(std::string setPassword, std::string repeatedPassword) {
+    if (setPassword == repeatedPassword) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// const ErrorCode checkPasswordRules(std::string password) {
+//     int random_error;
+//     srand (time(NULL));
+//     random_error = rand() % 5 + 1;
+//     return static_cast<ErrorCode>(random_error);
+// }
+
+const ErrorCode checkPasswordRules(std::string password) {
+    if (password.size()<9){
+        return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+    } else if (std::count_if(password.cbegin(), password.cend(), [](unsigned char c){ return std::isdigit(c); })<1){
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    } else if (std::count_if(password.cbegin(), password.cend(), [](unsigned char c){ return std::ispunct(c); })<1){
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    } else if (std::count_if(password.cbegin(), password.cend(), [](unsigned char c){ return std::isupper(c); })<1){
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    } else {
+        return ErrorCode::Ok;
+    }
+}
+
+const ErrorCode checkPassword(std::string setPassword, std::string repeatedPassword) {
+    bool password_match = doPasswordsMatch(setPassword, repeatedPassword);
+    if (password_match) {
+        return checkPasswordRules(setPassword);
+    } else {
+        return ErrorCode::PasswordsDoNotMatch;
+    }
+}
+
+const std::string getErrorMessage(enum ErrorCode errorCode)
+{
+    std::string result;
+    switch (errorCode)
+    {
+    case ErrorCode::Ok:
+    {
+        return result = "Ok";
+        break;
+    }
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+    {
+        return result = "Password needs to have at least nine characters";
+        break;
+    }
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
+    {
+        return result = "Password needs to have at least one number";
+        break;
+    }
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+    {
+        return result = "Password needs to have at least one special character";
+        break;
+    }
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+    {
+        return result = "Password needs to have at least one uppercase letter";
+        break;
+    }
+    case ErrorCode::PasswordsDoNotMatch:
+    {
+        return result = "Passwords do not match";
+        break;
+    }
+    }
+    return 0;
+}
