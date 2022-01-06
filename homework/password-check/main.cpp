74577b1e21c4f013eb6410f7fc82bc098dbd9bb9
MPelikanow <michal.p574@gmail.com>
2022-01-06 14:13:13 +0100

    added main

--- homework/password-check/main.cpp
+++ homework/password-check/main.cpp
@@ -6,9 +6,13 @@ int main() {
     std::string password;
     std::string repeatedPassword;
     std::cout << "Set new password: ";
-    std::cin >> password;
+    if (std::cin.peek() != '\n')
+        std::cin >> password;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::cout << "Repeat password: ";
-    std::cin >> repeatedPassword;
+    if (std::cin.peek() != '\n')
+        std::cin >> repeatedPassword;;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     auto result = checkPassword(password, repeatedPassword);
     std::cout << getErrorMessage(result) << '\n';
 
