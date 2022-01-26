85ead0faa12f039a966f149d36fcd2fa2700350a
Thomas <93840059+tomyk729@users.noreply.github.com>
2022-01-26 21:36:16 +0100

    Update validation.cpp

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,45 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+#include <bits/stdc++.h>
+std::string getErrorMessage(int a){
+    std::string feedback = "?";
+    switch(a){
+        case 1:{feedback = "Ok";
+        break;}
+        case 2:{feedback = "PasswordNeedsAtLeastNineCharacters";
+        break;}
+        case 3:{feedback = "PasswordNeedsAtLeastOneNumber";
+        break;}
+        case 4:{feedback = "PasswordNeedsAtLeastOneSpecialCharacter";
+        break;}
+        case 5:{feedback = "PasswordNeedsAtLeastOneUppercaseLetter";
+        break;}
+        case 6:{feedback = "PasswordsDoNotMatch";
+        break;}
+    }
+    return feedback;
+}
+bool doPasswordsMatch(std::string pass1, std::string pass2){
+    bool check = false;
+    if(pass1==pass2){
+        check = true;
+    }
+    return check;
+}
+int checkPasswordRules(std::string pass1){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(Ok,PasswordNeedsAtLeastOneUppercaseLetter);
+    int a = dist(gen);
+    return a;
+}
+int checkPassword(std::string pass1, std::string pass2){
+    int back = 0;
+    bool check = false;
+    check = doPasswordsMatch(pass1, pass2);
+    if(check){
+        back = checkPasswordRules(pass1);
+    } else {
+        back = PasswordsDoNotMatch;
+    }
+    return back;
+}
