c6d65ab2208a382b61dbdde05f661530dd3394df
kogrodnik <94931123+kogrodnik@users.noreply.github.com>
2022-03-17 18:38:11 +0100

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -13,51 +13,12 @@
 };
 
 //konwersja enum na string 
-std::string getErrorMessage(int err){
-switch (err) {
-    case 0:
-    return"Ok";
-    break;
-    case 1:
-    return "Password needs to have at least nine characters";
-    break;
-    case 2:
-    return "Password needs to have at least one number";
-    break;
-    case 3:
-    return "Password needs to have at least one special character";
-    break;
-    case 4:
-    return  "Password needs to have at least one uppercase letter";
-    break;
-    case 5:
-    return "Passwords do not match";
-    break;
-}
-return "";
-}
+std::string getErrorMessage(int err);
 
 //sprawdzenie poprawnosci hasel
-bool doPasswordMatch (std::string wordOne, std::string wordTwo){
-    if(wordOne == wordTwo){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
+bool doPasswordMatch (std::string wordOne, std::string wordTwo);
 
-int checkPasswordRules (std::string pass){
- return ErrorCode::ok;      
-}
+int checkPasswordRules (std::string pass);
 
 //sprawdzenie hasla 
-int checkPassword (std::string passFirst, std::string passSecond){
-    auto check = doPasswordMatch(passFirst, passSecond);
-    if (check == true){
-        return ErrorCode::PasswordsDoNotMatch;
-    }
-    else {
-        return checkPasswordRules(passFirst);
-    }
-} 
+int checkPassword (std::string passFirst, std::string passSecond);
