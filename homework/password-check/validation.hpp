3ca23f1b9bdd04fd674de10085666b95080af494
kogrodnik <94931123+kogrodnik@users.noreply.github.com>
2022-03-17 18:24:41 +0100

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,63 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#pragma once
+#include <iostream>
+#include <string>
+
+//kody bledow
+    enum ErrorCode {
+    ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+
+//konwersja enum na string 
+std::string getErrorMessage(int err){
+switch (err) {
+    case 0:
+    return"Ok";
+    break;
+    case 1:
+    return "Password needs to have at least nine characters";
+    break;
+    case 2:
+    return "Password needs to have at least one number";
+    break;
+    case 3:
+    return "Password needs to have at least one special character";
+    break;
+    case 4:
+    return  "Password needs to have at least one uppercase letter";
+    break;
+    case 5:
+    return "Passwords do not match";
+    break;
+}
+return "";
+}
+
+//sprawdzenie poprawnosci hasel
+bool doPasswordMatch (std::string wordOne, std::string wordTwo){
+    if(wordOne == wordTwo){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int checkPasswordRules (std::string pass){
+ return ErrorCode::ok;      
+}
+
+//sprawdzenie hasla 
+int checkPassword (std::string passFirst, std::string passSecond){
+    auto check = doPasswordMatch(passFirst, passSecond);
+    if (check == true){
+        return ErrorCode::PasswordsDoNotMatch;
+    }
+    else {
+        return checkPasswordRules(passFirst);
+    }
+} 
