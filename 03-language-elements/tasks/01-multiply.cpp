38fd08460198afd7ea1569d5641f6fa0270e2f30
Sl0mek <92469177+Sl0mek@users.noreply.github.com>
2022-03-22 22:00:39 +0100

    Update 01-multiply.cpp

--- 03-language-elements/tasks/01-multiply.cpp
+++ 03-language-elements/tasks/01-multiply.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 // Write missing function here
+int multiply(const int&, const int&);
 
 int main() {
     std::cout << "4 * 5: " << multiply(4, 5) << "\n";
@@ -8,4 +9,9 @@ int main() {
     std::cout << "-5 * 5: " << multiply(-5, 5) << "\n";
 
     return 0;
+}
+
+int multiply(const int& a, const int& b)
+{
+    return a * b;
 }
