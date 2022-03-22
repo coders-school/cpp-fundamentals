ef677adc1cf15890dfe7d1617c9ca01a228e1f6c
Sl0mek <92469177+Sl0mek@users.noreply.github.com>
2022-03-22 22:04:37 +0100

    Update 03-printString.cpp

--- 03-language-elements/tasks/03-printString.cpp
+++ 03-language-elements/tasks/03-printString.cpp
@@ -2,6 +2,8 @@
 
 // Write your function here
 
+void printString(const std::string&, const int&);
+
 int main() {
     printString("Hello", 5);
     std::cout << "\n";
@@ -13,4 +15,12 @@ int main() {
     std::cout << "\n";
 
     return 0;
+}
+
+void printString(const std::string& str, const int& howManyTimes)
+{
+  for(int i = 0; i < howManyTimes; i++)
+  {
+     std::cout << str << std::endl;
+  }
 }
