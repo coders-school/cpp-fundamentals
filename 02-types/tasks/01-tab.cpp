a96c40bfc5fbbd048cb630083633945892c4fb00
Sl0mek <92469177+Sl0mek@users.noreply.github.com>
2022-03-22 21:52:54 +0100

    Update 01-tab.cpp

--- 02-types/tasks/01-tab.cpp
+++ 02-types/tasks/01-tab.cpp
@@ -6,7 +6,14 @@ int main() {
     int tab[tab_size];
 
     for (size_t i = 0; i < tab_size; ++i) {
-        tab[i] = i;
+        if(i == 0)
+        {
+          tab[i] = 1;
+        }
+        else
+        {
+          tab[i] = tab[i - 1] + 2;
+        }
     }
 
     for (size_t i = 0; i < tab_size; ++i) {
