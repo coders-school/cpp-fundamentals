eb72523ec22b8b2593b2fc6ff57fde686d41c85b
Sl0mek <92469177+Sl0mek@users.noreply.github.com>
2022-03-22 23:24:52 +0100

    Update 04-sharedPtr.cpp

--- 02-types/tasks/04-sharedPtr.cpp
+++ 02-types/tasks/04-sharedPtr.cpp
@@ -6,11 +6,23 @@
 // It should also display the value of this int and the number of how many pointers are pointing to it - use `use_count()`.
 // Display the same information in main() before and after calling foo()
 
+void foo(std::shared_ptr<int>);
+
 int main() {
     std::shared_ptr<int> number = std::make_shared<int>(10);
     // display the value under number pointer and use_count() of it
+    std::cout << "Value of pointer (before function): " << *number << std::endl;
+    std::cout << "Number of pointers per int object (before function): " << number.use_count() << std::endl << std::endl;
     foo(number);
     // display the value under number pointer and use_count() of it
-
+    std::cout << "Value of pointer (after function): " << *number << std::endl;
+    std::cout << "Number of pointers per int object (after function): " << number.use_count() << std::endl << std::endl;
     return 0;
+}
+
+void foo(std::shared_ptr<int> number)
+{
+    *number = 20;
+    std::cout << "Value of pointer (in function): " << *number << std::endl;
+    std::cout << "Number of pointers per int object (in function): " << number.use_count() << std::endl << std::endl;
 }
