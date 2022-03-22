a823e6cd2fb22ea1731edb45909e0581bccd5077
Sl0mek <92469177+Sl0mek@users.noreply.github.com>
2022-03-22 21:59:16 +0100

    Update 05-calculateProduct.cpp

--- 02-types/tasks/05-calculateProduct.cpp
+++ 02-types/tasks/05-calculateProduct.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <memory>
 
 // TODO: Implement calculateProduct()
 // It should take 2 int values and return their product as a shared_ptr.
 // Additionally, check how many owners are there.
 
+std::shared_ptr<int> calculateProduct(int, int);
+
 int main() {
     auto number = calculateProduct(10, 20);
-    std::cout << "num: " << *number << " | owners: " << number.use_count() << "\n";
+    std::cout << "num: " << *number << " | owners after function: " << number.use_count() << "\n";
 
     return 0;
+}
+std::shared_ptr<int> calculateProduct(int a, int b)
+{
+    auto ptr = std::make_shared<int>(a * b);
+    std::cout << "num: " << *ptr << " | owners in function: " << ptr.use_count() << "\n";
+    return ptr;
 }
