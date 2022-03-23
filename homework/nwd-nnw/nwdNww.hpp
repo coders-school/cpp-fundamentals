930bfd82eb975fb9206c2025acc2070b1aea0f4a
Sl0mek <andrzejslomkapraca@gmail.com>
2022-03-23 21:34:08 +0100

    Implementation of the nwd_nnw function

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs) 
+{
+    if (lhs == 0 && rhs == 0) return 0;
+    if (lhs < 0 ) lhs *= -1;
+    if (rhs < 0 ) rhs *= -1;
+    int tmp;
+    while (rhs != 0)
+    {
+        tmp = rhs;
+        rhs = lhs % rhs;
+        lhs = tmp;
+    }
+    return lhs;
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int lhs, int rhs) 
+{
+    if (lhs == 0 && rhs == 0) return 0;
+    if (lhs < 0 ) lhs *= -1;
+    if (rhs < 0 ) rhs *= -1;
+    return lhs * rhs / NWD(lhs, rhs);
 }
