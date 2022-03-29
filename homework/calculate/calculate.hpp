76f44c092c7abbf026327ddd9db9d10fb27ef015
wkktoria <abcwiktoria00@gmail.com>
2022-03-29 18:00:09 +0200

    division works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -10,6 +10,12 @@ std::string calculate(const std::string& command, int first, int second) {
         result = std::to_string(first - second);
     } else if (command == "multiply") {
         result = std::to_string(first * second);
+    } else if (command == "divide") {
+        if (second == 0) {
+            result = "Division by 0";
+        } else {
+            result = std::to_string(first / second);
+        }
     }
 
     return result;
