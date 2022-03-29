a0efa7cbb404d0041da18775414b20a6de260ffa
wkktoria <abcwiktoria00@gmail.com>
2022-03-29 19:59:12 +0200

    Iterative calculation works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,24 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    }
+
+    if (sequence == 1) {
+        return 1;
+    }
+
+    int previousNumber = 0;
+    int currentNumber = 1;
+
+    for (int i = 2; i <= sequence; ++i) {
+        int sum = previousNumber + currentNumber;
+        previousNumber = currentNumber;
+        currentNumber = sum;
+    }
+
+    return currentNumber;
 }
 
 int fibonacci_recursive(int sequence) {
