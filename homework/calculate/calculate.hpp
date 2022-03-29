1cc11248ad078d9f48fdcd1401db61b615bf18b9
wkktoria <abcwiktoria00@gmail.com>
2022-03-29 17:53:35 +0200

    adding works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,11 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    std::string result = "";
+
+    if (command == "add") {
+        result = std::to_string(first + second);
+    }
+
+    return result;
 }
