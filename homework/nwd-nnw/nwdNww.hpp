fdc6f94449c21df8a12d0ad6b328eb830d1148f8
wkktoria <abcwiktoria00@gmail.com>
2022-03-30 11:47:53 +0200

    NWD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs < 0) {
+        lhs = lhs * -1;
+    }
+
+    if (rhs < 0) {
+        rhs = rhs * -1;
+    }
+
+    while (rhs) {
+        auto temp = lhs;
+        lhs = rhs;
+        rhs = temp  % rhs;
+    }
+
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
