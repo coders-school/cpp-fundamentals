6816b4000656c32f64d57038d14c1d8b6577de48
tadeuszgraczyk <49392885+tadeuszgraczyk@users.noreply.github.com>
2022-04-02 11:58:45 +0200

    From GitHub test 02

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -7,6 +7,6 @@ std::string calculate(const std::string& command, int first, int second) {
     if(command=="multiply") return std::to_string(first*second);
     if(command=="divide" && second!=0) return std::to_string(first/second);
     if(command=="divide" && second==0) return "Division by 0";
-    return "Invalid data";
+    return "Invalid data" ;
 }
 
