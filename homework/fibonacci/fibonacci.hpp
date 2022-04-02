729a1598742547aa9ac180aded3fe37f9e9b4690
tadeuszgraczyk <49392885+tadeuszgraczyk@users.noreply.github.com>
2022-04-02 18:12:35 +0200

    Form GitHub fibonacci test 01

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int a {0};
+    int b {1};
+    int temp {1};
+    for(int i=0; i<sequence; ++i) {
+        temp = a;
+        a = a+b;
+        b = temp;
+    }
+    return a;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence<=1)
+        return sequence;
+    else
+        return fibonacci_recursive(sequence-1)+ fibonacci_recursive(sequence-2);
 }
