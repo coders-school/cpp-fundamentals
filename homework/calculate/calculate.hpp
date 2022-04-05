4b1930be9d766d451322c70cf48f0a05f10c45ba
Majbor <71440562+Majbor1@users.noreply.github.com>
2022-04-05 12:17:31 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,16 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+    if(command == "add"){
+        return to_string(first + second);
+    }else if(command == "substract"){
+        return to_string(first - second);
+    }else if(command == "multiply"){
+        return to_string(first * second);
+    }else if(command == "devide" && second != 0){
+        return to_string(first / second);
+    }else{
+        return "Invalid data";
+    }
     return "";
 }
