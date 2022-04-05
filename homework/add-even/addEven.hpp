9815b2d8cb3667e890187a522e6ac41f9f91046d
wkktoria <abcwiktoria00@gmail.com>
2022-04-05 19:26:08 +0200

    returning sum of all numbers

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -4,5 +4,11 @@
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
-    return -1;
+    unsigned int sum = 0;
+
+    for (auto element : numbers) {
+        sum += element;
+    }
+
+    return sum;
 }
