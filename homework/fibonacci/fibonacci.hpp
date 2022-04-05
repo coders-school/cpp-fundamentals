0ccca5f20f048afd9f36050989a08a297ccb4e02
Igor Kiełbus <71440525+Anty150@users.noreply.github.com>
2022-04-05 13:07:34 +0200

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int iArray[sequence];
+    for(int i = 0; i < sequence; i++){
+        if(i == 0)
+            iArray[i] = 0;
+        else if(i == 1)
+            iArray[i] = 1;
+        else
+        {
+            iArray[i] = iArray[i - 1] + iArray[i - 2];
+        }
+    }
+
+    return iArray[sequence];
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 1)
+        return sequence;
+    return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
 }
