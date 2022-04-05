8fca87e59880f8af72ec9c25393ada00b3046851
NBright <71440500+NBright04@users.noreply.github.com>
2022-04-05 11:05:41 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -15,6 +15,9 @@ std::string calculate(const std::string& command, int first, int second) {
     }
 
     if(command == "divide") {
+        if(second == 0) {
+            return "Division by 0";
+        }
         return std::to_string(first / second);
     }
 
