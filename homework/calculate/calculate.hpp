c69ea715fe6b0f45fc03c32a7e1942de692e2810
KHodyl <71666413+KHodyl@users.noreply.github.com>
2022-04-05 12:48:07 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,14 +5,17 @@ std::string calculate(const std::string& command, int first, int second) {
         if(command == "add") return std::to_string(first+second);
         if(command == "subtract") return std::to_string(first-second);
         if(command == "multiply") return std::to_string(first*second);
-        if(command == "divide"){
+        if(command == "divide")
+        {
+            if(second == 0) return "Division by 0";
+            if(first%second == 0) return std::to_string(first/second);
             float wyn,_f,_s;
             _f = first;
             _s = second;
             wyn = _f/_s;
             return std::to_string(wyn);
         }
-        
+
         if(command != "add" ||
            command != "subtract" ||
            command != "multiply" ||
