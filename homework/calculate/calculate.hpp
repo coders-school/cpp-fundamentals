8c1513227a6441f6403dda2671045747a946ea21
DJMixu <71440578+DJMixu@users.noreply.github.com>
2022-04-05 12:22:15 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,15 +3,15 @@
 
 std::string calculate(const std::string& command, int first, int second) {
      if(command=="add"){
-        return to_string(first+second);
+        return std::to_string(first+second);
     }else if(command=="multiply"){
-        return to_string(first*second);
+        return std::to_string(first*second);
     }else if(command=="substract"){
-        return to_string(first-second);
+        return std::to_string(first-second);
     }else if(command=="divide"){
         if(second==0)
             return "Invalid data";
-        return to_string(first/second);
+        return std::to_string(first/second);
     }else{
         return "Invalid data";
     }
