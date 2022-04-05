a6e2090a96711c3ba390a2cd100e007850fa08e8
Kacper Kachel <71440570+Kapibara44@users.noreply.github.com>
2022-04-05 12:25:06 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,34 @@
-#pragma once
+#include <iostream>
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    float result;
+
+    if(command = "add"){
+        result = first + second;
+    }
+    else if(command = "subtract"){
+        result = first - second;
+    }
+    else if(command = "multiply"){
+        result = first * second;
+    }
+    else if(command = "divide" && second != 0){
+        result = first / second;
+    }
+    else{
+        result = "Invalid data";
+    }
+
+    std::to_string(result);
+
+    return result;
+}
+
+int main() {
+    auto result = calculate("add", 2, 3);  // result = "5"
+    //result = calculate("multiply", 2, 3);  // result = "6"
+    //result = calculate("hello", 2, 3);     // result = "Invalid data"
+
+    std::cout << result << endl;
 }
