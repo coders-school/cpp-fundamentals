ef275a29baca554d1fa1e9eaacbcea888f37fe1a
HubertRudowicz <71440489+HubertRudowicz@users.noreply.github.com>
2022-04-05 13:04:17 +0200

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,26 @@
+#include <iostream>
 #pragma once
+using namespace std;
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
-}
+int wynik = 1, n = 1, tmp = 1;
+    if(sequence == 0) {
+        return 0;
+    }
+    for (int i = 0; i < sequence - 1; i++) {
+        tmp = wynik;
+        wynik = n;
+        n += tmp;
+    }
 
+    return wynik;
+}
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+    if(sequence == 0) {
+        return 0;
+    }
+    if (sequence <= 1)
+        return sequence;
+    return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
     return 0;
 }
