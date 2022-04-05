aafa745272f462cd1564e9b8c3409c5c7db000f8
Kacper Kachel <71440570+Kapibara44@users.noreply.github.com>
2022-04-05 18:17:08 +0200

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
+    int nwd;
+
+    if(lhs > 0 && rhs > 0){
+        while(lhs!=rhs){
+        if(lhs > rhs)
+            lhs -= rhs;
+        else
+            rhs -= lhs;
+        }
+        return lhs;
+    }
     return -1;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
+    int nww;
+
+    if(lhs > 0 && rhs > 0){
+        nww = (lhs * rhs) / NWD(lhs, rhs);
+        return nww;
+        }
     return -1;
 }
