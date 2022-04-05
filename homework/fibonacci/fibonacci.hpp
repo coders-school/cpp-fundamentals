e729b512827146ec99db002a3a3fac63f7fc5f05
NBright <71440500+NBright04@users.noreply.github.com>
2022-04-05 13:00:02 +0200

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -3,6 +3,10 @@
 int fibonacci_iterative(int sequence) {
     int one = 1, two = 1, temp = 1;
 
+    if(sequence == 0) {
+        return 0;
+    }
+    
     for (int i = 0; i < sequence - 1; i++) {
         temp = one;
         one = two;
