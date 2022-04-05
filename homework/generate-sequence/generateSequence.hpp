7749688e7ca225d91efc37b3cb956b8fd8c66084
Paul <71824144+Nister37@users.noreply.github.com>
2022-04-05 13:05:45 +0200

    Update generateSequence.hpp

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -8,7 +8,7 @@ vector<int> generateSequence(int count,int step){
     vector<int> array;
 
     if(count>0) {
-        array.push_back(count);
+        array.push_back(step);
         if(count>1) {
             for (int i = 1; i < count; i++) {
                 array.push_back(step + array[i - 1]);
