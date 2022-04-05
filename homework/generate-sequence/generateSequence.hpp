b2f2b2b562e77e8862fc17ea860e4a3a305f70f7
Paul <71824144+Nister37@users.noreply.github.com>
2022-04-05 13:03:59 +0200

    Update generateSequence.hpp

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -7,27 +7,11 @@ using namespace std;
 vector<int> generateSequence(int count,int step){
     vector<int> array;
 
-    if(step<0)
-    {
-        step=(step*step)/step;
-    }
-
-    if(step==0){
-        array[0]=0;
-    }
-    else {
-        if(step>=1) {
-
-            array.push_back(step);
-
-            if(step>=2) {
-                array.push_back(step*2);
-
-                if(step>=3) {
-                    for (int i = 2; i < count; i++) {
-                        array.push_back(array[i-2]+array[i-1]);
-                    }
-                }
+    if(count>0) {
+        array.push_back(count);
+        if(count>1) {
+            for (int i = 1; i < count; i++) {
+                array.push_back(step + array[i - 1]);
             }
         }
     }
