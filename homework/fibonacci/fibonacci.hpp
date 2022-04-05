730d4f0f242b39c4a35f3497adbb0e768dbbd0e8
Kacper Kachel <71440570+Kapibara44@users.noreply.github.com>
2022-04-05 17:24:50 +0200

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    long long a = 0, b = 1;
+int fibonacci_iterative(int sequence){
+    int a = 0, b = 1;
 
-     for(int i = 0; i < sequence - 1; i++)
-     {
-            b += a;
-            a = b - a;
-     }
-     return b;
+    for(int i = 0; i < sequence - 1; i++)
+    {
+        b += a;
+        a = b - a;
+    }
+    return b;
 }
 
-int fibonacci_recursive(int sequence) {
+int fibonacci_recursive(int sequence){
     if(sequence < 3)
-		return 1;
-	return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
+        return 1;
+    return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
 }
