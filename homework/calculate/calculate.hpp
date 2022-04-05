dfa0b4d828538a8c3ae427e24fe55c682d0cfef7
Kropeczka17 <71440587+Kropeczka17@users.noreply.github.com>
2022-04-05 11:06:29 +0200

    calculate_myImplementation

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,20 @@
 #pragma once
 #include <string>
+#include <iostream>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+using namespace std;
+
+string calculate(const string& command, int first, int second) {
+    if(command == "add") {
+        return to_string(first + second);
+    } else if(command == "subtract") {
+        return to_string(first - second);
+    } else if(command == "multiply") {
+        return to_string(first * second);
+    } else if(command == "divide") {
+        return to_string(first / second);
+    } else {
+        return "Invalid data";
+    }
     return "";
 }
