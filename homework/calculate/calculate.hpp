d11201d10db39cda869513cb53a7f409fc4eba37
HubertRudowicz <71440489+HubertRudowicz@users.noreply.github.com>
2022-04-05 10:55:32 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,30 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    int wynik;
+    if(command == "add"){
+        wynik = first + second;
+    }
+    else if(command == "substract"){
+        wynik = first - second;
+    }
+    else if(command == "multply"){
+        wynik = first * second;
+    }
+    else if(command == "divide"){
+        wynik = first / second;
+    }
+    else {
+        cout<<"Invalid data";
+        return "";
+    }
+    string wynik1 = to_string(wynik);
+    return wynik1;
+}
+
+int main()
+{
+    auto result = calculate("substrac", 2, 3);
+    cout<<result;
+    return 0;
 }
