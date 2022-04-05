2e7f148c690a444db52ccca9fb7ae6fee0afc941
Igor Kiełbus <71440525+Anty150@users.noreply.github.com>
2022-04-05 18:07:08 +0200

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
+    if(lhs > 0 && rhs > 0){
+        int iNWD;
+        for(int i = 0; i < rhs; i++){
+            if(lhs % i == 0 && rhs % i == 0){
+                iNWD = i;
+            }
+        }
+        return NWD;
+    }
     return -1;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
+    if(lhs > 0 && rhs > 0){
+        for(int i = 0;i < rhs * lhs;i++){
+            if(i % rhs == 0 && i % lhs == 0)
+                return i;
+        }
+    }
     return -1;
 }
