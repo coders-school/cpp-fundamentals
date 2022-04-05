855c280f742b7781cbf557944e1f883328c7aec0
ICEMAN <71440572+ICEMANICY@users.noreply.github.com>
2022-04-05 11:20:45 +0200

    Update addEven.hpp

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,26 @@
 #pragma once
+#include <iostream>
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) {
+int addEven(const std::vector<int>& numbers)
+{
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
-    return -1;
+    int equality = 0;
+    for  (int i = 0; i < numbers.size(); i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            equality += numbers[i];
+        }
+    }
+    return equality;
+}
+
+int main()
+{
+    std::vector<int> vec{ 1, 2, 3, 4, 5 };
+    //auto result = addEven(vec);  // result = 6;
+    std::cout << addEven(vec) << std::endl;
+    return 0;
 }
