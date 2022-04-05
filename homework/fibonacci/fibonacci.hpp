6cd7582d7b09bd606ab21aa9b5f3c43cc546231a
NBright <71440500+NBright04@users.noreply.github.com>
2022-04-05 12:58:09 +0200

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    int one = 0, two = 1, temp = 0;
-
-    if(sequence == 0 || sequence == 1) {
-        return sequence;
-    }
+    int one = 1, two = 1, temp = 1;
 
     for (int i = 0; i < sequence - 1; i++) {
         temp = one;
