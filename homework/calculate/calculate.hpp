bce6efa5ba4ebc28c6c8d9f58e0de47ab27ca055
Borys Borkowski <71440568+Falkor525@users.noreply.github.com>
2022-04-05 12:46:56 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -22,14 +22,14 @@ string calculate(const string& command, int first, int second)
   }
    else if(command=="divide")
   {
-      if(second==0) return "you can't divide by 0!";
+      if(second==0) return "Divison by 0";
          operation = first / second;
         return to_string(operation);
          
    }
    else
    {
-      return "Invalid Data";
+      return "Invalid data";
    }
 }
 
