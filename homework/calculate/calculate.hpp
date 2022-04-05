cbbaf29031085b767c0a520bb19860a2f1ab0ac1
Majbor <71440562+Majbor1@users.noreply.github.com>
2022-04-05 12:48:43 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -6,11 +6,11 @@ using namespace std;
 string calculate(const std::string& command, int first, int second) {
     if(command == "add"){
         return to_string(first + second);
-    }else if(command == "substract"){
+    }else if(command == "subtract"){
         return to_string(first - second);
     }else if(command == "multiply"){
         return to_string(first * second);
-    }else if(command == "devide"){
+    }else if(command == "divide"){
         if(second != 0)
             return to_string(first / second);
         return "Devision by 0";
