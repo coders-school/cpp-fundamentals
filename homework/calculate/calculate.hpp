4ad9643863352b32f6785512d6c3212f49ba0432
Majbor <71440562+Majbor1@users.noreply.github.com>
2022-04-05 12:50:49 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -11,9 +11,10 @@ string calculate(const std::string& command, int first, int second) {
     }else if(command == "multiply"){
         return to_string(first * second);
     }else if(command == "divide"){
-        if(second != 0)
-            return to_string(first / second);
-        return "Devision by 0";
+        if(second == 0)
+            return "Devision by 0";
+        return to_string(first / second);
+        
     }else{
         return "Invalid data";
         }
