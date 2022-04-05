8e427cddba02a5bb09a5c9f1bd259c7cba7c65f9
wkktoria <abcwiktoria00@gmail.com>
2022-04-05 19:28:36 +0200

    returning sum of only even numbers works

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,12 +2,12 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
     unsigned int sum = 0;
 
     for (auto element : numbers) {
-        sum += element;
+        if (element % 2 == 0) {
+            sum += element;
+        }
     }
 
     return sum;
