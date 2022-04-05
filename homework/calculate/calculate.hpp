3dd381c26eef09e86bbeb484d01a5f795cfb496f
DJMixu <71440578+DJMixu@users.noreply.github.com>
2022-04-05 12:07:47 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,13 +3,13 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     if(command=="add"){
-        return first+second;
-    }else if(command="multiply"){
-        return first*second;
-    }else if(command="substract"){
-        return first-second;
-    }else if(command="divide"){
-        return first/second;
+        return first+second+"";
+    }else if(command=="multiply"){
+        return first*second+"";
+    }else if(command=="substract"){
+        return first-second+"";
+    }else if(command=="divide"){
+        return first/second+"";
     }else{
         return "Invalid data";
     }
