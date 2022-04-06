0bb954bf471c19062b5691cef82e7996477e01a5
wkktoria <abcwiktoria00@gmail.com>
2022-04-06 16:53:36 +0200

    generating sequence works

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,16 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> vec = {};
+
+    for (auto i = 0; i < count; ++i) {
+        if (vec.empty()) {
+            vec.push_back(step);
+        } else {
+            auto last_element = vec.back();
+            vec.push_back(step + last_element);
+        }
+    }
+
+    return vec;
 }
