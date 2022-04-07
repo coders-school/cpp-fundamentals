953c5f16fc2dfffd201b02e5e2407e75b51cac6c
KHodyl <71666413+KHodyl@users.noreply.github.com>
2022-04-07 22:04:34 +0200

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int wyn = 0;
+    int mem_org = 0;
+    int mem_add = 1;
+
+    for(int i = sequence; i > 0; i--)
+    {
+        mem_org = wyn;
+        wyn = mem_org + mem_add;
+        mem_add = mem_org;
+    }
+
+    return wyn;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+    if(sequence == 1 || sequence == 2)
+    {
+        return 1;
+    }
+    else
+        return fibonacci_recursive(sequence-1)+ fibonacci_recursive(sequence-2);
 }
