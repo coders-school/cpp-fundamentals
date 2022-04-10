63989b1e3ff4342159a6f1d3b63ecf3a84ac7fd2
Kropeczka17 <71440587+Kropeczka17@users.noreply.github.com>
2022-04-10 22:16:09 +0200

    fibonnaci_myImplementation_v1

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,30 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int fib1 = 0;
+    int fib2 = 1;
+    int fib3;
+    if(sequence == 0){
+        return 0;
+    } else if(sequence == 1) {
+        return 1;
+    } else {
+        while (sequence != 1) {
+            fib3 = fib2;
+            fib2 = fib1 + fib3;
+            fib1 = fib3;
+            sequence--;
+        }
+        return fib2;
+    }
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence == 0) {
+        return 0;
+    }
+    if(sequence == 1) {
+        return 1;
+    }
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
