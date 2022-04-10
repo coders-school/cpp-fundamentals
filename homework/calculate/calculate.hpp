8b6fc3e8ba2f20d946b3f2786a6e02e2e679eaf8
Paweł Rozbejko <71440430+pabelito05@users.noreply.github.com>
2022-04-10 11:19:20 +0200

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,9 +2,13 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
+    float a;
     if(command=="add") return std::to_string(first + second);
     if(command=="subtract") return std::to_string(first - second);
     if(command=="multiply") return std::to_string(first * second);
-    if(command=="divide") return std::to_string(first / second);
+    if(command=="divide"){
+        a=first / second;
+        return std::to_string(a);
+    }
     return "Invalid data";
 }
