efe22dfe0871e8c29908ecc344c77d765d6b2491
Kropeczka17 <71440587+Kropeczka17@users.noreply.github.com>
2022-04-11 21:24:39 +0200

    nwdNWW_myImlementation_v1

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,14 @@
 #pragma once
+#include <cstdlib>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 && rhs == 0) return 0;
+    if(rhs != 0) return abs(NWD(rhs, lhs%rhs));
+    return lhs;
+
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 && rhs == 0) return 0;
+    return abs((lhs*rhs)/NWD(lhs, rhs));
 }
