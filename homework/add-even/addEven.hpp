a16c38d0d7b9252e2587ecc1fb19f7ff8b80b4f1
Kropeczka17 <71440587+Kropeczka17@users.noreply.github.com>
2022-04-11 19:54:23 +0200

    addEven_myImplementation_v2

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,9 +1,7 @@
 #pragma once
 #include <vector>
 
-using namespace std;
-
-int addEven(vector<int>& numbers) {
+int addEven(std::vector<int>& numbers) {
     int suma = 0;
     for(int i = 0; i < numbers.size(); i++){
         if(numbers[i] % 2 == 0) suma += numbers[i];
