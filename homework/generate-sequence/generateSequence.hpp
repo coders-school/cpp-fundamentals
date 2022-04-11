41b1888fcd208a49ceb574eb840a464f6deae650
Kropeczka17 <71440587+Kropeczka17@users.noreply.github.com>
2022-04-11 20:35:42 +0200

    generateSequence_myImplementation_v2

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -5,7 +5,7 @@ std::vector<int> generateSequence(int _count, int step) {
     std::vector<int> numbers;
     if(_count < 0) return numbers;
     for(int i = 0; i < _count; i++) {
-        numbers[i] = step;
+        numbers.push_back(step);
         step+=step;
     }
     return numbers;
