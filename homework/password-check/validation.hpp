bac7fa8747c495993ce67c926f9a0ed93599fbb5
Igor Kiełbus <71440525+Anty150@users.noreply.github.com>
2022-04-11 20:41:42 +0200

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,6 +1,11 @@
 #pragma once
+#include<iostream>
 
-
-enum ErrorCode {Ok,PasswordNeedsAtLeastNineCharacters,PasswordNeedsAtLeastOneNumber
+enum class ErrorCode {Ok,PasswordNeedsAtLeastNineCharacters,PasswordNeedsAtLeastOneNumber
 ,PasswordNeedsAtLeastOneSpecialCharacter,PasswordNeedsAtLeastOneUppercaseLetter,PasswordsDoNotMatch};
+
+std::string getErrorMessage(const ErrorCode& errCode);
+bool doPasswordsMatch(const std::string& pas1, const std::string& pass2);
+ErrorCode checkPasswordRules(const std::string& pass);
+ErrorCode checkPassword(const std::string& pass1, const std::string& pass2);
 
