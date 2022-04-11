fa5b3795efd3db78b9db794e0071aceea44c304b
Igor Kiełbus <71440525+Anty150@users.noreply.github.com>
2022-04-11 19:38:00 +0200

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -7,13 +7,19 @@ int NWD(int lhs, int rhs) {
             else
                 rhs-=lhs;
         }
+        if(lhs < 0)
+            lhs *= -1;
         return lhs;
     }
     return 0;
 }
 
 int NWW(int lhs, int rhs) {
-    if(lhs != 0 || rhs != 0)
-        return lhs * rhs / NWD(lhs, rhs);
+    if(lhs != 0 || rhs != 0){
+        int iNWW = lhs * rhs / NWD(lhs, rhs);
+        if(iNWW < 0)
+            iNWW *= -1;
+        return iNWW;
+    }
     return 0;
 }
