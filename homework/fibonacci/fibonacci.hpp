44ffefc31ea199d35bad0ad54cab7763b6c9e622
Borys Borkowski <71440568+Falkor525@users.noreply.github.com>
2022-04-11 18:41:49 +0200

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -4,12 +4,12 @@ int fibonacci_iterative(int sequence) {
     int a=0,b=1;
    for(int i=0;i<sequence;i++)
    {
-       cout << b << endl;
+       return b;
     b=a+b;
     a=b-a;
    
    }
-    return 0;
+   
 }
 
 int fibonacci_recursive(int sequence) {
