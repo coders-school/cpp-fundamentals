8e0060aca9b82f7bbd2cfc7d3f413cbd5f8ca7dc
Borys Borkowski <71440568+Falkor525@users.noreply.github.com>
2022-04-11 21:25:08 +0200

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,18 +1,25 @@
 #pragma once
-
+#include <cmath>
 int NWD(int lhs, int rhs) {
-   
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+   if(lhs!=0 && rhs!=0)
+   {
     do {
         if(lhs>rhs)
         {
         lhs = lhs - rhs;
         } else rhs = rhs - lhs;
     }while(lhs!=rhs);
-
+   }
     return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+   if(lhs!=0 || rhs!=0)
+   {
 return (lhs*rhs)/NWD(lhs,rhs);
+   }
 }
