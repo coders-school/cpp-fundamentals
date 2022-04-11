73ffbd54f5546b9482b8f727d19fabce8d68e97b
Borys Borkowski <71440568+Falkor525@users.noreply.github.com>
2022-04-11 18:13:38 +0200

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
+    int a=0,b=1;
+   for(int i=0;i<sequence;i++)
+   {
+    b=a+b;
+    a=b-a;
+   }
     return 0;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+    
     return 0;
 }
