fdc0f6ef20183f80281147914891eb75a0419bd5
Borys Borkowski <71440568+Falkor525@users.noreply.github.com>
2022-04-13 09:00:03 +0200

    Update maxOfVector.hpp

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,9 +3,9 @@
 #include <limits>
 using namespace std;
 int maxOfVector(const std::vector<int>& vec) {
-    int maxV = max_element(vec.begin(),vec.end());
-    for (int value : maxV ) {
-        if(value > maxV) maxV = value;
+    int maxV = vec[0];
+    for (int i=0;i<vec.size();i++) {
+        if(vec[i] > maxV) maxV = vec[i];
     }
     return maxV;
 }
