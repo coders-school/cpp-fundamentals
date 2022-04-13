92dfba5ca6f071e425f87ff438bc3089365e4d6b
DJMixu <71440578+DJMixu@users.noreply.github.com>
2022-04-13 09:07:53 +0200

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -12,5 +12,5 @@ int fibonacci_iterative(int sequence) {
 int fibonacci_recursive(int sequence) {
     if(sequence==0) return 0;
     else if(sequence==1) return 1;
-    return 0;
+    return fibonacci_recursive(sequence-2)+fibonacci_recursive(sequence-1);
 }
