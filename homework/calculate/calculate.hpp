8e73bd02b02475d41e50f13df80f192a4d0e930c
rodbergerrone <rodbergerrone@icloud.com>
2022-04-18 12:30:23 +0200

    Calculator works with all functions

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,18 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    if (command == "add") {
+        return std::to_string(first + second);
+    } else if (command == "subtract") {
+        return std::to_string(first - second);
+    } else if (command == "multiply") {
+        return std::to_string(first * second);
+    } else if (command == "divide") {
+        if (second == 0) return "Division by 0";
+        else return std::to_string(first / second);
+    } else if (command == "quit") {
+        exit(0);
+    } else {
+        return "Invalid data";
+    }    
 }
