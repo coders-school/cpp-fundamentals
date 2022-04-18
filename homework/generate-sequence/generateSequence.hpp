4b8b298ca3f5a16f9bca4f7105c140d8fe521df1
rodbergerrone <rodbergerrone@icloud.com>
2022-04-18 19:35:12 +0200

    Implementation done

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,11 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> result;
+    int a = 0;
+    for (int i = 0; i < count; i++) {
+        a = a + step;
+        result.push_back(a);
+    }
+    return result;
 }
