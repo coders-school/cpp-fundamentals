52e22d682832d9305b267dd4e970ea259d374b12
rodbergerrone <rodbergerrone@icloud.com>
2022-04-18 14:40:50 +0200

    NWD and NWW functions done

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int & a = lhs;
+    int & b = rhs;
+    int c = 0;
+    while (b !=0 ) {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    if (a < 0) {
+        a = a*-1;
+    }
+    return a;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 && rhs == 0) return 0;
+    int result = (lhs*rhs) /NWD(lhs,rhs);
+    if (result < 0) result = result * -1;
+    return result;
 }
