d852fd7feb92d3d3e728f504427a1697137a858b
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-04-21 18:16:21 +0200

    koncowe cos tam

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -4,7 +4,23 @@ using namespace std;
 string calculate(const string& command, int first, int second) {
     if(command == "add")
     {
-    	return to_string(first+second);
+    return to_string(first+second);
+
+	}else if(command == "subtract" )
+	{
+        return to_string(first-second);
+		
+	}else if(command == "multiply" )
+	{
+        return to_string(first*second);
+			
+			
+	}else if(command == "divide" ){
+        if(second!=0)
+		    return to_string(first/second);
+            else { return "Division by 0";}
+           
+	}else{
+		return "Invalid data";
 	}
-    return "";
 }
