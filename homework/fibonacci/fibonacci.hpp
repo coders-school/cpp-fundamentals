29bb3774046d8e213de7b430e6423c55e5e69cde
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-04-21 20:20:56 +0200

    Fibonaci iteracyjnie

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,24 @@
 #pragma once
-
+#include <vector>
+using namespace std;
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+    int wynik = 0;
+    vector <int> vec{0,1};
+    if(sequence == 0)
+            return 0;
+     if(sequence == 1)
+        return 1;
+
+    for(size_t i = sequence ; i>=2 ;i--)
+    {
+         vec.push_back(vec[i-2]+vec[i-1]) ;
+    }
+
+    return vec.back();
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+
     return 0;
 }
