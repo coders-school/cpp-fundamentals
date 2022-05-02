1d65cb9036e4877fd32f86f4856c598b04ceae9c
Paweł Rozbejko <71440430+pabelito05@users.noreply.github.com>
2022-05-02 13:53:11 +0200

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs<0)lhs*=-1;
+    if (rhs<0)rhs*=-1;
+    while(lhs!=rhs){
+        if(lhs>rhs)lhs-=rhs;
+        else rhs-=lhs;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 || rhs == 0) return 0;
+    if(lhs*rhs/NWD(lhs, rhs))return lhs*rhs/NWD(lhs, rhs)*-1;
+    return lhs*rhs/NWD(lhs, rhs);
 }
