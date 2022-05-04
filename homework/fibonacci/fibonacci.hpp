9b349ae61da84b375d46b977791797e840c24f1b
Tomasz Eichler <tomasz.eichler@wp.pl>
2022-05-04 23:27:41 +0200

    Added fibo iterative

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,24 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    if(sequence == 0) {
+        return 0;
+    } else if(sequence == 1) {
+        return 1;
+    }
+
+    int n_1 = 1;
+    int n_2 = 0;
+    int f_n = 0;
+
+    for(int i = 2; i <= sequence; i++) {
+        f_n = n_1 + n_2;
+        n_2 = n_1;
+        n_1 = f_n;        
+    }
+
+    return f_n;
 }
 
 int fibonacci_recursive(int sequence) {
