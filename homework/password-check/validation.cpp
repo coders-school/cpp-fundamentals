42475ec29a2c925e455c0f862fcd9fb1449ccb02
Tomasz Eichler <tomasz.eichler@wp.pl>
2022-05-06 13:49:13 +0200

    Added getErrorMessage implementation

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,38 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+#include <iostream>
+
+std::string getErrorMessage(ErrorCode errorCode) {
+    switch (errorCode) {
+    case ErrorCode::Ok: {
+        return "Ok";
+    } break;
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters: {
+        return "Password needs to have at least nine characters";
+    } break;
+    case ErrorCode::PasswordNeedsAtLeastOneNumber: {
+        return "Password needs to have at least one number";
+    } break;
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter: {
+        return "Password needs to have at least one special character";
+    } break;
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter: {
+        return "Password needs to have at least one uppercase letter";
+    } break;
+    case ErrorCode::PasswordsDoNotMatch: {
+        return "Passwords do not match";
+    } break;
+    }
+    return "";
+}
+
+bool doPasswordsMatch(std::string first, std::string second) {
+    return false;
+}
+
+ErrorCode checkPasswordRules(const std::string password) {
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(std::string first, std::string second) {
+    return ErrorCode::Ok;
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,19 @@
+#pragma once
+#include <string>
+
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+
+std::string getErrorMessage(ErrorCode errorCode);
+bool doPasswordsMatch(std::string first, std::string second);
+ErrorCode checkPasswordRules(const std::string password);
+ErrorCode checkPassword(std::string first, std::string second);
