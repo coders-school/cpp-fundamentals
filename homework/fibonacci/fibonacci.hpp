ba3cb127c360ede16f853ed08727931d0e63acca
Penthurion <68010070+Penthurion@users.noreply.github.com>
2022-05-10 09:19:28 +0200

    Ebook Testing
    Just a quick change in file to test the e-book.

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,9 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    return 1;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    return 1;
 }
