ac2f4db482f56a06567d88d9b09f0373f2a14b78
Mateusz <stacewicz.mateusz@gmail.com>
2022-05-10 08:52:07 +0200

    changed main.cpp

--- homework/fibonacci/main.cpp
+++ homework/fibonacci/main.cpp
@@ -4,6 +4,7 @@
 int main() {
     std::cout << fibonacci_iterative(10) << "\n";
     std::cout << fibonacci_recursive(10) << "\n";
+    //This is just for ebook test porpouses only.
 
     return 0;
 }
