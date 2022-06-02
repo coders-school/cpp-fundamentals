b740c57fcc1905a1aa4a0a64ba1909a82743a9d0
Kurzawskyy <60553233+Kurzawskyy@users.noreply.github.com>
2022-06-02 12:10:18 +0200

    Update calculate.hpp
    second try

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,14 +5,18 @@ std::string calculate(const std::string& command, int first, int second) {
     std::string text = "";
     if(command == "add") {
         text = std::to_string(first+second);
+        return text;
     } else if(command == "subtract") {
         text = std::to_string(first-second);
+        return text;
     } else if(command == "multiply") {
         text = std::to_string(first*second);
+        return text;
     } else if(command == "divide") {
         if(second != 0) {
             text = std::to_string(first/second);
+            return text;
         } else text = "Invalid data";
     } else text = "Invalid data";
-    return text;
+    return "";
 }
