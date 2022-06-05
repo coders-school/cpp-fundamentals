8775df5523f3b4e0e485e39ec60fd46b6f1fc183
mahazel-dev <adam.kozlowski94@gmail.com>
2022-06-05 21:13:46 +0200

    fibo done

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,40 @@
 #pragma once
+#include <vector>
+
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int result;
+    
+    
+    if (sequence <= 1) { result = sequence;
+        return result;
+    }
+    
+    std::vector <int> list;
+    list.push_back(0);
+    list.push_back(1);
+    
+    for (int i = 2; i <= sequence; i++) {
+        list.push_back(list[i-2] + list[i-1]);
+    }
+    
+    result = list.back();
+    return result;
 }
 
+
+
+
+
+
+
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int result;
+    if (sequence <= 1) {
+        result = sequence;
+    } else {
+        result = fibonacci_recursive(sequence-2) + fibonacci_recursive(sequence - 1);
+    }
+    return result;
+    
 }
