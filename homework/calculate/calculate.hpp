e088a9cd768b6e22929e2afd0928b2a3a4f4da47
Dario Github <dario.github@wp.pl>
2022-08-15 23:22:30 +0200

    everything works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,22 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    std::string output = "";
+    if (command == "add") {
+        output = std::to_string(first + second);
+    } else if (command == "subtract") {
+        output = std::to_string(first - second);
+    } else if (command == "multiply") {
+        output = std::to_string(first * second);
+    } else if (command == "divide") {
+        if (second != 0) {
+            output = std::to_string(first / second);
+        } else {
+            output = "Division by 0";
+        }
+    } else {
+        output = "Invalid data";
+    }
+
+    return output;
 }
