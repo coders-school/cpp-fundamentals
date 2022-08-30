0c267fccf7522d7724a1455dc24813580ed22d18
Dario Github <dario.github@wp.pl>
2022-08-30 20:13:08 +0200

    Refactor

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -22,8 +22,12 @@ int fibonacci_iterative(int sequence) {
      if(sequence==ind) return sum;
      v.push_back(sum);
      ind++;
-     if(sequence==ind) return v[2]+v[1];
-     v.push_back(v[2]+v[1]);
+     sum = v[ind-1]+v[ind-2];
+     if(sequence==ind) return sum;
+     v.push_back(sum);
+     ind++;
+     sum = v[ind-1]+v[ind-2];
+     if(sequence==ind) return sum;
      if(sequence==4) return v[3]+v[2];
      v.push_back(v[3]+v[2]);
      if(sequence==5) return v[4]+v[3];
