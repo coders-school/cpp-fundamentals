b12d67159940910786e6880cfa6d49b4ced22f51
Dario Github <dario.github@wp.pl>
2022-08-30 20:10:30 +0200

    Refactor to find sequence of instruction

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -15,10 +15,18 @@ void printVec(std::vector<int> v)
 int fibonacci_iterative(int sequence) {
      if(!sequence) return 0;
      if(sequence==1) return 1;
-     if(sequence==2) return 1;
-     if(sequence==3) return 2;
-     if(sequence==4) return 3;
-     if(sequence==5) return 5;
+     vector<int> v{0, 1};
+     
+     int ind=2;
+     int sum = v[ind-1]+v[ind-2];
+     if(sequence==ind) return sum;
+     v.push_back(sum);
+     ind++;
+     if(sequence==ind) return v[2]+v[1];
+     v.push_back(v[2]+v[1]);
+     if(sequence==4) return v[3]+v[2];
+     v.push_back(v[3]+v[2]);
+     if(sequence==5) return v[4]+v[3];
      if(sequence==6) return 8;
      int output = 0; 
      return output;
