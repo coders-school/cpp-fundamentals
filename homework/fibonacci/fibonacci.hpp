192e661112031e9f0c62e980d20191559042e9c0
Dario Github <dario.github@wp.pl>
2022-08-30 21:26:57 +0200

    Added recursive implementation'

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,17 +1,14 @@
 #pragma once
 #include <iostream>
 #include <vector>
- 
-using namespace std;
- 
-int fibonacci_iterative(int sequence) {
 
+int fibonacci_iterative(int sequence) {
     if (!sequence)
         return 0;
     if (sequence == 1)
         return 1;
-    vector<int> v{0, 1};
- 
+    std::vector<int> v{0, 1};
+
     for (int ind = 2;; ind++) {
         int sum = v[ind - 1] + v[ind - 2];
         if (sequence == ind)
@@ -19,12 +16,11 @@ int fibonacci_iterative(int sequence) {
         v.push_back(sum);
     }
 }
- 
-int fibonacci_recursive(int sequence) {
 
+int fibonacci_recursive(int sequence) {
     if (!sequence)
         return 0;
     if (sequence == 1)
         return 1;
     return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
- }
+}
