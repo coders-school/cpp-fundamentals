980f0bad2019ff25b8a83a87c5d58b058f6bdb0d
Patryk Koniarczyk <patrick@legion.localdomain>
2022-08-30 07:12:27 +0200

    fixing style

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,8 +2,6 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    
     std::string result = "";
 
     if (command == "add") {
