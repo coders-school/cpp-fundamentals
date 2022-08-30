ba7e631ad9d72ea76ab828310179fa881d30048f
DarioDarioCS <109880326+DarioDarioCS@users.noreply.github.com>
2022-08-30 22:00:22 +0200

    Clang-format intendations

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,24 +1,30 @@
 #pragma once
-#include <vector>
 #include <iostream>
-
+#include <vector>
+ 
 using namespace std;
-
+ 
 int fibonacci_iterative(int sequence) {
-    if(!sequence) return 0;
-    if(sequence==1) return 1;
-    vector<int> v{0, 1};
 
-    for(int ind=2; ;ind++)
-    {
-        int sum = v[ind-1]+v[ind-2];
-        if(sequence==ind) return sum;
+    if (!sequence)
+        return 0;
+    if (sequence == 1)
+        return 1;
+    vector<int> v{0, 1};
+ 
+    for (int ind = 2;; ind++) {
+        int sum = v[ind - 1] + v[ind - 2];
+        if (sequence == ind)
+            return sum;
         v.push_back(sum);
     }
 }
-
+ 
 int fibonacci_recursive(int sequence) {
-    if(!sequence) return 0;
-    if(sequence==1) return 1;
-    return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
-}
+
+    if (!sequence)
+        return 0;
+    if (sequence == 1)
+        return 1;
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+ }
