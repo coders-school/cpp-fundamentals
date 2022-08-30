8843c91ea2059b37cca97f5d712197d49e98c1e8
Dario Github <dario.github@wp.pl>
2022-08-30 21:26:57 +0200

    Added recursive implementation'

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -26,6 +26,7 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(!sequence) return 0;
+    if(sequence==1) return 1;
+    return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
 }
