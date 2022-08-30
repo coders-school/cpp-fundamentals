3a18c76554c3868ddebe5f54bcc1d3fd020b7926
Dario Github <dario.github@wp.pl>
2022-08-30 21:09:59 +0200

    Remove unnecessary code

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -17,35 +17,13 @@ int fibonacci_iterative(int sequence) {
      if(sequence==1) return 1;
      vector<int> v{0, 1};
 
-     int sum = 0;
      for(int ind=2; ;ind++)
      {
-        sum = v[ind-1]+v[ind-2];
+        int sum = v[ind-1]+v[ind-2];
         if(sequence==ind) return sum;
         v.push_back(sum);
      }
-     return sum;
-/*
-     int ind=2;
-     int sum = v[ind-1]+v[ind-2];
-     if(sequence==ind) return sum;
-     v.push_back(sum);
-     ind++;
-     sum = v[ind-1]+v[ind-2];
-     if(sequence==ind) return sum;
-     v.push_back(sum);
-     ind++;
-     sum = v[ind-1]+v[ind-2];
-     if(sequence==ind) return sum;
-     if(sequence==4) return v[3]+v[2];
-     v.push_back(v[3]+v[2]);
-     if(sequence==5) return v[4]+v[3];
-     if(sequence==6) return 8;
-     int output = 0; 
-     return output;
-*/
 }
-
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
