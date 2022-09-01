f95e7425edf9b5803652c285dd54369d7c65b364
Dario Github <dario.github@wp.pl>
2022-09-02 01:31:56 +0200

    Added minimal working version

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,13 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> result{};
+    if (count == 0)
+        return result;
+    if (count-- > 0)
+        result.push_back(step);
+    for (int i = 0; i < count; ++i) {
+        result.push_back(result[i] + step);
+    }
+    return result;
 }
