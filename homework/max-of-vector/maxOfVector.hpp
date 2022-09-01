98e593ac8ebca16a7e5d3503950b56ac20bd6710
Dario Github <dario.github@wp.pl>
2022-09-02 01:11:01 +0200

    Added minimal working implementation

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,23 @@
 #pragma once
+#include <algorithm>
 #include <limits>
 #include <vector>
 
+template <class ForwardIt>
+int maxOfVector(ForwardIt first, ForwardIt last) {
+    if (first == last)
+        return *last;
+
+    ForwardIt largest = first;
+    ++first;
+    for (; first != last; ++first) {
+        if (*largest < *first) {
+            largest = first;
+        }
+    }
+    return *largest;
+}
+
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    return maxOfVector(vec.begin(), vec.end());
 }
