6906c178a400a8edd0f930600e06aae74fbb527c
Dario Github <dario.github@wp.pl>
2022-09-05 23:18:30 +0200

    commit: Added minimal implementation
    to make project compilable

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,22 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+
+std::string getErrorMessage(enum ErrorCode error)
+{
+    return "";
+}
+
+bool doPasswordsMatch(const std::string& pass1, const std::string pass2)
+{
+    return false;
+}
+
+enum ErrorCode checkPasswordRules(const std::string& pass)
+{
+    return ErrorCode::PasswordNeedsAtLeastOneNumber;
+}
+
+enum ErrorCode checkPassword(const std::string& pass1, const std::string pass2)
+{
+    return ErrorCode::PasswordNeedsAtLeastOneNumber;
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,16 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#pragma once
+#include <string>
+
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+
+std::string getErrorMessage(enum ErrorCode error);
+bool doPasswordsMatch(const std::string& pass1, const std::string pass2);
+enum ErrorCode checkPasswordRules(const std::string& pass);
+enum ErrorCode checkPassword(const std::string& pass1, const std::string pass2);
