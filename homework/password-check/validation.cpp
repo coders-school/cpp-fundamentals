b8f4e860141a2c8c279a687367878ea9b2028f14
Dario Github <dario.github@wp.pl>
2022-09-05 23:35:43 +0200

    Made getErrorMessageTests passed

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -3,8 +3,41 @@
 
 std::string getErrorMessage(enum ErrorCode error)
 {
-    return "";
+    std::string error_message;
+    switch(error)
+    {
+        case ErrorCode::Ok:
+            error_message = "Ok";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+            error_message = "Password needs to have at least nine characters";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+            error_message = "Password needs to have at least one number";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+            error_message = "Password needs to have at least one special character";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+            error_message = "Password needs to have at least one uppercase letter";
+            break;
+        case ErrorCode::PasswordsDoNotMatch:
+            error_message = "Passwords do not match";
+            break;
+        default:
+            error_message = "xxx"; //TODO: worth to handle
+            break;
+    }
+    return error_message;
 }
+
+/*
+    EXPECT_EQ(getErrorMessage(ErrorCode::PasswordNeedsAtLeastNineCharacters), "Password needs to have at least nine characters");
+    EXPECT_EQ(getErrorMessage(ErrorCode::PasswordNeedsAtLeastOneNumber), "Password needs to have at least one number");
+    EXPECT_EQ(getErrorMessage(ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter), "Password needs to have at least one special character");
+    EXPECT_EQ(getErrorMessage(ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter), "Password needs to have at least one uppercase letter");
+    EXPECT_EQ(getErrorMessage(ErrorCode::PasswordsDoNotMatch), "Passwords do not match");
+*/
 
 bool doPasswordsMatch(const std::string& pass1, const std::string pass2)
 {
