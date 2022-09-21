5ec600798b5384d21774a529aea405b7ccb1238a
Pawel <66121057+pawelkolimaga@users.noreply.github.com>
2022-09-21 16:47:19 +0200

    task implementation

--- 02-types/tasks/01-tab.cpp
+++ 02-types/tasks/01-tab.cpp
@@ -4,9 +4,10 @@ constexpr size_t tab_size = 100;
 
 int main() {
     int tab[tab_size];
+    int j = 1;
 
-    for (size_t i = 0; i < tab_size; ++i) {
-        tab[i] = i;
+    for (size_t i = 0; i < tab_size; ++i, ++j) {
+        tab[i] = j++;
     }
 
     for (size_t i = 0; i < tab_size; ++i) {
