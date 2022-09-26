1438e21d528c432590a530ceb15baadbbd6c6f2e
automationdream <automationdream@gmail.com>
2022-09-26 23:32:44 +0200

    implementation for the subtract function

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -6,10 +6,17 @@ void add(std::string &string, const int first, const int second) {
     string = std::to_string(result);
 }
 
+void subtract(std::string &string, const int first, const int second) {
+    int result = first - second;
+    string = std::to_string(result);
+}
+
 std::string calculate(const std::string &command, const int first, const int second) {
     std::string result = "";
     if (command == "add") {
         add(result, first, second);
+    } else if (command == "subtract") {
+        subtract(result, first, second);
     }
 
     return result;
