09db4d235dc9d68a0777f4ffcd12cc9d8a083983
adrian <7adrian.mail@gmail.com>
2022-09-27 23:39:23 +0200

    Implementation password-check (restore after branch disappear)

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,45 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(const ErrorCode& message) {
+    if (message == ErrorCode::Ok) {
+        return "Ok";
+    } else if (message == ErrorCode::PasswordNeedsAtLeastNineCharacters) {
+        return "Password needs to have at least nine characters";
+    } else if (message == ErrorCode::PasswordNeedsAtLeastOneNumber) {
+        return "Password needs to have at least one number";
+    } else if (message == ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter) {
+        return "Password needs to have at least one special character";
+    } else if (message == ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter) {
+        return "Password needs to have at least one uppercase letter";
+    } else if (message == ErrorCode::PasswordsDoNotMatch) {
+        return "Passwords do not match";
+    }
+    return "Message not found!";
+}
+
+bool doPasswordsMatch(const std::string& password1, const std::string& password2) {
+    return password1 == password2;
+}
+
+ErrorCode checkPasswordRules(const std::string& password) {
+    int random = rand() % 5;
+    if (random == 0) {
+        return ErrorCode::Ok;
+    } else if (random == 1) {
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    } else if (random == 2) {
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    } else if (random == 3) {
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    } else if (random == 4) {
+        return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+    }
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(const std::string& password1, const std::string& password2) {
+    if (!doPasswordsMatch(password1, password2)) {
+        return ErrorCode::PasswordsDoNotMatch;
+    }
+    return checkPasswordRules(password1);
+}
