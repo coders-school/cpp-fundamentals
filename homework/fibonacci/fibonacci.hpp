16880e188811fd845a800deb2811b014525cca40
automationdream <automationdream@gmail.com>
2022-09-27 22:53:04 +0200

    green- calculate fibonacci iteratively

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,24 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int f;
+    int f_prev = 0;
+    int f_curr = 1;
+
+    if (sequence == 0) {
+        return 0;
+    }
+
+    if (sequence == 1) {
+        return 1;
+    }
+
+    for (int i = 1; i < sequence; i++) {
+        f = f_prev + f_curr;
+        f_prev = f_curr;
+        f_curr = f;
+    }
+    return f;
 }
 
 int fibonacci_recursive(int sequence) {
