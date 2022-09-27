0e9ec1a764660ba8eee4515e39eb962cac6c2236
adrian <7adrian.mail@gmail.com>
2022-09-27 20:03:06 +0200

    NWD implementation

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    while (rhs != 0) {
+        int r = lhs % rhs;
+        lhs = rhs;
+        rhs = r;
+    }
+    return abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
