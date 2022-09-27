cda4de3455c611615311991c625f6717fb8bc4ba
adrian <7adrian.mail@gmail.com>
2022-09-27 19:23:18 +0200

    Fibonacci iterative

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 0) {
+        return 0;
+    }
+    unsigned int fibo = 1;
+    unsigned int fibo_prev = 1;
+    unsigned int fibo_prev2 = 0;
+    for (int i = 1; i < sequence; i++) {
+        fibo += fibo_prev2;
+        fibo_prev2 = fibo_prev;
+        fibo_prev = fibo;
+    }
+    return fibo;
 }
 
 int fibonacci_recursive(int sequence) {
