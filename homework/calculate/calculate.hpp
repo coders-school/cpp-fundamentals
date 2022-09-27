31d171ea23b04165713263888697826a8a127c56
adrian <7adrian.mail@gmail.com>
2022-09-27 18:11:24 +0200

    "multiply" function

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -8,6 +8,8 @@ std::string calculate(const std::string &command, int first, int second) {
         return std::to_string(first + second);
     } else if (command.compare("subtract") == 0) {
         return std::to_string(first - second);
+    } else if (command.compare("multiply") == 0) {
+        return std::to_string(first * second);
     }
     return "Invalid data";
 }
