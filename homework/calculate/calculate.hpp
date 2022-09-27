1dd70d61f99e63883b65352d9dec6ccc9a6160f2
adrian <7adrian.mail@gmail.com>
2022-09-27 18:48:32 +0200

    Fixed clang-format failure

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,13 +3,13 @@
 #include <string>
 
 std::string calculate(const std::string &command, int first, int second) {
-    if (command.compare("add") == 0) {
+    if (command == "add") {
         return std::to_string(first + second);
-    } else if (command.compare("subtract") == 0) {
+    } else if (command == "subtract") {
         return std::to_string(first - second);
-    } else if (command.compare("multiply") == 0) {
+    } else if (command == "multiply") {
         return std::to_string(first * second);
-    } else if (command.compare("divide") == 0) {
+    } else if (command == "divide") {
         if (second != 0) {
             return std::to_string(first / second);
         } else {
