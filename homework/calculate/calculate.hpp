2678d7e1e26bbd4bf4ef1d57f778e4bc7789d184
adrian <7adrian.mail@gmail.com>
2022-09-27 18:05:29 +0200

    "add" function

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,11 @@
 #pragma once
+
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) {
+std::string calculate(const std::string &command, int first, int second) {
     // TODO: Implement your solution here and return proper value
+    if (command.compare("add") == 0) {
+        return std::to_string(first + second);
+    }
     return "";
 }
