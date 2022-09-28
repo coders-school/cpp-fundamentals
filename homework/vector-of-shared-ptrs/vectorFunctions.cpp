6f67c86ad28f43ef07f978d45407b4c3ccc72d15
adrian <7adrian.mail@gmail.com>
2022-09-28 11:07:53 +0200

    Prepare for implementation vectorFunctions

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,21 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    return std::vector<std::shared_ptr<int>>();
+}
+
+void print(const std::vector<std::shared_ptr<int>>& vec) {
+
+}
+
+void add10(const std::vector<std::shared_ptr<int>>& vec) {
+
+}
+
+void sub10(int *num) {
+
+}
+
+void sub10(const std::vector<std::shared_ptr<int>>& vec) {
+
+}
---
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+std::vector<std::shared_ptr<int>> generate(int count);
+
+void print(const std::vector<std::shared_ptr<int>>& vec);
+
+void add10(const std::vector<std::shared_ptr<int>>& vec);
+
+void sub10(int *num);
+
+void sub10(const std::vector<std::shared_ptr<int>>& vec);
