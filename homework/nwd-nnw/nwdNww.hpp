448d2dabb4982c50ad01043563321f6848bedd91
automationdream <automationdream@gmail.com>
2022-09-29 08:27:26 +0200

    NWD implementation

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs < 0) {
+        lhs *= -1;
+    }
+    if (rhs < 0) {
+        rhs *= -1;
+    }
+
+    // Use euclidean algorithm
+    while (rhs != 0) {
+        auto rest = lhs % rhs;
+        lhs = rhs;
+        rhs = rest;
+    }
+
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
