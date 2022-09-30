8cbfcafc6d155f8191ddd37afff249c5980ea0d8
srazik <srazik@gmail.com>
2022-09-30 10:42:01 +0200

    Finished Task

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,45 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_iterative(int sequence)
+{
+      // TODO: Your implementation goes here
+      //
+      int Fibonacci[sequence];
+
+      for (int i =0; i <= sequence; i++)
+      {
+
+            if (i == 0)  //added for 1 exeption - than it have to be one without anything
+            {
+                  Fibonacci[0]=0;
+            }
+            else if (i == 1)
+            {
+                  Fibonacci[1]=1;
+            }
+            else
+            {
+                  Fibonacci[i] = Fibonacci [i -2] + Fibonacci[i -1];
+            }
+      }
+
+      return Fibonacci[sequence];
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_recursive(int sequence)
+{
+      // TODO: Your implementation goes here
+      int num = 0; //Additional variable for returning value
+      switch(sequence)
+      {
+      case 0:
+            num = 0;
+            break;
+      case 1:
+            num = 1;
+            break;
+      default:
+            num = fibonacci_recursive(sequence - 2) + fibonacci_recursive( sequence - 1);
+      }
+      return num;
 }
