aab35a2285d6fc302a4bbeb6231d4c152c467b31
Pawel <66121057+pawelkolimaga@users.noreply.github.com>
2022-10-02 23:45:18 +0200

    Implementation of foo()

--- 02-types/tasks/04-sharedPtr.cpp
+++ 02-types/tasks/04-sharedPtr.cpp
@@ -6,11 +6,32 @@
 // It should also display the value of this int and the number of how many pointers are pointing to it - use `use_count()`.
 // Display the same information in main() before and after calling foo()
 
+void foo(std::shared_ptr<int> number)
+{
+	std::cout << "Value of received int pointer: "
+		<< *number
+		<< "\nNumber of pointers to received int: "
+		<< number.use_count()
+		<< "\n\n";
+	*number = 20;
+}
+
 int main() {
     std::shared_ptr<int> number = std::make_shared<int>(10);
-    // display the value under number pointer and use_count() of it
+    
+    std::cout << "Value of 'number' pointer: "
+		<< *number
+		<< "\nNumber of pointers to 'number': "
+		<< number.use_count()
+		<< "\n\n";
+    
     foo(number);
-    // display the value under number pointer and use_count() of it
+    
+    std::cout << "Value of 'number' pointer: "
+		<< *number
+		<< "\nNumber of pointers to 'number': "
+		<< number.use_count()
+		<< "\n\n";
 
     return 0;
 }
