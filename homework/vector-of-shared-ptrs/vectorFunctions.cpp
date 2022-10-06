bad160ccbfae9a3f06cf24ccd2057009d18bb5d9
automationdream <automationdream@gmail.com>
2022-10-06 11:57:13 +0200

    implements function declarations and definitions

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,16 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+}
+
+void print(std::vector<std::shared_ptr<int>> vec) {
+}
+
+void add10(std::vector<std::shared_ptr<int>> vec) {
+}
+
+void sub10(const int* ptr){
+}
+
+void sub10(std::vector<std::shared_ptr<int>> vec){
+}
---
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "memory"
+#include "vector"
+
+std::vector<std::shared_ptr<int>> generate(int count);
+void print(std::vector<std::shared_ptr<int>> vec);
+void add10(std::vector<std::shared_ptr<int>> vec);
+void sub10(const int* ptr);
+void sub10(std::vector<std::shared_ptr<int>> vec);
