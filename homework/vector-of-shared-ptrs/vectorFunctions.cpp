3b93a8e1d971af13ecdd6fcc23480046168ff8fc
automationdream <automationdream@gmail.com>
2022-10-06 11:58:15 +0200

    implementation of the generate function

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,6 +1,11 @@
 #include "vectorFunctions.hpp"
 
 std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> vec;
+    for (int i = 0; i < count; ++i) {
+        vec.push_back(std::make_shared<int>(i));
+    }
+    return vec;
 }
 
 void print(std::vector<std::shared_ptr<int>> vec) {
