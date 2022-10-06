59608e1fac7deb953a5a94018f5e8fab63b8cd2c
srazik <srazik@gmail.com>
2022-10-06 19:39:00 +0200

    Finished

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,7 +1,25 @@
 #pragma once
+#include <iostream>
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+  // TODO: Implement me :)
+  //
+  std::vector<int> vec = {};
+  if (count <= 0) {
+    return vec;
+  }
+
+  switch (count) {
+  case 1:
+    vec.push_back(step);
+    break;
+  default:
+    vec.push_back(step);
+    for (int i = 0; i < count - 1; i++) {
+      vec.push_back(vec[i] + step);
+    }
+  }
+
+  return vec;
 }
--- homework/generate-sequence/main.cpp
+++ homework/generate-sequence/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include "generateSequence.hpp"
+#include <iostream>
 
 int main() {
-    for (const int element : generateSequence(10, 5))
-        std::cout << element << " | ";
+  for (const int element : generateSequence(3, 3))
+    std::cout << element << " | ";
 
-    return 0;
+  return 0;
 }
