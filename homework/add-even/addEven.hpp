fee58d8c5aed54b8fdf443b5b85c9ad89f38753e
srazik <srazik@gmail.com>
2022-10-06 14:10:03 +0200

    Works OK

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,17 @@
 #pragma once
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+int addEven(const std::vector<int> &numbers) {
+  // TODO: Your implementation goes here
+  // Below return is only to make this function compile now
+  //
+  auto result = 0; // Declaration of result
+                   //
+  for (int i = 0; i < numbers.size(); i++) {
+    if (numbers[i] % 2 == 0) { // check if value is even - if not do nothing
+      result += numbers[i];
+    }
+  }
+
+  return result;
 }
--- homework/add-even/main.cpp
+++ homework/add-even/main.cpp
@@ -1,10 +1,10 @@
+#include "addEven.hpp"
 #include <iostream>
 #include <vector>
-#include "addEven.hpp"
 
 int main() {
-    std::vector<int> vec{1, 2, 3, 4, 5};
-    std::cout << addEven(vec) << "\n";
+  std::vector<int> vec{1, 2, 3, 4, 5};
+  std::cout << addEven(vec) << "\n";
 
-    return 0;
+  return 0;
 }
