19a9d538b214ddcfc87f7e07fecbbb6523855988
srazik <srazik@gmail.com>
2022-10-06 14:44:55 +0200

    Working OK max_of_Vector

--- homework/max-of-vector/main.cpp
+++ homework/max-of-vector/main.cpp
@@ -1,10 +1,10 @@
+#include "maxOfVector.hpp"
 #include <iostream>
 #include <vector>
-#include "maxOfVector.hpp"
 
 int main() {
-    std::vector<int> vec{4, 3, 2, 5, 6, 7, 9, 1, 0};
-    std::cout << maxOfVector(vec) << '\n';
+  std::vector<int> vec{-5, -1};
+  std::cout << maxOfVector(vec) << '\n';
 
-    return 0;
+  return 0;
 }
--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -2,7 +2,16 @@
 #include <limits>
 #include <vector>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(const std::vector<int> &vec) {
+  // TODO: Implement me :)
+  //
+  auto result = vec[0];
+
+  for (const int &i : vec) {
+    if (result < i) {
+      result = i;
+    }
+  }
+
+  return result;
 }
