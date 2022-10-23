09501e9304ee097f8e4f8beeda6870f77516643a
PawelN93 <paweln1993@gmail.com>
2022-10-23 16:33:37 +0200

    Fib iter and recur done

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int back = 0;
+    int next = 1;
+    if (sequence == 0) {
+        return back;
+    } else if (sequence == 1) {
+        return next;
+    } else {
+        for (int i = 2; i <= sequence; i++) {
+            int temp = next;
+            next = next + back;
+            back = temp;
+        }
+    }
+    return next;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+    if (sequence == 0) {
+        return 0;
+    } else if (sequence == 1) {
+        return 1;
+    } else {
+        return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
+    }
     return 0;
 }
