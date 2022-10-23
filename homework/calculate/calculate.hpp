dc14edab7fa99152af13517bb3b774491fe2b58d
PawelN93 <paweln1993@gmail.com>
2022-10-23 14:01:06 +0200

    Calculate works OK

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -8,7 +8,29 @@ std::string calculate(const std::string& command, int first, int second) {
     {
         result = first + second;
     }
-
+    else if (command == "subtract")
+    {
+        result = first - second;
+    }
+    else if (command == "multiply")
+    {
+        result = first * second;
+    }
+    else if (command == "divide")
+    {
+        if (second != 0)
+        {
+            result = first / second;
+        }
+        else
+        {
+            return "Division by 0";
+        }
+    }
+    else
+    {
+        return "Invalid data";
+    }
     return std::to_string(result);
-   // return "";
+
 }
