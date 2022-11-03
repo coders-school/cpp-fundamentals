f492629d8c37f1c132291aac5782ec7dc619d6bc
KrzucH <115778673+KrzucH@users.noreply.github.com>
2022-11-03 12:41:53 +0100

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,19 +1,19 @@
 #pragma once
 #include <string>
- 
-std::string calculate(const std::string& command, int first, int second) { 
-    if(command == "add") {        
+
+std::string calculate(const std::string& command, int first, int second) {
+    if (command == "add") {
         return std::to_string(first + second);
     } else if (command == "subtract") {
         return std::to_string(first - second);
-    } else if (command == "multiply") {        
+    } else if (command == "multiply") {
         return std::to_string(first * second);
     } else if (command == "divide") {
         if (second != 0) {
-           return std::to_string(first / second);
+            return std::to_string(first / second);
+        } else {
+            return "Division by 0";
         }
-         return "Division by 0";
-    } else {
-        return "Invalid data";
     }
+    return "Invalid data";
 }
