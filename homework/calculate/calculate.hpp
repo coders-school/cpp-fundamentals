1e03240435d481713e8aefd2dc889d5e2b7d52fe
KrzucH <115778673+KrzucH@users.noreply.github.com>
2022-11-03 11:25:49 +0100

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,8 +2,7 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-<<<<<<< HEAD
- 
+
  int result;
     
  if(command == "add"){
@@ -31,25 +30,4 @@ std::string calculate(const std::string& command, int first, int second) {
         return "Invalid data";
     }
  return  std::to_string(result);
-=======
-   std::string output = "";
-    return "";
-    if (command == "add") {
-        output = std::to_string(first + second);
-    } else if (command == "subtract") {
-        output = std::to_string(first - second);
-    } else if (command == "multiply") {
-        output = std::to_string(first * second);
-    } else if (command == "divide") {
-        if (second != 0) {
-            output = std::to_string(first / second);
-        } else {
-            output = "Division by 0";
-        }
-    } else {
-        output = "Invalid data";
-    }
-
-    return output;
->>>>>>> refs/remotes/origin/calculate
 }
