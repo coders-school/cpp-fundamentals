4bacdc689c00a0909e3d04422452fa462bea8901
KrzucH <115778673+KrzucH@users.noreply.github.com>
2022-11-06 09:52:30 +0100

    Update maxOfVector.hpp

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -5,12 +5,10 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     std::vector<int> vec1;
-    int a {0};
-    for(auto n : vec) {
+    int a{0};
+    for (auto n : vec) {
         vec1.push_back(n);
-
     }
-
     sort(vec1.begin(), vec1.end());
     a = vec1.back();
 
