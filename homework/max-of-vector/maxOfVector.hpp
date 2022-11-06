772d0b41c378b78a107bffda03419a449608a079
Krzysiek Nowak <krzynowak93@gmail.com>
2022-11-06 09:45:24 +0100

    MaxofVectorHomework

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,18 @@
 #pragma once
 #include <limits>
 #include <vector>
+#include <algorithm>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> vec1;
+    int a {0};
+        for(auto n : vec) {
+            vec1.push_back(n);
+
+        }
+
+    sort(vec1.begin(), vec1.end());
+    a = vec1.back();
+
+    return a;
 }
