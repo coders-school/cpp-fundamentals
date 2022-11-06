b08d53c4260fa0db79a94232e0ea768c9ed0869a
KrzucH <115778673+KrzucH@users.noreply.github.com>
2022-11-06 10:41:55 +0100

    Update generateSequence.hpp

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -4,9 +4,11 @@
 std::vector<int> generateSequence(int count, int step) {
     std::vector<int> vec;
     const int a = step;
+    
     for (auto i{0}; i < count; i++) {
         vec.push_back(step);
         step += a; 
     }
+    
     return vec;
 }
