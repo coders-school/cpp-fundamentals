5a59b87df8b00e540d11bddd6d3d7f7f2a61d8ea
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2022-11-14 20:26:20 +0100

    Dividing done

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -12,6 +12,12 @@ std::string calculate(const std::string& command, int first, int second) {
     else if (command == "multiply") {
         result = std::to_string(first * second);
     }
+    else if (command == "divide") {
+        if (second == 0) {
+            return "Division by 0";
+        }
+        result = std::to_string(first / second);
+    }
 
     return result;
 }
