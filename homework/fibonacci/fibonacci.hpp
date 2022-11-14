51f2c9ae4fbe9f4b102633bcfaf33599a4292b8e
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2022-11-14 21:06:55 +0100

    Iterative done

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    }
+    if (sequence == 1) {
+        return 1;
+    }
+    int result, prev1 = 0, prev2 = 1;
+    for (std::size_t i = 2; i <= sequence; i++) {
+        result = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = result;
+    }
+
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
