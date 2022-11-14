2f172292400bc5efb789f165dd979fa781ad00bb
KrzucH <115778673+KrzucH@users.noreply.github.com>
2022-11-14 15:25:30 +0100

    Update vectorFunctions.cpp

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -5,11 +5,10 @@ std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> vec{};
     vec.reserve(count);
 
-    for (auto i {0}; i < count; i++) {
+    for (auto i{0}; i < count; i++) {
         vec.push_back(std::make_shared<int>(i));
     }
     return vec;
-    
 }
 
 void print(const std::vector<std::shared_ptr<int>>& vec) {
@@ -21,10 +20,9 @@ void print(const std::vector<std::shared_ptr<int>>& vec) {
 void add10(std::vector<std::shared_ptr<int>>& vec) {
     for (auto& n : vec) {
         if (n) {
-            *n +=10;
+            *n += 10;
         }
     }
-
 }
 
 void sub10(int* const ptr) {
