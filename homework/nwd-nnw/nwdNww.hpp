ca9143c977653572ae2f796d39e3a83bf9313e9a
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2022-11-16 18:34:52 +0100

    GCD implemented

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (rhs == 0) {
+        return lhs >= 0 ? lhs : (-1) * lhs;
+    }
+    return NWD(rhs, lhs % rhs);
 }
 
 int NWW(int lhs, int rhs) {
