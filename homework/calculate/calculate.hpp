769b831496b176e257c2f2952b6a0c3aaa9fecf1
Pontikol <Pontikol@wp.pl>
2022-12-01 16:47:46 +0100

    adding works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,9 +5,9 @@ std::string calculate(const std::string& command, int first, int second) {
     int wynik = 0;
 
     if (command == "add") {
-	wynik = first + second;
+	 wynik = first + second;
     } else if (command == "subtract") {
-	wynik = first - second;
+	 wynik = first - second;
     } else if (command == "multiply") {
         wynik = first * second;
     } else if (command == "divide" && second != 0) {
@@ -15,7 +15,7 @@ std::string calculate(const std::string& command, int first, int second) {
     } else if (command == "divide" && second == 0) {
         return "Division by 0";
     } else {
-	return "Invalid data";
+	 return "Invalid data";
     }
     std::string s = std::to_string(wynik);
     return s;
