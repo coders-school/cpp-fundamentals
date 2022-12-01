a22c16be2d51f175f28c64794ff233f7d132d8f7
Pontikol <Pontikol@wp.pl>
2022-12-01 16:28:06 +0100

    calculate working

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,28 +2,21 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-	    int wynik = 0;
+    int wynik = 0;
 
-	        if (command == "add") {
-			        wynik = first + second;
-				    }
-		    else if (command == "subtract") {
-			            wynik = first - second;
-				        }
-		        else if (command == "multiply") {
-				        wynik = first * second;
-					    }
-			    else if (command == "divide" && second != 0) {
-				            wynik = first / second;
-					        }
-			        else if (command == "divide" && second == 0) {
-					        return "Division by 0";
-						    }
-				    else {
-					            return "Invalid data";
-						        }
-
-				        std::string s = std::to_string(wynik);
-
-					    return s;
+    if (command == "add") {
+	wynik = first + second;
+    } else if (command == "subtract") {
+	wynik = first - second;
+    } else if (command == "multiply") {
+        wynik = first * second;
+    } else if (command == "divide" && second != 0) {
+        wynik = first / second;
+    } else if (command == "divide" && second == 0) {
+        return "Division by 0";
+    } else {
+	return "Invalid data";
+    }
+    std::string s = std::to_string(wynik);
+    return s;
 }
