c2256c90fc1a666b814c2d84bdecb12e6bb926ba
root <root@DESKTOP-2PFAQAK.localdomain>
2022-12-08 19:21:45 +0100

    Fibonacci dziala

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,25 @@
-#pragma once
+#include <iostream>
+#include <string>
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+	    int x = 0;
+	        int y = 1;
+
+		    for (int i = 0; i < sequence; i++) {
+			            y += x;
+				            x = y - x;
+
+					        }
+return x;		      
+
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+	    if (sequence == 0) return 0;
+	        if (sequence <= 2) return 1;
+
+		    return fibonacci_iterative(sequence-1) + fibonacci_iterative(sequence-2);
+
+
 }
