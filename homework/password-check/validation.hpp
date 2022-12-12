17c6e0af978aaf6089d847f6e89dbfd7b62f7013
Krzysiek Nowak <krzynowak93@gmail.com>
2022-12-12 16:09:39 +0100

    Password Check

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include <algorithm>
+#include <cctype>
 #include <iostream>
 #include <string>
-#include <cctype>
 
 enum class ErrorCode {
     Ok,
@@ -14,8 +14,6 @@ enum class ErrorCode {
 
 };
 std::string getErrorMessage(ErrorCode err);
-bool doPasswordsMatch(const std::string & pas1, const std::string & pas2);
+bool doPasswordsMatch(const std::string& pas1, const std::string& pas2);
 ErrorCode checkPasswordRules(const std::string& pas);
 ErrorCode checkPassword(std::string str1, std::string str2);
-
-    
