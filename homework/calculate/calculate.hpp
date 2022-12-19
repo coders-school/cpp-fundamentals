262e78a65a0fdf92bf0c1630f12d37e125bb4a90
Spror <wiktorspringer27@gmail.com>
2022-12-19 01:40:14 +0100

    adding solution to the calculate task

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,31 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    std::string strResult;
+    if(command == "add")
+    {
+        strResult = std::to_string(first + second);
+    }
+    else if(command == "subtract")
+    {
+        strResult = std::to_string(first - second);
+    }
+    else if(command == "multiply")
+    {
+        strResult = std::to_string(first * second);
+    }
+    else if(command == "divide")
+    {
+        if(second == 0)
+        {
+            strResult = "Division by 0";
+        }
+        else
+            strResult = std::to_string(first / second);
+    }
+    else{
+        strResult = "Invalid data";
+    }
+
+    return strResult;
 }
