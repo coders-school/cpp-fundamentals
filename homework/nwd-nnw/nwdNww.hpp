57f41f8a3f753270b26d9721d4ae4e124217d040
Spror <wiktorspringer27@gmail.com>
2022-12-19 02:51:25 +0100

    add clang-format

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,7 +2,6 @@
 #include <cmath>
 
 int NWD(int lhs, int rhs) {
-
     int temp;
 
     while (rhs != 0) {
@@ -16,7 +15,7 @@ int NWD(int lhs, int rhs) {
 
 int NWW(int lhs, int rhs) {
     auto nwd = NWD(lhs, rhs);
-    if(nwd == 0)
+    if (nwd == 0)
         return 0;
 
     return std::abs((lhs / nwd) * rhs);
