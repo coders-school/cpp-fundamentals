bdad2f7e28f3c6ee63390dd839400dba913b2afa
Spror <wiktorspringer27@gmail.com>
2022-12-19 02:22:46 +0100

    adding fibonacci solution

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,33 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int result = 1;
+
+    if (sequence == 0)
+        result = 0;
+    else if (sequence == 1)
+        result = 1;
+
+    else {
+        auto previous = 1;
+
+        for (auto i = 2; i < sequence; i++) {
+            result += previous;
+            previous = result - previous;
+        }
+    }
+
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    }
+
+    else if (sequence < 3) {
+        return 1;
+    }
+
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
