94c7f66d5fa6bcb6b58a8ee7162a08c3f659f489
Krzysztof Goldon <krzysztofgoldon@gmail.com>
2022-12-20 21:13:45 +0100

    Add implementation

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,10 +2,28 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 and rhs == 0) {
+        return 0;
+    }
+    int nwd = -1;
+
+    if (rhs == 0) {
+        nwd = std::abs(lhs);
+    } else {
+        nwd = NWD(rhs, lhs % rhs);
+    }
+
+    return nwd;
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 and rhs == 0) {
+        return 0;
+    }
+    int nww = -1;
+
+    nww = std::abs(lhs * rhs / NWD(lhs, rhs));
+
+    return nww;
 }
