704aa635a156baf92319cc4291fcb47e7e3e62a5
Krzysztof Goldon <krzysztofgoldon@gmail.com>
2022-12-20 20:45:53 +0100

    Add implementation

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,31 @@
 #pragma once
+#include <array>
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    }
+    std::array<int, 3> fib = {0, 1, 1};
+
+    for (std::size_t i = 2; i < sequence; ++i) {
+        fib[0] = fib[1];
+        fib[1] = fib[2];
+        fib[2] = fib[0] + fib[1];
+    }
+
+    return fib[2];
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int fib = 0;
+
+    if (sequence == 1) {
+        fib = 1;
+    } else if (sequence > 1) {
+        fib = fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
+
+    return fib;
 }
