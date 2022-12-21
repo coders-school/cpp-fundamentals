692de345d2689ca56c824fb566f399f59169002b
Krzysztof Goldon <krzysztofgoldon@gmail.com>
2022-12-21 12:19:34 +0100

    Fix formating

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -15,8 +15,8 @@ enum class ErrorCode {
 
 std::string getErrorMessage(ErrorCode);
 
-bool doPasswordsMatch(const std::string &, const std::string &);
+bool doPasswordsMatch(const std::string&, const std::string&);
 
-ErrorCode checkPasswordRules(const std::string &);
+ErrorCode checkPasswordRules(const std::string&);
 
-ErrorCode checkPassword(const std::string &, const std::string &);
+ErrorCode checkPassword(const std::string&, const std::string&);
