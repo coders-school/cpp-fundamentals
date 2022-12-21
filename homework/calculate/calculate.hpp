f999ee5895ae1a7e6722e31fd056514254c086bd
ca5per1 <119765241+ca5per1@users.noreply.github.com>
2022-12-22 00:15:26 +0100

    calculate second try

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,22 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    int result;
+
+    if (command == "add") {
+        result = first + second;
+    } else if (command == "subtract") {
+        result = first - second;
+    } else if (command == "multiply") {
+        result = first * second;
+    } else if (command == "divide") {
+        if (second != 0) {
+            result = first / second;
+        } else {
+            return "Division by 0";
+        }
+    } else {
+        return "Invalid data";
+    }
+    return std::to_string(result);
 }
