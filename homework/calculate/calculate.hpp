6cd261dbda2d28184948a2c833116fcb4384ebcc
Shiar4 <drow01@wp.pl>
2022-12-22 23:08:53 +0100

    adding works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,25 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    
+    int amount;
+
+    if (command == "add"){
+    amount = first + second;
+    }else if (command == "subtract"){
+    amount = first - second;
+    }else if (command == "multiply"){
+    amount = first * second;
+    }else if (command == "divide"){
+	    if(second == 0){
+		return "Division by 0";	
+	    }else{
+    amount = first / second;}
+    }
+
+    if(command == "add" || command == "subtract" || command =="multiply" || command =="divide"){
+      return std::to_string(amount);  
+    }else{
+        return "Invalid data";
+    }
 }
--- homework/calculate/main.cpp
+++ homework/calculate/main.cpp
@@ -2,27 +2,6 @@
 #include <string>
 #include "calculate.hpp"
 
-std::string calculate(const std::string& command, int first, int second)
-{
-    int amount;
-
-    if (command == "add"){
-    amount = first + second;
-    }else if (command == "subtract"){
-    amount = first - second;
-    }else if (command == "multiply"){
-    amount = first * second;
-    }else if (command == "divide"){
-    amount = first / second;
-    }
-
-    if(command == "add" || command == "subtract" || command =="multiply" || command =="divide"){
-      return std::to_string(amount);  
-    }else{
-        return "Invalid data";
-    }
-}
-
 int main() {
     while (true) {
         std::cout << "Provide command: \"add\", \"subtract\","
