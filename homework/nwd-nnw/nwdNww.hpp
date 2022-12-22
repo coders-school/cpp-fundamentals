6f9e4b2aedc1bcc31cdd8737e9342e53d220b65e
PaSawi <119161328+PaSawi@users.noreply.github.com>
2022-12-22 18:52:27 +0100

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,49 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs < 0)
+    {
+        lhs = -lhs;   
+    }
+    if(rhs < 0)
+    {
+        rhs = -rhs;   
+    }
+
+    if(lhs > rhs) 
+    {
+        return NWD(rhs, lhs);
+    }
+    else
+    {
+        if(lhs == 0 && rhs == 0) 
+        {
+            return 0;
+        }
+        else if(lhs == 0)
+        {
+            return rhs;
+        }
+        else{
+            return NWD(rhs % lhs, lhs);
+        }
+    }
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs < 0)
+    {
+        lhs = -lhs;   
+    }
+    if(rhs < 0)
+    {
+        rhs = -rhs;   
+    }
+    if(lhs == 0 && rhs == 0)
+    {
+            return 0;
+    }
+    else{
+        return (lhs * rhs) / NWD(lhs, rhs);
+    }
 }
