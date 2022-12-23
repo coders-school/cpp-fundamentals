59ae5d78d885664864506756c24d41f6faead522
PaSawi <119161328+PaSawi@users.noreply.github.com>
2022-12-23 19:37:07 +0100

    Update 02-modifyString.cpp

--- 02-types/tasks/02-modifyString.cpp
+++ 02-types/tasks/02-modifyString.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 
-// TODO: Implement modifyString()
-// It should modify passed string to text "Other string"
+void modifyString(std::string & str)
+{
+    str = "Other string";
+}
 
 int main() {
     std::string str{"Some string"};
