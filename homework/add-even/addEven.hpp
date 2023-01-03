22ddb6125bc047349a1e9c16aaa05683a93a3f3c
PaSawi <sawicki.patryk@gmail.com>
2023-01-03 22:29:12 +0100

    add even

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,18 @@
 #pragma once
+#include<iostream>
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
+    int result = 0;
+    for(const int n :  numbers)
+    {
+        if((n % 2 == 0)) {
+           
+           result += n;
+           std::cout << n << std::endl;
+        }
+    }
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
-    return -1;
+    return result;
 }
