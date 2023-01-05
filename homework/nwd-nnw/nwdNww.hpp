e8ca4c2557e807f5e85b03748cece5acc1d8eb88
root <root@DESKTOP-2PFAQAK.localdomain>
2023-01-05 19:02:32 +0100

    everything Works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,35 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
+int NWD(int a, int b) {
     // TODO: Implement me :)
-    return -1;
+while (a != b && a != 0 && b != 0) {
+	if (a < 0)
+		a = a * -1;
+        else if (b < 0)
+	        b = b * -1;
+        else if (a > b)
+	        a -= b;
+        else
+	        b -= a;}
+	if (a == 0 && b != 0)
+		a = b;
+	else if(b == 0 && a != 0)
+	return a;
+
+	return a;
 }
 
-int NWW(int lhs, int rhs) {
+int NWW(int a, int b) {
     // TODO: Implement me :)
-    return -1;
+	if (a != 0 && b != 0)
+		a = a * b / NWD(a, b);
+
+	else if (b == 0)
+		a = b;
+	
+	if (a < 0)
+	return a * -1;
+	
+	else
+	return a;
 }
