e9b2fceb137d9bc1a1b667612b7729f1ba2208cb
Benia <beniab333@gmail.com>
2023-01-06 18:32:00 +0100

    Format

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,19 +2,19 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    if(command == "add") {
-	return std::to_string(first + second);
+    if (command == "add") {
+        return std::to_string(first + second);
     } else if (command == "subtract") {
-	return std::to_string(first - second);
+        return std::to_string(first - second);
     } else if (command == "multiply") {
-	return std::to_string(first * second);
+        return std::to_string(first * second);
     } else if (command == "divide") {
-      if(second == 0 ){
-	return "Division by 0";
-      } else {
-	return std::to_string(first / second);
-	}
+        if (second == 0) {
+            return "Division by 0";
+        } else {
+            return std::to_string(first / second);
+        }
     } else {
-    return "Invalid data";
+        return "Invalid data";
     }
 }
