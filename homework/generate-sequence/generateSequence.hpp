e0f1d407deb9dd453121fab6bcf011bc368ec6ce
PaSawi <sawicki.patryk@gmail.com>
2023-01-06 21:24:08 +0100

    generate-squence3

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -4,11 +4,7 @@
 std::vector<int> generateSequence(int count, int step) {
     std::vector<int> vec{};
     vec.clear();
-<<<<<<< HEAD
-    if (count == 0) {
-=======
     if (count <= 0) {
->>>>>>> refs/remotes/origin/generate-sequence
         return vec;
     }
     vec.push_back(step);
