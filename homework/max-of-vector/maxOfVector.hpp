2bc20a9ca22e0dc9e019914576ec53e521fc4009
PaSawi <sawicki.patryk@gmail.com>
2023-01-06 16:19:14 +0100

    max-of-vector-working

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,16 @@
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max{};
+    if (!vec.empty()) {
+        max = vec.front();
+    } else {
+        return 0;
+    }
+    for (auto& num : vec) {
+        if (num > max) {
+            max = num;
+        }
+    }
+    return max;
 }
