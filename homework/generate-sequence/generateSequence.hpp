ac30a8e49a448e99f9a791b867a07c8f11317f7c
Benia <beniab333@gmail.com>
2023-01-07 15:28:23 +0100

    Format

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -4,9 +4,9 @@
 std::vector<int> generateSequence(int count, int step) {
     std::vector<int> result;
     int current = step;
-    for(int i = 0; i < count; i++){
-     result.push_back(current);
-     current += step;
-   }
-   return result;
+    for (int i = 0; i < count; i++) {
+        result.push_back(current);
+        current += step;
+    }
+    return result;
 }
