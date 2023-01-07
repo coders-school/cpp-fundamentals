35a6f09983e82638935189f12d1a22af934a280e
Benia <beniab333@gmail.com>
2023-01-07 14:03:44 +0100

    clang format

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,8 +1,7 @@
 #pragma once
 #include <vector>
 
-int addEven(const std::vector<int>& numbers)
-{
+int addEven(const std::vector<int>& numbers) {
     int sum = 0;
     for (int number : numbers) {
         if (number % 2 == 0) {
