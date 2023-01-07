e53e1163c321072d09ddc2faa2a98cde062f0020
Benia <beniab333@gmail.com>
2023-01-07 13:43:48 +0100

    Format style

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,14 +1,13 @@
 #pragma once
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) {
+int addEven(const std::vector<int>& numbers)
+{
     int sum = 0;
-    for (int number : numbers)
-	{
-	   if (number % 2 == 0)
-		{
-		   sum +=number;
-		}
-	}
-	return sum;
+    for (int number : numbers) {
+        if (number % 2 == 0) {
+            sum += number;
+        }
+    }
+    return sum;
 }
