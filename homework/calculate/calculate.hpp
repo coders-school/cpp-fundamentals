bc5bda072787ab482646c027b650f783c468531a
Adam Pietrzyk <apietrzyk92@gmail.com>
2023-01-09 00:54:30 +0100

    small formatting improvement

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,20 +2,20 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    std::string tmp;
-   if (command=="add") {
-        tmp = std::to_string(first + second);
+    if (command=="add") {
+        return std::to_string(first + second);
    } else if (command=="subtract") {
-        tmp = std::to_string(first - second);
+        return std::to_string(first - second);
    } else if (command=="multiply") {
-        tmp = std::to_string(first * second);
+        return std::to_string(first * second);
    } else if (command=="divide") {
         if (second == 0) {
-            tmp = "Division by 0";
+            return "Division by 0";
         } else {
-            tmp = std::to_string(first / second);
+            return std::to_string(first / second);
         }
    } else {
-        tmp = "Invalid data";
-   }    return tmp;
+        return "Invalid data";
+   }    
+ 
 }
