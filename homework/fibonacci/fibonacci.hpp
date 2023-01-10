0c9ee1abd55d04e49baf639a08bd23ff52457df4
Adam Pietrzyk <apietrzyk92@gmail.com>
2023-01-10 23:18:09 +0100

    Iterative and recursive function implementations.

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,22 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int a0 = 0;
+    int a1 = 1;
+
+    for(int i=0; i<sequence; i++) {
+        a1 += a0;
+        a0 = a1 - a0;
+    }
+    return a0;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    } else if (sequence < 3) {
+        return 1;
+    } else {
+        return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
+    }
 }
