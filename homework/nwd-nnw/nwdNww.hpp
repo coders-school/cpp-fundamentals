61f58eea8bada4da8b171da4542fa5bdd6a7bcfd
Adam Pietrzyk <apietrzyk92@gmail.com>
2023-01-11 17:23:03 +0100

    NWW implementation

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -31,6 +31,9 @@ int NWD(int lhs, int rhs) {
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 || rhs == 0) {
+        return 0;
+    } else {
+        return abs(lhs / NWD(lhs, rhs) * rhs);
+    }
 }
