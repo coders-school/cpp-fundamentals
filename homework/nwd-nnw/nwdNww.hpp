edc781803590f50d006b31eff4e8e824363d5cf0
Adam Pietrzyk <apietrzyk92@gmail.com>
2023-01-11 13:16:08 +0100

    First implementation of NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,7 +2,31 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    if (lhs == 0) {
+        return abs(rhs);
+    } else if (rhs == 0) {
+        return abs(lhs);
+    } else if (abs(lhs) < abs(rhs)) {
+        if (rhs % lhs == 0) {
+            return abs(lhs);
+        } else {
+            int tmp = lhs;
+            lhs = rhs % lhs;
+            rhs = tmp;
+            return abs(NWD(lhs, rhs));
+        }
+    } else if (abs(lhs) > abs(rhs)) {
+        if ( lhs % rhs == 0) {
+            return abs(rhs);
+        } else {
+            int tmp = rhs;
+            rhs = lhs % rhs;
+            lhs = tmp;
+            return abs(NWD(lhs, rhs));
+        }
+    } else {
+        return abs(lhs);
+    }
 }
 
 int NWW(int lhs, int rhs) {
