68f5d3a3d2cdb34040b96078530066393edc52aa
Adam Pietrzyk <apietrzyk92@gmail.com>
2023-01-14 14:07:39 +0100

    addEven first implementation

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,14 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int tmp = 0;
+    if (numbers.size() != 0) {
+        for (auto i = numbers.begin(); i != numbers.end(); ++i) {
+            int element = *i;
+            if (element % 2 == 0) {
+                tmp += element;
+            }   
+        }
+    }
+    return tmp;
 }
