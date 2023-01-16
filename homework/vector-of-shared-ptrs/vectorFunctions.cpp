9c16c0667452566833e4690eaaa2b6f46b70b2de
PaSawi <sawicki.patryk@gmail.com>
2023-01-16 11:46:38 +0100

    vector of shared ptrs

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,17 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count){
+
+}
+void print(std::vector<std::shared_ptr<int>> vec){
+
+}
+void add10(std::vector<std::shared_ptr<int>> vec){
+
+}
+void sub10(int* const raw_point){
+    
+}
+void sub10(std::vector<std::shared_ptr<int>> vec){
+
+}
