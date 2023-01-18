00777b83e4f3db5bd0d36d693f97acaa62522440
PaSawi <sawicki.patryk@gmail.com>
2023-01-18 11:31:40 +0100

    password-check-bonus-4

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -9,22 +9,16 @@ std::string getErrorMessage(ErrorCode errorCode) {
     switch (errorCode) {
     case ErrorCode::Ok:
         return "Ok";
-        break;
     case ErrorCode::PasswordNeedsAtLeastNineCharacters:
         return "Password needs to have at least nine characters";
-        break;
     case ErrorCode::PasswordNeedsAtLeastOneNumber:
         return "Password needs to have at least one number";
-        break;
     case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
         return "Password needs to have at least one special character";
-        break;
     case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
         return "Password needs to have at least one uppercase letter";
-        break;
     case ErrorCode::PasswordsDoNotMatch:
         return "Passwords do not match";
-        break;
     }
 }
 
