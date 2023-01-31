03bd317b243713559b3db9136feb14b1bb943471
Adam Pietrzyk <apietrzyk92@gmail.com>
2023-01-31 11:43:02 +0100

    error message enum + getErrorMessage initial draft

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,26 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+std::string getErrorMessage(class ErrorCode error) {
+    std::string msg;
+    switch (error) {
+    case OK:
+        msg = "OK";
+        break;
+    case PasswordNeedsAtLeastNineCharacters:
+        msg = "PasswordNeedsAtLeastNineCharacters";
+        break;
+    case PasswordNeedsAtLeastOneNumber:
+        msg = "PasswordNeedsAtLeastOneNumber";
+        break;
+    case PasswordNeedsAtLeastOneSpecialCharacter:
+        msg = "PasswordNeedsAtLeastOneSpecialCharacter";
+        break;
+    case PasswordNeedsAtLeastOneUppercaseLetter:
+        msg = "PasswordNeedsAtLeastOneUppercaseLetter";
+        break;
+    case PasswordsDoNotMatch:
+        msg = "PasswordsDoNotMatch";
+        break;
+    }
+    return msg;
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,15 @@
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+#pragma once
+#include "validation.cpp"
+#include <string>
+
+enum class ErrorCode {
+    OK,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+std::string getErrorMessage(class ErrorCode error);
