382485f1fca40a38ec602f7c710bbac26fca467d
MarekSkrobarczyk <114141740+MarekSkrobarczyk@users.noreply.github.com>
2023-02-07 20:44:46 +0100

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,17 @@
+\
 #pragma once
+#include<iostream>
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs)
+{
+    if (rhs != 0)
+        return NWD(rhs, lhs % rhs);
+
+    return lhs;
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int lhs, int rhs)
+{
+    return (lhs / NWD(lhs, rhs) * rhs);
 }
+
