abbe761e99498f62384c85a853dde8b28921e63e
root <root@DESKTOP-2PFAQAK.localdomain>
2023-02-08 16:26:52 +0100

    everything works

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,14 @@
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    	int x = vec[0];
+	for (int i = 1; i < vec.size(); i++){
+		if (x < vec[i]) {
+	  	    x = vec[i];
+		}
+		else {
+		    x = x;
+		}
+	}
+	return x;
 }
