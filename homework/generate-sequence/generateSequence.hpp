7593009b34128d8023999e25c367261b747b2d4d
root <root@DESKTOP-2PFAQAK.localdomain>
2023-02-10 15:07:10 +0100

    generate works

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,16 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+	if (count > 0){
+	std::vector<int> x;
+	x.push_back(step);
+	
+	for (int i = 0; i < count - 1; i++) {
+	x.push_back(x[i] + step);
+	}
+	
+	return x;
+	}
+	else
+		return {};
 }
