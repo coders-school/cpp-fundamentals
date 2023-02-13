2fa373fa8bba8efbca1726e2247c6a9c54325003
Marcin <123724557+Toladim@users.noreply.github.com>
2023-02-13 21:14:56 +0100

    Update 01-multiply.cpp

--- 03-language-elements/tasks/01-multiply.cpp
+++ 03-language-elements/tasks/01-multiply.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> 
 
 int multiply(int a, int b)
     {return a * b;}
@@ -7,6 +7,7 @@ int main() {
     std::cout << "4 * 5: " << multiply(4, 5) << "\n";
     std::cout << "10 * 5: " << multiply(10, 5) << "\n";
     std::cout << "-5 * 5: " << multiply(-5, 5) << "\n";
+    
   
     return 0;
 }
