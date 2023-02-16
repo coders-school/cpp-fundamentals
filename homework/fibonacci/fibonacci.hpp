af7e2902afceb25fe7c09be58b6de84031f12dba
Marcin <123724557+Toladim@users.noreply.github.com>
2023-02-16 17:43:16 +0100

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    auto result = 0;
+    long result = 0;
     for (int i = 0; i <= sequence; i++) {
         result = result + i;
-    } 
+   } 
    return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    int result = 0;
+    long result = 0;
     if (sequence < 3)
         return 1;
     return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
