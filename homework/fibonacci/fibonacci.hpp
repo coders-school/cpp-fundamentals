18128203a71e2eec584d72a2d4f3fd3a44f20dd6
Marcin <123724557+Toladim@users.noreply.github.com>
2023-02-16 18:54:32 +0100

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -6,13 +6,12 @@ int fibonacci_iterative(int sequence) {
         return 0;
     } else if (sequence == 1) {
         return 1;
-    }
-    else {
+    } else {
         for (int i = 0; i < sequence; i++) {
             second += first;
             first = second - first;
         }
-         return first;
+        return first;
     }
 }
 
@@ -21,8 +20,7 @@ int fibonacci_recursive(int sequence) {
         return 0;
     } else if (sequence == 1) {
         return 1;
-    }
-    else {
+    } else {
         return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
     }
 }
