407ec06459ae52d5329923d503d88c66cbdab6f9
Marcin <123724557+Toladim@users.noreply.github.com>
2023-02-18 23:10:00 +0100

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,35 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int REST = 0;
+    if (rhs == 0) {
+        return lhs;
+    }
+    else {
+        while (lhs % rhs != 0) {
+            REST = lhs % rhs;
+            lhs = rhs;
+            rhs = REST;
+        }
+        if (rhs < 0) {
+            return rhs * -1;
+        }
+        else {
+            return rhs;
+        }
+    }
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    
+    if (lhs == 0 || rhs == 0) {
+        return 0;
+    } else {
+        if ((lhs * rhs) / NWD(lhs, rhs) < 0) {
+            return (lhs * rhs) / NWD(lhs, rhs) * -1;
+        } else {
+            return (lhs * rhs) / NWD(lhs, rhs);
+        }
+    }
+ 
 }
