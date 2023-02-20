264136f7d8a69cefebd4c107910a1d79af8a0348
BeniaB3 <beniab333@gmail.com>
2023-02-20 21:30:41 +0100

    Working fibo

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,25 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0 || sequence == 1){
+        return sequence;
+    }
+    int first = 0;
+    int second = 1;
+    int third = 0;
+
+    for (int i = 1; i < sequence; i++){
+        third = first + second;
+        first = second;
+        second = third;
+    }
+    return third;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0 || sequence == 1){
+        return sequence;
+    }
+    unsigned long long result = fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    return result;
 }
