a95f3ff3644a5ad603c8bc4e09e4e963310446fd
BeniaB3 <beniab333@gmail.com>
2023-02-20 21:08:05 +0100

    WebKit format

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,23 +1,24 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string &command, int first, int second) {
-  if (command == "add") {
-    return std::to_string(first + second);
+std::string calculate(const std::string& command, int first, int second)
+{
+    if (command == "add") {
+        return std::to_string(first + second);
 
-  } else if (command == "subtract") {
-    return std::to_string(first - second);
+    } else if (command == "subtract") {
+        return std::to_string(first - second);
 
-  } else if (command == "multiply") {
-    return std::to_string(first * second);
+    } else if (command == "multiply") {
+        return std::to_string(first * second);
 
-  } else if (command == "divide") {
-    if (first == 0 || second == 0) {
-      return "Division by 0";
+    } else if (command == "divide") {
+        if (first == 0 || second == 0) {
+            return "Division by 0";
+        }
+        return std::to_string(first / second);
+
+    } else {
+        return "Invalid data";
     }
-    return std::to_string(first / second);
-
-  } else {
-    return "Invalid data";
-  }
 }
