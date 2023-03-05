428e2ad676c55a66059df26bb51683ad046ad17c
BeniaB3 <beniab333@gmail.com>
2023-03-05 21:42:10 +0100

    completed getErrorMessage definition and doPasswordsMatch definition

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,28 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+auto getErrorMessage(ErrorCode code_to_convert) -> std::string {
+    switch (code_to_convert) {
+        case ErrorCode::Ok:
+            return "Ok";
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+            return "Password needs to have at least one number";
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+            return "Password needs to have at least one special character";
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+            return "Password needs to have at least one uppercase letter";
+        case ErrorCode::PasswordsDoNotMatch:
+            return "Password do not match";
+    }
+}
+
+auto doPasswordsMatch(std::string password_first, std::string password_second) -> bool {
+    return password_first == password_second;
+}
+
+auto checkPasswordRules(std::string password) -> ErrorCode {
+
+}
+
+auto checkPassword(std::string password_first, std::string password_second) -> ErrorCode {
+
+}
