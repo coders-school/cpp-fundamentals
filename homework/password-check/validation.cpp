f800afb44e466b9f062221381a51703eccf935f6
BeniaB3 <beniab333@gmail.com>
2023-03-05 22:39:05 +0100

    completed definition for checkPasswordRules fun

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,43 +1,44 @@
 #include "validation.hpp"
-#include <cctype>
 #include <algorithm>
 #include <ranges>
+#include <string>
 
 auto getErrorMessage(ErrorCode code_to_convert) -> std::string {
     switch (code_to_convert) {
         case ErrorCode::Ok:
             return "Ok";
-        case ErrorCode::PasswordNeedsAtLeastOneNumber: //ok
+        case ErrorCode::PasswordNeedsAtLeastOneNumber: 
             return "Password needs to have at least one number";
         case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
             return "Password needs to have at least one special character";
-        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter: //ok
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
             return "Password needs to have at least one uppercase letter";
-        case ErrorCode::PasswordNeedsAtLeastNineCharacters: // ok
-            return "Password need to have at least nine characters";
-        case ErrorCode::PasswordsDoNotMatch: // ok
-            return "Password do not match";
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+            return "Password needs to have at least nine characters";
+        case ErrorCode::PasswordsDoNotMatch:
+            return "Passwords do not match";
     }
+    return "Error ocurred";
 }
 
-auto doPasswordsMatch(std::string password_first, std::string password_second) -> bool {
+bool doPasswordsMatch(std::string password_first, std::string password_second) {
     return password_first == password_second;
 }
 
-auto checkPasswordRules(std::string password) -> ErrorCode {
+ErrorCode checkPasswordRules(std::string password){
     if (password.size() < 9) {
         return ErrorCode::PasswordNeedsAtLeastNineCharacters;
     } 
-    if (std::count_if(password.begin(), password.end(), [](int x){ std::isupper(x);}) < 1) {
+    if (!std::none_of(password.begin(), password.end(), [](auto x){ return std::isupper(x);})) {
         return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
     }
-    if (std::none_of(password.begin(), password.end(), [](int x){ std::isdigit(x);})) {
+    if (!std::none_of(password.begin(), password.end(), [](auto x){ return std::isdigit(x);})) {
         return ErrorCode::PasswordNeedsAtLeastOneNumber;
     }
-    if (std::none_of(password.begin(), password.end(), [](int x) { std::ispunct(x);})) {
+    if (!std::none_of(password.begin(), password.end(), [](auto x){ return std::ispunct(x);})) {
         return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
     }
-
+    return ErrorCode::Ok;
 }
 
 auto checkPassword(std::string password_first, std::string password_second) -> ErrorCode {
