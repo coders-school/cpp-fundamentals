a5e98101ac4107c01671add0ce552a9be7196e47
Benia <104169955+BeniaB3@users.noreply.github.com>
2023-03-06 18:02:34 +0100

    Update validation.cpp

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,59 +1,52 @@
 #include "validation.hpp"
 // TODO: Put implementations here
-std::string getErrorMessage(ErrorCode code){
+std::string getErrorMessage(ErrorCode code) {
     std::string message;
-    switch(code){
-        case ErrorCode::Ok:
-            message = "Ok";
-            break;
-        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
-            message = "Password needs to have at least nine characters";
-            break;
-        case ErrorCode::PasswordNeedsAtLeastOneNumber:
-            message = "Password needs to have at least one number";
-            break;
-        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
-            message = "Password needs to have at least one special character";
-            break;
-        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
-            message = "Password needs to have at least one uppercase letter";
-            break;
-        case ErrorCode::PasswordsDoNotMatch:
-            message = "Passwords do not match";
-            break;
+    switch (code) {
+    case ErrorCode::Ok:
+        message = "Ok";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        message = "Password needs to have at least nine characters";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        message = "Password needs to have at least one number";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+        message = "Password needs to have at least one special character";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        message = "Password needs to have at least one uppercase letter";
+        break;
+    case ErrorCode::PasswordsDoNotMatch:
+        message = "Passwords do not match";
+        break;
     }
     return message;
 }
 
-bool doPasswordsMatch(std::string password, std::string repeatedPassword){
+bool doPasswordsMatch(std::string password, std::string repeatedPassword) {
     return password == repeatedPassword;
 }
 
-ErrorCode checkPasswordRules(std::string password){
-    if(password.length() < 9){
+ErrorCode checkPasswordRules(std::string password) {
+    if (password.length() < 9) {
         return ErrorCode::PasswordNeedsAtLeastNineCharacters;
-    }
-    else if(std::none_of(password.begin(), password.end(), ::isdigit)){
+    } else if (std::none_of(password.begin(), password.end(), ::isdigit)) {
         return ErrorCode::PasswordNeedsAtLeastOneNumber;
-    }
-    else if(std::none_of(password.begin(), password.end(), ::isupper)){
+    } else if (std::none_of(password.begin(), password.end(), ::isupper)) {
         return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
-    }
-    else if(std::none_of(password.begin(), password.end(), [](char c) { return std::ispunct(c) || std::isspace(c); })){
+    } else if (std::none_of(password.begin(), password.end(), [](char c) { return std::ispunct(c) || std::isspace(c); })) {
         return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
-    }
-    else{
+    } else {
         return ErrorCode::Ok;
     }
 }
 
-ErrorCode checkPassword(std::string password, std::string repeatedPassword){
-    if(!doPasswordsMatch(password, repeatedPassword)){
+ErrorCode checkPassword(std::string password, std::string repeatedPassword) {
+    if (!doPasswordsMatch(password, repeatedPassword)) {
         return ErrorCode::PasswordsDoNotMatch;
-    } 
-    else{
+    } else {
         return checkPasswordRules(password);
     }
 }
-
-
