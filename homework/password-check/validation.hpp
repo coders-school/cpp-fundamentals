c10bfcd0f685d5f63308c5556332f7ea987c6e15
Benia <104169955+BeniaB3@users.noreply.github.com>
2023-03-06 17:57:30 +0100

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,19 +1,19 @@
-#pragma once
-#include <string>
 
-enum class ErrorCode {
-    Ok = 0,
-    PasswordNeedsAtLeastNineCharacters = 1,
-    PasswordNeedsAtLeastOneNumber = 2,
-    PasswordNeedsAtLeastOneSpecialCharacter = 3,
-    PasswordNeedsAtLeastOneUppercaseLetter = 4,
-    PasswordsDoNotMatch = 5
+#pragma once
+#include <algorithm>
+#include <string>
+// TODO: I'm empty :) Put enum and function headers here.
+enum class ErrorCode{
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
 };
 
-auto getErrorMessage(ErrorCode code_to_convert) -> std::string;
-
-auto doPasswordsMatch(std::string password_first, std::string password_second) -> bool;
-
-auto checkPasswordRules(std::string password) -> ErrorCode;
-
-auto checkPassword(std::string password_first, std::string password_second) -> ErrorCode;
+std::string getErrorMessage(ErrorCode code);
+bool doPasswordsMatch(std::string password, std::string repeatedPassword);
+ErrorCode checkPasswordRules(std::string password);
+ErrorCode checkPassword(std::string password, std::string repeatedPassword);
+// Don't forget the header guard - #pragma once
