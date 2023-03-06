47b71c3bbc698ed2771715ef5b79e592d10b32f8
BeniaB3 <beniab333@gmail.com>
2023-03-06 17:02:57 +0100

    generate fun defintion done

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,21 +1,21 @@
 #include "vectorFunctions.hpp"
 
 auto generate(int count) -> std::vector<std::shared_ptr<int>> {
-
+    std::vector<std::shared_ptr<int>> result_vector;
+    for (int i = 0; i < count; i++) {
+        result_vector.push_back(std::make_shared<int>(i));
+    }
+    return result_vector;
 }
 
 auto print(std::vector<std::shared_ptr<int>> vector) -> void {
-
 }
 
 auto add10(std::vector<std::shared_ptr<int>> vector) -> void {
-
 }
 
-auto sub10(int * const pointer) -> void {
-
+auto sub10(int* const pointer) -> void {
 }
 
 auto sub10(std::vector<std::shared_ptr<int>> vector) -> void {
-
 }
