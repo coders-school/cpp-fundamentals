12b3bf66521c05dae8f1ac91124f4e10f7a8c627
wojcikbartosz <w.bartosz@outlook.com>
2023-03-14 18:12:41 +0100

    fibonacci done

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,37 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+	int f0 = 0, f1 = 1, result = 0;
+	if(sequence == 0)
+		return f0;
+	else if(sequence == 1)
+		return f1;
+	else
+	{
+		for(int i=2;i<=sequence;i++) {
+			result = f0 + f1;
+			f0 = f1;
+			f1 = result;
+		}
+	}
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+	static int  f0 = 0;
+	if(sequence == 0)
+		return 0;
+	else if(sequence == 1)
+	{
+		return 1 + fibonacci_recursive(sequence - 1);
+	}
+	else
+	{
+		int fibPrevResult = fibonacci_recursive(sequence - 1);
+		int result = f0 + fibPrevResult;
+		f0 = fibPrevResult;
+		return result;
+	}
+
     return 0;
 }
