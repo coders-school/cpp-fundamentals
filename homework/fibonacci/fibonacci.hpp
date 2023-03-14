5d4da92eebbe76723c71162d1641eaebefe59fa7
wojcikbartosz <w.bartosz@outlook.com>
2023-03-14 18:21:28 +0100

    new formatting

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -6,8 +6,7 @@ int fibonacci_iterative(int sequence) {
 		return f0;
 	else if(sequence == 1)
 		return f1;
-	else
-	{
+	else {
 		for(int i=2;i<=sequence;i++) {
 			result = f0 + f1;
 			f0 = f1;
@@ -21,17 +20,12 @@ int fibonacci_recursive(int sequence) {
 	static int  f0 = 0;
 	if(sequence == 0)
 		return 0;
-	else if(sequence == 1)
-	{
+	else if(sequence == 1) {
 		return 1 + fibonacci_recursive(sequence - 1);
-	}
-	else
-	{
+	} else {
 		int fibPrevResult = fibonacci_recursive(sequence - 1);
 		int result = f0 + fibPrevResult;
 		f0 = fibPrevResult;
 		return result;
 	}
-
-    return 0;
 }
