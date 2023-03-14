975bd01d8c91d2fdad4ce065113ce95926dd49a3
wojcikbartosz <w.bartosz@outlook.com>
2023-03-14 16:51:00 +0100

    newest formatting

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,14 +5,14 @@ std::string calculate(const std::string& command, int first, int second) {
 	if(command == "add") {
 		return std::to_string(first + second);
 	} else if(command == "subtract") {
-                return std::to_string(first - second);
-        } else if(command == "multiply") {
-                return std::to_string(first * second);
-        } else if(command == "divide") {
+		return std::to_string(first - second);
+	} else if(command == "multiply") {
+		return std::to_string(first * second);
+	} else if(command == "divide") {
 		if(second != 0)
-                	return std::to_string(first / second);
+			return std::to_string(first / second);
 		else
 			return "Division by 0";
-        }
+	}
 	return "Invalid data";
 }
