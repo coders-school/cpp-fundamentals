2346c1f53189bf6ec62383a82ce3d6ba555a18c9
ca5per1 <119765241+ca5per1@users.noreply.github.com>
2023-03-28 23:49:12 +0200

    adding vectorFunctions.cpp

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,32 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> vec;
+    for (size_t i = 0; i < count; i++) {
+        vec.push_back(std::make_shared<int>(i));
+    }
+    return vec;
+}
+void print(std::vector<std::shared_ptr<int>>& vec) {
+    for (const auto& el : vec) {
+        std::cout << *el << " ";
+    }
+    std::cout << '\n';
+}
+void add10(std::vector<std::shared_ptr<int>>& vec) {
+    for (const auto& el : vec) {
+        if (el != nullptr) {
+            *el += 10;
+        }
+    }
+}
+void sub10(int* const A) {
+    if (A != nullptr) {
+        *A -= 10;
+    }
+}
+void sub10(std::vector<std::shared_ptr<int>>& vec) {
+    for (const auto& el : vec) {
+        sub10(el.get());
+    }
+}
