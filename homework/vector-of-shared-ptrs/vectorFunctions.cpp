f409bc8cc67a7b960da3256834c5651b40de564b
ca5per1 <119765241+ca5per1@users.noreply.github.com>
2023-03-28 23:53:33 +0200

    Update vectorFunctions.cpp

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,32 +1,30 @@
 #include "vectorFunctions.hpp"
+#include <iostream>
 
 std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> vec;
-    for (size_t i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++) {
         vec.push_back(std::make_shared<int>(i));
     }
     return vec;
 }
-void print(std::vector<std::shared_ptr<int>>& vec) {
+void print(std::vector<std::shared_ptr<int>> vec) {
     for (const auto& el : vec) {
         std::cout << *el << " ";
     }
-    std::cout << '\n';
 }
-void add10(std::vector<std::shared_ptr<int>>& vec) {
-    for (const auto& el : vec) {
-        if (el != nullptr) {
+void add10(std::vector<std::shared_ptr<int>> vec) {
+    for (auto& el : vec) {
+        if (el)
             *el += 10;
-        }
     }
 }
-void sub10(int* const A) {
-    if (A != nullptr) {
-        *A -= 10;
-    }
+void sub10(int* const el) {
+    if (el)
+        *el -= 10;
 }
 void sub10(std::vector<std::shared_ptr<int>>& vec) {
-    for (const auto& el : vec) {
-        sub10(el.get());
+    for (auto& i : vec) {
+        sub10(i.get());
     }
 }
