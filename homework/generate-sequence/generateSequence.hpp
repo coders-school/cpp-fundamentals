468ff0c47618df655c6861ffeaf7434b2715e6cf
ca5per1 <119765241+ca5per1@users.noreply.github.com>
2023-03-28 23:25:05 +0200

    Update generateSequence.hpp

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,7 +1,14 @@
-#pragma once
 #include <vector>
+#pragma once
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    if (count <= 0) {
+        return {};
+    }
+
+    std::vector<int> sequence(count);
+    for (int i = 0; i < count; i++) {
+        sequence[i] = (i == 0) ? step : sequence[i - 1] + step;
+    }
+    return sequence;
 }
