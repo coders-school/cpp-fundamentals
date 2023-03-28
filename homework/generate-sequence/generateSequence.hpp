a8169744453df9f225259fdce6d33fb4d565faec
AdamLatynos <118278456+AdamLatynos@users.noreply.github.com>
2023-03-28 21:09:05 +0200

    Update generateSequence.hpp
    am"adding works"

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,8 +1,11 @@
-
-#pragma once
 #include <vector>
+#pragma once
 
 std::vector<int> generateSequence(int count, int step) {
+    if (count <= 0) {
+        return {};
+    }
+
     std::vector<int> sequence(count);
     for (int i = 0; i < count; i++) {
         sequence[i] = (i == 0) ? step : sequence[i - 1] + step;
