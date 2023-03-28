687e16410c24a020da247bf84adc41da7fb1a713
Bartosz Wójcik <w.bartosz@outlook.com>
2023-03-28 20:28:57 +0200

    compilation error fixed

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,52 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+std::string getErrorMessage(ErrorCode code)
+{
+	switch (code)
+	{
+		case ErrorCode::Ok:
+		{
+			return "Ok";
+		}
+		case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+		{
+			return "Password needs to have at least nine characters";
+		}
+		case ErrorCode::PasswordNeedsAtLeastOneNumber:
+		{
+			return "Password needs to have at least one number";
+		}
+		case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+		{
+			return "Password needs to have at least one special character";
+		}
+		case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+		{
+			return "Password needs to have at least one uppercase letter";
+		}
+		case ErrorCode::PasswordsDoNotMatch:
+		{
+			return "Passwords do not match";
+		}
+	}
+}
+bool doPasswordsMatch(std::string a, std::string b)
+{
+	if(a.compare(b) == 0)
+		return true;
+	else
+		return false;
+}
+ErrorCode checkPasswordRules(std::string a)
+{
+	srand (time(NULL));
+	switch (rand()%6)
+	{
+
+	}
+	return er;
+}
+ErrorCode checkPassword(std::string a, std::string b)
+{
+	ErrorCode er;
+	return er;
+}
