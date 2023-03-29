947120749f89c77d3ed79ed8b24a673dcb92a5ee
Bartosz Wójcik <w.bartosz@outlook.com>
2023-03-29 10:36:31 +0200

    create, print ready

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,4 +1,5 @@
 #include "vectorFunctions.hpp"
+#include <iostream>
 std::vector<std::shared_ptr<int>> generate(int count)
 {
 	std::vector<std::shared_ptr<int>> v;
@@ -10,7 +11,10 @@ std::vector<std::shared_ptr<int>> generate(int count)
 }
 void print(std::vector<std::shared_ptr<int>>& v)
 {
-	
+	for(std::shared_ptr<int> element : v)
+	{
+		std::cout<<*element<<std::endl;
+	}
 }
 void add10(std::vector<std::shared_ptr<int>>& v)
 {
