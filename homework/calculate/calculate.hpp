4c52171c57e881f544504963821a203cf1d3aee5
Klaudiusz Ogrodnik <ogrodnik.klaudiusz@gmail.com>
2023-04-02 18:11:33 +0200

    New format style

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -4,16 +4,20 @@
 std::string calculate(const std::string& command, int first, int second) {
     if (command == "add") {
         return std::to_string(first + second);
-    } else if (command == "subtract") {
+    } 
+    else if (command == "subtract") {
         return std::to_string(first - second);
-    } else if (command == "multiply") {
+    } 
+    else if (command == "multiply") {
         return std::to_string(first * second); 
-    } else if (command == "divide") {
+    } 
+    else if (command == "divide") {
         if (second == 0) {
             return "Division by 0";
         }
         return std::to_string(first / second);
-    } else {
+    } 
+    else {
         return "Invalid data";
     }
     return "";
