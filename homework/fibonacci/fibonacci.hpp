611284ea4989ae17eadce662596281a0db305e45
Klaudiusz Ogrodnik <ogrodnik.klaudiusz@gmail.com>
2023-04-02 20:01:49 +0200

    Adding Fibonacci Recursively

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -11,6 +11,9 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+    if (sequence <= 1) {
+        return sequence;
+    }
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
     return 0;
 }
