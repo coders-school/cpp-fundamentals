0bd4a3580eabc4d9df5fb0b298fc4a516ed52c0b
dominikpalatynski <palatynski.dominik@gmail.com>
2023-04-02 13:06:02 +0200

    adding

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,32 @@
 #pragma once
+#include <algorithm>
+#include <cctype>
+#include <iostream>
 #include <string>
+#include <vector>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    double score;
+    std::string checker = command;
+    std::string b;
+
+    // transforming toLower letters
+    std::transform(checker.begin(), checker.end(), checker.begin(), [](unsigned char c) { return ::tolower(c); });
+
+    // removing white spaces
+    std::remove_if(checker.begin(), checker.end(), [](unsigned char c) { return c == ' '; });
+
+    if (checker == "add") {
+        score = first + second;
+    } else if (checker == "subtract") {
+        score = first - second;
+    } else if (checker == "multiply") {
+        score = first * second;
+    } else if (checker == "divide") {
+        score = first / second;
+    } else {
+        return "invalid Data";
+    }
+
+    return std::to_string(score);
 }
