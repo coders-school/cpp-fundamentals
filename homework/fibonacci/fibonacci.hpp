8da94b9a09b68fe8b55b492f841b842b352a5a00
Klaudiusz Ogrodnik <ogrodnik.klaudiusz@gmail.com>
2023-04-02 19:58:31 +0200

    Adding Fibonacci Iteratively

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int fib[sequence];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i <= sequence; i++) {
+        fib[i] = fib[i -1] + fib[i -2];
+    }
+    return fib[sequence];
 }
 
 int fibonacci_recursive(int sequence) {
