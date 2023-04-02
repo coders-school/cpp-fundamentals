4f983ff0fbad13c9936ac12762a559ea6fb90662
Klaudiusz Ogrodnik <ogrodnik.klaudiusz@gmail.com>
2023-04-02 17:44:17 +0200

    Adding subtract function

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,8 +2,9 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    if ("add") {
+    if (command == "add") {
         return std::to_string(first + second);
+    } else if (command == "subtract") {
+        return std::to_string(first - second);
     }
-    return "";
 }
