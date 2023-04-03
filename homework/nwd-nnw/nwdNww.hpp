4725e0b7e19f4c7ea84e19a2e149e7cf4e88b945
Klaudiusz Ogrodnik <ogrodnik.klaudiusz@gmail.com>
2023-04-03 20:28:20 +0200

    Adding NWD function

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (rhs < 0) {
+        return rhs * (-1);
+    }
+    
+    int variable = 0;
+    while (rhs != 0) {
+        variable = rhs;
+        rhs = lhs % rhs;
+        lhs = variable;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
