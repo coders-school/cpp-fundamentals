0e6853a7f37b89193b8b2693d01a55913eab934c
dominikpalatynski <palatynski.dominik@gmail.com>
2023-04-03 21:06:51 +0200

    check

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,20 @@
 #pragma once
+#include <math.h>
+#include <cmath>
+#include <cstdlib>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int nwd;
+    int counter = 1;
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+    while (counter <= lhs && counter <= rhs) {
+        if (lhs % counter == 0 && rhs % counter == 0) {
+            nwd = counter;
+        }
+        counter++;
+    }
+    return nwd;
 }
 
 int NWW(int lhs, int rhs) {
