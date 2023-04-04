e85dee1697ff2c97a7b151d008deef3dfe23eb84
michalmaj <michalpmaj@gmail.com>
2023-04-04 19:12:21 +0200

    all works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,36 @@
-#pragma once
+#include <iostream>
 #include <string>
+#include <array>
+#include <algorithm>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+
+/**
+ * \brief A function whose task is to perform arithmetic operations on two numbers.
+ * \param command Type of action: add, subtract, multiply, divide.
+ * \param first First number.
+ * \param second Second number.
+ * \return Result as a string.
+ */
+std::string calculate(const std::string& command, int first, int second)
+{
+	std::array<std::string, 4> valid{ "add", "subtract", "multiply", "divide" };
+	if (std::find(valid.begin(), valid.end(), command) == valid.end())
+		return "Invalid data";
+
+	if (command == valid.at(0))
+		return std::to_string(first + second);
+	if (command == valid.at(1))
+		return std::to_string(first - second);
+	if (command == valid.at(2))
+		return std::to_string(first * second);
+	// In this case check if second numbers is not 0.
+	if (command == valid.at(3))
+		return (second != 0)? std::to_string(first / second) : "Division by 0";
+
+	// Improvement: cast second number to double, to get a floating point result. Comment if above and uncomment this below.
+	// The trick to set precision is get the substring (substr method) from 0 to dot character + arbitrary value
+	/*if (command == valid.at(3))
+		return (second != 0) ? std::to_string(first / static_cast<double>(second)).substr(0, std::to_string(first / static_cast<double>(second)).find(".") + 3) : "ZeroDivisionError";*/
+
+	return "Something went wrong";
 }
