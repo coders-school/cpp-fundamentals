6ad4fe91a31900e23f4b41dc8b4d4e467dc4054d
michalmaj <michalpmaj@gmail.com>
2023-04-12 19:37:55 +0200

    done

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,7 +2,6 @@
 #include <string>
 
 std::string calculate(const std::string &command, int first, int second) {
-
     if (command == "add")
         return std::to_string(first + second);
     if (command == "subtract")
