ed253e6b8c1f12e567f278d1c0ff2788c3c7367b
michalmaj <michalpmaj@gmail.com>
2023-04-12 19:11:37 +0200

    done

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -8,7 +8,6 @@ std::string calculate(const std::string &command, int first, int second) {
     return std::to_string(first - second);
   if (command == "multiply")
     return std::to_string(first * second);
-
   if (command == "divide")
     return (second != 0) ? std::to_string(first / second) : "Division by 0";
 
