20d2c4d1cb3fa49361fb467628625042c852fa1e
michalmaj <michalpmaj@gmail.com>
2023-04-12 19:23:49 +0200

    done

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,14 +2,14 @@
 #include <string>
 
 std::string calculate(const std::string &command, int first, int second) {
-   if (command == "add")
-       return std::to_string(first + second);
-   if (command == "subtract")
-       return std::to_string(first - second);
-   if (command == "multiply")
-       return std::to_string(first * second);
-   if (command == "divide")
-       return (second != 0) ? std::to_string(first / second) : "Division by 0";
+    if (command == "add")
+        return std::to_string(first + second);
+    if (command == "subtract")
+        return std::to_string(first - second);
+    if (command == "multiply")
+        return std::to_string(first * second);
+    if (command == "divide")
+        return (second != 0) ? std::to_string(first / second) : "Division by 0";
 
-   return "Invalid data";
+    return "Invalid data";
 }
