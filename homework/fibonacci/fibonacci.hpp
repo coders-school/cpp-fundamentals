85c239634a2ed5314832af5224d5df7e3be7a858
michalmaj <michalpmaj@gmail.com>
2023-04-13 19:16:43 +0200

    Recursive version

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence <= 0)
+        return 0;
+    if (sequence == 1)
+        return 1;
+
+    int f{ 0 };
+    int s{ 1 };
+    int output{};
+    for(int i{2}; i < sequence; ++i){
+        output = f + s;
+        f = s;
+        s = output;
+    }
+    output = f + s;
+    return output;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+    if (sequence <= 0)
+        return 0;
+    else if (sequence == 1)
+        return 1;
+    else
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
     return 0;
 }
