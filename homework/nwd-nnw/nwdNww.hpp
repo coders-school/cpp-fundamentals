da4984d051af91e28fb365f86668f4220f6ab241
michalmaj <michalpmaj@gmail.com>
2023-04-13 19:43:02 +0200

    Full implementation

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    while (rhs != 0) {
+        int t = rhs;
+        rhs = lhs % rhs;
+        lhs = t;
+    }
+
+return std::abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (rhs == 0)
+        return 0;
+    return std::abs(lhs * rhs / NWD(lhs, rhs));
 }
