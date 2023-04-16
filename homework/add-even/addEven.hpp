dabcc897b768bf071509b0a437678ba7fc96360d
michalmaj <michalpmaj@gmail.com>
2023-04-16 19:38:15 +0200

    Sum of even elements

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -5,7 +5,7 @@ int addEven(const std::vector<int>& numbers) {
     int sum{0};
     for(const auto& n : numbers) {
         if (n % 2 == 0)
-        sum += n;
+            sum += n;
     }
 
     return sum;
