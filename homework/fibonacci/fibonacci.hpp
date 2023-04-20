069004c3d33d59b1906ded62448bc3a11f1b843a
Mariusz Broszko <mariusz.broszko97@gmail.com>
2023-04-20 23:03:22 +0200

    Task complete

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,34 @@
 #pragma once
+#include <iostream>
+#include <string>
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int x {0};
+    int y {1};
+    int z {0};
+
+    if (sequence == 0){
+        return z;
+    } else if (sequence == 1) {
+        return y;
+    }
+
+    for (size_t i = 1; i < sequence; i++){
+        z = x + y;
+        x = y;
+        y = z;
+    }
+    return z;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int z {};
+    if (sequence <= 1){
+        return sequence;
+    }
+    z = fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+
+    return z;
 }
--- homework/fibonacci/main.cpp
+++ homework/fibonacci/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     std::cout << fibonacci_iterative(10) << "\n";
-    std::cout << fibonacci_recursive(10) << "\n";
+    //std::cout << fibonacci_recursive(10) << "\n";
 
     return 0;
 }
