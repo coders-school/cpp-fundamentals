e30b90076a775bf13cedfaa12a98dbb30317f94e
Mariusz Broszko <mariusz.broszko97@gmail.com>
2023-04-23 16:12:04 +0200

    MAx-of-vectors task dome

--- homework/max-of-vector/main.cpp
+++ homework/max-of-vector/main.cpp
@@ -3,7 +3,8 @@
 #include "maxOfVector.hpp"
 
 int main() {
-    std::vector<int> vec{4, 3, 2, 5, 6, 7, 9, 1, 0};
+    //std::vector<int> vec{4, 3, 2, 5, 6, 7, 9, 1, 0};
+    std::vector<int> vec{22, 88, 10, 0, 5, 6, 7, 8, 9};
     std::cout << maxOfVector(vec) << '\n';
 
     return 0;
--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -4,5 +4,19 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    int max_of_vector {vec[0]};
+    int count {0};
+    for (auto element : vec){
+        if (count == 0){
+            if (vec[count] > vec[0]){
+                max_of_vector = vec[count];
+            }
+        } else {
+            if (vec[count] > max_of_vector){
+                max_of_vector = vec[count];
+            }
+        }
+        count ++;
+    }
+    return max_of_vector;
 }
