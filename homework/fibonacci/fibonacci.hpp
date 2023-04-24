0a0eb115b4a4f2e41210d1182cafb36420be682b
Hubert Hodowaniec <hubert.hodowaniec@gmail.com>
2023-04-24 21:58:19 +0200

    Iterative calculation works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,23 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int zeroth = 0;
+    int first = 1;
+    int current = 0;
+
+    if (sequence == 0) {
+        return zeroth;
+    } else if (sequence == 1) {
+        return first;
+    } else {
+        for (int i = 2; i <= sequence; i++) {
+            current = first + zeroth;
+            zeroth = first;
+            first = current;
+        }
+    }
+
+    return current;
 }
 
 int fibonacci_recursive(int sequence) {
