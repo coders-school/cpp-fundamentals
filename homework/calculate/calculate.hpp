52f19ea1ddce5cc064f7cdb7243cf3531b64ff9b
Hubert Hodowaniec <hubert.hodowaniec@gmail.com>
2023-04-24 20:52:14 +0200

    Division works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -4,14 +4,22 @@
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
     int result = 0;
+    std::string retval = "";
     if (command.compare("add") == 0) {
         result = first + second;
     } else if (command.compare("subtract") == 0) {
         result = first - second;
     } else if (command.compare("multiply") == 0) {
         result = first * second;
+    } else if (command.compare("divide") == 0) {
+        if (second == 0) {
+            retval = "Division by 0";
+            return retval;
+        } else {
+            result = first / second;
+        }
     }
     
-    std::string retval{std::to_string(result)};
+    retval = std::to_string(result);
     return retval;
 }
