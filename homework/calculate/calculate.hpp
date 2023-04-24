1d29a31c3226a9f072361d958c5df2e5a41683d3
Hubert Hodowaniec <hubert.hodowaniec@gmail.com>
2023-04-24 20:22:18 +0200

    Addition works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,11 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    int result = 0;
+    if (command.compare("add") == 0) {
+        result = first + second;
+    }
+    
+    std::string retval{std::to_string(result)};
+    return retval;
 }
