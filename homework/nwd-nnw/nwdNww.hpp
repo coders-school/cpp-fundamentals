411158016b655879735fe542c5a90e164d8ec1b1
yasioo111 <janko111@gmail.com>
2023-06-25 08:09:36 +0200

    nwdNww done

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,32 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if (lhs == 0 && rhs != 0)
+		return std::abs(rhs);
+	else if (rhs == 0 && lhs != 0)
+		return std::abs(lhs);
+	else if (lhs == 0 && rhs ==0)
+		return 0;
+	
+	int max_divider = 1;
+	if (std::abs(lhs) < std::abs(rhs))
+		max_divider = std::abs(lhs);
+	else
+		max_divider = std::abs(rhs);
+		
+	int nwd = 1;
+	
+	for (int i = 2; i <= max_divider; i++) {
+		if (std::abs(lhs) % i == 0 && std::abs(rhs) % i == 0)
+			nwd = i;
+	}
+	
+	return nwd;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 || rhs == 0)
+		return 0;
+	return std::abs(lhs) / NWD(lhs, rhs) * std::abs(rhs);
 }
