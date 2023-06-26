1d358a0b48ffad77b3749d9553dfebfe7a514e81
yasioo111 <113231855+yasioo111@users.noreply.github.com>
2023-06-26 19:37:56 +0200

    formating corrected

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    if (sequence == 0)
+    if (sequence == 0) {
         return 0;
-    if (sequence == 1)
+    }
+    if (sequence == 1) {
         return 1;
+    }
     int a = 0;
     int b = 1;
     for (int i = 2; i <= sequence; i++) {
@@ -17,7 +19,8 @@ int fibonacci_iterative(int sequence) {
 
 
 int fibonacci_recursive(int sequence) {
-    if (sequence <= 1)
+    if (sequence <= 1) {
         return sequence;
+    }
     return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
