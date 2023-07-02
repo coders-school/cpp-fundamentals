a20c14636297d7ba2ab17860ad688078945e1066
Lucas <85084337+lukasz-bielerzewski@users.noreply.github.com>
2023-07-02 20:02:33 +0200

    Update 03-printString.cpp

--- 03-language-elements/tasks/03-printString.cpp
+++ 03-language-elements/tasks/03-printString.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-// Write your function here
+void printString(std::string string, int num)
+{
+    for(int i = 0; i < num; i++)
+        {
+            std::cout<< string;
+        }
+}
 
 int main() {
     printString("Hello", 5);
