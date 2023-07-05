826d0cd4578865d3262996e15c929eaa2a162bc1
AdamWiercioch95 <adamwiercioch95@gmail.com>
2023-07-05 10:07:08 +0200

    NWD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,18 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    while (rhs != 0) {
+        int temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
+    }
+    return abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
+    
     return -1;
 }
