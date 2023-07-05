f3e6cf90104e41efcb36fc462ee1289d35df3f37
AdamWiercioch95 <adamwiercioch95@gmail.com>
2023-07-05 11:43:23 +0200

    formating correction

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -4,12 +4,12 @@
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
     if (count <= 0) {
-        std::vector <int> vec;
+        std::vector<int> vec;
         return vec;
     }
-    std::vector <int> vec(count);
+    std::vector<int> vec(count);
     vec[0] = step;
-    for(int i = 1; i < count; i++) {
+    for (int i = 1; i < count; i++) {
         vec[i] = vec[i - 1] + step;
     } 
 
