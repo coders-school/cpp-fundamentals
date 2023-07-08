e864ce6d7a7c98c980b019610d6db9ff76222583
AdamWiercioch95 <adamwiercioch95@gmail.com>
2023-07-08 12:20:41 +0200

    make works

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,1 +1,15 @@
 #include "vectorFunctions.hpp"
+
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> vec;
+    return vec;
+}
+
+void print(std::vector<std::shared_ptr<int>> vec) {}
+
+void add10(std::vector<std::shared_ptr<int>> vec) {}
+
+void sub10(int* const ptr) {}
+
+void sub10(std::vector<std::shared_ptr<int>> vec) {}
