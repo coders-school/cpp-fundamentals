1f4e4221c507397dd6d1428b4cd4b73f1eacaeb7
AdamWiercioch95 <adamwiercioch95@gmail.com>
2023-07-10 09:01:36 +0200

    generate function added

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -3,6 +3,9 @@
 
 std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> vec;
+    for(int i = 0; i < count; i++) {
+        vec.push_back(std::make_shared<int>(i));
+    }
     return vec;
 }
 
