5d23a0369d42e1486e0426a2c465fc266fea5825
Andrzej Slomka <andrzejslomkapraca@gmail.com>
2023-07-11 22:36:13 +0200

    Added .clangformat file

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) 
+std::string calculate(const std::string &command, int first, int second)
 {
     if (command == "add")
     {
         int result = first + second;
         return std::to_string(result);
     }
-    else if(command == "subtract")
+    else if (command == "subtract")
     {
         int result = first - second;
         return std::to_string(result);
@@ -20,7 +20,7 @@ std::string calculate(const std::string& command, int first, int second)
     }
     else if (command == "divide")
     {
-        if(second == 0)
+        if (second == 0)
         {
             return "Division by 0";
         }
