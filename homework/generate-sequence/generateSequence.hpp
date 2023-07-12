a2085c3b1d55666cb9c7d305097ccf2c095c0479
Andrzej Slomka <andrzejslomkapraca@gmail.com>
2023-07-12 22:55:15 +0200

    Added .clang-format

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -1,13 +1,12 @@
 #pragma once
 #include <vector>
 
-std::vector<int> generateSequence(int count, int step)
-{
+std::vector<int> generateSequence(int count, int step) {
     std::vector<int> vec;
-    if(count <= 0) return vec;
+    if (count <= 0)
+        return vec;
     vec.push_back(step);
-    for (int i = 1; i < count; i++)
-    {
+    for (int i = 1; i < count; i++) {
         vec.push_back(vec.back() + step);
     }
     return vec;
