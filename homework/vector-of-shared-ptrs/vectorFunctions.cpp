924aeaa5c2870ec72561b1acef5c4a8eb00907f2
Andrzej Slomka <andrzejslomkapraca@gmail.com>
2023-07-12 23:14:27 +0200

    Added .clang-format

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,46 +1,35 @@
-#include"vectorFunctions.hpp"
+#include "vectorFunctions.hpp"
 
-std::vector<std::shared_ptr<int>> generate(int count)
-{
+std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> vec;
-    for (int i = 0; i < count; i++)
-    {
+    for (int i = 0; i < count; i++) {
         vec.push_back(std::make_shared<int>(i));
     }
     return vec;
 }
 
-void print(const std::vector<std::shared_ptr<int>>& vec)
-{
-    for(auto number : vec)
-    {
+void print(const std::vector<std::shared_ptr<int>>& vec) {
+    for (auto number : vec) {
         std::cout << *number << " ";
     }
     std::cout << std::endl;
 }
 
-void add10(std::vector<std::shared_ptr<int>>& vec)
-{
-    for(auto number : vec)
-    {
-        if(number)
-        {
+void add10(std::vector<std::shared_ptr<int>>& vec) {
+    for (auto number : vec) {
+        if (number) {
             *number += 10;
         }
     }
 }
 
-void sub10(int* const wsk)
-{
-    if(wsk)
-    {
+void sub10(int* const wsk) {
+    if (wsk) {
         *wsk -= 10;
     }
 }
-void sub10(std::vector<std::shared_ptr<int>>& vec)
-{
-    for(auto number : vec)
-    {
+void sub10(std::vector<std::shared_ptr<int>>& vec) {
+    for (auto number : vec) {
         sub10(number.get());
     }
 }
