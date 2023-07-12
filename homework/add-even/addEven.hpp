521f4b022e8b3e2f41532cd45477bf629dccd3b5
Andrzej Slomka <andrzejslomkapraca@gmail.com>
2023-07-12 22:18:39 +0200

    Added .clang-format

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -1,13 +1,10 @@
 #pragma once
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) 
-{
+int addEven(const std::vector<int>& numbers) {
     int sum = 0;
-    for(auto number : numbers)
-    {
-        if(number % 2 == 0)
-        {
+    for (auto number : numbers) {
+        if (number % 2 == 0) {
             sum += number;
         }
     }
