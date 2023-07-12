807f4f674c8832e1964f4769c0efecf0806cbf96
Andrzej Slomka <andrzejslomkapraca@gmail.com>
2023-07-12 21:47:05 +0200

    Added .clang-format

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
-int NWD(int lhs, int rhs) 
-{
-    if (lhs == 0 && rhs == 0) return 0;
-    if (lhs < 0 ) lhs *= -1;
-    if (rhs < 0 ) rhs *= -1;
+int NWD(int lhs, int rhs) {
+    if (lhs == 0 && rhs == 0)
+        return 0;
+    if (lhs < 0)
+        lhs *= -1;
+    if (rhs < 0)
+        rhs *= -1;
     int tmp;
-    while (rhs != 0)
-    {
+    while (rhs != 0) {
         tmp = rhs;
         rhs = lhs % rhs;
         lhs = tmp;
@@ -15,10 +16,12 @@ int NWD(int lhs, int rhs)
     return lhs;
 }
 
-int NWW(int lhs, int rhs) 
-{
-    if (lhs == 0 && rhs == 0) return 0;
-    if (lhs < 0 ) lhs *= -1;
-    if (rhs < 0 ) rhs *= -1;
+int NWW(int lhs, int rhs) {
+    if (lhs == 0 && rhs == 0)
+        return 0;
+    if (lhs < 0)
+        lhs *= -1;
+    if (rhs < 0)
+        rhs *= -1;
     return lhs * rhs / NWD(lhs, rhs);
 }
