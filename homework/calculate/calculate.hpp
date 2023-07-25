cf77fcda0c395cdaa00b35d659be61e207fa7408
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-25 18:24:54 +0200

    subtracting works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -7,7 +7,13 @@ std::string calculate(const std::string& command, int first, int second) {
     if (command == "add"){
             int result = first + second;
             std::string str_result = std::to_string(result);
-             return str_result;
+            return str_result;
     }
+    else if(command == "subtract"){
+            int result = first - second;
+            std::string str_result = std::to_string(result);
+            return str_result;
+    }
+
     return "something";
 }
