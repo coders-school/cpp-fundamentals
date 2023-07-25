a53c5bdc2d7a1444af64a1265ed40a1cf37fbb3c
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-25 18:30:23 +0200

    dividing works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -19,6 +19,15 @@ std::string calculate(const std::string& command, int first, int second) {
             std::string str_result = std::to_string(result);
             return str_result;
     }
+    else if(command == "divide"){
+            if (second == 0){
+                return "Division by 0";
+            }
+            int result = first / second;
+            std::string str_result = std::to_string(result);
+            return str_result;
+    }
+
 
     return "something";
 }
