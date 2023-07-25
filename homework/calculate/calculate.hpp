f5b5cabe2b34b1051fcc1ae0f71a8105b372d539
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-25 18:51:55 +0200

    formating correction2

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -4,19 +4,19 @@
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
  
-    if (command == "add"){
+    if (command == "add") {
             int result = first + second;
             std::string str_result = std::to_string(result);
             return str_result;
-    } else if(command == "subtract"){
+    } else if(command == "subtract") {
             int result = first - second;
             std::string str_result = std::to_string(result);
             return str_result;
-    } else if(command == "multiply"){
+    } else if(command == "multiply") {
             int result = first * second;
             std::string str_result = std::to_string(result);
             return str_result;
-    } else if(command == "divide"){
+    } else if(command == "divide") {
             if (second == 0){
                 return "Division by 0";
             }
