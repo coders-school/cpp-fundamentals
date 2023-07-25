d5cdfd3b92bda9dd3578bccad3531e41af993055
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-25 19:30:07 +0200

    Iteratively works

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,26 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    
+    if (sequence > 1) {
+        int Fn_2 = 0;
+        int Fn_1 = 1;
+        int Fn = Fn_2 + Fn_1;
+
+        for(int i = 0; i < sequence-2 ;i++)
+        {
+            Fn_2 = Fn_1;
+            Fn_1 = Fn;
+            Fn = Fn_1 + Fn_2;
+        }
+        return Fn;
+    } else if (sequence == 1){
+        int Fn = 1;
+        return Fn;
+    }
+    
+    int Fn = 0;
+    return Fn;
 }
 
 int fibonacci_recursive(int sequence) {
