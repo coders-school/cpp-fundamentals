5064451aa8af49c189c9284d5d045c9b562b048b
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-25 18:26:36 +0200

    multiplying works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -14,6 +14,11 @@ std::string calculate(const std::string& command, int first, int second) {
             std::string str_result = std::to_string(result);
             return str_result;
     }
+    else if(command == "multiply"){
+            int result = first * second;
+            std::string str_result = std::to_string(result);
+            return str_result;
+    }
 
     return "something";
 }
