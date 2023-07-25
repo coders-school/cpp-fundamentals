9ab4894db311ee8659c6b7b6024ea94dac380aa0
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-25 19:46:04 +0200

    Formatting corrected 2

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -25,7 +25,7 @@ int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
     if (sequence == 0) {
         return 0;
-    } else if(sequence < 3) {
+    } else if (sequence < 3) {
         return 1;
     }
     return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
