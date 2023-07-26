99cdede8e737a760764feab4e6f574909a53897e
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-26 19:51:54 +0200

    Max-of-vector works

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,15 @@
 #pragma once
 #include <limits>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    int max = vec.front();
+    for (auto el : vec) {
+        int temp = el;
+        if (temp > max) max = temp;
+    }
+    return max;
 }
