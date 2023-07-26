0936a58c6752b54e3bbb3f90870ecbd7bab498f7
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-26 18:33:13 +0200

    NWD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,17 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    // Optimized Euclid's algorithm
+    
+    int temporary =  abs(rhs);
+    while (rhs != 0) {
+        temporary = abs(rhs);
+        rhs = abs(lhs) % abs(rhs);
+        lhs = abs(temporary);
+    }
+    return abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
