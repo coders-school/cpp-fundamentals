dd9065d11345df65715b2ecce6bef8aaa1224c21
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-26 19:13:47 +0200

    mFormat corrected

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -5,8 +5,8 @@ int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
     int sum = 0;
-    for(auto el : numbers) {
-        if ( !(el % 2)){
+    for (auto el : numbers) {
+        if (!(el % 2)) {
             sum += el;
         }
     }
