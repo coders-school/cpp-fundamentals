08edc9f2a1b94697b4d5f9fc09ab140d774dc0a0
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-27 20:30:49 +0200

    All tests passed!

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,44 @@
 #include "validation.hpp"
 // TODO: Put implementations here
+
+std::string getErrorMessage(const ErrorCode& code)
+{
+    std::string result {"Unknown error"};
+    if (code == ErrorCode::Ok) {
+        return result = "Ok";
+    } else if (code == ErrorCode::PasswordNeedsAtLeastNineCharacters) {
+        return result = "Password needs to have at least nine characters";
+    } else if (code == ErrorCode::PasswordNeedsAtLeastOneNumber) {
+        return result = "Password needs to have at least one number";
+    } else if (code == ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter) {
+        return result = "Password needs to have at least one special character";
+    } else if (code == ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter) {
+        return result = "Password needs to have at least one uppercase letter";
+    } else if (code == ErrorCode::PasswordsDoNotMatch) {
+        return result = "Passwords do not match";
+    }
+    return result;
+};
+
+bool doPasswordsMatch(const std::string& password,const std::string& repeatedPassword)
+{
+    if (password == repeatedPassword) {
+        return true;
+    }
+    return false;
+};
+
+ErrorCode checkPasswordRules(const std::string& password)
+{
+    srand(time(0));
+    ErrorCode randomCode = static_cast<ErrorCode>(rand() % 4);
+    return randomCode;
+};
+
+ErrorCode checkPassword(const std::string& password, const std::string& repeatedPassword)
+{
+    if (password != repeatedPassword) {
+        return ErrorCode::PasswordsDoNotMatch;
+    }
+    return checkPasswordRules(password);
+};
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,23 @@
+#pragma once
+#include <string>
+#include <stdio.h>
+#include <time.h>
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard - #pragma once
+
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+
+std::string getErrorMessage(const ErrorCode& code);
+
+bool doPasswordsMatch(const std::string& password,const std::string& repeatedPassword);
+
+ErrorCode checkPasswordRules(const std::string& password);
+
+ErrorCode checkPassword(const std::string& password, const std::string& repeatedPassword);
