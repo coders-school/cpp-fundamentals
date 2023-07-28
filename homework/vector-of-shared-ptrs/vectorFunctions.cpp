b48bf4c9fd97b63961109d96a49cc600dd30aa7c
Bartosz Łoziński <lozinski.bart@gmail.com>
2023-07-28 21:45:00 +0200

    Format corrected

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -10,7 +10,7 @@ std::vector<std::shared_ptr<int>> generate(int count) {
 
 void print(const std::vector<std::shared_ptr<int>>& vec) {
     for (auto el : vec) {
-        std::cout << *el <<" \n";
+        std::cout << *el <<"\n";
     }
 };
 
