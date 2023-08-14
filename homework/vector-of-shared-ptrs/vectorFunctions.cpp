cead0d7d0c62b8d2ba6a707b0e70ae3f0b65aac8
Tomasz Fornalczyk <janko111@gmail.com>
2023-08-14 13:18:56 +0200

    vectorFunctions.cpp created

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,38 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> result;
+
+    for (int i = 0; i < count; ++i) {
+        result.push_back(std::make_shared<int>(i));
+    }
+
+    return result;
+}
+
+void print(const std::vector<std::shared_ptr<int>>& vec){
+    for (const auto& ptr : vec) {
+        std::cout << *ptr << " ";
+    }
+    std::cout << std::endl;
+}
+
+void add10(std::vector<std::shared_ptr<int>>& vec) {
+    for (auto& ptr : vec) {
+        *ptr += 10;
+    }
+}
+
+void sub10(int* ptr) {
+    if (ptr != nullptr) {
+        *ptr -= 10;
+    } else {
+        std::cout << "Błąd: Przekazany wskaźnik jest nullptr." << std::endl;
+    }
+}
+
+void sub10(std::vector<std::shared_ptr<int>>& vec) {
+    for (const auto& ptr : vec) {
+        sub10(ptr.get());
+    }
+}
