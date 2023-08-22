97265f410b3305caf6e0cb15457dd7825afa5dcd
Hubert Hodowaniec <hubert.hodowaniec@gmail.com>
2023-08-22 21:14:01 +0200

    Implement working solution

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,28 @@
 #pragma once
+#include <cstdlib>
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+    if (rhs == 0) {
+        return lhs;
+    } else {
+        if (lhs % rhs == 0) {
+            return rhs;
+        } else {
+            return NWD(rhs, lhs % rhs);
+        }
+    }
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+    if (rhs == 0) {
+        return 0;
+    } else {
+        return lhs * rhs / NWD(lhs, rhs);
+    }
 }
