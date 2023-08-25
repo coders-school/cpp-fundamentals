57a2164054d0eb2c4289bebb5410777152af8c1f
Jacek Prętki <jacekpiotrpretki@gmail.com>
2023-08-25 21:55:10 +0200

    adding works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,22 @@
 #pragma once
 #include <string>
+#include <vector>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+std::string calculate(const std::string command, int first, int second) {
+    if (command == "add") {
+        return std::to_string(first + second);
+    } else if (command == "subtract") {
+        return std::to_string(first - second);
+    } else if (command == "multiply") {
+        return std::to_string(first * second);
+    } else if (command == "divide") {
+        if (second == 0) {
+            return "Division by 0";
+        }
+        return std::to_string(first/second);
+    }
+    else{
+        return std::string("Invalid data");
+    }
     return "";
 }
