22a60f04a671e2abf719c2416be51b08a6259f61
lukasz.dlucik <lukasz.dlucik@abastran.com>
2023-08-27 13:26:36 +0200

    all works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,38 @@
 #pragma once
 #include <string>
 
+std::string divide(const std::string& command, int a, int b) {
+    if (b == 0) {
+        return "Division by 0";
+    }
+    return std::to_string(a / b);
+}
+
+std::string add(const std::string& command, int a, int b) {
+    return std::to_string(a + b);
+}
+
+std::string subtract(const std::string& command, int a, int b) {
+    return std::to_string(a - b);
+}
+
+std::string multiply(const std::string& command, int a, int b) {
+    return std::to_string(a * b);
+}
+
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    std::string resoult = "Invalid data";
+
+    if (command == "divide") {
+        resoult = divide(command, first, second);
+    } else if (command == "subtract") {
+        resoult = subtract(command, first, second);
+    } else if (command == "add") {
+        resoult = add(command, first, second);
+    } else if (command == "multiply") {
+        resoult = multiply(command, first, second);
+    }
+
+    return resoult;
 }
