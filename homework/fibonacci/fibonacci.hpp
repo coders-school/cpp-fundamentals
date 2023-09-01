51c5bb37c77ad0882a3a054b0e3f961634f1fef3
hockit <marcelmd@wp.pl>
2023-09-01 14:24:24 +0200

    Add iterative module

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int a = 0;
+    int b = 1;
+
+    for (int i = 0; i < sequence; ++i)
+    {
+        b += a;
+        a = b - a;
+    }
+    return b;
 }
 
 int fibonacci_recursive(int sequence) {
