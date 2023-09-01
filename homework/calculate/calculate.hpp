385b8cb2397851aa58e47bf0f72d816ae463ff06
Maciej Dawiec <dmaciek85@gmail.com>
2023-09-01 21:37:54 +0200

    adding works3

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -5,20 +5,19 @@ std::string calculate(const std::string& command, int first, int second) {
 
     int result = 0;
 
-    if(command == "add")
+    if (command == "add")
         result = first + second;
-    else if(command == "subtract")
+    else if (command == "subtract")
         result = first - second;
-    else if(command == "multiply")
+    else if (command == "multiply")
         result = first * second;
-
-    else if(command == "divide") {
-        if(second == 0) {
+    else if (command == "divide") {
+        if (second == 0) {
             return "Division by 0";
-            }
-        else    result = first / second;                
-        }
-    else    return "Invalid data";
+            }else
+                result = first / second;                
+        }else
+            return "Invalid data";
 
     std::string resultToString = std::to_string(result);
     return resultToString;
