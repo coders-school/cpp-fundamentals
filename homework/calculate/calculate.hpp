9c7154a8cf34b0cb3dbb76ad7e7e3161b928b87a
Maciej Dawiec <dmaciek85@gmail.com>
2023-09-01 20:58:53 +0200

    adding works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,24 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+
+    int result = 0;
+
+    if(command == "add")             result = first + second;
+    else if(command == "subtract")   result = first - second;
+    else if(command == "multiply")   result = first * second;
+
+    else if(command == "divide")
+        {
+            if(second == 0)
+            {
+                return "Division by 0";
+            }
+           else    result = first / second;                
+        }
+    else    return "Invalid data";
+
+
+    std::string resultToString = std::to_string(result);
+    return resultToString;
 }
