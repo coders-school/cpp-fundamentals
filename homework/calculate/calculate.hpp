b66836a74b972a61a4b55378fc1c6729750b5690
Marcin <marcinjagielak90@gmail.com>
2023-09-02 23:43:26 +0200

    Formatting fixed

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,18 +2,18 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    if(command.compare("add") == 0){
+    if (command.compare("add") == 0) {
         return std::to_string(first + second);
-    }else if(command.compare("subtract") == 0){
+    } else if (command.compare("subtract") == 0) {
         return std::to_string(first - second);
-    }else if(command.compare("multiply") == 0){
+    } else if (command.compare("multiply") == 0) {
         return std::to_string(first * second);
-    }else if (command.compare("divide") == 0){
-        if(first == 0 || second == 0){
+    } else if (command.compare("divide") == 0) {
+        if (first == 0 || second == 0) {
             return "Division by 0";
         }
         return std::to_string(first / second);
-    }else {
+    } else {
         return "Invalid data";
     }
 }
