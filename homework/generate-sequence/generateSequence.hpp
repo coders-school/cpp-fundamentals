54ec8ddfdc256cfb99a28ffc1efd9aaa32457651
Marcin <marcinjagielak90@gmail.com>
2023-09-04 00:43:55 +0200

    some fixes

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -6,7 +6,7 @@ std::vector<int> generateSequence(int count, int step) {
     if (count <= 0) {
         return result;
     }
-    for (size_t i = 0; i < count; ++i) {
+    for (int i = 0; i < count; ++i) {
         if (i == 0) {
             result.push_back(step);
         } else {
