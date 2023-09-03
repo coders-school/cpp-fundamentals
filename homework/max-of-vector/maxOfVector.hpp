5f27efdfff0bf28c38f3df9c2188adbde04e4254
Marcin <marcinjagielak90@gmail.com>
2023-09-04 00:18:10 +0200

    formatting fixed

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -4,8 +4,8 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     int result = INT_MIN;
-    for(auto n : vec){
-        if(n > result){
+    for (auto n : vec) {
+        if (n > result) {
             result = n;
         }
     }
