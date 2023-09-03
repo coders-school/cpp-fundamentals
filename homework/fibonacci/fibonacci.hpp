6e55af4824ecb4de812a1ad56659f4175d54f108
Maciej Dawiec <dmaciek85@gmail.com>
2023-09-03 13:14:16 +0200

    adding formating

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    
+int fibonacci_iterative(int sequence) {   
     int fibo = 0;
     int n1 = 0;
     int n2 = 1;
@@ -9,8 +8,7 @@ int fibonacci_iterative(int sequence) {
     if (sequence == 0 || sequence == 1)
         return sequence;
 
-    for (int i = 2; i <= sequence; ++i)
-    {
+    for (int i = 2; i <= sequence; ++i) {
         fibo = n1 + n2;
         n1 = n2;
         n2 = fibo;        
@@ -19,8 +17,7 @@ int fibonacci_iterative(int sequence) {
     return fibo;
 }
 
-int fibonacci_recursive(int sequence) {
-    
+int fibonacci_recursive(int sequence) {    
     if (sequence == 0 || sequence == 1)
         return sequence;
 
