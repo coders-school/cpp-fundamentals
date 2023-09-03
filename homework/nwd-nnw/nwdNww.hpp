642d3f1ecd19b0abb16a41f461c88bd0e9c62eef
Marcin <marcinjagielak90@gmail.com>
2023-09-03 05:48:55 +0200

    nwd-nnw completed

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,29 @@
 #pragma once
+#include <iostream>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+    if(lhs == 0 && rhs == 0){
+        return 0;
+    }else if(lhs == 0){
+        return rhs;
+    }else if(rhs == 0){
+        return lhs;
+    }
+    while(lhs % rhs != 0){
+        int tmp = lhs % rhs;
+        lhs = rhs;
+        rhs = tmp;
+    }
+    return rhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+    if(lhs == 0 && rhs == 0){
+        return 0;
+    }
+    return lhs*rhs/NWD(lhs, rhs);
 }
