7337570819bdb213e6ac3b8e86018fdb0c7b284f
Marcin <marcinjagielak90@gmail.com>
2023-09-04 00:02:01 +0200

    add-even completed

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,11 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int result = 0;
+    for (auto n : numbers) {
+        if (n % 2 == 0) {
+            result += n;
+        }
+    }
+    return result;
 }
