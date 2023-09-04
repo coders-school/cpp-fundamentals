800ce620df439ff5144f7a1f10542fe9e257e118
Maciej Dawiec <dmaciek85@gmail.com>
2023-09-04 10:28:36 +0200

    adding NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,16 @@
 #pragma once
+#include <cmath>
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs) { //224 1270
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+    int helpNum = 0; 
+    while (rhs != 0) {
+        helpNum = rhs;
+        rhs = lhs % rhs;
+        lhs = helpNum;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
