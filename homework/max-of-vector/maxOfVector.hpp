41fd3856d337161eddd169b777da1d2cd1e9b5e1
Maciej Dawiec <dmaciek85@gmail.com>
2023-09-04 15:19:59 +0200

    adding maxOfVector

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,11 @@
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max = vec[0];
+    for (int i = 1; i < vec.size(); ++i) {
+        if (vec[i] > max) {
+            max = vec[i];
+        }
+    }
+    return max;
 }
