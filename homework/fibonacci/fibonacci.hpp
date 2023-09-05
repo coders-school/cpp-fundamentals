c9aed0a3070d7caa5e6c085267ed13c3bacb2b3e
Maciej Dawiec <dmaciek85@gmail.com>
2023-09-05 12:27:07 +0200

    adding next formating

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -4,8 +4,9 @@ int fibonacci_iterative(int sequence) {
     int fibo = 0;
     int n1 = 0;
     int n2 = 1;
-    if (sequence == 0 || sequence == 1)
+    if (sequence == 0 || sequence == 1) {
         return sequence;
+    }
     for (int i = 2; i <= sequence; ++i) {
         fibo = n1 + n2;
         n1 = n2;
@@ -15,7 +16,8 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {    
-    if (sequence == 0 || sequence == 1)
+    if (sequence == 0 || sequence == 1) {
         return sequence;
+    }
     return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
