aae595d2397c98895fe0cf6ce8fd4e8d76e06a92
Maciej Dawiec <dmaciek85@gmail.com>
2023-09-24 21:50:36 +0200

    adding optional work

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,11 +1,10 @@
 #include "validation.hpp"
 
-
 ErrorCode errorCode;
 
 std::string getErrorMessage(ErrorCode result)
 {
-    switch(result) {
+    switch (result) {
         case ErrorCode::Ok:
             return "Ok";
             break;
@@ -35,20 +34,48 @@ bool doPasswordsMatch(std::string password, std::string repeatedPassword)
 
 ErrorCode checkPasswordRules(std::string password)
 {
-    std::default_random_engine generator;
-    std::uniform_int_distribution<int> distribution(0,4);
-    int error = distribution(generator);
-    ErrorCode randomError = static_cast<ErrorCode>(error);
-    return randomError;
+    if(oneNumber(password) != true)         return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    if(specialCharacter(password) != true)  return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    if(uppercaseLetter(password) != true)   return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    if(nineCharacter(password) == true)     return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+    if(okPassword(password) == true)        return ErrorCode::Ok;
 }
 
 ErrorCode checkPassword(std::string password, std::string repeatedPassword)
 {
     bool compare = doPasswordsMatch(password, repeatedPassword);
-    if(compare != true) {
+    if (compare != true) {
         return ErrorCode::PasswordsDoNotMatch;
     }
     else {
         return checkPasswordRules(password);
     }
+}
+
+bool okPassword(std::string password)
+{
+    return std::all_of(password.begin(), password.end(), ::isgraph);
+}
+
+bool nineCharacter(std::string password)
+{
+    if (password.size() < 9) {
+        return true;
+    }
+    return false;
+}
+
+bool oneNumber(std::string password)
+{
+    return std::any_of(password.begin(), password.end(), ::isdigit);
+}
+
+bool specialCharacter(std::string password)
+{
+    return std::any_of(password.begin(), password.end(), ::ispunct);                  
+}
+
+bool uppercaseLetter(std::string password)
+{
+    return std::any_of(password.begin(), password.end(), ::isupper);
 }
