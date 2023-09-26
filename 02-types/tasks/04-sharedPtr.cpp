2d88f17d9df0ee85d5bd05aed38d1edc9f1de0b5
Maciej Dawiec <dmaciek85@gmail.com>
2023-09-26 22:28:44 +0200

    ading 04

--- 02-types/tasks/04-sharedPtr.cpp
+++ 02-types/tasks/04-sharedPtr.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <memory>
 
-// TODO: Implement foo()
-// It should take shared_ptr to int and assign value 20 to the pointed int.
-// It should also display the value of this int and the number of how many pointers are pointing to it - use `use_count()`.
-// Display the same information in main() before and after calling foo()
+void foo(std::shared_ptr<int> number) {
+    number = std::make_shared<int> (20);
+    std::cout << *number << "\n";
+    std::cout << number.use_count() << "\n";
+}
 
 int main() {
     std::shared_ptr<int> number = std::make_shared<int>(10);
-    // display the value under number pointer and use_count() of it
     foo(number);
-    // display the value under number pointer and use_count() of it
 
     return 0;
 }
