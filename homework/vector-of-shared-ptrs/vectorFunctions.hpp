9f0df0fb513ebc97ed670631cacd6ded169735d8
Maciej Dawiec <dmaciek85@gmail.com>
2023-10-04 11:36:21 +0200

    adding works5

--- homework/vector-of-shared-ptrs/vectorFunctions.hpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -1,7 +1,7 @@
 #pragma once
-#include <vector>
-#include <memory>
 #include <iostream>
+#include <memory>
+#include <vector>
 
 std::vector<std::shared_ptr<int>> generate(int count);
 void print(std::vector<std::shared_ptr<int>> vec);
