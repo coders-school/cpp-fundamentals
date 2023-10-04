27e1a091cf8c85cdc47fe2e4df6347a6d8424055
Maciej Dawiec <dmaciek85@gmail.com>
2023-10-04 13:39:32 +0200

    adding multiply

--- 03-language-elements/tasks/01-multiply.cpp
+++ 03-language-elements/tasks/01-multiply.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-// Write missing function here
+int multiply(int lNum, int rNum) {
+    return lNum * rNum;
+}
 
 int main() {
     std::cout << "4 * 5: " << multiply(4, 5) << "\n";
