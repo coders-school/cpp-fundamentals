978ef7bc88c99e61fc80e04b372f30b3da0d9e63
Dominik Lamcha <lamcha.dominik@gmail.com>
2023-10-16 13:51:39 +0200

    Exercise third done

--- 02-types/tasks/03-modifyPointers.cpp
+++ 02-types/tasks/03-modifyPointers.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
-// TODO: Implement foo() and bar()
-// foo() should modify value under passed pointer to 10
-// bar() should modify value under passed pointer to 20
 // Can we have a pointer to const or a const pointer?
+// If we want to modify content of variable named number, we could use const pointer to set its addres 
+// But we cannot use pointer to const becouse it would be unmodifiable 
+void foo(int* const ptr){
+*ptr = 10;
+}
+void bar(int* ptr){
+*ptr = 20;
+}
 
 int main() {
     int number = 5;
