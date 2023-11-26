d96ce662bec06f8b3a7fe66efd59da1406274948
Kamil Wojcik <kamil.fiz@gmail.com>
2023-11-26 22:23:37 +0100

    nwd and nww implemented

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,26 @@
 #pragma once
+#include <algorithm>
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+    if (lhs == 0)
+        return rhs;
+    else if (rhs == 0)
+        return lhs;
+    else {
+        while (rhs)
+            std::swap(lhs %= rhs, rhs);
+        return lhs;
+    }
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+    if (lhs == 0 or rhs == 0)
+        return 0;
+    else
+        return lhs * rhs / NWD(lhs, rhs);
 }
