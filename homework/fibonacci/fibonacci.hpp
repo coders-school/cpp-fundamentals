9ee7602d9ec0fb16290285bc42db32cc1b5dcf76
Kamil Wojcik <kamil.fiz@gmail.com>
2023-11-26 18:52:58 +0100

    fibonacci-hpp was clang-formatted

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -4,8 +4,7 @@ int fibonacci_iterative(int sequence) {
     int one_before = 1;
     int current = 0;
     int buffer;
-    for (int i=0; i<sequence; i++)
-    {
+    for (int i = 0; i < sequence; i++) {
         buffer = current;
         current += one_before;
         one_before = buffer;
@@ -14,7 +13,10 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    if (sequence == 1) return 1;
-    else if (sequence > 1 ) return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
-    else return 0;
+    if (sequence == 1)
+        return 1;
+    else if (sequence > 1)
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    else
+        return 0;
 }
