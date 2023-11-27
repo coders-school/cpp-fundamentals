847787a580a3d685461e2ab970b7e48d709774cc
Kamil Wojcik <kamil.fiz@gmail.com>
2023-11-27 15:41:50 +0100

    homework done

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,31 @@
+#include "vectorFunctions.hpp"
+#include <iostream>
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> vec;
+    vec.reserve(count);
+    for (int i = 0; i < count; i++)
+        vec.emplace_back(std::make_shared<int>(i));
+    return vec;
+}
+
+void print(std::vector<std::shared_ptr<int>> vec) {
+    for (auto i : vec)
+        std::cout << *i << std::endl;
+}
+
+void add10(std::vector<std::shared_ptr<int>> vec) {
+    for (auto i : vec)
+        if (i)
+            *i += 10;
+}
+
+void sub10(int* const ptr) {
+    if (ptr)
+        *ptr -= 10;
+}
+
+void sub10(std::vector<std::shared_ptr<int>> vec) {
+    for (auto i : vec)
+        sub10(i.get());
+}
