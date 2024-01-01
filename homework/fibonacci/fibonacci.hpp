02832cf94d66c77a2c36e3b58aaec21306893d2e
Krzysiek <krzysztof.feliksiak@gmail.com>
2024-01-01 14:53:45 +0100

    add recurcive version

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -22,6 +22,12 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    
+    if (sequence == 0) {
+        return 0;
+    } else if (sequence == 1) {
+        return 1;
+    } else {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
     return 0;
 }
