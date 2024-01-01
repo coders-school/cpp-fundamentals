928223bf097fa8abb5cdd5c4371b05b4d9a45c5d
Krzysiek <krzysztof.feliksiak@gmail.com>
2024-01-01 14:28:13 +0100

    fibonacci iterative added

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,7 +1,21 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
+    if (sequence == 0) {
+        return 0;
+    } else if (sequence == 1) {
+        return 1;
+    } else {
+        int fibonacci_tab[sequence];
+        fibonacci_tab[0] = 0;
+        fibonacci_tab[1] = 1;
+        
+        for (int i = 2; i <= sequence; i++) {
+            fibonacci_tab[i] = fibonacci_tab[i-1] + fibonacci_tab[i-2];
+        }
+        return fibonacci_tab[sequence];
+    }
+
     return 0;
 }
 
