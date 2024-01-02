6c23aa65d0a9ce064e885e890eca4b642f1fa8ce
Krzysiek <krzysztof.feliksiak@gmail.com>
2024-01-02 11:21:03 +0100

    add formatting fix

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -5,11 +5,11 @@ int NWD(int lhs, int rhs) {
     lhs = std::abs(lhs);
     rhs = std::abs(rhs);
 
-    if (lhs == 0 && rhs == 0){
+    if (lhs == 0 && rhs == 0) {
         return 0;
     } else if (lhs == 0 && rhs != 0) {
         return rhs;
-    } else if (lhs != 0 && rhs == 0){
+    } else if (lhs != 0 && rhs == 0) {
         return lhs;
     } else {
         while (lhs != rhs) {
