f9c0e3fbde521ce5cceb3cc13401e31ae7088b72
Krzysiek <krzysztof.feliksiak@gmail.com>
2024-01-02 11:11:33 +0100

    add NWD and NWW algo

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,28 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+
+    if (lhs == 0 && rhs == 0){
+        return 0;
+    } else if (lhs == 0 && rhs != 0) {
+        return rhs;
+    } else if (lhs != 0 && rhs == 0){
+        return lhs;
+    } else {
+        while (lhs != rhs) {
+            if (lhs > rhs) {
+                lhs = lhs - rhs;
+            } else {
+                rhs = rhs - lhs;
+            }
+        }
+        return lhs;
+    }
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    return (std::abs(lhs) * std::abs(rhs)) / NWD(lhs, rhs);
 }
