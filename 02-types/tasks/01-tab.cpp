0136fe8674395b03486af7d63ff52c08e1834c6f
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-05 18:43:48 +0100

    Update 01-tab.cpp

--- 02-types/tasks/01-tab.cpp
+++ 02-types/tasks/01-tab.cpp
@@ -5,8 +5,8 @@ constexpr size_t tab_size = 100;
 int main() {
     int tab[tab_size];
 
-    for (size_t i = 0; i < tab_size; ++i) {
-        tab[i] = i;
+    for (size_t i = 0; i < tab_size; i+=2) {
+        tab[i] = i+1;
     }
 
     for (size_t i = 0; i < tab_size; ++i) {
