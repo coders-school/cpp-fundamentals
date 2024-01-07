a65756a1906b77ce934852f1577838fb096237b8
baldingTomato <tomek.kluba12@gmail.com>
2024-01-07 17:40:10 +0100

    Added iterative version of Fibonacci

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,26 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+
+    if (sequence == 0) {
+        return 0;
+    }
+
+    if (sequence == 1) {
+        return 1;
+    }
+
+    int result = 0;
+    int a = 0;
+    int b = 1;
+
+    for (size_t i = 2; i <= sequence; ++i) {
+        result = a + b;
+        std::swap(a, b);
+        b += a;
+    }
+
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
