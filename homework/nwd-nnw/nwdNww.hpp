1f6c6364b975ad8649e462c6c547dedcbef3ef26
baldingTomato <tomek.kluba12@gmail.com>
2024-01-07 19:49:59 +0100

    Added NWD algorithm

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    int result = 0;
+
+    if (lhs == 0 && rhs == 0) {
+        result = 0;
+    } else if (lhs == 0 && rhs != 0) {
+        result = rhs;
+        return std::abs(result);
+    } else if (lhs != 0 && rhs == 0) {
+        result = lhs;
+        return std::abs(result);
+    }
+
+    while (rhs != 0) {
+        int temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
+    }
+
+    result = lhs;
+
+    return std::abs(result);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
+    
     return -1;
 }
