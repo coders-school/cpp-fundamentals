774fbd29bdd3cf51c45e8fbbfdc57f9dec694c67
baldingTomato <tomek.kluba12@gmail.com>
2024-01-07 16:59:33 +0100

    Fixed formatting with clang-format

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,25 +2,23 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-
-    if(command == "add"){
+    if (command == "add") {
         std::string result = std::to_string(first + second);
         return result;
-    }else if(command == "subtract"){
+    } else if (command == "subtract") {
         std::string result = std::to_string(first - second);
         return result;
-    }else if(command == "multiply"){
+    } else if (command == "multiply") {
         std::string result = std::to_string(first * second);
         return result;
-    }else if(command == "divide"){
-        if(second == 0){
+    } else if (command == "divide") {
+        if (second == 0) {
             return "Division by 0";
-        }else{
+        } else {
             std::string result = std::to_string(first / second);
             return result;
         }
-    }else{
+    } else {
         return "Invalid data";
     }
-
 }
