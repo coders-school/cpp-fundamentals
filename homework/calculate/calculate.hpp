a64a38d84eca7e1d37f95a60c3cd3609e1353c57
baldingTomato <tomek.kluba12@gmail.com>
2024-01-07 15:05:43 +0100

    Added handling multiplication

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -9,6 +9,9 @@ std::string calculate(const std::string& command, int first, int second) {
     }else if(command == "subtract"){
         std::string result = std::to_string(first - second);
         return result;
+    }else if(command == "multiply"){
+        std::string result = std::to_string(first * second);
+        return result;
     }
 
     return "";
