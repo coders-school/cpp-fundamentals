759f1b126868534db5a97c26f04824923dbb6a76
baldingTomato <tomek.kluba12@gmail.com>
2024-01-07 14:59:58 +0100

    Added handling addition

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,10 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
+    if(command == "add"){
+        std::string result = std::to_string(first + second);
+        return result;
+    }
+
     return "";
 }
