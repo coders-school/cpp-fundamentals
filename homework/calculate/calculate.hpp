5091c21b8d6973050f9b47a8f99f5cb74d926c08
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-08 15:21:36 +0100

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,18 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    switch (command)
+    {
+        case add
+            return "std::to_string(first+second)"; break;
+        case substract
+            return "std::to_string(first-second)"; break;
+        case multiply
+            return "std::to_string(first+*second)"; break;
+        case divide
+            return "std::to_string(first/second)"; break;
+        default
+            return "Invalid data."; break;
+            
+    }
 }
