94d9c20e6408d67fee8fb3d83bb43384e98c1c23
Krzysiek <krzysztof.feliksiak@gmail.com>
2024-01-08 12:46:54 +0100

    add functionality

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,9 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> final_vec;
+    for (int i = 1; i < (count + 1); ++i) {
+        final_vec.push_back(step * i);
+    }
+    return final_vec;
 }
