61e56b77030ad9b50afba30beefe5050e74bf961
Krzysiek <krzysztof.feliksiak@gmail.com>
2024-01-08 11:34:35 +0100

    add functionality

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,11 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    unsigned int even_sum = 0;
+    for (const auto& elem : numbers) {
+        if (elem % 2 == 0) {
+            even_sum += elem;
+        }
+    }
+    return even_sum;
 }
