cf5ea4d8ef8bcf67295ade8d99cab0d9492a24b6
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-09 20:31:10 +0100

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,14 +3,14 @@
 
 std::string calculate(const std::string& command, int first, int second) 
  {
-    if (command=="add")
-            return std::to_string(first+second);
-    else if (command=="substract")
-            return std::to_string(first-second);
-    else if (command=="multiply")
-            return std::to_string(first*second);
-    else if (command=="divide")
-            return std::to_string(first/second);
+    if (command == "add")
+            return std::to_string(first + second);
+    else if (command == "substract")
+            return std::to_string(first - second);
+    else if (command == "multiply")
+            return std::to_string(first * second);
+    else if (command == "divide")
+            return std::to_string(first / second);
     else
-            return "Invalid data";
+        return "Invalid data";
 }
