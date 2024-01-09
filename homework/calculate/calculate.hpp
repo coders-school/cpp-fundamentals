37c2b2e15a329c181a8b9ae8828cf4cb936c77fa
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-09 21:14:36 +0100

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,8 +1,7 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) 
- {
+std::string calculate(const std::string& command, int first, int second) {
     if (command == "add")
             return std::to_string(first + second);
     else if (command == "substract")
