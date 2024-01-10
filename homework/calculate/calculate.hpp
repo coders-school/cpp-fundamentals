e26fadbb2d286e45338ef6b6d473e9498b8772d8
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-10 15:30:33 +0100

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -4,7 +4,7 @@
 std::string calculate(const std::string& command, int first, int second) {
     if (command == "add")
         return std::to_string(first + second);
-    else if (command == "substract")
+    else if (command == "subtract")
         return std::to_string(first - second);
     else if (command == "multiply")
         return std::to_string(first * second);
