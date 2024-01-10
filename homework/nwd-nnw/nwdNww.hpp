efa7e8d9a4a46a87efbbff47307e70217a679755
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-10 22:54:57 +0100

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int i;
+    do
+    {
+        if (lhs = rhs) return lhs;
+        else if (lhs > rhs) lhs=lhs-rhs;
+        else rhs=rhs-lhs;
+    }
+    while (lhs != rhs);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    return lhs * rhs / NWD(lhs, rhs);
 }
