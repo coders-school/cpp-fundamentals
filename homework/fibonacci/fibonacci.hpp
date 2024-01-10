3d2a207a5c008636ca97efde80f47dd313786358
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-10 21:19:48 +0100

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -4,12 +4,12 @@ int fibonacci_iterative(int sequence) {
     int i, x, y, number;
     x = 0;
     y = 1;
-    for (i = 1; i < sequence; ++i)
-        if (sequence == 0 )
-            return x;
-        else if (sequence == 1 )
-            return y;
-        else {
+    if (sequence == 0 )
+        return x;
+    else if (sequence == 1 )
+        return y;
+    else
+        for (i = 1; i < sequence; ++i){
             number = x + y;
             x = y;
             y = number;}
