3674820607cfff59313af5c36fde995249ad459c
baldingTomato <tomek.kluba12@gmail.com>
2024-01-14 15:38:13 +0100

    Prepared .hpp file

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,19 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#pragma once
+#include "string"
+
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+};
+
+std::string getErrorMessage(const ErrorCode&);
+
+bool doPasswordsMatch(const std::string&, const std::string&);
+
+ErrorCode checkPasswordRules(const std::string&);
+
+ErrorCode checkPassword(const std::string&, const std::string&);
