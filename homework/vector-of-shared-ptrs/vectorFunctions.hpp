6efd97c33f679f1815bb97c36eb62a362c570a77
baldingTomato <tomek.kluba12@gmail.com>
2024-01-16 19:38:38 +0100

    Fixed formatting

--- homework/vector-of-shared-ptrs/vectorFunctions.hpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -1,14 +1,13 @@
 #pragma once
-#include <vector>
 #include <memory>
+#include <vector>
 
-std::vector<std::shared_ptr<int>> generate(const int &count);
+std::vector<std::shared_ptr<int>> generate(const int& count);
 
-void print(const std::vector<std::shared_ptr<int>> &vec);
+void print(const std::vector<std::shared_ptr<int>>& vec);
 
-void add10(std::vector<std::shared_ptr<int>> &vec);
+void add10(std::vector<std::shared_ptr<int>>& vec);
 
 void sub10(int* const ptr);
 
-void sub10(std::vector<std::shared_ptr<int>> &vec);
-
+void sub10(std::vector<std::shared_ptr<int>>& vec);
