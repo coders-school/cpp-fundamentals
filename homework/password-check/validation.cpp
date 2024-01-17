2ac2fab237813f7ca5c4a946c3a0ddc41e68ea66
Krzysiek <krzysztof.feliksiak@gmail.com>
2024-01-17 15:20:54 +0100

    add validation functionality

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,35 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+#include <string>
+
+std::string getErrorMessage(ErrorCode error_code) {
+    switch(error_code) {
+        case ErrorCode::Ok:
+            return "Ok";
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+            return "Password needs to have at least nine characters";
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+            return "Password needs to have at least one number";
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+            return "Password needs to have at least one special character";
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+            return "Password needs to have at least one uppercase letter";
+        case ErrorCode::PasswordsDoNotMatch:
+            return "Passwords do not match";
+    }
+}
+
+bool doPasswordsMatch(const std::string& password1, const std::string& password2) {
+    return password1 == password2;
+}
+
+ErrorCode checkPasswordRules(const std::string& password) {
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(const std::string& password1, const std::string& password2) {
+    if (doPasswordsMatch(password1, password2)) {
+        return checkPasswordRules(password1);
+    } else {
+        return ErrorCode::PasswordsDoNotMatch;
+    }
+}
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,2 +1,21 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
+#ifndef VALIDATION_HPP
+#define VALIDATION_HPP
+
+#pragma once
+#include <string>
+
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch,
+};
+
+std::string getErrorMessage(ErrorCode);
+bool doPasswordsMatch(const std::string&, const std::string&);
+ErrorCode checkPasswordRules(const std::string&);
+ErrorCode checkPassword(const std::string&, const std::string&);
+
+#endif // VALIDATION_HPP
