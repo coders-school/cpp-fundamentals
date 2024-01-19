b9545eacc97a981df506fd5d3408abc9f0d722c9
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-19 20:57:13 +0100

    Update 03-createSortedList.cpp

--- 04-stl-basics/tasks/03-createSortedList.cpp
+++ 04-stl-basics/tasks/03-createSortedList.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
-// Implement createSortedList
-// add proper include :)
+std::vector<int> createSortedList(std::vector<int> incoming) {
+    size_t i, j, k;
+    auto it = incoming.begin();
+    int container1, container2;
+    for (i = 0; i < incoming.size(); ++i)
+        for (j = (incoming.size()-1); j > i; --j) {
+            it = incoming.begin();
+            for (k = 0; k < j; ++k) {
+                ++it;
+            }
+            container1 = *it;
+            it = incoming.begin();
+            for (k = 0; k < j-1; ++k) {
+                ++it;
+            }
+            container2 = *it;
+            if (container1 < container2) {
+                it = incoming.begin();
+                for (k = 0; k < (j); ++k) {
+                    ++it;
+                }
+                *it=container2;
+                it = incoming.begin();
+                for (k = 0; k < (j-1); ++k) {
+                    ++it;
+                }
+                *it=container1;
+            }
+        }
+    return incoming;
+}
 
 int main() {
     std::vector<int> vec{2, 3, 4, 1, 6, 5, 8, 7, 9, 0};
