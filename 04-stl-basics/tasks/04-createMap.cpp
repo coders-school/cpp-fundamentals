0ebbb020b55e875ebb9a7eeec6b62ac5db84b244
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-22 20:27:45 +0100

    Update 04-createMap.cpp

--- 04-stl-basics/tasks/04-createMap.cpp
+++ 04-stl-basics/tasks/04-createMap.cpp
@@ -2,10 +2,44 @@
 #include <list>
 #include <string>
 #include <vector>
+#include <map>
 
-// Implement createMap. It should take a vector and list and
-// return a map of merge them as keys from the vector and values from the list
-
+auto createMap (std::vector<int> vecin, std::list<std::string>  listin) {
+    size_t i, j, k;
+    i = vecin.size();
+    j = listin.size();
+    std::map<int, std::string> mapout;
+    if (i == j) {
+        for (i = 0; i < j; ++i) {
+            auto it = listin.begin();
+            for (k = 0; k < i; ++k) {
+                ++it;
+            }
+            mapout.insert({vecin[i], *it});
+        }
+    }
+    else if (i > j ) {
+        for (i = 0; i < j; ++i) {
+            auto it = listin.begin();
+            for (k = 0; k < i; ++k) {
+                ++it;
+            }
+            mapout.insert({vecin[i], *it});
+        }
+        std::cout << "More vector than list."<< '\n';
+    }
+    else {
+        for (j = 0; j < i; ++j) {
+            auto it = listin.begin();
+            for (k = 0; k < j; ++k) {
+                ++it;
+            }
+            mapout.insert({vecin[j], *it});
+        }
+        std::cout << "More list than vector."<< '\n';
+    }
+    return mapout;
+}
 
 int main() {
     std::vector<int> vec{1, 2, 3, 4, 5};
