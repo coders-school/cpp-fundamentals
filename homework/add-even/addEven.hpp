eaf5f6bb87e110e13e7b2413c06cbbde5ce3619e
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-24 20:56:01 +0100

    Update addEven.hpp

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -5,9 +5,8 @@ int addEven(const std::vector<int>& numbers) {
     int i, sumeven;
     sumeven = 0;
     for (i = 0; i < numbers.size(); ++i) {
-        if (numbers[i]%2 == 0) {
+        if (numbers[i] % 2 == 0) {
             sumeven += numbers[i];
         }
     }
     return sumeven;
-}
