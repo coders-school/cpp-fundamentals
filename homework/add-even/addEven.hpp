d9a9f9d7ceb6efc05613cf1d6fdded247427c8b9
Josseye <155636321+Josseye@users.noreply.github.com>
2024-01-24 20:49:30 +0100

    Update addEven.hpp

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,12 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int i, sumeven;
+    sumeven = 0;
+    for (i = 0; i < numbers.size(); ++i) {
+        if (numbers[i]%2 == 0) {
+            sumeven += numbers[i];
+        }
+    }
+    return sumeven;
 }
