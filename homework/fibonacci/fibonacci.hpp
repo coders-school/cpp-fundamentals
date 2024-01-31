847c507c0e34acee1c00e31605dc80d954c4c98f
Marcin Pawłowski <mnl@spyro-soft.com>
2024-01-31 19:02:55 +0100

    Fix formatting

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -5,8 +5,7 @@ int fibonacci_iterative(int sequence) {
     unsigned result = 0;
     unsigned a = 0, b = 1;
 
-    for (unsigned i = 0; i < sequence; i++)
-    {
+    for (unsigned i = 0; i < sequence; i++) {
         b = b + a;
         a = b - a;
 
@@ -17,16 +16,11 @@ int fibonacci_iterative(int sequence) {
 
 int fibonacci_recursive(int sequence) {
     
-    if (sequence == 0)
-    {
+    if (sequence == 0) {
         return 0;
-    }
-    else if (sequence < 3)
-    {
+    } else if (sequence < 3) {
         return 1;
-    }
-    else
-    {    
+    } else {
         return (fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1));
     }
 }
