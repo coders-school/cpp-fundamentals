99ee57e0a60c94d2a577218e85ef8d12570283fb
Marcin Pawłowski <mnl@spyro-soft.com>
2024-01-31 18:46:19 +0100

    Add fibonacci functions iterative and recursive

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,32 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    unsigned result = 0;
+    unsigned a = 0, b = 1;
+
+    for (unsigned i = 0; i < sequence; i++)
+    {
+        b = b + a;
+        a = b - a;
+
+        result = a; 
+    }
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    if (sequence == 0)
+    {
+        return 0;
+    }
+    else if (sequence < 3)
+    {
+        return 1;
+    }
+    else
+    {    
+        return (fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1));
+    }
 }
