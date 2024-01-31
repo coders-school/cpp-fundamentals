4f59f99fb757c340222e94a23ac7ab84aa26be6c
Marcin Pawłowski <mnl@spyro-soft.com>
2024-01-31 18:29:57 +0100

    Add handlers for specific functions

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,7 +1,21 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+std::string calculate(const std::string& command, int first, int second)
+{
+    if (command == "add") {
+        return std::to_string(first + second);
+    } else if (command == "subtract") {
+        return std::to_string(first - second);
+    } else if (command == "multiply") {
+        return std::to_string(first * second);
+    } else if (command == "divide") {
+        if (second == 0) {
+            return "Division by 0";
+        }
+        return std::to_string(first / second);
+    } else {
+        return "Invalid data";
+    }
     return "";
 }
