bd9fe353447fd6e02fabf5129468da0fc7f3512b
Marcin Pawłowski <mnl@spyro-soft.com>
2024-01-31 19:07:23 +0100

    Added function for NNM and NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,37 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    
+    int temp;
+
+    if (lhs < 0) {
+        lhs = lhs * (-1);
+    }
+    if (rhs < 0) {
+        rhs = rhs * (-1);
+    }
+
+    while (rhs != 0) {
+        temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
+    }
+    return lhs; //or rhs
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    
+    if (lhs < 0) {
+        lhs = lhs * (-1);
+    }
+    if (rhs < 0) {
+        rhs = rhs * (-1);
+    }
+
+    if (lhs == 0 || rhs == 0) {
+        return 0;
+    }
+    else {    
+        return ((lhs * rhs) / NWD(lhs, rhs));
+    }
 }
