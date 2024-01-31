660c030d3de48415a84ac76cfa5bd3f4910edd9c
Marcin Pawłowski <mnl@spyro-soft.com>
2024-01-31 21:55:53 +0100

    Add solution with algorithm max_element for maxOfVector function

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,11 @@
 #pragma once
-#include <limits>
+#include <limits> //sugestia? :D
 #include <vector>
 
+#include <algorithm> //method with max_element algorithm
+
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    
+    std::vector<int>::const_iterator resultIt = std::max_element(vec.begin(), vec.end());
+    return *resultIt;
 }
