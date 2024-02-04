45a29ca5e6e6af515eefcac9c9f6295e6f192f7b
Marcin Sarnecki <sarnecki.marcin@gmail.com>
2024-02-04 18:17:44 +0100

    Task. An implementation.

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,15 @@
 #pragma once
+#include <algorithm>
 #include <limits>
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    if (vec.empty()) {
+        return 0;
+    }
+
+    auto it = std::max_element(vec.begin(), vec.end());
+
+    return *it;
 }
