79c03b187233ecf23325444e82cb2af00d8c5f15
Marcin Sarnecki <sarnecki.marcin@gmail.com>
2024-02-04 17:33:21 +0100

    Task. Add even. An implementation.

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -4,5 +4,14 @@
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
-    return -1;
+
+    int even_numers_sum{0};
+
+    for (auto& it : numbers) {
+        if (it %2 == 0) {
+            even_numers_sum += it;
+        }
+    }
+
+    return even_numers_sum;
 }
