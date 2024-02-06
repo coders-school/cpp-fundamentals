2587dbf1c0e08bc0ff2e18b29dfdec48f3a03d37
Marcin Sarnecki <sarnecki.marcin@gmail.com>
2024-02-06 16:33:14 +0100

    Task. Fibonacci. An implementation.

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,30 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 0) {
+        return 0;
+    }
+
+    if (sequence <= 2) {
+        return 1;
+    }
+
+    int fib{};
+    for (int count = 2, fib_1 = 1, fib_2 = 1, t = 0;
+         count <= sequence;
+         t = fib_2, fib_2 += fib_1, fib_1 = t, ++count) {
+        fib = fib_2;
+    }
+    return fib;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 0) {
+        return 0;
+    }
+
+    if (sequence <= 2) {
+        return 1;
+    }
+    return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
 }
