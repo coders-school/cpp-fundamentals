2ac4fa63a9f0be172f52d5a1d893f7a3941d7a1a
Marcin Sarnecki <sarnecki.marcin@gmail.com>
2024-02-11 18:14:04 +0100

    Task. The NWD NWW homework. An implementation.

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -2,10 +2,59 @@
 
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
+
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+
+    if (lhs == 0 && rhs == 0) {
+        return 0;
+    }
+
+    if (lhs == 0 && rhs != 0) {
+        return rhs;
+    }
+
+    if (lhs != 0 && rhs == 0) {
+        return lhs;
+    }
+
+    if (rhs >= lhs) {
+        int tmp{0};
+        tmp = lhs;
+        lhs = rhs;
+        rhs = tmp;
+    }
+
+    if (lhs % rhs == 0) {
+        return rhs;
+    }
+
+    while (rhs > 0) {
+        int tmp{0};
+        tmp = rhs;
+        rhs = lhs % rhs;
+        lhs = tmp;
+    }
+
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+
+    if (lhs == 0 || rhs == 0) {
+        return 0;
+    }
+
+    if(lhs == rhs) {
+        return lhs;
+    }
+
+    int gcd = NWD(lhs, rhs);
+
+    if (gcd > 0) {
+        return lhs * rhs / gcd;
+    }
+    return lhs * rhs;
 }
