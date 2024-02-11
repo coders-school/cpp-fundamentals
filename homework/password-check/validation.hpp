9976d701f626f39274b1c5e2d1013d37948ff915
Josseye <155636321+Josseye@users.noreply.github.com>
2024-02-11 20:40:03 +0100

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -68,17 +68,3 @@ int checkPassword(std::string firstpassword, std::string secondpassword) {
     else
         return checkPasswordRules(firstpassword);
 }
-
-int main() {
- 
-    std::string password;
-    std::string repeatedPassword;
-    std::cout << "Set new password: ";
-    std::cin >> password;
-    std::cout << "Repeat password: ";
-    std::cin >> repeatedPassword;
-    auto result = checkPassword(password, repeatedPassword);
-    std::cout << getErrorMessage(result) << '\n';
-
-    return 0;
-}
