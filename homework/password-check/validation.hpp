af7643bf1d8a5c75f8eb2f51660936200208f1d5
Josseye <155636321+Josseye@users.noreply.github.com>
2024-02-15 14:57:08 +0100

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,6 +1,1 @@
-int checkPassword(std::string firstpassword, std::string secondpassword) {
-    if (!doPasswordsMatch(firstpassword, secondpassword))
-        return static_cast<int>(Error::PasswordsDoNotMatch);
-    else
-        return checkPasswordRules(firstpassword);
-}
+
