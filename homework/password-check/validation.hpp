fe073bb533158f03f332c5aa33c780c959722526
Josseye <155636321+Josseye@users.noreply.github.com>
2024-02-15 17:08:27 +0100

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -8,11 +8,7 @@ enum class Error {
         PasswordNeedsAtLeastOneUppercaseLetter,
         PasswordsDoNotMatch
     };
-std::string getErrorMessage(int errcode){
-}
-bool doPasswordsMatch(std::string psw1, std::string psw2) {
-}
-int checkPasswordRules(std::string pass) {
-}
-int checkPassword(std::string firstpassword, std::string secondpassword) {
-}
+std::string getErrorMessage(int errcode)
+bool doPasswordsMatch(std::string psw1, std::string psw2)
+int checkPasswordRules(std::string pass)
+int checkPassword(std::string firstpassword, std::string secondpassword)
