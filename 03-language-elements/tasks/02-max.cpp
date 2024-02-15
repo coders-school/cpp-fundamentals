94b846921f76c030c3e19c54e43980720236aa74
u2024 <157313309+u2024@users.noreply.github.com>
2024-02-15 14:31:47 +0100

    Update 02-max.cpp

--- 03-language-elements/tasks/02-max.cpp
+++ 03-language-elements/tasks/02-max.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-// Write your function here
+int max(int a, int b, int c)
+{
+    if(a>b)
+    {
+        if(a>c)
+        {
+            return a;
+        }
+        else return c;
+    }
+    else if(b>c)
+    {
+        return b;
+    }
+    else return c;
+}
 
 int main() {
     std::cout << "max (1, 2, 3): " << max (1, 2, 3) << "\n";
