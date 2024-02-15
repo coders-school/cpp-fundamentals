b1b04397c7e4a6f48cb3e40b29a8b5a26e6d4e27
Josseye <155636321+Josseye@users.noreply.github.com>
2024-02-15 15:41:29 +0100

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,8 +1,18 @@
-# include validation.cpp
+#pragma once
 
+enum class Error {
+        Ok,
+        PasswordNeedsAtLeastNineCharacters,
+        PasswordNeedsAtLeastOneNumber,
+        PasswordNeedsAtLeastOneSpecialCharacter,
+        PasswordNeedsAtLeastOneUppercaseLetter,
+        PasswordsDoNotMatch
+    };
+std::string getErrorMessage(int errcode){
+}
+bool doPasswordsMatch(std::string psw1, std::string psw2) {
+}
+int checkPasswordRules(std::string pass) {
+}
 int checkPassword(std::string firstpassword, std::string secondpassword) {
-    if (!doPasswordsMatch(firstpassword, secondpassword))
-        return static_cast<int>(Error::PasswordsDoNotMatch);
-    else
-        return checkPasswordRules(firstpassword);
 }
