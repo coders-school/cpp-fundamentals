42a2b933f49d314fff214a78579f54779a934509
Josseye <155636321+Josseye@users.noreply.github.com>
2024-02-16 18:52:12 +0100

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -2,13 +2,13 @@
 #include <string>
 
 enum class ErrorCode {
-        Ok,
-        PasswordNeedsAtLeastNineCharacters,
-        PasswordNeedsAtLeastOneNumber,
-        PasswordNeedsAtLeastOneSpecialCharacter,
-        PasswordNeedsAtLeastOneUppercaseLetter,
-        PasswordsDoNotMatch
-};
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
+}; 
 std::string getErrorMessage(ErrorCode faultcode);
 bool doPasswordsMatch(std::string psw1, std::string psw2);
 ErrorCode checkPasswordRules(std::string pass);
