96e2afedc8bbe17f57bc1945a5b155cb7385ff63
Josseye <155636321+Josseye@users.noreply.github.com>
2024-02-26 16:40:38 +0100

    Create vectorFunctions.cpp

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,37 @@
+#include "vectorFunctions.hpp"
+#include <iostream>
+#include <vector>
+#include <memory>
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    int i = 0;
+    for (i; i<count; ++i){
+        smartvector.push_back(std::make_shared<int>(10+i));
+    }
+    return smartvector;
+}
+
+void print(auto printout) {
+    std::cout << "\n";
+    for (auto & element : printout) {
+        std::cout << * element << "\n";
+    }
+}
+
+void add10(auto addten) {
+    for (auto & element : addten) {
+        * element += 10;
+    }
+}
+
+void sub10( int * const p) {
+    * p -= 10;
+}
+
+void sub10(std::vector<std::shared_ptr<int>>  subten) {
+    int * pointer;
+    for (auto element : subten) {
+        pointer = element.get();
+        sub10( pointer );
+    }
+}
