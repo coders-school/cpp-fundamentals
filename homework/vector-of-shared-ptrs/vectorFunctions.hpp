1691a018eb439b555b9a32b32d00750f8f641f1c
Josseye <155636321+Josseye@users.noreply.github.com>
2024-02-27 19:56:30 +0100

    Update vectorFunctions.hpp

--- homework/vector-of-shared-ptrs/vectorFunctions.hpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -1,9 +1,9 @@
 #pragma once
-#include <vector>
 #include <memory>
+#include <vector>
 
 std::vector<std::shared_ptr<int>> generate(int count);
 void print(std::vector<std::shared_ptr<int>> printout);
 void add10(std::vector<std::shared_ptr<int>> addten);
-void sub10( int * const p);
-void sub10(std::vector<std::shared_ptr<int>>  subten);
+void sub10( int* const p);
+void sub10(std::vector<std::shared_ptr<int>> subten);
