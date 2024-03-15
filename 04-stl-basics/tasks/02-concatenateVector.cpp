eabd2e4551f324f39c6e6dc0997a959918a34ce5
u2024 <157313309+u2024@users.noreply.github.com>
2024-03-15 18:30:19 +0100

    Update 02-concatenateVector.cpp

--- 04-stl-basics/tasks/02-concatenateVector.cpp
+++ 04-stl-basics/tasks/02-concatenateVector.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <vector>
 
-// Implement concatenateVector.
-// It should take 2 vectors and return one which has alternately elements from the first and the other one.
+auto concatenateVector(std::vector<int> vec1, std::vector<int> vec2)
+{
+    std::vector<int> v;
+    for(int el1 : vec1)
+    {
+        v.push_back(vec1[el1-1]);
+        v.push_back(vec2[el1-1]); 
+    }
+    return v;
+}
 
 int main() {
     std::vector<int> vec1 {1, 2, 3, 4, 5};
