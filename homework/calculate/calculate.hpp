49aa76f1671b6159fcafab4f36354d3781e3ae57
Rafał Leszczyński <85461555+Rafal-Leszczynski@users.noreply.github.com>
2024-03-18 19:38:20 +0100

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,20 +1,22 @@
-#pragma once
+// copyright Rafał Leszczyński
+#include <iostream>
 #include <string>
+// #include "calculate.hpp"
 
-std::string calculate(const std::string &command, int first, int second) {
-  if (command == "add") {
-    return std::to_string(first + second);
-  } else if (command == "subtract") {
-    return std::to_string(first - second);
-  } else if (command == "multiply") {
-    return std::to_string(first * second);
-  } else if (command == "divide") {
-    if ((first != 0) && (second != 0)) {
-      return std::to_string(first / second);
+std::string calculate(const std::string& command, int first, int second) {
+    if (command == "add") {
+        return std::to_string(first + second);
+    } else if (command == "subtract") {
+        return std::to_string(first - second);
+    } else if (command == "multiply") {
+        return std::to_string(first * second);
+    } else if (command == "divide") {
+        if ((first != 0) && (second != 0)) {
+            return std::to_string(first / second);
+        } else {
+            return "Division by 0";
+        }
     } else {
-      return "Division by 0";
+        return "Invalid data";
     }
-  } else {
-    return "Invalid data";
-  }
 }
