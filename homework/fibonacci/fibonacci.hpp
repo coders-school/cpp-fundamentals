04490a5a38b12a2173866e6eafc4625aaf0a0cf9
Rafał Leszczyński <85461555+Rafal-Leszczynski@users.noreply.github.com>
2024-03-28 04:30:57 +0100

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,9 +2,9 @@
 
 int fibonacci_iterative(int sequence) {
     int a = 0, b = 1, temp;
-    if (sequence ==0)
+    if (sequence == 0)
         return a;
-    for (int i = 2; i <= sequence; i++) {
+    for (int i = 2; i <= sequence; ++i) {
         temp = a + b;
         a = b;
         b = temp;
@@ -16,4 +16,11 @@ int fibonacci_recursive(int sequence) {
     if (sequence <= 1)
         return sequence;
     return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+}
+
+int main() {
+    std::cout << fibonacci_iterative(10) << "\n";
+    std::cout << fibonacci_recursive(10) << "\n";
+
+    return 0;
 }
